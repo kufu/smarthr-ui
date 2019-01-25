48ea5c48addc8c17b5b071e3d2ec4a495dc1d23f
nabeliwo <hiroki.c.watanabe@gmail.com>
2019-01-25 16:29:46 +0900

    :bug: Remove hoge

--- src/components/Modal/ModalEraser.tsx
+++ src/components/Modal/ModalEraser.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 export const ModalEraser: React.FC<{}> = ({ children }) => (
-  <button className="ModalEraser hoge">{children}</button>
+  <button className="ModalEraser">{children}</button>
 )
 
 ModalEraser.displayName = 'ModalEraser'
