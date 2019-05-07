82099ad03d4c673f2292ba4380b07a9dc7697e60
im36-123 <shota.takata@smarthr.co.jp>
2019-05-07 14:57:24 +0900

    add test for AppBar component

--- src/components/AppBar/__test__/index.test.tsx
+++ src/components/AppBar/__test__/index.test.tsx
@@ -1,3 +1,4 @@
+import { mount } from 'enzyme'
 import React from 'react'
 import ReactTestRenderer from 'react-test-renderer'
 import { AppBar } from '../AppBar'
@@ -5,5 +6,14 @@ import { AppBar } from '../AppBar'
 describe('index', () => {
   it('should be able to render without crashing', () => {
     ReactTestRenderer.create(<AppBar />)
+  })
+
+  it('should be render a `<AppBarComponent />`', () => {
+    const component = mount(
+      <AppBar pcSize="l" tabletSize="m" spSize="s">
+        AppBar Component
+      </AppBar>,
+    )
+    expect(component.find('AppBarComponent')).toHaveLength(1)
   })
 })
