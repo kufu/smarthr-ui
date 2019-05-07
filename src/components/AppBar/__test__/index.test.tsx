511152a966ed779e88ba96ea7e05510335baea01
im36-123 <shota.takata@smarthr.co.jp>
2019-05-07 14:52:03 +0900

    Moved test file from src/ to AppBer/__test__/

--- src/components/AppBar/__test__/index.test.tsx
+++ src/components/AppBar/__test__/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactTestRenderer from 'react-test-renderer'
-import { AppBar } from './'
+import { AppBar } from '../AppBar'
 
 describe('index', () => {
   it('should be able to render without crashing', () => {
