033da172bf0cf51a696a7690410fc47991b362e4
kazuya wakui <kazuya.wakui@gmail.com>
2019-06-21 14:52:26 +0900

    :books: maintain storybook

--- src/components/Tag/Tag.stories.tsx
+++ src/components/Tag/Tag.stories.tsx
@@ -3,23 +3,53 @@ import * as React from 'react'
 import styled from 'styled-components'
 
 import { Tag } from './Tag'
+import readme from './README.md'
 
-storiesOf('Tag', module).add('all', () => (
-  <List>
-    <li>
-      <Tag type="success">サクセス</Tag>
-    </li>
-    <li>
-      <Tag type="warning">警告</Tag>
-    </li>
-    <li>
-      <Tag type="error">エラー</Tag>
-    </li>
-    <li>
-      <Tag type="require">必須</Tag>
-    </li>
-  </List>
-))
+storiesOf('Tag', module)
+  .addParameters({
+    readme: {
+      sidebar: readme,
+    },
+  })
+  .add('all', () => (
+    <List>
+      <li>
+        <Tag>デフォルト</Tag>
+      </li>
+      <li>
+        <Tag type="success">サクセス</Tag>
+      </li>
+      <li>
+        <Tag type="warning">警告</Tag>
+      </li>
+      <li>
+        <Tag type="error">エラー</Tag>
+      </li>
+      <li>
+        <Tag type="require">必須</Tag>
+      </li>
+      <li>
+        <Tag type="success" skeleton={true}>
+          サクセス
+        </Tag>
+      </li>
+      <li>
+        <Tag type="warning" skeleton={true}>
+          警告
+        </Tag>
+      </li>
+      <li>
+        <Tag type="error" skeleton={true}>
+          エラー
+        </Tag>
+      </li>
+      <li>
+        <Tag type="require" skeleton={true}>
+          必須
+        </Tag>
+      </li>
+    </List>
+  ))
 
 const List = styled.ul`
   padding: 8px 24px;
