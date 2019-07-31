3214a2bd85c113232584a03e2ebea6cee1cd5141
im36-123 <shota.takata@smarthr.co.jp>
2019-07-31 18:41:50 +0900

    chore: change color

--- src/components/Modal/Box.tsx
+++ src/components/Modal/Box.tsx
@@ -95,7 +95,7 @@ const Background = styled.div`
       left: 0;
       width: 100%;
       height: 100%;
-      background-color: ${theme.palette.Overlay};
+      background-color: ${theme.palette.Scrim};
     `
   }}
 `
