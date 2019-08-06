f6aa1c46c389e77a8f5f41bc2d18fd77a7822e35
Ouji Miyahara <agagagagagagagagaga@gmail.com>
2019-08-06 18:11:50 +0900

    fix: fetch index.test.tsx from master

--- src/components/Header/index.ts
+++ src/components/Header/index.ts
@@ -1,4 +1,4 @@
-export { Header } from './Header'
-export { HeaderButton } from './HeaderButton'
-export { HeaderDropDown } from './HeaderDropDown'
-export { HeaderNotification } from './HeaderNotification'
+export * from './Header'
+export * from './HeaderButton'
+export * from './HeaderDropDown'
+export * from './HeaderNotification'
