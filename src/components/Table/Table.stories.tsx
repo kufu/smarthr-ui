7c9811105b497a0d5fa27f97f1a93aa625e66c76
im36-123 <shota.takata@smarthr.co.jp>
2019-08-16 17:05:36 +0900

    chore: change test data

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -9,36 +9,7 @@ import Head from './Head'
 import Row from './Row'
 import Cell from './Cell'
 import { action } from '@storybook/addon-actions'
-
-class TableSampleData {
-  readonly employeeNumber: string
-  readonly name: string
-  readonly permission: string
-  readonly employmentType: string
-  readonly role: string
-  readonly team: string
-
-  constructor(
-    _employeeNumber: string,
-    _name: string,
-    _permission: string,
-    _employmentType: string,
-    _role: string,
-    _team: string,
-  ) {
-    this.employeeNumber = _employeeNumber
-    this.name = _name
-    this.permission = _permission
-    this.employmentType = _employmentType
-    this.role = _role
-    this.team = _team
-  }
-}
-
-const tableSampleDataSet = [
-  new TableSampleData('01', '佐藤 一郎', '', '正社員', 'メンバー', 'デザイン'),
-  new TableSampleData('02', '鈴木 二郎', '', '正社員', 'メンバー', 'エンジニア'),
-]
+import { Icon } from '../Icon'
 
 storiesOf('Table', module).add('all', () => (
   <Ul>
@@ -46,47 +17,23 @@ storiesOf('Table', module).add('all', () => (
       <Table>
         <Head>
           <Row>
-            <Cell onClick={action('clicked')}>社員番号</Cell>
-            <Cell>権限</Cell>
-            <Cell>雇用形態</Cell>
-            <Cell>役職</Cell>
-            <Cell>部署</Cell>
+            <Cell onClick={action('clicked')}>
+              Clickable cell <Icon name="fa-angle-down" />
+            </Cell>
+            <Cell>cell</Cell>
+            <Cell>cell</Cell>
+            <Cell>cell</Cell>
+            <Cell>cell</Cell>
           </Row>
         </Head>
         <Body>
-          {tableSampleDataSet.map(member => (
-            <Row key={member.employeeNumber}>
-              <Cell>{member.employeeNumber}</Cell>
-              <Cell>{member.role}</Cell>
-              <Cell>{member.employmentType}</Cell>
-              <Cell>{member.role}</Cell>
-              <Cell>{member.team}</Cell>
-            </Row>
-          ))}
-        </Body>
-      </Table>
-    </li>
-    <li>
-      <Table>
-        <Head>
           <Row>
-            <Cell>社員番号</Cell>
-            <Cell>権限</Cell>
-            <Cell>雇用形態</Cell>
-            <Cell>役職</Cell>
-            <Cell>部署</Cell>
+            <Cell>cell</Cell>
+            <Cell>cell</Cell>
+            <Cell>cell</Cell>
+            <Cell>cell</Cell>
+            <Cell>cell</Cell>
           </Row>
-        </Head>
-        <Body>
-          {tableSampleDataSet.map(member => (
-            <Row key={member.employeeNumber}>
-              <Cell>{member.employeeNumber}</Cell>
-              <Cell>{member.role}</Cell>
-              <Cell>{member.employmentType}</Cell>
-              <Cell>{member.role}</Cell>
-              <Cell>{member.team}</Cell>
-            </Row>
-          ))}
         </Body>
       </Table>
     </li>
