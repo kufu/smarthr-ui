4911f470993cfd3dda642fd76d60034ea89ddf08
im36-123 <shota.takata@smarthr.co.jp>
2019-08-30 11:41:41 +0900

    chore: remove comment

--- src/components/TabBar/TabBar.stories.tsx
+++ src/components/TabBar/TabBar.stories.tsx
@@ -1,7 +1,6 @@
 import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
-// import styled from 'styled-components'
 import { Tab } from './Tab'
 import { TabBar } from './TabBar'
 
