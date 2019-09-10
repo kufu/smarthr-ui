844ccdff6e1d04e12bdf30836cc6270d31d0645a
nabeliwo <hiroki.c.watanabe@gmail.com>
2019-09-10 12:46:47 +0900

    lint: fix lint error

--- src/components/Image/Images.stories.tsx
+++ src/components/Image/Images.stories.tsx
@@ -6,15 +6,15 @@ import { BlankImage } from './BlankImage'
 
 storiesOf('Image', module).add('BlankImage', () => (
   <List>
-    <dt>BlankImage size="s"</dt>
+    <dt>BlankImage size=s</dt>
     <dd>
       <BlankImage title="S size" size="s" />
     </dd>
-    <dt>BlankImage size="m"</dt>
+    <dt>BlankImage size=m</dt>
     <dd>
       <BlankImage title="M size" size="m" />
     </dd>
-    <dt>BlankImage size="l"</dt>
+    <dt>BlankImage size=l</dt>
     <dd>
       <BlankImage title="L size" size="l" />
     </dd>
