f55c9bbe80c31ff64698630297c66e4223efab4c
Toru Kobayashi <koba0004@gmail.com>
2019-10-18 00:03:52 +0900

    fix: support uncontrollable Dropdown

--- src/components/Dropdown/DropdownContent.tsx
+++ src/components/Dropdown/DropdownContent.tsx
@@ -3,6 +3,7 @@ import { createPortal } from 'react-dom'
 
 import { DropdownContext } from './Dropdown'
 import { DropdownContentInner } from './DropdownContentInner'
+import { DropdownCloser } from './DropdownCloser'
 
 export const DropdownContentContext = React.createContext<{
   onClickCloser: () => void
@@ -14,14 +15,16 @@ type Props = {
   controllable?: boolean
 }
 
-export const DropdownContent: React.FC<Props> = ({ children }) => {
+export const DropdownContent: React.FC<Props> = ({ controllable = false, children }) => {
   const { element, active, triggerRect, onClickCloser } = useContext(DropdownContext)
 
   if (!active || !element) return null
 
   return createPortal(
     <DropdownContentContext.Provider value={{ onClickCloser }}>
-      <DropdownContentInner triggerRect={triggerRect}>{children}</DropdownContentInner>
+      <DropdownContentInner triggerRect={triggerRect}>
+        {controllable ? children : <DropdownCloser>{children}</DropdownCloser>}
+      </DropdownContentInner>
     </DropdownContentContext.Provider>,
     element,
   )
