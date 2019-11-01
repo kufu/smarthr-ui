06bc9bbf631c17b5f8bab4ef3675c80678b81af2
im36-123 <shota.takata@smarthr.co.jp>
2019-11-01 21:04:25 +0900

    doc: update story

--- src/components/AccordionPanel/AccordionPanel.stories.tsx
+++ src/components/AccordionPanel/AccordionPanel.stories.tsx
@@ -8,10 +8,11 @@ import { AccordionPanelTrigger } from './AccordionPanelTrigger'
 import { AccordionPanelContent } from './AccordionPanelContent'
 import { Base as BaseComponent } from '../Base'
 import readme from './README.md'
+import { action } from '@storybook/addon-actions'
 
 const arr = Array.from({ length: 3 })
 // prettier-ignore
-const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nullap ariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
 storiesOf('AccordionPanel', module)
   .addParameters({
@@ -19,93 +20,142 @@ storiesOf('AccordionPanel', module)
       sidebar: readme,
     },
   })
-  .add('AccordionPanel', () => {
-    // const [state, setstate] = useState('')
-
-    // const handleClick = (name: string, _expanded: boolean) => {
-    //   setstate(name)
-    // }
-
-    return (
-      <>
-        <Base>
-          <AccordionPanel>
-            <ul>
-              {arr.map((_, index) => (
-                <li className="border" key={index}>
-                  <AccordionPanelItem name={`left-icon-${index}`}>
-                    <AccordionPanelTrigger>Left Icon (default) {index}</AccordionPanelTrigger>
-                    <AccordionPanelContent>
-                      <Content>
-                        <div>{lorem}</div>
-                      </Content>
-                    </AccordionPanelContent>
-                  </AccordionPanelItem>
-                </li>
-              ))}
-            </ul>
-          </AccordionPanel>
-        </Base>
-        {/* <Base>
+  .add('Accordion style', () => (
+    <>
+      <Base>
+        <AccordionPanel onClick={action('click')}>
           <ul>
             {arr.map((_, index) => (
               <li className="border" key={index}>
-                <AccordionPanelItem
-                  name={`right-icon-${index}`}
-                  expanded={state === `right-icon-${index}`}
-                  icon="right"
-                  onClick={handleClick}
-                >
+                <AccordionPanelItem name={`left-icon-${index}`}>
+                  <AccordionPanelTrigger>Left Icon (default) {index}</AccordionPanelTrigger>
+                  <AccordionPanelContent>
+                    <Content>
+                      <div>{lorem}</div>
+                    </Content>
+                  </AccordionPanelContent>
+                </AccordionPanelItem>
+              </li>
+            ))}
+          </ul>
+        </AccordionPanel>
+      </Base>
+      <Base>
+        <AccordionPanel icon="right">
+          <ul>
+            {arr.map((_, index) => (
+              <li className="border" key={index}>
+                <AccordionPanelItem name={`right-icon-${index}`}>
                   <AccordionPanelTrigger>Right Icon {index}</AccordionPanelTrigger>
                   <AccordionPanelContent>
-                    <Content>{lorem}</Content>
+                    <Content>
+                      <div>{lorem}</div>
+                    </Content>
                   </AccordionPanelContent>
                 </AccordionPanelItem>
               </li>
             ))}
           </ul>
-        </Base>
-        <Base>
+        </AccordionPanel>
+      </Base>
+      <Base>
+        <AccordionPanel icon="none">
           <ul>
             {arr.map((_, index) => (
               <li className="border" key={index}>
-                <AccordionPanelItem
-                  name={`no-icon-${index}`}
-                  expanded={state === `no-icon-${index}`}
-                  icon="none"
-                  onClick={handleClick}
-                >
+                <AccordionPanelItem name={`no-icon-${index}`}>
                   <AccordionPanelTrigger>No Icon {index}</AccordionPanelTrigger>
                   <AccordionPanelContent>
-                    <Content>{lorem}</Content>
+                    <Content>
+                      <div>{lorem}</div>
+                    </Content>
                   </AccordionPanelContent>
                 </AccordionPanelItem>
               </li>
             ))}
           </ul>
-        </Base>
-        <Base>
+        </AccordionPanel>
+      </Base>
+      <Base>
+        <AccordionPanel icon="none">
           <ul>
             {arr.map((_, index) => (
               <li key={index}>
-                <AccordionPanelItem
-                  name={`no-border-${index}`}
-                  expanded={state === `no-border-${index}`}
-                  icon="none"
-                  onClick={handleClick}
-                >
+                <AccordionPanelItem name={`no-border-${index}`}>
                   <AccordionPanelTrigger>No Border {index}</AccordionPanelTrigger>
                   <AccordionPanelContent>
-                    <Content>{lorem}</Content>
+                    <Content>
+                      <div>{lorem}</div>
+                    </Content>
                   </AccordionPanelContent>
                 </AccordionPanelItem>
               </li>
             ))}
           </ul>
-        </Base> */}
-      </>
-    )
-  })
+        </AccordionPanel>
+      </Base>
+    </>
+  ))
+  .add('Expanded options', () => (
+    <>
+      <Base>
+        <AccordionPanel icon="none" expandableMultiply={true}>
+          <ul>
+            {arr.map((_, index) => (
+              <li key={index}>
+                <AccordionPanelItem name={`expandable-multiply-${index}`}>
+                  <AccordionPanelTrigger>Expandable Multiply {index}</AccordionPanelTrigger>
+                  <AccordionPanelContent>
+                    <Content>
+                      <div>{lorem}</div>
+                    </Content>
+                  </AccordionPanelContent>
+                </AccordionPanelItem>
+              </li>
+            ))}
+          </ul>
+        </AccordionPanel>
+      </Base>
+      <Base>
+        <AccordionPanel icon="none" defaultExpanded={['default-expanded-0']}>
+          <ul>
+            {arr.map((_, index) => (
+              <li className="" key={index}>
+                <AccordionPanelItem name={`default-expanded-${index}`}>
+                  <AccordionPanelTrigger>Default Expanded {index}</AccordionPanelTrigger>
+                  <AccordionPanelContent>
+                    <Content>
+                      <div>{lorem}</div>
+                    </Content>
+                  </AccordionPanelContent>
+                </AccordionPanelItem>
+              </li>
+            ))}
+          </ul>
+        </AccordionPanel>
+      </Base>
+    </>
+  ))
+  .add('Callback', () => (
+    <Base>
+      <AccordionPanel icon="none" expandableMultiply={true} onClick={action('Clicked')}>
+        <ul>
+          {arr.map((_, index) => (
+            <li key={index}>
+              <AccordionPanelItem name={`expandable-multiply-${index}`}>
+                <AccordionPanelTrigger>Expandable Multiply {index}</AccordionPanelTrigger>
+                <AccordionPanelContent>
+                  <Content>
+                    <div>{lorem}</div>
+                  </Content>
+                </AccordionPanelContent>
+              </AccordionPanelItem>
+            </li>
+          ))}
+        </ul>
+      </AccordionPanel>
+    </Base>
+  ))
 
 const Base = styled(BaseComponent)`
   margin: 1rem;
