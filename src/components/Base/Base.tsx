497e698a5596b8d1379804e53be32653a39ce3c7
nabeliwo <hiroki.c.watanabe@gmail.com>
2019-11-06 19:53:15 +0900

    style: Specify background-color

--- src/components/Base/Base.tsx
+++ src/components/Base/Base.tsx
@@ -25,6 +25,7 @@ const Wrapper = styled.div<{ radius: string }>`
       overflow: hidden;
       border-radius: ${radius};
       box-shadow: rgba(51, 51, 51, 0.3) 1px 1px 4px 0;
+      background-color: #fff;
     `
   }}
 `
