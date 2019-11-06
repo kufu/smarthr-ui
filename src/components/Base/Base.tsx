47fe111f0253845c9c2bc404eddaf45d11dde22d
nabeliwo <hiroki.c.watanabe@gmail.com>
2019-11-06 20:25:44 +0900

    fix: Add ref props

--- src/components/Base/Base.tsx
+++ src/components/Base/Base.tsx
@@ -1,28 +1,23 @@
-import React, { FC } from 'react'
+import React, { FC, Ref } from 'react'
 import styled, { css } from 'styled-components'
 
-type Props = {
+type BaseProps = {
   radius?: 's' | 'm'
   className?: string
+  ref?: Ref<HTMLDivElement>
 }
 
-export const Base: FC<Props> = ({ radius = 'm', className = '', children }) => {
+export const Base: FC<BaseProps> = ({ radius = 'm', ...props }) => {
   const radiusMap = {
     s: '6px',
     m: '8px',
   }
-
-  return (
-    <Wrapper radius={radiusMap[radius]} className={className}>
-      {children}
-    </Wrapper>
-  )
+  return <Wrapper radius={radiusMap[radius]} {...props} />
 }
 
 const Wrapper = styled.div<{ radius: string }>`
   ${({ radius }) => {
     return css`
-      overflow: hidden;
       border-radius: ${radius};
       box-shadow: rgba(51, 51, 51, 0.3) 1px 1px 4px 0;
       background-color: #fff;
