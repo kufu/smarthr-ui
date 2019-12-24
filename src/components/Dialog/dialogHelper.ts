0753bf1783b48b2fea94f1cba04acf70605e7b22
Tokky0425 <lily.yarn.5@gmail.com>
2019-12-24 12:03:44 +0900

    feat: add useOffsetHeight hook

---
+++ src/components/Dialog/dialogHelper.ts
@@ -0,0 +1,33 @@
+import { RefObject, useContext, useEffect, useRef, useState } from 'react'
+import { PositionContext } from './DialogPositionProvider'
+import { useTheme } from '../../hooks/useTheme'
+
+type offsetHeightValues = {
+  offsetHeight: number
+  titleRef: RefObject<HTMLParagraphElement>
+  bottomRef: RefObject<HTMLDivElement>
+}
+
+export const useOffsetHeight = (): offsetHeightValues => {
+  const theme = useTheme()
+  const baseSpace = theme.size.space.L
+  const { top, bottom } = useContext(PositionContext)
+  const titleRef = useRef<HTMLParagraphElement>(null)
+  const bottomRef = useRef<HTMLDivElement>(null)
+  const [offsetHeight, setOffsetHeight] = useState<number>(0)
+
+  useEffect(() => {
+    const topSpace = top ? top : baseSpace
+    const bottomSpace = bottom ? bottom : baseSpace
+    const titleHeight = titleRef.current ? titleRef.current.offsetHeight : 0
+    const bottomHeight = bottomRef.current ? bottomRef.current.offsetHeight : 0
+
+    setOffsetHeight(topSpace + bottomSpace + titleHeight + bottomHeight)
+  }, [top, bottom, baseSpace])
+
+  return {
+    offsetHeight,
+    titleRef,
+    bottomRef,
+  }
+}
