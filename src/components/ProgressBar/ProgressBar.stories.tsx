4c16fdc97e35c63c83220427c27cd968f965a01c
Ouji Miyahara <agagagagagagagagaga@gmail.com>
2020-01-22 13:55:52 +0900

    feat: add TBD stories (#598)
    * feat: add DatePicker TBD story
    
    * feat: add SegmentedControl story
    
    * feat: add Downloader story
    
    * fix: add [TBD] prefix to story title
    
    * feat: add ProgressBar story

---
+++ src/components/ProgressBar/ProgressBar.stories.tsx
@@ -0,0 +1,56 @@
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+import styled from 'styled-components'
+import { Icon } from '../Icon'
+
+storiesOf('[TBD] ProgressBar', module).add('all', () => (
+  <Wrapper>
+    <Title>To Be Developed</Title>
+    <Description>This component will develop in the near future.</Description>
+    <Link
+      href="https://smarthr.invisionapp.com/share/ADUDJ8BW74C#/391861064_progress_Bar"
+      target="_blank"
+    >
+      <LinkText>Design of ProgressBar (InVision)</LinkText>
+      <LinkIcon name="fa-external-link-alt" size={14} />
+    </Link>
+  </Wrapper>
+))
+
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  padding: 24px;
+  border-radius: 6px;
+  box-shadow: rgba(51, 51, 51, 0.3) 1px 1px 4px 0;
+  color: #333;
+  font-size: 14px;
+  text-align: center;
+  line-height: 1;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const Title = styled.h1`
+  display: block;
+  margin-bottom: 16px;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+const Description = styled.div`
+  margin-bottom: 16px;
+`
+
+const Link = styled.a`
+  color: #007bc2;
+`
+const LinkText = styled.span`
+  vertical-align: middle;
+`
+
+const LinkIcon = styled(Icon)`
+  margin-left: 4px;
+  vertical-align: middle;
+`
