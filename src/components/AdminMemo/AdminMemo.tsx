699e0baafd01d31e559b30d941788c62b456ab9f
ouji-miyahara <agagagagagagagagaga@gmail.com>
2020-04-16 11:49:21 +0900

    feat: add position:fixed style

--- src/components/AdminMemo/AdminMemo.tsx
+++ src/components/AdminMemo/AdminMemo.tsx
@@ -15,6 +15,7 @@ interface Props {
   items?: AdminMemoItemProps[]
   submitButtonOnClick?: (e: ClickEvent) => void
   submitButtonLabel?: string
+  width?: number
 }
 
 export const AdminMemo: FC<Props> = ({
@@ -22,11 +23,12 @@ export const AdminMemo: FC<Props> = ({
   items,
   submitButtonOnClick,
   submitButtonLabel = '送信',
+  width = 270,
 }) => {
   const theme = useTheme()
 
   return (
-    <AdminMemoWrapper themes={theme}>
+    <AdminMemoWrapper themes={theme} width={width}>
       {title && (
         <AdminMemoTitle type="sectionTitle" themes={theme}>
           {title}
@@ -53,14 +55,20 @@ export const AdminMemo: FC<Props> = ({
   )
 }
 
-const AdminMemoWrapper = styled.div<{ themes: Theme }>`
-  ${({ themes }) => {
+const AdminMemoWrapper = styled.div<{ themes: Theme; width: number }>`
+  ${({ themes, width }) => {
     const { size, palette } = themes
 
     return css`
+      width: ${size.pxToRem(width)};
       padding: ${size.pxToRem(size.space.XS)};
       background-color: ${palette.COLUMN};
-      overflow: hidden;
+      box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 8px;
+      overflow: hidden scroll;
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
     `
   }}
 `
@@ -84,6 +92,7 @@ const AdminMemoTextArea = styled(Textarea)<{ themes: Theme }>`
       display: block;
       width: 100%;
       max-width: 100%;
+      min-width: 100%;
       box-sizing: border-box;
       margin-bottom: ${pxToRem(space.XS)};
     `
