b329b608de68b45556f9893b284d101d71fdad99
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-05-08 10:11:11 +0900

    feat: add width and height props to SmartHRLogo

---
+++ src/components/SmartHRLogo/README.md
@@ -0,0 +1,17 @@
+# SmartHRLogo
+
+```tsx
+import { SmartHRLogo } from 'smarthr-ui'
+
+<SmartHRLogo title="SmartHR" width={111} height={20} fill="#00c4cc" />
+```
+
+## props
+
+| Name      | Required | Type       | DefaultValue | Description                      |
+| --------- | -------- | ---------- | ------------ | -------------------------------- |
+| title     | -        | **string** | 'SmartHR'    | title of svg tag                 |
+| width     | -        | **number** | 150          | width of svg tag                 |
+| height    | -        | **number** | 27           | height of svg tag                |
+| fill      | -        | **string** | '#fff'       | fill color of svg tag            |
+| className | -        | **string** | ''           | className of wrapper div element |
--- src/components/SmartHRLogo/SmartHRLogo.stories.tsx
+++ src/components/SmartHRLogo/SmartHRLogo.stories.tsx
@@ -1,21 +1,33 @@
-import { storiesOf } from '@storybook/react'
 import * as React from 'react'
+import styled from 'styled-components'
+import { storiesOf } from '@storybook/react'
+
+import readme from './README.md'
+
 import { SmartHRLogo } from './SmartHRLogo'
 
-import styled from 'styled-components'
-
-storiesOf('SmartHRLogo', module).add('all', () => (
-  <List>
-    <li>
-      <Text>default</Text>
-      <SmartHRLogo />
-    </li>
-    <li>
-      <Text>You can set custom title and custom fill color</Text>
-      <SmartHRLogo title="custom title" fill="#008d91" />
-    </li>
-  </List>
-))
+storiesOf('SmartHRLogo', module)
+  .addParameters({
+    readme: {
+      sidebar: readme,
+    },
+  })
+  .add('all', () => (
+    <List>
+      <li>
+        <Text>default</Text>
+        <SmartHRLogo />
+      </li>
+      <li>
+        <Text>You can set custom title and custom fill color</Text>
+        <SmartHRLogo title="custom title" fill="#008d91" />
+      </li>
+      <li>
+        <Text>You can set custom width and height</Text>
+        <SmartHRLogo title="custom title" width={111} height={20} />
+      </li>
+    </List>
+  ))
 
 const List = styled.ul`
   margin: 0;
