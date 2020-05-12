fb9a27ebffbab7abd2a23c125d3dd963a50e5771
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-05-12 18:14:07 +0900

    feat: add MessageScreen Component (#768)
    * feat: add width and height props to SmartHRLogo
    
    * feat: add MessageScreen component
    
    * fix: change title tag
    
    Co-authored-by: Soichi Masuda <masuP9@gmail.com>
    
    * fix: corrected style writing
    
    * fix: change title props type to React.ReactNode
    
    * fix: add line-height to Link
    
    Co-authored-by: Soichi Masuda <masuP9@gmail.com>

---
+++ src/components/MessageScreen/MessageScreen.tsx
@@ -0,0 +1,151 @@
+import React, { FC, ReactNode } from 'react'
+import styled, { css } from 'styled-components'
+
+import { Theme, useTheme } from '../../hooks/useTheme'
+
+import { SmartHRLogo } from '../SmartHRLogo'
+import { Footer } from '../Footer'
+import { Icon } from '../Icon'
+
+type Props = {
+  title?: ReactNode
+  links?: Array<{
+    label: string
+    url: string
+    target?: string
+  }>
+  className?: string
+}
+
+const LOGO_HEIGHT = 20
+
+export const MessageScreen: FC<Props> = ({ title, links, children, className = '' }) => {
+  const theme = useTheme()
+
+  return (
+    <Wrapper themes={theme} className={className}>
+      <Box>
+        <Logo>
+          <SmartHRLogo width={111} height={LOGO_HEIGHT} fill={theme.palette.BRAND} />
+        </Logo>
+
+        {title && <Title themes={theme}>{title}</Title>}
+
+        {children && <Content themes={theme}>{children}</Content>}
+
+        {links && links.length && (
+          <Links themes={theme}>
+            {links.map((link) => (
+              <li key={link.label}>
+                <Link
+                  href={link.url}
+                  {...(link.target ? { target: link.target } : {})}
+                  themes={theme}
+                >
+                  {link.label}
+                  {link.target === '_blank' && <ExternalIcon color={theme.palette.TEXT_LINK} />}
+                </Link>
+              </li>
+            ))}
+          </Links>
+        )}
+      </Box>
+
+      <FooterArea>
+        <Footer />
+      </FooterArea>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div<{ themes: Theme }>`
+  ${({ themes }) => {
+    const { palette } = themes
+
+    return css`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-color: ${palette.BACKGROUND};
+    `
+  }}
+`
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const Logo = styled.div`
+  height: ${LOGO_HEIGHT}px;
+`
+const Title = styled.h1<{ themes: Theme }>`
+  ${({ themes }) => {
+    const { size, palette } = themes
+    const { pxToRem, space, font } = size
+
+    return css`
+      margin: ${pxToRem(space.S)} 0 0;
+      background-color: ${palette.BACKGROUND};
+      color: ${palette.TEXT_BLACK};
+      font-weight: normal;
+      font-size: ${font.VENTI}px;
+      line-height: 1;
+    `
+  }}
+`
+const Content = styled.div<{ themes: Theme }>`
+  ${({ themes }) => {
+    const { pxToRem, space } = themes.size
+
+    return css`
+      margin-top: ${pxToRem(space.XS)};
+    `
+  }}
+`
+const Links = styled.ul<{ themes: Theme }>`
+  ${({ themes }) => {
+    const { pxToRem, space } = themes.size
+
+    return css`
+      margin: ${pxToRem(space.XS)} 0 0;
+      padding: 0;
+      list-style: none;
+      text-align: center;
+      line-height: 1;
+
+      > li:not(:first-child) {
+        margin-top: ${pxToRem(space.XS)};
+      }
+    `
+  }}
+`
+const Link = styled.a<{ themes: Theme }>`
+  ${({ themes }) => {
+    const { palette, size } = themes
+
+    return css`
+      color: ${palette.TEXT_LINK};
+      font-size: ${size.font.TALL}px;
+      line-height: 1.4;
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    `
+  }}
+`
+const ExternalIcon = styled(Icon).attrs(() => ({
+  name: 'fa-external-link-alt',
+  size: 14,
+}))`
+  margin-left: 0.4rem;
+  vertical-align: -1px;
+`
+const FooterArea = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+`
---
+++ src/components/MessageScreen/README.md
@@ -0,0 +1,38 @@
+# MessageScreen
+
+```tsx
+import { MessageScreen } from 'smarthr-ui'
+
+<MessageScreen
+  title="お探しのページはみつかりませんでした"
+  links={[
+    {
+      label: 'ホームへ',
+      url: 'http://example.com',
+    },
+  ]}
+>
+  <p>
+    お探しのページは一時的にアクセスができない状況にあるか、
+    <br />
+    移動もしくは削除された可能性があります。
+  </p>
+</MessageScreen>
+```
+
+## props
+
+| Name      | Required | Type                | DefaultValue | Description                                                     |
+| --------- | -------- | ------------------- | ------------ | --------------------------------------------------------------- |
+| title     | -        | **React.ReactNode** | -            | title string to display at the top of the content               |
+| links     | -        | **Link[]**          | -            | List of anchor elements to display at the bottom of the content |
+| children  | -        | **React.ReactNode** | -            | children content                                                |
+| className | -        | **string**          | ''           | className of wrapper div element                                |
+
+### type of Link
+
+| Name   | Required | Type       | DefaultValue | Description                                                         |
+| ------ | -------- | ---------- | ------------ | ------------------------------------------------------------------- |
+| label  | ✓        | **string** | -            | inner text of anchor element                                        |
+| url    | ✓        | **string** | -            | href of anchor element                                              |
+| target | -        | **string** | -            | target of anchor element. For "\_blank", ExternalIcon is displayed |
