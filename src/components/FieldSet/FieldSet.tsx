01eea8be4770edb3f1d4c3e07df0a5631b4307cb
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-05-12 19:53:29 +0900

    feat: add labelSuffix props to FieldSet component (#769)

--- src/components/FieldSet/FieldSet.tsx
+++ src/components/FieldSet/FieldSet.tsx
@@ -1,13 +1,12 @@
 import React, { FC, ReactNode } from 'react'
 import styled, { css } from 'styled-components'
 
+import { Theme, useTheme } from '../../hooks/useTheme'
+
 import { Input, Props as InputProps } from '../Input'
 import { Heading, HeadingTagTypes, HeadingTypes } from '../Heading'
 import { StatusLabel } from '../StatusLabel'
 import { Icon } from '../Icon'
-import { Theme, useTheme } from '../../hooks/useTheme'
-
-type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
 
 type Props = Omit<InputProps, 'error'> & {
   label: string
@@ -15,8 +14,8 @@ type Props = Omit<InputProps, 'error'> & {
   labelTagType?: HeadingTagTypes
   errorMessage?: string
   helpMessage?: string
+  labelSuffix?: ReactNode
   className?: string
-  children?: ReactNode
 }
 
 export const FieldSet: FC<Props> = ({
@@ -26,6 +25,7 @@ export const FieldSet: FC<Props> = ({
   errorMessage,
   helpMessage,
   className = '',
+  labelSuffix,
   children,
   ...props
 }) => {
@@ -38,41 +38,45 @@ export const FieldSet: FC<Props> = ({
           {label}
         </TitleText>
 
-        {props.required && (
-          <StatusLabelWrapper themes={theme}>
-            <StatusLabel type="required">必須</StatusLabel>
-          </StatusLabelWrapper>
-        )}
+        {props.required && <StatusLabel type="required">必須</StatusLabel>}
+
+        {labelSuffix && labelSuffix}
       </Title>
+
       {children ? children : <Input {...props} error={!!errorMessage} />}
+
       {errorMessage && (
         <Error themes={theme}>
-          <ErrorIcon name="fa-exclamation-circle" themes={theme} color={theme.palette.DANGER} />
+          <ErrorIcon name="fa-exclamation-circle" color={theme.palette.DANGER} />
           <ErrorText>{errorMessage}</ErrorText>
         </Error>
       )}
+
       {helpMessage && <Help themes={theme}>{helpMessage}</Help>}
     </Wrapper>
   )
 }
 
-const Wrapper: any = styled.div<{ width: string | number }>`
+const Wrapper = styled.div<{ width: string | number }>`
   ${({ width }) => css`
     display: inline-block;
     width: ${typeof width === 'number' ? `${width}px` : width};
   `}
 `
-
 const Title = styled.div<{ themes: Theme }>`
   ${({ themes }) => css`
+    display: flex;
+    align-items: center;
     margin: 0 0 ${themes.size.pxToRem(themes.size.space.XXS)};
+
+    > *:not(:first-child) {
+      margin-left: ${themes.size.pxToRem(themes.size.space.XXS)};
+    }
   `}
 `
-
 const TitleText = styled(Heading)`
   display: inline-block;
 `
-
 const Help = styled.div<{ themes: Theme }>`
   ${({ themes }) => css`
     margin: ${themes.size.pxToRem(themes.size.space.XXS)} 0 0 0;
@@ -88,20 +92,10 @@ const Error = styled.div<{ themes: Theme }>`
     line-height: 1;
   `}
 `
-
-const ErrorIcon = styled(Icon)<{ themes: Theme }>`
-  ${({ themes }) => css`
-    margin-right: ${themes.size.pxToRem(4)};
-    vertical-align: middle;
-  `}
-`
-
-const ErrorText = styled.span`
+const ErrorIcon = styled(Icon)`
+  margin-right: 0.4rem;
   vertical-align: middle;
 `
-
-const StatusLabelWrapper = styled.span<{ themes: Theme }>`
-  ${({ themes }) => css`
-    margin-left: ${themes.size.pxToRem(themes.size.space.XXS)};
-  `}
+const ErrorText = styled.span`
+  vertical-align: middle;
 `
