7de107f211282361b0fbf58a3590dd292e8f48d4
im36-123 <shota.takata@smarthr.co.jp>
2020-05-29 17:11:48 +0900

    Revert "fix: change console.error to throw new Error"
    This reverts commit b69dc27d175afce6bd26b5314f1ee744900edfb0.

--- src/components/BottomFixedArea/bottomFixedAreaHelper.ts
+++ src/components/BottomFixedArea/bottomFixedAreaHelper.ts
@@ -4,7 +4,7 @@ export const validateElement = (primary?: Primary, secondary?: Secondary) => {
   if (primary) {
     const { displayName } = primary.type
     if (displayName !== 'PrimaryButton' && displayName !== 'PrimaryButtonAnchor') {
-      throw new Error(
+      console.error(
         'SmartHR UI: the primaryButton props accepts PrimaryButton or PrimaryButtonAnchor component',
       )
     }
@@ -12,7 +12,7 @@ export const validateElement = (primary?: Primary, secondary?: Secondary) => {
   if (secondary) {
     const { displayName } = secondary.type
     if (displayName !== 'SecondaryButton' && displayName !== 'SecondaryButtonAnchor') {
-      throw new Error(
+      console.error(
         'SmartHR UI: the secondaryButton props accepts SecondaryButton or SecondaryButtonAnchor component',
       )
     }
