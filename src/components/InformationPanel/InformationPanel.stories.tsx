2a25ea007669216e98d098dcc7aff3adfb18557a
wmoai <wmoaism@gmail.com>
2020-06-01 15:54:29 +0900

    feat: add togglable option

--- src/components/InformationPanel/InformationPanel.stories.tsx
+++ src/components/InformationPanel/InformationPanel.stories.tsx
@@ -12,17 +12,37 @@ storiesOf('InformationPanel', module)
     },
   })
   .add('all', () => (
-    <Wrapper>
-      <InformationPanel title="Panel Title" openButtonLabel="OPEN" closeButtonLabel="CLOSE">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </InformationPanel>
-    </Wrapper>
+    <List>
+      <li>
+        <Wrapper>
+          <InformationPanel title="Panel Title" openButtonLabel="OPEN" closeButtonLabel="CLOSE">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+          </InformationPanel>
+        </Wrapper>
+      </li>
+      <li>
+        <Wrapper>
+          <InformationPanel title="Panel without a toggle button" togglable={false}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+          </InformationPanel>
+        </Wrapper>
+      </li>
+    </List>
   ))
 
+const List = styled.ul`
+  list-style: none;
+`
 const Wrapper = styled.div`
   width: 1140px;
   margin: 32px auto;
