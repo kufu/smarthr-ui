486bb5768f07739b518a928021c0d81671f3772f
shota takata <36581513+im36-123@users.noreply.github.com>
2020-06-02 11:38:38 +0900

    feat: Add BottomFixedArea component (#777)
    * feat: add TertialyLink component
    
    * feat: add base of BottomFixedArea
    
    * feat: add style
    
    * feat: separate validate logic into helper file
    
    * feat: sort import
    
    * fix: refactoring
    
    * feat: add position style
    
    * fix: export component from root index file
    
    * fix: change style
    
    * fix: refactoring
    
    * fix: eslint --fix
    
    * feat: add README
    
    * feat: update README
    
    * fix: undo unnecessary work
    
    * fix: change console.error to throw new Error
    
    * fix: typo
    
    * fix: enable outline
    
    * Revert "fix: change console.error to throw new Error"
    
    This reverts commit b69dc27d175afce6bd26b5314f1ee744900edfb0.

---
+++ src/components/BottomFixedArea/BottomFixedArea.stories.tsx
@@ -0,0 +1,22 @@
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+
+import { BottomFixedArea } from './BottomFixedArea'
+import { PrimaryButton, SecondaryButton } from '../Button'
+import { action } from '@storybook/addon-actions'
+import readme from './README.md'
+
+storiesOf('BottomFixedArea', module)
+  .addParameters({
+    readme: {
+      sidebar: readme,
+    },
+  })
+  .add('BottomFixedArea', () => (
+    <BottomFixedArea
+      description="This is description."
+      primaryButton={<PrimaryButton>Primary Button</PrimaryButton>}
+      secondaryButton={<SecondaryButton>Secondary Button</SecondaryButton>}
+      tertiaryLinks={[{ text: 'tertiary_1', iconName: 'fa-trash', onClick: action('click_1') }]}
+    />
+  ))
---
+++ src/components/BottomFixedArea/BottomFixedArea.tsx
@@ -0,0 +1,130 @@
+import React, { ComponentProps, FC, FunctionComponentElement, ReactNode, useEffect } from 'react'
+import styled, { css } from 'styled-components'
+
+import { TertiaryLink } from './TertiaryLink'
+import { validateElement } from './bottomFixedAreaHelper'
+
+import { Base as BaseComponent } from '../Base'
+import {
+  PrimaryButton,
+  PrimaryButtonAnchor,
+  SecondaryButton,
+  SecondaryButtonAnchor,
+} from '../Button'
+import { Theme, useTheme } from '../../hooks/useTheme'
+
+export type Primary =
+  | FunctionComponentElement<ComponentProps<typeof PrimaryButton>>
+  | FunctionComponentElement<ComponentProps<typeof PrimaryButtonAnchor>>
+
+export type Secondary =
+  | FunctionComponentElement<ComponentProps<typeof SecondaryButton>>
+  | FunctionComponentElement<ComponentProps<typeof SecondaryButtonAnchor>>
+
+type Props = {
+  description?: ReactNode
+  primaryButton?: Primary
+  secondaryButton?: Secondary
+  tertiaryLinks?: Array<React.ComponentProps<typeof TertiaryLink>>
+  zIndex?: number
+  className?: string
+}
+
+export const BottomFixedArea: FC<Props> = (props) => {
+  const theme = useTheme()
+  const {
+    description,
+    primaryButton,
+    secondaryButton,
+    tertiaryLinks,
+    zIndex = 500,
+    className = '',
+  } = props
+
+  useEffect(() => {
+    validateElement(primaryButton, secondaryButton)
+  }, [primaryButton, secondaryButton])
+
+  return (
+    <Base themes={theme} zIndex={zIndex} className={className}>
+      {description && <Text>{description}</Text>}
+      {(secondaryButton || primaryButton) && (
+        <ButtonList themes={theme}>
+          {secondaryButton && <li>{secondaryButton}</li>}
+          {primaryButton && <li>{primaryButton}</li>}
+        </ButtonList>
+      )}
+      {tertiaryLinks && tertiaryLinks.length > 0 && (
+        <TertiaryList themes={theme}>
+          {tertiaryLinks.map((tertiaryLink, index) => (
+            <li key={index}>
+              <TertiaryLink {...tertiaryLink} />
+            </li>
+          ))}
+        </TertiaryList>
+      )}
+    </Base>
+  )
+}
+
+const Base = styled(BaseComponent)<{ themes: Theme; zIndex: number }>`
+  ${({ themes, zIndex }) => {
+    const { pxToRem, space } = themes.size
+
+    return css`
+      display: flex;
+      flex-direction: column;
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      padding: ${pxToRem(space.S)};
+      text-align: center;
+      z-index: ${zIndex};
+    `
+  }}
+`
+const Text = styled.div`
+  margin: 0;
+`
+const ButtonList = styled.ul<{ themes: Theme }>`
+  ${({ themes }) => {
+    const { pxToRem, space } = themes.size
+
+    return css`
+      margin: ${pxToRem(space.XS)} 0 0 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+
+      > li {
+        list-style: none;
+        margin-right: ${pxToRem(space.XS)};
+
+        &:last-of-type {
+          margin-right: 0;
+        }
+      }
+    `
+  }}
+`
+const TertiaryList = styled.ul<{ themes: Theme }>`
+  ${({ themes }) => {
+    const { pxToRem, space } = themes.size
+
+    return css`
+      margin: ${pxToRem(space.XS)} 0 0 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+
+      > li {
+        list-style: none;
+        margin-right: ${pxToRem(space.XS)};
+
+        &:last-of-type {
+          margin-right: 0;
+        }
+      }
+    `
+  }}
+`
