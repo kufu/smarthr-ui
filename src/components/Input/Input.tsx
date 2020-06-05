b433f8bcaa8ff0bf0ff49a1616a124847823f31c
wmoai <wmoaism@gmail.com>
2020-06-05 14:57:39 +0900

    feat: add shaping value for currency type

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React, { FC, InputHTMLAttributes, useEffect, useRef } from 'react'
+import React, { FC, FocusEvent, InputHTMLAttributes, useEffect, useRef } from 'react'
 import styled, { css } from 'styled-components'
 
 import { Theme, useTheme } from '../../hooks/useTheme'
@@ -24,12 +24,42 @@ export type Props = InputHTMLAttributes<HTMLInputElement> & {
   thousandsSeparated?: boolean
 }
 
-export const Input: FC<Props> = ({ type, autoFocus, thousandsSeparated, ...props }) => {
+export const Input: FC<Props> = ({
+  type,
+  onFocus,
+  onBlur,
+  autoFocus,
+  thousandsSeparated,
+  ...props
+}) => {
   const theme = useTheme()
   const ref = useRef<HTMLInputElement>(null)
 
   const isCurrency = type === 'number' && thousandsSeparated
   const actualType = isCurrency ? 'text' : type
+
+  const handleFocus = (e: FocusEvent<HTMLInputElement>) => {
+    if (isCurrency && ref.current) {
+      const commaExcluded = ref.current.value.replace(/,/g, '')
+      ref.current.value = commaExcluded
+    }
+    onFocus && onFocus(e)
+  }
+
+  const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+    if (isCurrency && ref.current) {
+      const value = ref.current.value
+      const shaped = value
+        .replace(/[０-９．]/g, (s) => String.fromCharCode(s.charCodeAt(0) - 0xfee0)) // convert number and dot to half-width
+        .replace(/[−ー]/, '-') // replace full-width minus
+        .replace(/[^0-9.-]|(?!^)-|^\.+|\.+$/g, '') // exclude non-numeric characters
+      const splited = shaped.split('.')
+      const integerPart = splited[0].replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') // add comma to integer every 3 digits
+      const formattedArray = Object.assign(splited, [integerPart])
+      ref.current.value = formattedArray.join('.')
+    }
+    onBlur && onBlur(e)
+  }
 
   useEffect(() => {
     if (autoFocus && ref && ref.current) {
@@ -37,7 +67,16 @@ export const Input: FC<Props> = ({ type, autoFocus, thousandsSeparated, ...props
     }
   }, [autoFocus])
 
-  return <StyledInput type={actualType} {...props} ref={ref} themes={theme} />
+  return (
+    <StyledInput
+      type={actualType}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      {...props}
+      ref={ref}
+      themes={theme}
+    />
+  )
 }
 
 const StyledInput = styled.input<Props & { themes: Theme }>`
