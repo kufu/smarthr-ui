43fe9fb09351a2d2a6bd31b169e2f53fbff5fc5a
Toru Kobayashi <koba0004@gmail.com>
2020-06-08 11:32:33 +0900

    test: add a test whether build files are es5 or not (#779)
    * test: add a test whether build files are es5 or not
    
    * chore: update yarn.lock

---
+++ scripts/build-test.ts
@@ -0,0 +1,32 @@
+const { ECMAVersionValidatorPlugin } = require('ecma-version-validator-webpack-plugin')
+const webpack = require('webpack')
+const path = require('path')
+const MemoryFS = require('memory-fs')
+
+const compiler = webpack({
+  mode: 'development',
+  entry: path.resolve('src', 'index.ts'),
+  devtool: 'nosources-source-map',
+  plugins: [new ECMAVersionValidatorPlugin()],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+})
+compiler.outputFileSystem = new MemoryFS()
+compiler.run((err, stats) => {
+  if (err !== null) {
+    throw err
+  }
+  if (stats.compilation.errors.length > 0) {
+    throw stats.compilation.errors
+  }
+})
