6045206bc941a29323df2f976f51527bf09109aa
wmoai <wmoaism@gmail.com>
2020-06-09 19:56:26 +0900

    docs: update docs

--- src/components/Dropdown/FilterDropdown/FilterDropdown.stories.tsx
+++ src/components/Dropdown/FilterDropdown/FilterDropdown.stories.tsx
@@ -23,53 +23,66 @@ storiesOf('Dropdown/filter', module)
 
     return (
       <Wrapper>
-        <FilterDropdown
-          onApply={action('apply')}
-          onCancel={action('cancel')}
-          onReset={() => {
-            setValue('hoge')
-            setText('')
-          }}
-        >
-          <Text>
-            `FilterDropdown` provide specific interface to be able to filter data.
-            <br />
-            You can control inputs for filtering conditions as children components.
-          </Text>
-          <RadioButtonList>
-            <li>
-              <RadioButtonLabel
-                name="hoge"
-                label="hoge"
-                checked={value === 'hoge'}
-                onChange={onChangeValue}
-              />
-            </li>
-            <li>
-              <RadioButtonLabel
-                name="fuga"
-                label="fuga"
-                checked={value === 'fuga'}
-                onChange={onChangeValue}
-              />
-            </li>
-            <li>
-              <RadioButtonLabel
-                name="piyo"
-                label="piyo"
-                checked={value === 'piyo'}
-                onChange={onChangeValue}
-              />
-            </li>
-            <li>
-              <Input name="test" value={text} onChange={onChangeText} />
-            </li>
-          </RadioButtonList>
-          <Description>
-            ↓<br />↓
-          </Description>
-          <Text>Children content is scrollable.</Text>
-        </FilterDropdown>
+        <List>
+          <dt>Controlled</dt>
+          <dd>
+            <FilterDropdown
+              onApply={action('apply')}
+              onCancel={action('cancel')}
+              onReset={() => {
+                setValue('hoge')
+                setText('')
+              }}
+            >
+              <Text>
+                `FilterDropdown` provide specific interface to be able to filter data.
+                <br />
+                You can control inputs for filtering conditions as children components.
+              </Text>
+              <RadioButtonList>
+                <li>
+                  <RadioButtonLabel
+                    name="hoge"
+                    label="hoge"
+                    checked={value === 'hoge'}
+                    onChange={onChangeValue}
+                  />
+                </li>
+                <li>
+                  <RadioButtonLabel
+                    name="fuga"
+                    label="fuga"
+                    checked={value === 'fuga'}
+                    onChange={onChangeValue}
+                  />
+                </li>
+                <li>
+                  <RadioButtonLabel
+                    name="piyo"
+                    label="piyo"
+                    checked={value === 'piyo'}
+                    onChange={onChangeValue}
+                  />
+                </li>
+                <li>
+                  <Input name="test" value={text} onChange={onChangeText} />
+                </li>
+              </RadioButtonList>
+              <Description>
+                ↓<br />↓
+              </Description>
+              <Text>Children content is scrollable.</Text>
+            </FilterDropdown>
+          </dd>
+          <dt>Filtered</dt>
+          <dd>
+            <FilterDropdown isFiltered onApply={action('apply')} onReset={action('reset')}>
+              <Text>
+                You can change border color of the trigger button by setting `isFiltered`.
+              </Text>
+            </FilterDropdown>
+          </dd>
+        </List>
       </Wrapper>
     )
   })
@@ -77,6 +90,18 @@ storiesOf('Dropdown/filter', module)
 const Wrapper = styled.div`
   padding: 24px;
   color: #333;
+`
+const List = styled.dl`
+  margin: 0;
+  dt {
+    margin-bottom: 8px;
+    &:not(:first-child) {
+      margin-top: 24px;
+    }
+  }
+  dd {
+    margin: 0;
+  }
 `
 const Text = styled.p`
   margin: 0;
--- src/components/Dropdown/FilterDropdown/README.md
+++ src/components/Dropdown/FilterDropdown/README.md
@@ -13,9 +13,10 @@ import { FilterDropdown } from 'smarthr-ui'
 
 **FilterDropdown**
 
-| Name     | Required | Type                | DefaultValue | Description                                           |
-| -------- | -------- | ------------------- | ------------ | ----------------------------------------------------- |
-| onApply  | true     | **function**        | -            | Fired when the Apply button is clicked                |
-| onCancel | -        | **function**        | -            | Fired when the Cancel button is clicked               |
-| onReset  | -        | **function**        | -            | Fired when the Reset button is clicked                |
-| children | true     | **React.ReactNode** | -            | Components to control Inputs for filtering conditions |
+| Name       | Required | Type                | DefaultValue | Description                                           |
+| ---------- | -------- | ------------------- | ------------ | ----------------------------------------------------- |
+| isFiltered | -        | **boolean**         | false        | Flag to switch border color of the trigger button     |
+| onApply    | true     | **function**        | -            | Fired when the Apply button is clicked                |
+| onCancel   | -        | **function**        | -            | Fired when the Cancel button is clicked               |
+| onReset    | -        | **function**        | -            | Fired when the Reset button is clicked                |
+| children   | true     | **React.ReactNode** | -            | Components to control Inputs for filtering conditions |
