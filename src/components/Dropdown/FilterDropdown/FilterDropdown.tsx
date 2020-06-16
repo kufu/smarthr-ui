b2e94fc01579b41d5c6b13280023b97783935d15
wmoai <wmoaism@gmail.com>
2020-06-16 18:20:23 +0900

    feat: add FilterDropdown (#825)
    * feat: add FilterDropdown
    
    * test: update storybook
    
    * fix: modify props type
    
    * fix: move to FilterDropdown directory
    
    * fix: add export
    
    * docs: add README
    
    * fix: add root export
    
    * fix: design feedback
    
    * feat: add isFiltered option
    
    * docs: update docs
    
    * fix: elements hierarchy

---
+++ src/components/Dropdown/FilterDropdown/FilterDropdown.tsx
@@ -0,0 +1,91 @@
+import React, { FC, ReactNode } from 'react'
+import styled, { css } from 'styled-components'
+
+import { Theme, useTheme } from '../../../hooks/useTheme'
+import { Dropdown } from '../Dropdown'
+import { DropdownTrigger } from '../DropdownTrigger'
+import { DropdownContent } from '../DropdownContent'
+import { DropdownCloser } from '../DropdownCloser'
+import { DropdownScrollArea } from '../DropdownScrollArea'
+import { PrimaryButton, SecondaryButton, TextButton } from '../../Button'
+import { Icon } from '../../Icon'
+
+type Props = {
+  isFiltered?: boolean
+  onApply: () => void
+  onCancel?: () => void
+  onReset?: () => void
+  children: ReactNode
+}
+
+export const FilterDropdown: FC<Props> = ({
+  isFiltered = false,
+  onApply,
+  onCancel,
+  onReset,
+  children,
+}: Props) => {
+  const themes = useTheme()
+
+  return (
+    <Dropdown>
+      <TriggerButtonWrapper themes={themes} isFiltered={isFiltered}>
+        <DropdownTrigger>
+          <SecondaryButton suffix={<Icon name="fa-caret-down" />}>絞り込み</SecondaryButton>
+        </DropdownTrigger>
+      </TriggerButtonWrapper>
+      <DropdownContent controllable>
+        <DropdownScrollArea>
+          <ContentLayout>{children}</ContentLayout>
+        </DropdownScrollArea>
+        <BottomLayout themes={themes}>
+          {onReset && (
+            <ResetButtonLayout>
+              <TextButton size="s" prefix={<Icon name="fa-undo-alt" />} onClick={() => onReset()}>
+                絞り込み条件を解除
+              </TextButton>
+            </ResetButtonLayout>
+          )}
+          <RightButtonLayout>
+            <DropdownCloser>
+              <SecondaryButton onClick={() => onCancel?.()}>キャンセル</SecondaryButton>
+            </DropdownCloser>
+            <DropdownCloser>
+              <PrimaryButton onClick={() => onApply()}>適用</PrimaryButton>
+            </DropdownCloser>
+          </RightButtonLayout>
+        </BottomLayout>
+      </DropdownContent>
+    </Dropdown>
+  )
+}
+
+const TriggerButtonWrapper = styled.div<{ themes: Theme; isFiltered: boolean }>`
+  ${({ themes, isFiltered }) =>
+    isFiltered &&
+    css`
+      button {
+        border-color: ${themes.palette.WARNING};
+      }
+    `}
+`
+
+const ContentLayout = styled.div`
+  padding: 24px;
+`
+const BottomLayout = styled.div<{ themes: Theme }>`
+  display: flex;
+  align-items: center;
+  border-top: 1px solid ${({ themes }) => themes.palette.BORDER};
+  padding: 16px 24px;
+`
+const ResetButtonLayout = styled.div`
+  margin-left: -8px;
+`
+const RightButtonLayout = styled.div`
+  display: flex;
+  margin-left: auto;
+  & > *:not(:first-child) {
+    margin-left: 16px;
+  }
+`
