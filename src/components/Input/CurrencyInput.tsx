f12eb42e16346639ea4c47e5ad42e2b6bef60dc5
wmoai <wmoaism@gmail.com>
2020-07-10 18:25:01 +0900

    feat: add converting currency by value and defaultValue

--- src/components/Input/CurrencyInput.tsx
+++ src/components/Input/CurrencyInput.tsx
@@ -1,44 +1,83 @@
-import React, { ChangeEvent, FC, FocusEvent, useState } from 'react'
+import React, { ChangeEvent, FC, FocusEvent, useCallback, useEffect, useState } from 'react'
 
 import { Input, Props as InputProps } from './Input'
 
-type Props = Omit<InputProps, 'type' | 'defaultValue'> & {
+type Props = Omit<InputProps, 'type' | 'value' | 'defaultValue'> & {
   value?: string
+  onChangeValue?: (value: string) => void
 }
 
-export const CurrencyInput: FC<Props> = ({ onChange, onFocus, onBlur, ...props }) => {
-  const [value, setValue] = useState(props.value || '')
+export const CurrencyInput: FC<Props> = ({
+  value,
+  onChangeValue,
+  onChange,
+  onFocus,
+  onBlur,
+  ...props
+}) => {
+  const [controlledValue, setControlledValue] = useState(value || '')
+  const [isFocused, setIsFocused] = useState(false)
+
+  const changeValue = useCallback(
+    (changed = '') => {
+      if (changed === controlledValue) {
+        return
+      }
+      setControlledValue(changed)
+      onChangeValue && onChangeValue(changed)
+    },
+    [controlledValue, onChangeValue],
+  )
+
+  useEffect(() => {
+    if (isFocused) {
+      setControlledValue(value || '')
+    } else {
+      changeValue(convertToCurrency(value))
+    }
+  }, [isFocused, value, controlledValue, changeValue])
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
+    changeValue(e.target.value)
     onChange && onChange(e)
   }
 
   const handleFocus = (e: FocusEvent<HTMLInputElement>) => {
-    const commaExcluded = value.replace(/,/g, '')
-    setValue(commaExcluded)
+    setIsFocused(true)
+    const commaExcluded = controlledValue.replace(/,/g, '')
+    changeValue(commaExcluded)
     onFocus && onFocus(e)
   }
 
   const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
-    const shaped = value
-      .replace(/[０-９．]/g, (s) => String.fromCharCode(s.charCodeAt(0) - 0xfee0)) // convert number and dot to half-width
-      .replace(/[−ー]/, '-') // replace full-width minus
-      .replace(/[^0-9.-]|(?!^)-|^\.+|\.+$/g, '') // exclude non-numeric characters
-    const splited = shaped.split('.')
-    const integerPart = splited[0].replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') // add comma to integer every 3 digits
-    const formattedArray = Object.assign(splited, [integerPart])
-    setValue(formattedArray.join('.'))
+    setIsFocused(false)
+    changeValue(convertToCurrency(controlledValue))
     onBlur && onBlur(e)
   }
 
   return (
     <Input
       type="text"
-      value={value}
+      value={controlledValue}
       onChange={handleChange}
       onFocus={handleFocus}
       onBlur={handleBlur}
+      {...props}
     />
   )
+}
+
+function convertToCurrency(value?: string) {
+  if (!value) {
+    return ''
+  }
+  const shaped = value
+    .replace(/[０-９．]/g, (s) => String.fromCharCode(s.charCodeAt(0) - 0xfee0)) // convert number and dot to half-width
+    .replace(/[−ー]/, '-') // replace full-width minus
+    // .replace(/[^0-9.-]|(?!^)-|^\.+|\.+$/g, '') // exclude non-numeric characters
+    .replace(/[^0-9.-]/g, '') // exclude non-numeric characters
+  const splited = shaped.split('.')
+  const integerPart = splited[0].replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') // add comma to integer every 3 digits
+  const formattedArray = Object.assign(splited, [integerPart])
+  return formattedArray.join('.')
 }
--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -8,58 +8,71 @@ import { CurrencyInput } from './CurrencyInput'
 
 import { Icon } from '../Icon'
 
-storiesOf('Input', module).add('all', () => (
-  <List>
-    <li>
-      <Txt>text</Txt>
-      <Input type="text" defaultValue="string" />
-    </li>
-    <li>
-      <Txt>number</Txt>
-      <Input type="number" defaultValue="1" />
-    </li>
-    <li>
-      <Txt>number (thousands separated)</Txt>
-      <CurrencyInput />
-    </li>
-    <li>
-      <Txt>password</Txt>
-      <Input type="password" defaultValue="password" />
-    </li>
-    <li>
-      <Txt>placeholder</Txt>
-      <Input placeholder="string" />
-    </li>
-    <li>
-      <Txt>width</Txt>
-      <Input defaultValue="width: 100%" width="100%" />
-    </li>
-    <li>
-      <Txt>onChange</Txt>
-      <Input onChange={action('onChange!!')} />
-    </li>
-    <li>
-      <Txt>onBlur</Txt>
-      <Input onBlur={action('onBlur!!')} />
-    </li>
-    <li>
-      <Txt>disabled</Txt>
-      <Input disabled={true} />
-    </li>
-    <li>
-      <Txt>error</Txt>
-      <Input error={true} />
-    </li>
-    <li>
-      <Txt>prefix</Txt>
-      <Input prefix={<Icon name="fa-search" color="#d6d6d6" />} />
-    </li>
-    <li>
-      <Txt>suffix</Txt>
-      <Input suffix={<Icon name="fa-search" color="#d6d6d6" />} />
-    </li>
-  </List>
-))
+storiesOf('Input', module)
+  .add('all', () => (
+    <List>
+      <li>
+        <Txt>text</Txt>
+        <Input type="text" defaultValue="string" />
+      </li>
+      <li>
+        <Txt>number</Txt>
+        <Input type="number" defaultValue="1" />
+      </li>
+      <li>
+        <Txt>password</Txt>
+        <Input type="password" defaultValue="password" />{' '}
+      </li>{' '}
+      <li>
+        {' '}
+        <Txt>placeholder</Txt>
+        <Input placeholder="string" />
+      </li>
+      <li>
+        <Txt>width</Txt>
+        <Input defaultValue="width: 100%" width="100%" />
+      </li>
+      <li>
+        <Txt>onChange</Txt>
+        <Input onChange={action('onChange!!')} />
+      </li>
+      <li>
+        <Txt>onBlur</Txt>
+        <Input onBlur={action('onBlur!!')} />
+      </li>
+      <li>
+        <Txt>disabled</Txt>
+        <Input disabled={true} />
+      </li>
+      <li>
+        <Txt>error</Txt>
+        <Input error={true} />
+      </li>
+      <li>
+        <Txt>prefix</Txt>
+        <Input prefix={<Icon name="fa-search" color="#d6d6d6" />} />
+      </li>
+      <li>
+        <Txt>suffix</Txt>
+        <Input suffix={<Icon name="fa-search" color="#d6d6d6" />} />
+      </li>
+    </List>
+  ))
+  .add('currency', () => {
+    const [value, setValue] = React.useState('1234567890')
+    return (
+      <Wrapper>
+        <Txt>currency (add comma to integer every 3 digits)</Txt>
+        <CurrencyInput
+          value={value}
+          onChangeValue={(changed) => {
+            action('changed value')(changed)
+            setValue(changed)
+          }}
+        />
+      </Wrapper>
+    )
+  })
 
 const List = styled.ul`
   padding: 0 24px;
@@ -71,4 +84,7 @@ const List = styled.ul`
 `
 const Txt = styled.p`
   margin: 0 0 8px 0;
+`
+const Wrapper = styled.div`
+  padding: 24px;
 `
