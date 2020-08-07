ded93f7285928ea93fd2bc88b93881e6c998b906
wmoai <wmoaism@gmail.com>
2020-08-07 18:23:05 +0900

    docs: add README

--- src/components/DatePicker/DatePicker.stories.tsx
+++ src/components/DatePicker/DatePicker.stories.tsx
@@ -4,33 +4,40 @@ import * as React from 'react'
 import styled from 'styled-components'
 
 import { DatePicker } from './DatePicker'
+import readme from './README.md'
 
-storiesOf('DatePicker', module).add('all', () => {
-  const [date, setDate] = React.useState<Date | null>(new Date(2020, 0, 1))
-  return (
-    <List>
-      <dt>DatePicker</dt>
-      <dd>
-        <DatePicker onChangeDate={action('change date')} />
-      </dd>
-      <dt>Custom format (ex. Date.toDateString)</dt>
-      <dd>
-        <DatePicker
-          date={date}
-          onChangeDate={(_date) => setDate(_date)}
-          formatDate={(_date) => {
-            if (!_date) return ''
-            return _date.toDateString()
-          }}
-        />
-      </dd>
-      <dt>Extending style (width: 50%)</dt>
-      <dd>
-        <ExtendingDatePicker />
-      </dd>
-    </List>
-  )
-})
+storiesOf('DatePicker', module)
+  .addParameters({
+    readme: {
+      sidebar: readme,
+    },
+  })
+  .add('all', () => {
+    const [date, setDate] = React.useState<Date | null>(new Date(2020, 0, 1))
+    return (
+      <List>
+        <dt>DatePicker</dt>
+        <dd>
+          <DatePicker onChangeDate={action('change date')} />
+        </dd>
+        <dt>Custom format (ex. Date.toDateString)</dt>
+        <dd>
+          <DatePicker
+            date={date}
+            onChangeDate={(_date) => setDate(_date)}
+            formatDate={(_date) => {
+              if (!_date) return ''
+              return _date.toDateString()
+            }}
+          />
+        </dd>
+        <dt>Extending style (width: 50%)</dt>
+        <dd>
+          <ExtendingDatePicker />
+        </dd>
+      </List>
+    )
+  })
 
 const List = styled.dl`
   padding: 24px;
