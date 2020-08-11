054e06acf9253d112f55af154bdbe90835ebc3ce
AtsushiM <at.mizoue@gmail.com>
2020-08-11 13:33:34 +0900

    fix: update font-family

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,1 @@
-export const FONT_FAMILY =
-  'system-ui, -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Hiragino Sans", "Hiragino Kaku Gothic ProN", "ヒラギノ角ゴ ProN W3", Meiryo, "メイリオ", sans-serif'
+export const FONT_FAMILY = 'system-ui, sans-serif'
