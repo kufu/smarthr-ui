429b3b5f0605885cc1f9666af7c79b4d6f15064f
moshisora <moshisora990@gmail.com>
2020-08-12 11:21:16 +0900

    chore: build package

--- lib/components/StatusLabel/StatusLabel.d.ts
+++ lib/components/StatusLabel/StatusLabel.d.ts
@@ -1,8 +1,7 @@
 import { FC } from 'react';
-declare type Props = {
+export declare type Props = {
     type?: 'done' | 'success' | 'process' | 'required' | 'disabled' | 'must' | 'warning' | 'error';
     className?: string;
     children: string;
 };
 export declare const StatusLabel: FC<Props>;
-export {};
