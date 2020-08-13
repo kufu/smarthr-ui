235161f66a21e87f4e8ec736620434d0ac971423
wmoai <wmoaism@gmail.com>
2020-08-13 18:18:07 +0900

    feat: add buldActionArea in Head of Table

--- src/components/Table/Head.tsx
+++ src/components/Table/Head.tsx
@@ -1,14 +1,38 @@
-import * as React from 'react'
+import React, { FC, ReactNode } from 'react'
+import styled, { css } from 'styled-components'
 
+import { Theme, useTheme } from '../../hooks/useTheme'
 import { TableGroupContext } from './Table'
 
 export type Props = {
-  children?: React.ReactNode
+  bulkActionArea?: ReactNode
+  children?: ReactNode
   className?: string
 }
 
-export const Head: React.FC<Props> = ({ className = '', children }) => (
-  <thead className={className}>
-    <TableGroupContext.Provider value={{ group: 'head' }}>{children}</TableGroupContext.Provider>
-  </thead>
-)
+export const Head: FC<Props> = ({ bulkActionArea, className = '', children }) => {
+  const themes = useTheme()
+  return (
+    <thead className={className}>
+      <TableGroupContext.Provider value={{ group: 'head' }}>{children}</TableGroupContext.Provider>
+      {bulkActionArea && (
+        <tr>
+          <BulkActionTD colSpan={1000} themes={themes}>
+            {bulkActionArea}
+          </BulkActionTD>
+        </tr>
+      )}
+    </thead>
+  )
+}
+
+const BulkActionTD = styled.td<{ themes: Theme }>(({ themes }) => {
+  const { frame } = themes
+  const { font, pxToRem, space } = themes.size
+  return css`
+    border-top: ${frame.border.default};
+    background-color: #eaeaea;
+    padding: ${pxToRem(space.XS)};
+    font-size: ${pxToRem(font.TALL)};
+  `
+})
