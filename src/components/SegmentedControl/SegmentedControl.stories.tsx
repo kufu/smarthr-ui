e405da9faf574a4f3e666c23dbed0694b78225ce
wmoai <wmoaism@gmail.com>
2020-08-18 13:30:47 +0900

    docs: add README

--- src/components/SegmentedControl/SegmentedControl.stories.tsx
+++ src/components/SegmentedControl/SegmentedControl.stories.tsx
@@ -6,94 +6,102 @@ import styled from 'styled-components'
 import { SegmentedControl } from './SegmentedControl'
 import { Icon } from '../Icon'
 
-storiesOf('SegmentedControl', module).add('all', () => {
-  const [value1, setValue1] = React.useState<string | null>('segment1')
-  const [value2, setValue2] = React.useState<string | null>('segment5')
-  const [value3, setValue3] = React.useState<string | null>('segment3')
-  const [value4, setValue4] = React.useState<string | null>(null)
-  const [value5, setValue5] = React.useState<string | null>(null)
+import readme from './README.md'
 
-  const options = [
-    { value: 'segment1', ariaLabel: 'segment1' },
-    { value: 'segment2', ariaLabel: 'segment1' },
-    { value: 'segment3', ariaLabel: 'segment1' },
-    { value: 'segment4', ariaLabel: 'segment1' },
-    { value: 'segment5', ariaLabel: 'segment1' },
-  ]
-  return (
-    <List>
-      <dt>Default</dt>
-      <dd>
-        <SegmentedControl
-          options={options.map((option) => ({ ...option, content: 'ボタン' }))}
-          value={value1}
-          onClickOption={(value) => {
-            action('clicked')(value)
-            setValue1(value)
-          }}
-        />
-      </dd>
-      <dt>Small</dt>
-      <dd>
-        <SegmentedControl
-          options={options.map((option) => ({ ...option, content: 'ボタン' }))}
-          value={value2}
-          onClickOption={(value) => {
-            action('clicked')(value)
-            setValue2(value)
-          }}
-          size="s"
-        />
-      </dd>
-      <dt>Icon</dt>
-      <dd>
-        <SegmentedControl
-          options={options.map((option) => ({
-            ...option,
-            content: <Icon size={16} name="fa-plus-circle" />,
-          }))}
-          value={value3}
-          onClickOption={(value) => {
-            action('clicked')(value)
-            setValue3(value)
-          }}
-          isSquare
-        />
-      </dd>
-      <dt>Small icon</dt>
-      <dd>
-        <SegmentedControl
-          options={options.map((option) => ({
-            ...option,
-            content: <Icon size={13} name="fa-plus-circle" />,
-          }))}
-          value={value4}
-          onClickOption={(value) => {
-            action('clicked')(value)
-            setValue4(value)
-          }}
-          size="s"
-          isSquare
-        />
-      </dd>
-      <dt>Disabled</dt>
-      <dd>
-        <SegmentedControl
-          options={options.map((option, i) => ({
-            ...option,
-            content: 'ボタン',
-            disabled: i > 2,
-          }))}
-          value={value5}
-          onClickOption={(value) => {
-            action('clicked')(value)
-            setValue5(value)
-          }}
-        />
-      </dd>
-    </List>
-  )
-})
+storiesOf('SegmentedControl', module)
+  .addParameters({
+    readme: {
+      sidebar: readme,
+    },
+  })
+  .add('all', () => {
+    const [value1, setValue1] = React.useState<string | null>('segment1')
+    const [value2, setValue2] = React.useState<string | null>('segment5')
+    const [value3, setValue3] = React.useState<string | null>('segment3')
+    const [value4, setValue4] = React.useState<string | null>(null)
+    const [value5, setValue5] = React.useState<string | null>(null)
+
+    const options = [
+      { value: 'segment1', ariaLabel: 'segment1' },
+      { value: 'segment2', ariaLabel: 'segment1' },
+      { value: 'segment3', ariaLabel: 'segment1' },
+      { value: 'segment4', ariaLabel: 'segment1' },
+      { value: 'segment5', ariaLabel: 'segment1' },
+    ]
+    return (
+      <List>
+        <dt>Default</dt>
+        <dd>
+          <SegmentedControl
+            options={options.map((option) => ({ ...option, content: 'ボタン' }))}
+            value={value1}
+            onClickOption={(value) => {
+              action('clicked')(value)
+              setValue1(value)
+            }}
+          />
+        </dd>
+        <dt>Small</dt>
+        <dd>
+          <SegmentedControl
+            options={options.map((option) => ({ ...option, content: 'ボタン' }))}
+            value={value2}
+            onClickOption={(value) => {
+              action('clicked')(value)
+              setValue2(value)
+            }}
+            size="s"
+          />
+        </dd>
+        <dt>Icon</dt>
+        <dd>
+          <SegmentedControl
+            options={options.map((option) => ({
+              ...option,
+              content: <Icon size={16} name="fa-plus-circle" />,
+            }))}
+            value={value3}
+            onClickOption={(value) => {
+              action('clicked')(value)
+              setValue3(value)
+            }}
+            isSquare
+          />
+        </dd>
+        <dt>Small icon</dt>
+        <dd>
+          <SegmentedControl
+            options={options.map((option) => ({
+              ...option,
+              content: <Icon size={13} name="fa-plus-circle" />,
+            }))}
+            value={value4}
+            onClickOption={(value) => {
+              action('clicked')(value)
+              setValue4(value)
+            }}
+            size="s"
+            isSquare
+          />
+        </dd>
+        <dt>Disabled</dt>
+        <dd>
+          <SegmentedControl
+            options={options.map((option, i) => ({
+              ...option,
+              content: 'ボタン',
+              disabled: i > 2,
+            }))}
+            value={value5}
+            onClickOption={(value) => {
+              action('clicked')(value)
+              setValue5(value)
+            }}
+          />
+        </dd>
+      </List>
+    )
+  })
 
 const List = styled.dl`
   padding: 1rem;
