125497ad605dde9fd60ed992e43da4869a3ba4d0
Toru Kobayashi <koba0004@gmail.com>
2020-08-20 14:58:57 +0900

    chore: migrate settings for storybook

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../**/*.stories.js'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-viewport',
+    'storybook-readme/register'
+  ]
+}
