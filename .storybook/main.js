251ce52337265c643f4907207a2c485e60907a60
Toru Kobayashi <koba0004@gmail.com>
2020-08-20 17:13:37 +0900

    chore: use @storybook/essentials

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,15 @@
 module.exports = {
   stories: ['../**/*.stories.js'],
   addons: [
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        // we are not ready to use them yet.
+        controls: false,
+        docs: false
+      }
+    },
     '@storybook/addon-a11y',
-    '@storybook/addon-actions',
-    '@storybook/addon-viewport',
-    'storybook-readme/register'
+    'storybook-readme'
   ]
 }
