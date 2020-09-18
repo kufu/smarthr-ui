54b0084952b09f1c53ab7002aca82e382aab9b63
wmoai <wmoaism@gmail.com>
2020-09-18 15:00:10 +0900

    chore: build

--- esm/components/DatePicker/DatePicker.js
+++ esm/components/DatePicker/DatePicker.js
@@ -25,19 +25,13 @@ import { useOuterClick } from './useOuterClick';
 import { useGlobalKeyDown } from './useGlobalKeyDown';
 import { parseJpnDateString } from './datePickerHelper';
 export var DatePicker = function (_a) {
-    var _b = _a.date, date = _b === void 0 ? null : _b, onChangeDate = _a.onChangeDate, _c = _a.parsingErrorMessage, parsingErrorMessage = _c === void 0 ? '非対応な入力形式です' : _c, parseInput = _a.parseInput, formatDate = _a.formatDate, className = _a.className;
-    var themes = useTheme();
-    var _d = useState(date), selectedDate = _d[0], setSelectedDate = _d[1];
-    var inputRef = useRef(null);
-    var inputWrapperRef = useRef(null);
-    var calendarRef = useRef(null);
-    var _e = useState({
-        top: 0,
-        left: 0,
-    }), calendarPosition = _e[0], setCalendarPosition = _e[1];
-    var _f = useState(false), isInputFocused = _f[0], setIsInputFocused = _f[1];
-    var _g = useState(false), isCalendarShown = _g[0], setIsCalendarShown = _g[1];
-    var _h = useState(false), existsParsingError = _h[0], setExistsParsingError = _h[1];
+    var _b = _a.value, value = _b === void 0 ? null : _b, onChangeDate = _a.onChangeDate, parseInput = _a.parseInput, formatDate = _a.formatDate, name = _a.name, disabled = _a.disabled, error = _a.error, className = _a.className;
+    var stringToDate = useCallback(function (str) {
+        if (!str) {
+            return null;
+        }
+        return parseInput ? parseInput(str) : parseJpnDateString(str);
+    }, [parseInput]);
     var dateToString = useCallback(function (_date) {
         if (formatDate) {
             return formatDate(_date);
@@ -47,22 +41,32 @@ export var DatePicker = function (_a) {
         }
         return dayjs(_date).format('YYYY/MM/DD');
     }, [formatDate]);
+    var themes = useTheme();
+    var _c = useState(stringToDate(value)), selectedDate = _c[0], setSelectedDate = _c[1];
+    var inputRef = useRef(null);
+    var inputWrapperRef = useRef(null);
+    var calendarRef = useRef(null);
+    var _d = useState({
+        top: 0,
+        left: 0,
+    }), calendarPosition = _d[0], setCalendarPosition = _d[1];
+    var _e = useState(false), isInputFocused = _e[0], setIsInputFocused = _e[1];
+    var _f = useState(false), isCalendarShown = _f[0], setIsCalendarShown = _f[1];
     var updateDate = useCallback(function (newDate) {
-        if (newDate && isNaN(newDate.getTime())) {
-            setExistsParsingError(true);
-            return;
-        }
         if (newDate === selectedDate ||
             (newDate && selectedDate && newDate.getTime() === selectedDate.getTime())) {
+            // Do not update date if the new date is same with the old one.
             return;
         }
         if (!inputRef.current) {
             return;
         }
-        inputRef.current.value = dateToString(newDate);
+        // Do not change input value if new date is invalid date
+        if (!newDate || dayjs(newDate).isValid()) {
+            inputRef.current.value = dateToString(newDate);
+        }
         setSelectedDate(newDate);
-        setExistsParsingError(false);
-        onChangeDate && onChangeDate(newDate);
+        onChangeDate && onChangeDate(newDate, inputRef.current.value);
     }, [selectedDate, dateToString, onChangeDate]);
     var switchCalendarVisibility = useCallback(function (isVisible) {
         if (!isVisible) {
@@ -75,15 +79,28 @@ export var DatePicker = function (_a) {
         setIsCalendarShown(true);
         var rect = inputWrapperRef.current.getBoundingClientRect();
         setCalendarPosition({
-            top: rect.top + rect.height - 4,
-            left: rect.left,
+            top: rect.top + rect.height - 4 + window.pageYOffset,
+            left: rect.left + window.pageXOffset,
         });
     }, []);
     useEffect(function () {
-        if (date && inputRef.current) {
-            inputRef.current.value = dateToString(date);
+        if (!value || !inputRef.current) {
+            return;
         }
-    }, [date, dateToString]);
+        /**
+         * Do not format the given value in the following cases
+         * - while input element is forcused.
+         * - if the given value is not date formattable.
+         */
+        if (!isInputFocused) {
+            var newDate = stringToDate(value);
+            if (newDate && dayjs(newDate).isValid()) {
+                inputRef.current.value = dateToString(newDate);
+                return;
+            }
+        }
+        inputRef.current.value = value;
+    }, [value, isInputFocused, dateToString, stringToDate]);
     useOuterClick([inputWrapperRef.current, calendarRef.current], useCallback(function () {
         switchCalendarVisibility(false);
     }, [switchCalendarVisibility]));
@@ -121,9 +138,8 @@ export var DatePicker = function (_a) {
         }
     }, [isInputFocused, switchCalendarVisibility]);
     useGlobalKeyDown(handleKeyDown);
-    var shouldShowError = !isInputFocused && !isCalendarShown && existsParsingError;
     return (React.createElement(Container, { className: className, onClick: function () {
-            if (!isCalendarShown) {
+            if (!disabled && !isCalendarShown) {
                 switchCalendarVisibility(true);
             }
         }, onKeyDown: function (e) {
@@ -137,7 +153,7 @@ export var DatePicker = function (_a) {
             }
         } },
         React.createElement(InputWrapper, { ref: inputWrapperRef },
-            React.createElement(StyledInput, { type: "text", onChange: function () {
+            React.createElement(StyledInput, { type: "text", name: name, onChange: function () {
                     if (isCalendarShown) {
                         switchCalendarVisibility(false);
                     }
@@ -161,7 +177,7 @@ export var DatePicker = function (_a) {
                     React.createElement(CalendarIconWrapper, { themes: themes },
                         React.createElement(Icon, { name: "fa-calendar-alt", color: isInputFocused || isCalendarShown
                                 ? themes.palette.TEXT_BLACK
-                                : themes.palette.BORDER }))), error: shouldShowError, ref: inputRef })),
+                                : themes.palette.BORDER }))), disabled: disabled, error: error, ref: inputRef })),
         isCalendarShown && (React.createElement(Portal, __assign({}, calendarPosition),
             React.createElement(Calendar, { value: selectedDate || undefined, onSelectDate: function (_, selected) {
                     updateDate(selected);
@@ -170,10 +186,7 @@ export var DatePicker = function (_a) {
                         switchCalendarVisibility(false);
                     });
                     inputRef.current && inputRef.current.focus();
-                }, ref: calendarRef }))),
-        shouldShowError && (React.createElement(Error, { themes: themes },
-            React.createElement(ErrorIcon, { name: "fa-exclamation-circle", color: themes.palette.DANGER }),
-            React.createElement(ErrorText, null, parsingErrorMessage)))));
+                }, ref: calendarRef })))));
 };
 var Container = styled.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  display: inline-block;\n"], ["\n  display: inline-block;\n"])));
 var InputWrapper = styled.div(templateObject_2 || (templateObject_2 = __makeTemplateObject([""], [""])));
@@ -188,12 +201,5 @@ var CalendarIconWrapper = styled.span(function (_a) {
     var palette = themes.palette, size = themes.size;
     return css(templateObject_5 || (templateObject_5 = __makeTemplateObject(["\n    display: flex;\n    align-items: center;\n    justify-content: center;\n    box-sizing: border-box;\n    height: 100%;\n    padding-left: ", ";\n    border-left: 1px solid ", ";\n    font-size: ", ";\n  "], ["\n    display: flex;\n    align-items: center;\n    justify-content: center;\n    box-sizing: border-box;\n    height: 100%;\n    padding-left: ", ";\n    border-left: 1px solid ", ";\n    font-size: ", ";\n  "])), size.pxToRem(size.space.XXS), palette.BORDER, size.pxToRem(size.font.TALL));
 });
-var Error = styled.div(function (_a) {
-    var themes = _a.themes;
-    var _b = themes.size, font = _b.font, pxToRem = _b.pxToRem, space = _b.space;
-    return css(templateObject_6 || (templateObject_6 = __makeTemplateObject(["\n    margin: ", " 0 0 0;\n    font-size: ", ";\n    line-height: 1;\n  "], ["\n    margin: ", " 0 0 0;\n    font-size: ", ";\n    line-height: 1;\n  "])), pxToRem(space.XXS), pxToRem(font.SHORT));
-});
-var ErrorIcon = styled(Icon)(templateObject_7 || (templateObject_7 = __makeTemplateObject(["\n  margin-right: 0.4rem;\n  vertical-align: middle;\n"], ["\n  margin-right: 0.4rem;\n  vertical-align: middle;\n"])));
-var ErrorText = styled.span(templateObject_8 || (templateObject_8 = __makeTemplateObject(["\n  vertical-align: middle;\n"], ["\n  vertical-align: middle;\n"])));
-var templateObject_1, templateObject_2, templateObject_3, templateObject_4, templateObject_5, templateObject_6, templateObject_7, templateObject_8;
+var templateObject_1, templateObject_2, templateObject_3, templateObject_4, templateObject_5;
 //# sourceMappingURL=DatePicker.js.map
