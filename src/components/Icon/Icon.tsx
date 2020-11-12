15301f389615542c43bce03aae128180163184c4
Toru Kobayashi <koba0004@gmail.com>
2020-11-12 15:31:08 +0900

    feat: expose all icons as individual components

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -621,42 +621,11 @@ export const iconMap = {
 }
 
 export type ComponentProps = Omit<Props, 'name'>
+type IconComponent = ComponentProps & { Component: React.ComponentType<ComponentProps> }
 
-const createIcon = (Component: React.ComponentType<ComponentProps>) => {
-  const Icon: React.FC<ComponentProps> = ({
-    className = '',
-    role = 'img',
-    visuallyHiddenText,
-    'aria-hidden': ariaHidden,
-    focusable = false,
-    ...props
-  }) => {
-    const hasAltText =
-      visuallyHiddenText !== undefined ||
-      props['aria-label'] !== undefined ||
-      props['aria-labelledby'] !== undefined
-    const isAriaHidden = ariaHidden !== undefined ? ariaHidden : !hasAltText
-    return (
-      <>
-        {visuallyHiddenText && <VisuallyHiddenText>{visuallyHiddenText}</VisuallyHiddenText>}
-        <Component
-          className={className}
-          role={role}
-          aria-hidden={isAriaHidden || undefined}
-          focusable={focusable}
-          {...props}
-        />
-      </>
-    )
-  }
-  return Icon
-}
-
-export const FaAddressBookIcon = createIcon(FaAddressBook)
-export const FaCaretDownIcon = createIcon(FaCaretDown)
-
-export const Icon: React.FC<Props> = ({
-  name,
+// This should be inlined in the createIcon function after the Icon component had been removed
+const IconComponent: React.FC<IconComponent> = ({
+  Component,
   className = '',
   role = 'img',
   visuallyHiddenText,
@@ -664,7 +633,6 @@ export const Icon: React.FC<Props> = ({
   focusable = false,
   ...props
 }) => {
-  const SvgIcon = iconMap[name]
   const hasAltText =
     visuallyHiddenText !== undefined ||
     props['aria-label'] !== undefined ||
@@ -673,7 +641,7 @@ export const Icon: React.FC<Props> = ({
   return (
     <>
       {visuallyHiddenText && <VisuallyHiddenText>{visuallyHiddenText}</VisuallyHiddenText>}
-      <SvgIcon
+      <Component
         className={className}
         role={role}
         aria-hidden={isAriaHidden || undefined}
@@ -683,6 +651,319 @@ export const Icon: React.FC<Props> = ({
     </>
   )
 }
+
+const createIcon = (SvgIcon: React.ComponentType<ComponentProps>) => {
+  const Icon: React.FC<ComponentProps> = (props: ComponentProps) => {
+    return <IconComponent {...props} Component={SvgIcon} />
+  }
+  return Icon
+}
+
+export const Icon: React.FC<Props> = ({ name, ...props }) => (
+  <IconComponent {...props} Component={iconMap[name]} />
+)
+
+export const FaAddressBookIcon = createIcon(FaAddressBook)
+export const FaAddressCardIcon = createIcon(FaAddressCard)
+export const FaAngleDoubleDownIcon = createIcon(FaAngleDoubleDown)
+export const FaAngleDoubleLeftIcon = createIcon(FaAngleDoubleLeft)
+export const FaAngleDoubleRightIcon = createIcon(FaAngleDoubleRight)
+export const FaAngleDownIcon = createIcon(FaAngleDown)
+export const FaAngleLeftIcon = createIcon(FaAngleLeft)
+export const FaAngleRightIcon = createIcon(FaAngleRight)
+export const FaAngleUpIcon = createIcon(FaAngleUp)
+export const FaArchiveIcon = createIcon(FaArchive)
+export const FaArrowCircleDownIcon = createIcon(FaArrowCircleDown)
+export const FaArrowAltCircleDownIcon = createIcon(FaArrowAltCircleDown)
+export const FaArrowAltCircleLeftIcon = createIcon(FaArrowAltCircleLeft)
+export const FaArrowAltCircleRightIcon = createIcon(FaArrowAltCircleRight)
+export const FaArrowAltCircleUpIcon = createIcon(FaArrowAltCircleUp)
+export const FaArrowDownIcon = createIcon(FaArrowDown)
+export const FaArrowLeftIcon = createIcon(FaArrowLeft)
+export const FaArrowRightIcon = createIcon(FaArrowRight)
+export const FaArrowUpIcon = createIcon(FaArrowUp)
+export const FaArrowsAltIcon = createIcon(FaArrowsAlt)
+export const FaArrowsAltHIcon = createIcon(FaArrowsAltH)
+export const FaArrowsAltVIcon = createIcon(FaArrowsAltV)
+export const FaAsteriskIcon = createIcon(FaAsterisk)
+export const FaBanIcon = createIcon(FaBan)
+export const FaBarsIcon = createIcon(FaBars)
+export const FaBellIcon = createIcon(FaBell)
+export const FaBellSlashIcon = createIcon(FaBellSlash)
+export const FaBirthdayCakeIcon = createIcon(FaBirthdayCake)
+export const FaBoltIcon = createIcon(FaBolt)
+export const FaBookIcon = createIcon(FaBook)
+export const FaBookOpenIcon = createIcon(FaBookOpen)
+export const FaBookmarkIcon = createIcon(FaBookmark)
+export const FaBoxIcon = createIcon(FaBox)
+export const FaBoxOpenIcon = createIcon(FaBoxOpen)
+export const FaBoxesIcon = createIcon(FaBoxes)
+export const FaBriefcaseIcon = createIcon(FaBriefcase)
+export const FaBuildingIcon = createIcon(FaBuilding)
+export const FaBullhornIcon = createIcon(FaBullhorn)
+export const FaBusIcon = createIcon(FaBus)
+export const FaBusAltIcon = createIcon(FaBusAlt)
+export const FaBusinessTimeIcon = createIcon(FaBusinessTime)
+export const FaCalculatorIcon = createIcon(FaCalculator)
+export const FaCalendarIcon = createIcon(FaCalendar)
+export const FaCalendarAltIcon = createIcon(FaCalendarAlt)
+export const FaCalendarCheckIcon = createIcon(FaCalendarCheck)
+export const FaCalendarDayIcon = createIcon(FaCalendarDay)
+export const FaCalendarMinusIcon = createIcon(FaCalendarMinus)
+export const FaCalendarPlusIcon = createIcon(FaCalendarPlus)
+export const FaCalendarTimesIcon = createIcon(FaCalendarTimes)
+export const FaCalendarWeekIcon = createIcon(FaCalendarWeek)
+export const FaCameraIcon = createIcon(FaCamera)
+export const FaCarIcon = createIcon(FaCar)
+export const FaCarAltIcon = createIcon(FaCarAlt)
+export const FaCarSideIcon = createIcon(FaCarSide)
+export const FaCaretDownIcon = createIcon(FaCaretDown)
+export const FaCaretLeftIcon = createIcon(FaCaretLeft)
+export const FaCaretRightIcon = createIcon(FaCaretRight)
+export const FaCaretSquareDownIcon = createIcon(FaCaretSquareDown)
+export const FaCaretSquareLeftIcon = createIcon(FaCaretSquareLeft)
+export const FaCaretSquareRightIcon = createIcon(FaCaretSquareRight)
+export const FaCaretSquareUpIcon = createIcon(FaCaretSquareUp)
+export const FaCaretUpIcon = createIcon(FaCaretUp)
+export const FaChartAreaIcon = createIcon(FaChartArea)
+export const FaChartBarIcon = createIcon(FaChartBar)
+export const FaChartLineIcon = createIcon(FaChartLine)
+export const FaChartPieIcon = createIcon(FaChartPie)
+export const FaCheckIcon = createIcon(FaCheck)
+export const FaCheckCircleIcon = createIcon(FaCheckCircle)
+export const FaCheckSquareIcon = createIcon(FaCheckSquare)
+export const FaChevronCircleDownIcon = createIcon(FaChevronCircleDown)
+export const FaChevronCircleLeftIcon = createIcon(FaChevronCircleLeft)
+export const FaChevronCircleRightIcon = createIcon(FaChevronCircleRight)
+export const FaChevronCircleUpIcon = createIcon(FaChevronCircleUp)
+export const FaChevronDownIcon = createIcon(FaChevronDown)
+export const FaChevronLeftIcon = createIcon(FaChevronLeft)
+export const FaChevronRightIcon = createIcon(FaChevronRight)
+export const FaChevronUpIcon = createIcon(FaChevronUp)
+export const FaCircleIcon = createIcon(FaCircle)
+export const FaClipboardIcon = createIcon(FaClipboard)
+export const FaClipboardCheckIcon = createIcon(FaClipboardCheck)
+export const FaClipboardListIcon = createIcon(FaClipboardList)
+export const FaClockIcon = createIcon(FaClock)
+export const FaCloneIcon = createIcon(FaClone)
+export const FaCloudIcon = createIcon(FaCloud)
+export const FaCloudDownloadAltIcon = createIcon(FaCloudDownloadAlt)
+export const FaCloudUploadAltIcon = createIcon(FaCloudUploadAlt)
+export const FaCodeIcon = createIcon(FaCode)
+export const FaCogIcon = createIcon(FaCog)
+export const FaCogsIcon = createIcon(FaCogs)
+export const FaCoinsIcon = createIcon(FaCoins)
+export const FaColumnsIcon = createIcon(FaColumns)
+export const FaCommentIcon = createIcon(FaComment)
+export const FaCommentAltIcon = createIcon(FaCommentAlt)
+export const FaCommentDotsIcon = createIcon(FaCommentDots)
+export const FaCommentSlashIcon = createIcon(FaCommentSlash)
+export const FaCommentsIcon = createIcon(FaComments)
+export const FaCompressIcon = createIcon(FaCompress)
+export const FaCopyIcon = createIcon(FaCopy)
+export const FaCreditCardIcon = createIcon(FaCreditCard)
+export const FaCubeIcon = createIcon(FaCube)
+export const FaCubesIcon = createIcon(FaCubes)
+export const FaDatabaseIcon = createIcon(FaDatabase)
+export const FaDoorClosedIcon = createIcon(FaDoorClosed)
+export const FaDoorOpenIcon = createIcon(FaDoorOpen)
+export const FaEditIcon = createIcon(FaEdit)
+export const FaEllipsisHIcon = createIcon(FaEllipsisH)
+export const FaEllipsisVIcon = createIcon(FaEllipsisV)
+export const FaEnvelopeIcon = createIcon(FaEnvelope)
+export const FaEnvelopeOpenIcon = createIcon(FaEnvelopeOpen)
+export const FaEnvelopeOpenTextIcon = createIcon(FaEnvelopeOpenText)
+export const FaExchangeAltIcon = createIcon(FaExchangeAlt)
+export const FaExclamationIcon = createIcon(FaExclamation)
+export const FaExclamationCircleIcon = createIcon(FaExclamationCircle)
+export const FaExclamationTriangleIcon = createIcon(FaExclamationTriangle)
+export const FaExpandIcon = createIcon(FaExpand)
+export const FaExpandArrowsAltIcon = createIcon(FaExpandArrowsAlt)
+export const FaExternalLinkAltIcon = createIcon(FaExternalLinkAlt)
+export const FaEyeIcon = createIcon(FaEye)
+export const FaEyeSlashIcon = createIcon(FaEyeSlash)
+export const FaFileIcon = createIcon(FaFile)
+export const FaFileAltIcon = createIcon(FaFileAlt)
+export const FaFileArchiveIcon = createIcon(FaFileArchive)
+export const FaFileDownloadIcon = createIcon(FaFileDownload)
+export const FaFileExportIcon = createIcon(FaFileExport)
+export const FaFileImportIcon = createIcon(FaFileImport)
+export const FaFileUploadIcon = createIcon(FaFileUpload)
+export const FaFilterIcon = createIcon(FaFilter)
+export const FaFlagIcon = createIcon(FaFlag)
+export const FaFolderIcon = createIcon(FaFolder)
+export const FaFolderMinusIcon = createIcon(FaFolderMinus)
+export const FaFolderOpenIcon = createIcon(FaFolderOpen)
+export const FaFolderPlusIcon = createIcon(FaFolderPlus)
+export const FaFontIcon = createIcon(FaFont)
+export const FaForwardIcon = createIcon(FaForward)
+export const FaGiftIcon = createIcon(FaGift)
+export const FaGlobeIcon = createIcon(FaGlobe)
+export const FaGraduationCapIcon = createIcon(FaGraduationCap)
+export const FaGripHorizontalIcon = createIcon(FaGripHorizontal)
+export const FaGripLinesIcon = createIcon(FaGripLines)
+export const FaGripLinesVerticalIcon = createIcon(FaGripLinesVertical)
+export const FaGripVerticalIcon = createIcon(FaGripVertical)
+export const FaHandPaperIcon = createIcon(FaHandPaper)
+export const FaHandPointDownIcon = createIcon(FaHandPointDown)
+export const FaHandPointLeftIcon = createIcon(FaHandPointLeft)
+export const FaHandPointRightIcon = createIcon(FaHandPointRight)
+export const FaHandPointUpIcon = createIcon(FaHandPointUp)
+export const FaHandsIcon = createIcon(FaHands)
+export const FaHandshakeIcon = createIcon(FaHandshake)
+export const FaHeartIcon = createIcon(FaHeart)
+export const FaHistoryIcon = createIcon(FaHistory)
+export const FaHomeIcon = createIcon(FaHome)
+export const FaHospitalIcon = createIcon(FaHospital)
+export const FaHospitalAltIcon = createIcon(FaHospitalAlt)
+export const FaHourglassIcon = createIcon(FaHourglass)
+export const FaHourglassEndIcon = createIcon(FaHourglassEnd)
+export const FaHourglassHalfIcon = createIcon(FaHourglassHalf)
+export const FaHourglassStartIcon = createIcon(FaHourglassStart)
+export const FaIdBadgeIcon = createIcon(FaIdBadge)
+export const FaIdCardIcon = createIcon(FaIdCard)
+export const FaIdCardAltIcon = createIcon(FaIdCardAlt)
+export const FaImageIcon = createIcon(FaImage)
+export const FaImagesIcon = createIcon(FaImages)
+export const FaInboxIcon = createIcon(FaInbox)
+export const FaInfoIcon = createIcon(FaInfo)
+export const FaInfoCircleIcon = createIcon(FaInfoCircle)
+export const FaKeyIcon = createIcon(FaKey)
+export const FaKeyboardIcon = createIcon(FaKeyboard)
+export const FaLightbulbIcon = createIcon(FaLightbulb)
+export const FaLinkIcon = createIcon(FaLink)
+export const FaListIcon = createIcon(FaList)
+export const FaListAltIcon = createIcon(FaListAlt)
+export const FaListOlIcon = createIcon(FaListOl)
+export const FaListUlIcon = createIcon(FaListUl)
+export const FaLockIcon = createIcon(FaLock)
+export const FaLockOpenIcon = createIcon(FaLockOpen)
+export const FaLongArrowAltDownIcon = createIcon(FaLongArrowAltDown)
+export const FaLongArrowAltLeftIcon = createIcon(FaLongArrowAltLeft)
+export const FaLongArrowAltRightIcon = createIcon(FaLongArrowAltRight)
+export const FaLongArrowAltUpIcon = createIcon(FaLongArrowAltUp)
+export const FaMinusIcon = createIcon(FaMinus)
+export const FaMinusCircleIcon = createIcon(FaMinusCircle)
+export const FaMinusSquareIcon = createIcon(FaMinusSquare)
+export const FaMobileIcon = createIcon(FaMobile)
+export const FaMobileAltIcon = createIcon(FaMobileAlt)
+export const FaMoneyBillIcon = createIcon(FaMoneyBill)
+export const FaMoneyBillAltIcon = createIcon(FaMoneyBillAlt)
+export const FaMoneyBillWaveIcon = createIcon(FaMoneyBillWave)
+export const FaMoneyBillWaveAltIcon = createIcon(FaMoneyBillWaveAlt)
+export const FaMoneyCheckIcon = createIcon(FaMoneyCheck)
+export const FaMoneyCheckAltIcon = createIcon(FaMoneyCheckAlt)
+export const FaPaperPlaneIcon = createIcon(FaPaperPlane)
+export const FaPaperclipIcon = createIcon(FaPaperclip)
+export const FaPasteIcon = createIcon(FaPaste)
+export const FaPenIcon = createIcon(FaPen)
+export const FaPencilAltIcon = createIcon(FaPencilAlt)
+export const FaPhoneIcon = createIcon(FaPhone)
+export const FaPhoneSlashIcon = createIcon(FaPhoneSlash)
+export const FaPiggyBankIcon = createIcon(FaPiggyBank)
+export const FaPlaneIcon = createIcon(FaPlane)
+export const FaPlayIcon = createIcon(FaPlay)
+export const FaPlayCircleIcon = createIcon(FaPlayCircle)
+export const FaPlusIcon = createIcon(FaPlus)
+export const FaPlusCircleIcon = createIcon(FaPlusCircle)
+export const FaPlusSquareIcon = createIcon(FaPlusSquare)
+export const FaPollIcon = createIcon(FaPoll)
+export const FaPollHIcon = createIcon(FaPollH)
+export const FaPortraitIcon = createIcon(FaPortrait)
+export const FaPowerOffIcon = createIcon(FaPowerOff)
+export const FaPrintIcon = createIcon(FaPrint)
+export const FaQrcodeIcon = createIcon(FaQrcode)
+export const FaQuestionIcon = createIcon(FaQuestion)
+export const FaQuestionCircleIcon = createIcon(FaQuestionCircle)
+export const FaRandomIcon = createIcon(FaRandom)
+export const FaReceiptIcon = createIcon(FaReceipt)
+export const FaRedoIcon = createIcon(FaRedo)
+export const FaRedoAltIcon = createIcon(FaRedoAlt)
+export const FaRegDotCircleIcon = createIcon(FaRegDotCircle)
+export const FaReplyIcon = createIcon(FaReply)
+export const FaReplyAllIcon = createIcon(FaReplyAll)
+export const FaRocketIcon = createIcon(FaRocket)
+export const FaSaveIcon = createIcon(FaSave)
+export const FaSearchIcon = createIcon(FaSearch)
+export const FaSearchMinusIcon = createIcon(FaSearchMinus)
+export const FaSearchPlusIcon = createIcon(FaSearchPlus)
+export const FaShareIcon = createIcon(FaShare)
+export const FaShareAltIcon = createIcon(FaShareAlt)
+export const FaShareSquareIcon = createIcon(FaShareSquare)
+export const FaShieldAltIcon = createIcon(FaShieldAlt)
+export const FaShoppingBagIcon = createIcon(FaShoppingBag)
+export const FaShoppingBasketIcon = createIcon(FaShoppingBasket)
+export const FaShoppingCartIcon = createIcon(FaShoppingCart)
+export const FaSignInAltIcon = createIcon(FaSignInAlt)
+export const FaSignOutAltIcon = createIcon(FaSignOutAlt)
+export const FaSlidersHIcon = createIcon(FaSlidersH)
+export const FaSortIcon = createIcon(FaSort)
+export const FaSortAlphaDownIcon = createIcon(FaSortAlphaDown)
+export const FaSortAlphaUpIcon = createIcon(FaSortAlphaUp)
+export const FaSortAmountDownIcon = createIcon(FaSortAmountDown)
+export const FaSortAmountUpIcon = createIcon(FaSortAmountUp)
+export const FaSortDownIcon = createIcon(FaSortDown)
+export const FaSortNumericDownIcon = createIcon(FaSortNumericDown)
+export const FaSortNumericUpIcon = createIcon(FaSortNumericUp)
+export const FaSortUpIcon = createIcon(FaSortUp)
+export const FaStarIcon = createIcon(FaStar)
+export const FaStepBackwardIcon = createIcon(FaStepBackward)
+export const FaStepForwardIcon = createIcon(FaStepForward)
+export const FaStickyNoteIcon = createIcon(FaStickyNote)
+export const FaStopIcon = createIcon(FaStop)
+export const FaStopCircleIcon = createIcon(FaStopCircle)
+export const FaStreamIcon = createIcon(FaStream)
+export const FaSubwayIcon = createIcon(FaSubway)
+export const FaSyncIcon = createIcon(FaSync)
+export const FaSyncAltIcon = createIcon(FaSyncAlt)
+export const FaTableIcon = createIcon(FaTable)
+export const FaTabletIcon = createIcon(FaTablet)
+export const FaTabletAltIcon = createIcon(FaTabletAlt)
+export const FaTagIcon = createIcon(FaTag)
+export const FaTagsIcon = createIcon(FaTags)
+export const FaTasksIcon = createIcon(FaTasks)
+export const FaTaxiIcon = createIcon(FaTaxi)
+export const FaThIcon = createIcon(FaTh)
+export const FaThLargeIcon = createIcon(FaThLarge)
+export const FaThListIcon = createIcon(FaThList)
+export const FaTimesIcon = createIcon(FaTimes)
+export const FaTimesCircleIcon = createIcon(FaTimesCircle)
+export const FaToolboxIcon = createIcon(FaToolbox)
+export const FaToolsIcon = createIcon(FaTools)
+export const FaTrashIcon = createIcon(FaTrash)
+export const FaTrashAltIcon = createIcon(FaTrashAlt)
+export const FaTrashRestoreIcon = createIcon(FaTrashRestore)
+export const FaTrashRestoreAltIcon = createIcon(FaTrashRestoreAlt)
+export const FaUndoIcon = createIcon(FaUndo)
+export const FaUndoAltIcon = createIcon(FaUndoAlt)
+export const FaUnlinkIcon = createIcon(FaUnlink)
+export const FaUnlockIcon = createIcon(FaUnlock)
+export const FaUnlockAltIcon = createIcon(FaUnlockAlt)
+export const FaUserIcon = createIcon(FaUser)
+export const FaUserAltIcon = createIcon(FaUserAlt)
+export const FaUserAltSlashIcon = createIcon(FaUserAltSlash)
+export const FaUserCheckIcon = createIcon(FaUserCheck)
+export const FaUserCircleIcon = createIcon(FaUserCircle)
+export const FaUserCogIcon = createIcon(FaUserCog)
+export const FaUserEditIcon = createIcon(FaUserEdit)
+export const FaUserMinusIcon = createIcon(FaUserMinus)
+export const FaUserPlusIcon = createIcon(FaUserPlus)
+export const FaUserSlashIcon = createIcon(FaUserSlash)
+export const FaUsersIcon = createIcon(FaUsers)
+export const FaUsersCogIcon = createIcon(FaUsersCog)
+export const FaVideoIcon = createIcon(FaVideo)
+export const FaVideoSlashIcon = createIcon(FaVideoSlash)
+export const FaVolumeDownIcon = createIcon(FaVolumeDown)
+export const FaVolumeMuteIcon = createIcon(FaVolumeMute)
+export const FaVolumeOffIcon = createIcon(FaVolumeOff)
+export const FaVolumeUpIcon = createIcon(FaVolumeUp)
+export const FaWalletIcon = createIcon(FaWallet)
+export const FaWindowCloseIcon = createIcon(FaWindowClose)
+export const FaWindowMaximizeIcon = createIcon(FaWindowMaximize)
+export const FaWindowMinimizeIcon = createIcon(FaWindowMinimize)
+export const FaWindowRestoreIcon = createIcon(FaWindowRestore)
+export const FaWrenchIcon = createIcon(FaWrench)
+export const FaYenSignIcon = createIcon(FaYenSign)
 
 const VisuallyHiddenText = styled.span`
   ${VISUALLY_HIDDEN_STYLE}
