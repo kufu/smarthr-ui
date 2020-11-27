c3fc6e5176e6ced99220cc346d3a908f77ad326e
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-11-27 18:52:46 +0900

    chore: wait for storybook to start and then start testcafe

--- e2e/helper.ts
+++ e2e/helper.ts
@@ -2,8 +2,9 @@ function isWin() {
   return process.platform.startsWith('win')
 }
 
-// IE11 は画面内に要素が収まっていないと反応しないのでリサイズする
-// mac など GUI 環境だと window 以上に大きくするとエラーになるので win 環境に限定する
+// In IE11, if the element does not fit in the screen, it will not react, so resize it.
+// In a GUI environment such as mac, if the screen is enlarged beyond the window, an error will occur, so limit it to the Windows environment.
+// cf. https://tech.medpeer.co.jp/entry/e2e-ie11
 export async function resizeWindow(t) {
   if (isWin()) await t.resizeWindow(5000, 2000)
 }
--- scripts/e2e.ts
+++ scripts/e2e.ts
@@ -10,8 +10,31 @@ function envSpawn(command: string) {
   }
 }
 
+function startTestcafe() {
+  const testcafe = envSpawn(
+    `yarn e2e ${process.env.CAFE_BROWSER} e2e/**/*.test.ts --host localhost`,
+  )
+
+  testcafe.stdout.on('data', (data) => {
+    console.log(`testcafe.stdout: ${data}`)
+  })
+  testcafe.stderr.on('data', (data) => {
+    console.error(`testcafe.stderr: ${data}`)
+  })
+  testcafe.on('close', (code) => {
+    console.log(`testcafe process exited with code ${code}`)
+    start.kill()
+    process.exit(code)
+  })
+}
+
 const start = envSpawn('yarn storybook')
+
 start.stdout.on('data', (data) => {
+  if (data && data.includes('started')) {
+    startTestcafe()
+  }
+
   console.log(`storybook.stdout: ${data}`)
 })
 start.stderr.on('data', (data) => {
@@ -19,17 +42,4 @@ start.stderr.on('data', (data) => {
 })
 start.on('close', (code) => {
   console.log(`storybook exited with code ${code}`)
-})
-
-const testcafe = envSpawn(`yarn e2e ${process.env.CAFE_BROWSER} e2e/**/*.test.ts --host localhost`)
-testcafe.stdout.on('data', (data) => {
-  console.log(`testcafe.stdout: ${data}`)
-})
-testcafe.stderr.on('data', (data) => {
-  console.error(`testcafe.stderr: ${data}`)
-})
-testcafe.on('close', (code) => {
-  console.log(`testcafe process exited with code ${code}`)
-  start.kill()
-  process.exit(code)
 })
