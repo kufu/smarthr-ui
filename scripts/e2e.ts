9e2af972e57dc6d6837667211edb06cf5f22f87b
Toru Kobayashi <koba0004@gmail.com>
2020-12-10 13:46:22 +0900

    test: build storybook for e2e in ahead of time

--- scripts/e2e.ts
+++ scripts/e2e.ts
@@ -1,23 +1,27 @@
-import { spawn } from 'child_process'
+import { spawn, spawnSync } from 'child_process'
 import waitForLocalhost from 'wait-for-localhost'
 ;(async () => {
-  const storybook = spawn('yarn', ['run', 'storybook', '--quiet'], {
+  const PORT = 6006
+
+  spawnSync('yarn', ['run', 'build-storybook'], {
     stdio: 'inherit',
     shell: true,
   })
-  await waitForLocalhost({ port: 6006, path: '/iframe.html' })
+  const httpServer = spawn('yarn', ['http-server', 'storybook-static', '--port', `${PORT}`], {
+    stdio: 'inherit',
+    shell: true,
+  })
+
+  await waitForLocalhost({ port: PORT, path: '/iframe.html' })
 
   const browser = process.env.TESTCAFE_BROWSER || 'chrome'
-  // This timeout is to wait for the build of webpack because
-  // storybook seems to have an additional build after serving iframe.html
-  const timeoutMs = 5 * 60 * 1000
-  const testcafeArgs = `e2e/**/*.test.ts --host localhost --skip-js-errors --page-load-timeout ${timeoutMs}`
+  const testcafeArgs = `e2e/**/*.test.ts --host localhost --skip-js-errors`
   const testcafe = spawn('yarn', ['run', 'testcafe', browser, ...testcafeArgs.split(' ')], {
     stdio: 'inherit',
     shell: true,
   })
   testcafe.on('close', (code) => {
-    storybook.kill()
+    httpServer.kill()
     process.exit(code)
   })
 })()
