4bfb2215b499dcd42a9d8eb09895e638298aee0e
Tokky0425 <lily.yarn.5@gmail.com>
2020-12-14 12:25:01 +0900

    docs: add README.md for RadioButton

--- src/components/RadioButton/RadioButton.stories.tsx
+++ src/components/RadioButton/RadioButton.stories.tsx
@@ -2,25 +2,32 @@ import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 import styled from 'styled-components'
+import readme from './README.md'
 
 import { RadioButton } from './RadioButton'
 
-storiesOf('RadioButton', module).add('all', () => (
-  <List>
-    <li>
-      <RadioButton name="sample" checked={false} onChange={action('onChange')} />
-    </li>
-    <li>
-      <RadioButton name="sample" checked={true} onChange={action('onChange')} />
-    </li>
-    <li>
-      <RadioButton name="sample" checked={false} disabled={true} onChange={action('onChange')} />
-    </li>
-    <li>
-      <RadioButton name="sample" checked={true} disabled={true} onChange={action('onChange')} />
-    </li>
-  </List>
-))
+storiesOf('RadioButton', module)
+  .addParameters({
+    readme: {
+      sidebar: readme,
+    },
+  })
+  .add('all', () => (
+    <List>
+      <li>
+        <RadioButton name="sample" checked={false} onChange={action('onChange')} />
+      </li>
+      <li>
+        <RadioButton name="sample" checked={true} onChange={action('onChange')} />
+      </li>
+      <li>
+        <RadioButton name="sample" checked={false} disabled={true} onChange={action('onChange')} />
+      </li>
+      <li>
+        <RadioButton name="sample" checked={true} disabled={true} onChange={action('onChange')} />
+      </li>
+    </List>
+  ))
 
 const List = styled.ul`
   padding: 0 24px;
