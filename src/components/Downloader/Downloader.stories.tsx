711fad6db144ba2f6137f08ca94ff28225192d98
MRTAFU <tafu.nakazaki@gmail.com>
2020-12-16 18:14:16 +0900

    docs: replace color code of old palette to theme

--- src/components/Downloader/Downloader.stories.tsx
+++ src/components/Downloader/Downloader.stories.tsx
@@ -1,21 +1,26 @@
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 import styled from 'styled-components'
+import { Theme, useTheme } from '../../hooks/useTheme'
 import { Icon } from '../Icon'
 
-storiesOf('[TBD] Downloader', module).add('all', () => (
-  <Wrapper>
-    <Title>To Be Developed</Title>
-    <Description>This component will develop in the near future.</Description>
-    <Link
-      href="https://smarthr.invisionapp.com/share/ADUDJ8BW74C#/386514188_downloaders"
-      target="_blank"
-    >
-      <LinkText>Design of Downloader (InVision)</LinkText>
-      <LinkIcon name="fa-external-link-alt" size={14} />
-    </Link>
-  </Wrapper>
-))
+storiesOf('[TBD] Downloader', module).add('all', () => {
+  const themes = useTheme()
+  return (
+    <Wrapper>
+      <Title>To Be Developed</Title>
+      <Description>This component will develop in the near future.</Description>
+      <Link
+        themes={themes}
+        href="https://smarthr.invisionapp.com/share/ADUDJ8BW74C#/386514188_downloaders"
+        target="_blank"
+      >
+        <LinkText>Design of Downloader (InVision)</LinkText>
+        <LinkIcon name="fa-external-link-alt" size={14} />
+      </Link>
+    </Wrapper>
+  )
+})
 
 const Wrapper = styled.div`
   box-sizing: border-box;
@@ -43,8 +48,8 @@ const Description = styled.div`
   margin-bottom: 16px;
 `
 
-const Link = styled.a`
-  color: #007bc2;
+const Link = styled.a<{ themes: Theme }>`
+  color: ${({ themes }) => themes.palette.TEXT_LINK};
 `
 const LinkText = styled.span`
   vertical-align: middle;
