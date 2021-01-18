f3c62cde713e08d9501cfc19f3cab82f0e2679ac
wmoai <wmoaism@gmail.com>
2021-01-18 12:05:41 +0900

    fix: add Segmented control ARIA (SHRUI-242) (#1223)
    * fix: add aria
    
    * fix: more aria
    
    * fix: add keyboard navigation
    
    * fix: modify not to replace button tag
    
    * fix: modify not to focus the buttons by tab key when group has focus
    
    * chore: modify return timing to be faster
    
    * docs: add description of a11y into readme

--- src/components/SegmentedControl/SegmentedControl.tsx
+++ src/components/SegmentedControl/SegmentedControl.tsx
@@ -1,8 +1,8 @@
-import React, { FC, ReactNode } from 'react'
+import React, { FC, ReactNode, useCallback, useEffect, useRef, useState } from 'react'
 import styled, { css } from 'styled-components'
 
 import { Theme, useTheme } from '../../hooks/useTheme'
-import { PrimaryButton, SecondaryButton } from '../Button'
+import { SecondaryButton } from '../Button'
 
 export type Option = {
   value: string
@@ -27,29 +27,107 @@ export const SegmentedControl: FC<Props> = ({
   size = 'default',
   isSquare = false,
   className,
+  ...props
 }) => {
   const themes = useTheme()
+  const [isFocused, setIsFocused] = useState(false)
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (!isFocused || !containerRef.current || !document.activeElement) {
+        return
+      }
+      const radios = Array.from(
+        containerRef.current.querySelectorAll('[role="radio"]:not(:disabled)'),
+      )
+      if (radios.length < 2) {
+        return
+      }
+      const focusedIndex = radios.indexOf(document.activeElement)
+      if (focusedIndex === -1) {
+        return
+      }
+      switch (e.key) {
+        case 'Down':
+        case 'ArrowDown':
+        case 'Right':
+        case 'ArrowRight': {
+          const nextIndex = focusedIndex + 1
+          const nextRadio = radios[nextIndex % radios.length]
+          if (nextRadio instanceof HTMLButtonElement) {
+            nextRadio.focus()
+          }
+          break
+        }
+        case 'Up':
+        case 'ArrowUp':
+        case 'Left':
+        case 'ArrowLeft': {
+          const nextIndex = focusedIndex - 1
+          const nextRadio = radios[(nextIndex + radios.length) % radios.length]
+          if (nextRadio instanceof HTMLButtonElement) {
+            nextRadio.focus()
+          }
+          break
+        }
+      }
+    },
+    [isFocused],
+  )
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown)
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [handleKeyDown])
+
+  const includesSelected = value && options.some((option) => option.value === value)
+  const getRovingTabIndex = useCallback(
+    (option: Option, index: number) => {
+      if (isFocused) {
+        return -1
+      }
+      if (!includesSelected) {
+        return index === 0 ? 0 : -1
+      }
+      return option.value === value ? 0 : -1
+    },
+    [includesSelected, isFocused, value],
+  )
 
   return (
-    <Container className={className}>
-      {options.map((option) => {
-        const isSelected = !!value && value === option.value
-        const Button = isSelected ? SelectedButton : DefaultButton
-        const onClick = onClickOption ? () => onClickOption(option.value) : undefined
-        return (
-          <Button
-            aria-label={option.ariaLabel}
-            key={option.value}
-            disabled={option.disabled}
-            onClick={onClick}
-            size={size}
-            square={isSquare}
-            themes={themes}
-          >
-            {option.content}
-          </Button>
-        )
-      })}
+    <Container
+      {...props}
+      className={className}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+      ref={containerRef}
+      role="toolbar"
+    >
+      <div role="radiogroup">
+        {options.map((option, i) => {
+          const isSelected = !!value && value === option.value
+          const onClick = onClickOption ? () => onClickOption(option.value) : undefined
+          return (
+            <Button
+              aria-label={option.ariaLabel}
+              key={option.value}
+              disabled={option.disabled}
+              onClick={onClick}
+              size={size}
+              square={isSquare}
+              themes={themes}
+              tabIndex={getRovingTabIndex(option, i)}
+              role="radio"
+              aria-checked={isSelected}
+            >
+              {option.content}
+            </Button>
+          )
+        })}
+      </div>
     </Container>
   )
 }
@@ -57,12 +135,20 @@ export const SegmentedControl: FC<Props> = ({
 const Container = styled.div`
   display: inline-flex;
 `
-const buttonStyle = css<{ themes: Theme }>(({ themes }) => {
-  const { border } = themes.frame
+const Button = styled(SecondaryButton)<{ themes: Theme }>(({ themes }) => {
+  const { palette, frame } = themes
+  const { border } = frame
   return css`
     border: ${border.default};
     border-radius: 0;
 
+    &[aria-checked='true'] {
+      color: #fff;
+      background-color: ${palette.MAIN};
+      &.hover {
+        background-color: ${palette.hoverColor(palette.MAIN)};
+      }
+    }
     &:first-child {
       border-top-left-radius: ${border.radius.m};
       border-bottom-left-radius: ${border.radius.m};
@@ -76,9 +162,3 @@ const buttonStyle = css<{ themes: Theme }>(({ themes }) => {
     }
   `
 })
-const DefaultButton = styled(SecondaryButton)<{ themes: Theme }>`
-  ${buttonStyle}
-`
-const SelectedButton = styled(PrimaryButton)<{ themes: Theme }>`
-  ${buttonStyle}
-`
