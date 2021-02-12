4136de4e786a36a0be61c17f43e1a895db2bd34c
wmoai <wmoaism@gmail.com>
2021-02-12 19:07:00 +0900

    fix: add light focus trap in Dialog

---
+++ src/components/Dialog/FocusTrap.tsx
@@ -0,0 +1,65 @@
+import React, { FC, ReactNode, useCallback, useEffect, useRef, useState } from 'react'
+
+import { tabbable } from '../Dropdown/tabbable'
+
+type Props = {
+  children: ReactNode
+}
+
+export const FocusTrap: FC<Props> = ({ children }) => {
+  const [triggerElement, setTriggerElement] = useState<Element | null>(null)
+  const ref = useRef<HTMLDivElement | null>(null)
+
+  const handleKeyDown = useCallback((e: KeyboardEvent) => {
+    if (e.key !== 'Tab' || ref.current === null) {
+      return
+    }
+    const tabbables = tabbable(ref.current).filter((elm) => elm.tabIndex >= 0)
+    if (tabbables.length === 0) {
+      return
+    }
+    const firstTabbale = tabbables[0]
+    const lastTabbale = tabbables[tabbables.length - 1]
+    const currentFocused = Array.from(tabbables).find((elm) => elm === e.target)
+    if (e.shiftKey && currentFocused === firstTabbale) {
+      lastTabbale.focus()
+      e.preventDefault()
+    } else if (!e.shiftKey && currentFocused === lastTabbale) {
+      firstTabbale.focus()
+      e.preventDefault()
+    }
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [handleKeyDown])
+
+  useEffect(() => {
+    if (typeof document !== 'undefined') {
+      setTriggerElement(document.activeElement)
+    }
+    requestAnimationFrame(() => {
+      if (ref.current === null) {
+        return
+      }
+      const tabbables = tabbable(ref.current)
+      const firstTabbale = tabbables[0]
+      if (firstTabbale) {
+        firstTabbale.focus()
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    return () => {
+      if (triggerElement instanceof HTMLElement) {
+        triggerElement.focus()
+      }
+    }
+  }, [triggerElement])
+
+  return <div ref={ref}>{children}</div>
+}
