e792501f185656f032c61cc6e2dad7633cb51ca3
yt-ymmt <ftbae1@gmail.com>
2021-02-22 13:00:42 +0900

    fix: improve attributes of AppNavi

--- src/components/AppNavi/AppNaviButton.tsx
+++ src/components/AppNavi/AppNaviButton.tsx
@@ -1,10 +1,9 @@
 import React, { ReactNode, VFC } from 'react'
 import styled from 'styled-components'
-
 import { useTheme } from '../../hooks/useTheme'
-
 import { IconNames, ComponentProps as IconProps } from '../Icon'
 import { ItemStyleProps, getIconComponent, getItemStyle } from './appNaviHelper'
+import { useClassNames } from './useClassNames'
 
 export type AppNaviButtonProps = {
   children: ReactNode
@@ -25,6 +24,7 @@ export const AppNaviButton: VFC<InnerProps> = ({
   onClick,
 }) => {
   const theme = useTheme()
+  const classNames = useClassNames()
   const iconComponent = getIconComponent(theme, { icon, current })
 
   return (
@@ -36,6 +36,7 @@ export const AppNaviButton: VFC<InnerProps> = ({
       disabled={isUnclickable}
       isUnclickable={isUnclickable}
       type="button"
+      className={classNames.button}
     >
       {iconComponent}
       {children}
