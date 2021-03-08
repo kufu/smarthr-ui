6fac9b92494bf5336f6246ae3a43a39d8050b910
AtsushiM <at.mizoue@gmail.com>
2021-03-08 12:14:05 +0900

    fix: refactoring: Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,51 +1,22 @@
-import React from 'react'
+import React, { ReactNode, VFC } from 'react'
 import styled, { css } from 'styled-components'
 
 import { Theme, useTheme } from '../../hooks/useTheme'
 
-export const Footer = () => {
+export const Footer: VFC = () => {
   const theme = useTheme()
 
   return (
     <Wrapper themes={theme}>
       <List themes={theme}>
-        <li>
-          <Item href="https://smarthr.jp/help" target="_blank" rel="noopener noreferrer">
-            ヘルプ
-          </Item>
-        </li>
-        <li>
-          <Item href="https://smarthr.jp/info" target="_blank" rel="noopener noreferrer">
-            お知らせ
-          </Item>
-        </li>
-        <li>
-          <Item href="https://smarthr.jp/terms" target="_blank" rel="noopener noreferrer">
-            利用規約
-          </Item>
-        </li>
-        <li>
-          <Item href="https://smarthr.jp/policy" target="_blank" rel="noopener noreferrer">
-            プライバシーポリシー
-          </Item>
-        </li>
-        <li>
-          <Item href="https://smarthr.jp/law" target="_blank" rel="noopener noreferrer">
-            特定商取引法に基づく表記
-          </Item>
-        </li>
-        <li>
-          <Item href="https://smarthr.co.jp" target="_blank" rel="noopener noreferrer">
-            運営会社
-          </Item>
-        </li>
-        <li>
-          <Item href="https://developer.smarthr.jp" target="_blank" rel="noopener noreferrer">
-            開発者向けAPI
-          </Item>
-        </li>
+        <Item href="https://smarthr.jp/help">ヘルプ</Item>
+        <Item href="https://smarthr.jp/info">お知らせ</Item>
+        <Item href="https://smarthr.jp/terms">利用規約</Item>
+        <Item href="https://smarthr.jp/policy">プライバシーポリシー</Item>
+        <Item href="https://smarthr.jp/law">特定商取引法に基づく表記</Item>
+        <Item href="https://smarthr.co.jp">運営会社</Item>
+        <Item href="https://developer.smarthr.jp">開発者向けAPI </Item>
       </List>
-
       <Copy themes={theme}>&copy; SmartHR, Inc.</Copy>
     </Wrapper>
   )
@@ -63,6 +34,7 @@ const Wrapper = styled.footer<{ themes: Theme }>`
     font-size: ${themes.size.pxToRem(themes.size.font.TALL)};
   `}
 `
+
 const List = styled.ul<{ themes: Theme }>`
   ${({ themes }) => css`
     display: flex;
@@ -76,7 +48,19 @@ const List = styled.ul<{ themes: Theme }>`
     }
   `}
 `
-const Item = styled.a`
+
+type ItemProp = {
+  children: ReactNode
+  href: string
+}
+const Item: React.VFC<ItemProp> = ({ children, href }) => (
+  <li>
+    <ItemPart target="_blank" rel="noopener noreferrer" href={href}>
+      {children}
+    </ItemPart>
+  </li>
+)
+const ItemPart = styled.a`
   color: #fff;
   text-decoration: none;
 
@@ -84,6 +68,7 @@ const Item = styled.a`
     text-decoration: underline;
   }
 `
+
 const Copy = styled.small<{ themes: Theme }>`
   ${({ themes }) => css`
     font-size: ${themes.size.pxToRem(themes.size.font.TALL)};
