4c822fb6a4497c13ad2ed16b12d71e688ac6fbe6
wmoai <wmoaism@gmail.com>
2021-03-19 20:49:37 +0900

    chore: add release workflows

---
+++ scripts/getLatestChangelog.ts
@@ -0,0 +1,29 @@
+import * as fs from 'fs'
+
+const stream = fs.createReadStream(`${__dirname}/../CHANGELOG.md`, {
+  encoding: 'utf8',
+  highWaterMark: 1024,
+})
+
+let buffer = ''
+
+stream.on('data', (chunk) => {
+  buffer += chunk.toString('utf8')
+  // The changelog after the second release header is for old version.
+  // Therefore, the part before that is considered as the changelog for latest release.
+  const matched = buffer.match(/#+\s\[?\d+\.\d+.\d+\]?/g)
+  if (matched === null || matched.length < 2) {
+    return
+  }
+  const prevHeader = matched[1]
+  const prevHeaderIndex = buffer.indexOf(prevHeader)
+  const latestChangelog = buffer.slice(0, prevHeaderIndex - 1)
+  console.log(latestChangelog)
+  stream.destroy()
+})
+
+stream.on('end', () => {
+  // This will only runs if two version headers are not found.
+  // In this case, the whole changelog is considered as the changelog for latest release.
+  console.log(buffer)
+})
