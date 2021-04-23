13575bcda4c1713323458f719c8c342a42c671a2
uknmr <y.kinmori@gmail.com>
2021-04-23 11:30:07 +0900

    test: テストを修正

--- src/themes/__tests__/createSpacing.ts
+++ src/themes/__tests__/createSpacing.ts
@@ -1,66 +1,54 @@
-import { createSpacing } from '../createSpacing'
-import { createSize } from '../createSize'
+import { createSpacing, createSpacingByChar } from '../createSpacing'
 
 describe('createSpacing', () => {
-  it('returns same spacing theme with createSize', () => {
-    const actual = createSpacing()
-    const expected = createSize()
-
-    expect(actual.XXS).toBe(expected.space.XXS)
-    expect(actual.XS).toBe(expected.space.XS)
-    expect(actual.S).toBe(expected.space.S)
-    expect(actual.M).toBe(expected.space.M)
-    expect(actual.L).toBe(expected.space.L)
-    expect(actual.XL).toBe(expected.space.XL)
-    expect(actual.XXL).toBe(expected.space.XXL)
-  })
-
-  it('returns same spacing theme with createSize when give base size', () => {
-    const actual = createSpacing({
-      baseSize: 17,
-    })
-    const expected = createSize({
-      space: {
-        defaultRem: 17,
-      },
-    })
-
-    expect(actual.XXS).toBe(expected.space.XXS)
-    expect(actual.XS).toBe(expected.space.XS)
-    expect(actual.S).toBe(expected.space.S)
-    expect(actual.M).toBe(expected.space.M)
-    expect(actual.L).toBe(expected.space.L)
-    expect(actual.XL).toBe(expected.space.XL)
-    expect(actual.XXL).toBe(expected.space.XXL)
-  })
-
   it('returns customized spacing theme when give base size', () => {
     const actual = createSpacing({
       baseSize: 13,
     })
 
-    expect(actual.XXS).toBe(13)
-    expect(actual.XS).toBe(13 * 2)
-    expect(actual.S).toBe(13 * 3)
-    expect(actual.M).toBe(13 * 4)
-    expect(actual.L).toBe(13 * 5)
-    expect(actual.XL).toBe(13 * 6)
-    expect(actual.XXL).toBe(13 * 7)
+    expect(actual.X3S).toBe(`${13 / 2}px`)
+    expect(actual.XXS).toBe('13px')
+    expect(actual.XS).toBe(`${13 * 2}px`)
+    expect(actual.S).toBe(`${13 * 3}px`)
+    expect(actual.M).toBe(`${13 * 4}px`)
+    expect(actual.L).toBe(`${13 * 5}px`)
+    expect(actual.XL).toBe(`${13 * 6}px`)
+    expect(actual.XXL).toBe(`${13 * 7}px`)
+    expect(actual.X3L).toBe(`${13 * 8}px`)
   })
 
-  it('returns customized spacing theme when give base size and some tokens', () => {
-    const actual = createSpacing({
-      baseSize: 13,
-      M: 120,
-      XL: 122,
-    })
+  it('returns customized spacingByChar theme when give base size', () => {
+    const actual = createSpacingByChar(createSpacing({ baseSize: 13 }))
 
-    expect(actual.XXS).toBe(13)
-    expect(actual.XS).toBe(13 * 2)
-    expect(actual.S).toBe(13 * 3)
-    expect(actual.M).toBe(120)
-    expect(actual.L).toBe(13 * 5)
-    expect(actual.XL).toBe(122)
-    expect(actual.XXL).toBe(13 * 7)
+    expect(actual['0.5']).toBe(`${13 / 2}px`)
+    expect(actual[1]).toBe('13px')
+    expect(actual[2]).toBe(`${13 * 2}px`)
+    expect(actual[3]).toBe(`${13 * 3}px`)
+    expect(actual[4]).toBe(`${13 * 4}px`)
+    expect(actual[5]).toBe(`${13 * 5}px`)
+    expect(actual[6]).toBe(`${13 * 6}px`)
+    expect(actual[7]).toBe(`${13 * 7}px`)
+    expect(actual[8]).toBe(`${13 * 8}px`)
+  })
+
+  it('returns customized spacingByChar theme when give base size and some tokens', () => {
+    const actual = createSpacingByChar(
+      createSpacing({
+        baseSize: 13,
+        M: '120px',
+        XL: '122px',
+        X3L: '320px',
+      }),
+    )
+
+    expect(actual['0.5']).toBe(`${13 / 2}px`)
+    expect(actual[1]).toBe('13px')
+    expect(actual[2]).toBe(`${13 * 2}px`)
+    expect(actual[3]).toBe(`${13 * 3}px`)
+    expect(actual[4]).toBe('120px')
+    expect(actual[5]).toBe(`${13 * 5}px`)
+    expect(actual[6]).toBe('122px')
+    expect(actual[7]).toBe(`${13 * 7}px`)
+    expect(actual[8]).toBe('320px')
   })
 })
