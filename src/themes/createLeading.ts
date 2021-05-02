645e7160d0cfe04995007606efdd3aca56e41b0f
uknmr <y.kinmori@gmail.com>
2021-05-02 14:31:40 +0900

    feat: 行間定義を追加

---
+++ src/themes/createLeading.ts
@@ -0,0 +1,26 @@
+import { merge } from 'lodash'
+
+interface Leading {
+  NONE: number
+  TIGHT: number
+  NORMAL: number
+  RELAXED: number
+}
+
+export type LeadingProperty = Partial<Omit<Leading, 'NONE'>>
+export type CreatedLeading = Leading
+
+export type Leadings = 'NORMAL' | 'TIGHT' | 'RELAXED' | 'NONE'
+
+export const defaultLeading: CreatedLeading = {
+  NONE: 1,
+  TIGHT: 1.25,
+  NORMAL: 1.5,
+  RELAXED: 1.75,
+}
+
+export const createLeading = (userLeading: LeadingProperty = {}) => {
+  const { ...userTokens } = userLeading
+  const created: CreatedLeading = merge(defaultLeading, userTokens)
+  return created
+}
