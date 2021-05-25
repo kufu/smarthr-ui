c5ba239a48f091eda0f6cedd0bd5222a592f44ae
wmoai <wmoaism@gmail.com>
2021-05-25 17:39:07 +0900

    fix: modify Body type to HTMLTableSectionElement
    https://developer.mozilla.org/en-US/docs/Web/API/HTMLTableSectionElement

--- src/components/Table/Body.tsx
+++ src/components/Table/Body.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react'
+import React, { HTMLAttributes, VFC } from 'react'
 import styled from 'styled-components'
 import { TableGroupContext } from './Table'
 
-export const Body: React.VFC<Record<string, unknown>> = (props) => (
+type ElementType = HTMLAttributes<HTMLTableSectionElement>
+
+export const Body: VFC<ElementType> = ({ children, ...props }) => (
   <Wrapper {...props}>
-    <TableGroupContext.Provider value={{ group: 'body' }}>
-      {props.children}
-    </TableGroupContext.Provider>
+    <TableGroupContext.Provider value={{ group: 'body' }}>{children}</TableGroupContext.Provider>
   </Wrapper>
 )
 
