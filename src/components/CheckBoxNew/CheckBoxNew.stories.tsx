28d0eef76b6310ff632bc9fe54e018e721877a10
nabeliwo <hiroki.c.watanabe@gmail.com>
2021-06-08 19:34:33 +0900

    fix: escape single quote

--- src/components/CheckBoxNew/CheckBoxNew.stories.tsx
+++ src/components/CheckBoxNew/CheckBoxNew.stories.tsx
@@ -104,7 +104,7 @@ storiesOf('CheckBoxNew', module)
             <li>
               <CheckBoxNew name="4" checked={checkedName.includes('4')} onChange={handleChange}>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an
                 unknown printer took a galley of type and scrambled it to make a type specimen book.
                 It has survived not only five centuries, but also the leap into electronic
                 typesetting, remaining essentially unchanged. It was popularised in the 1960s with
