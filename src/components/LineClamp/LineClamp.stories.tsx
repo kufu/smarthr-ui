651fdc94b0cf70da0c8a6acec170c9218c719984
wmoai <wmoaism@gmail.com>
2021-06-29 16:13:20 +0900

    test: fix lint warning of unescaped single quote

--- src/components/LineClamp/LineClamp.stories.tsx
+++ src/components/LineClamp/LineClamp.stories.tsx
@@ -6,6 +6,16 @@ import { PrimaryButton } from '../Button'
 import { LineClamp } from './LineClamp'
 import readme from './README.md'
 
+const longText = `
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+unknown printer took a galley of type and scrambled it to make a type specimen
+book. It has survived not only five centuries, but also the leap into electronic
+typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+with desktop publishing software like Aldus PageMaker including versions of Lorem
+Ipsum.`
+
 storiesOf('LineClamp', module)
   .addParameters({
     readme: {
@@ -20,30 +30,14 @@ storiesOf('LineClamp', module)
             <dt>Default</dt>
             <dd>
               <Text>
-                <LineClamp>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                  Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                  unknown printer took a galley of type and scrambled it to make a type specimen
-                  book. It has survived not only five centuries, but also the leap into electronic
-                  typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-                  with desktop publishing software like Aldus PageMaker including versions of Lorem
-                  Ipsum.
-                </LineClamp>
+                <LineClamp>{longText}</LineClamp>
               </Text>
             </dd>
             <dt>Max Lines 1 / with Light Tooltip</dt>
             <dd>
               <Text>
                 <LineClamp maxLines={1} withTooltip>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                  Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                  unknown printer took a galley of type and scrambled it to make a type specimen
-                  book. It has survived not only five centuries, but also the leap into electronic
-                  typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-                  with desktop publishing software like Aldus PageMaker including versions of Lorem
-                  Ipsum.
+                  {longText}
                 </LineClamp>
               </Text>
             </dd>
@@ -51,14 +45,7 @@ storiesOf('LineClamp', module)
             <dd>
               <Text>
                 <LineClamp maxLines={2} withTooltip>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                  Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                  unknown printer took a galley of type and scrambled it to make a type specimen
-                  book. It has survived not only five centuries, but also the leap into electronic
-                  typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-                  with desktop publishing software like Aldus PageMaker including versions of Lorem
-                  Ipsum.
+                  {longText}
                 </LineClamp>
               </Text>
             </dd>
@@ -66,14 +53,7 @@ storiesOf('LineClamp', module)
             <dd>
               <Text>
                 <LineClamp maxLines={4} toolTipType="dark" withTooltip>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                  Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                  unknown printer took a galley of type and scrambled it to make a type specimen
-                  book. It has survived not only five centuries, but also the leap into electronic
-                  typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-                  with desktop publishing software like Aldus PageMaker including versions of Lorem
-                  Ipsum.
+                  {longText}
                 </LineClamp>
               </Text>
             </dd>
@@ -82,7 +62,7 @@ storiesOf('LineClamp', module)
               <Button>
                 <LineClamp maxLines={1} toolTipType="dark" withTooltip>
                   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                  Ipsum has been the industry's standard dummy text ever since the 1500s.
+                  Ipsum has been the industry&apos;s standard dummy text ever since the 1500s.
                 </LineClamp>
               </Button>
             </dd>
