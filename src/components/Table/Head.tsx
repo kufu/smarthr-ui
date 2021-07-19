08e8657908bef6c4095ff4a82ffa7eef3a9d7d7c
kgsi <kuusai1998@gmail.com>
2021-07-19 15:00:01 +0900

    feat: add comment

--- src/components/Table/Head.tsx
+++ src/components/Table/Head.tsx
@@ -45,7 +45,7 @@ const StyledThead = styled.thead<{ themes: Theme; $fixed: boolean }>(({ themes, 
       position: sticky;
       top: 0;
       left: 0;
-      z-index: ${zIndex.FIXED_MENU};
+      z-index: ${zIndex.FIXED_MENU}; /* zIndexの値はセマンティクストークンとして管理しているため、明示的に値を指定しないと重なり順が崩れるため設定しています */
     `
   )
 })
