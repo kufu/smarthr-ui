da3b7eff1dc3ce7278d9144ba806eba9d672844d
Soichi Masuda <masuP9@gmail.com>
2021-09-15 17:13:06 +0900

    add `<Sidebar>` Component (#1888)
    * feat: add Layout type
    
    * feat: add Sidebar Component

---
+++ src/components/Layout/type.ts
@@ -0,0 +1,7 @@
+import { AbstractSize, CharRelativeSize } from '../../themes/createSpacing'
+
+export type Gap = CharRelativeSize | AbstractSize
+export type SeparateGap = {
+  row: Gap
+  column: Gap
+}
