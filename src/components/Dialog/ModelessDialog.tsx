fa8eab8ab71e353cfc893d1e6c3322399854ae46
KANAMORI Yu <y.kinmori@gmail.com>
2021-09-21 11:44:50 +0900

    feat: 各種 Dialog を body 直下以外からも生成できるようにする｜SHRUI-474 (#1891)
    * feat: ダイアログ各種を特定の要素内に生成できるようにする
    
    * docs: Dialog の Story を更新
    
    * fix: レビュー指摘修正。
    
    - portalParent の型定義を createPortal している場所に移動
    - Story で null を HTMLElement として扱っていたのを修正
    - 不要な記述を削除
    
    * fix: レビュー指摘修正。
    
    - ActionDialog の型定義記述位置をまとめる
    - portalParent の初期設定を useEffect 内に移動

--- src/components/Dialog/ModelessDialog.tsx
+++ src/components/Dialog/ModelessDialog.tsx
@@ -70,6 +70,10 @@ type Props = {
    * ダイアログを開いたときの初期 bottom 位置
    */
   bottom?: string | number
+  /**
+   * ポータルの container となる DOM 要素を追加する親要素
+   */
+  portalParent?: HTMLElement
 }
 
 export const ModelessDialog: React.VFC<Props & BaseElementProps> = ({
@@ -85,10 +89,11 @@ export const ModelessDialog: React.VFC<Props & BaseElementProps> = ({
   left,
   right,
   bottom,
+  portalParent,
   className = '',
   ...props
 }) => {
-  const portalParent = useRef(document.createElement('div')).current
+  const portalContainer = useRef(document.createElement('div')).current
   const wrapperRef = useRef<HTMLDivElement>(null)
   const [centering, setCentering] = useState<{
     top?: number
@@ -101,11 +106,13 @@ export const ModelessDialog: React.VFC<Props & BaseElementProps> = ({
   const theme = useTheme()
 
   useEffect(() => {
-    document.body.appendChild(portalParent)
+    // SSR を考慮し、useEffect 内で初期値 document.body を指定
+    const pp = portalParent || document.body
+    pp.appendChild(portalContainer)
     return () => {
-      document.body.removeChild(portalParent)
+      pp.removeChild(portalContainer)
     }
-  }, [portalParent])
+  }, [portalContainer, portalParent])
 
   useHandleEscape(
     useCallback(() => {
@@ -248,7 +255,7 @@ export const ModelessDialog: React.VFC<Props & BaseElementProps> = ({
         </Layout>
       </Draggable>
     </DialogOverlap>,
-    portalParent,
+    portalContainer,
   )
 }
 
