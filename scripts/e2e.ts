f5ec7339736569ae55d7f4a6c7262e929bf005ba
wmoai <wmoaism@gmail.com>
2021-09-21 14:35:10 +0900

    chore: remove wait-for-localhost

--- scripts/e2e.ts
+++ scripts/e2e.ts
@@ -1,34 +1,30 @@
 import { spawn, spawnSync } from 'child_process'
-import waitForLocalhost from 'wait-for-localhost'
-;(async () => {
-  const PORT = 6006
 
-  const buildSpawn = spawnSync('yarn', ['run', 'build-storybook', '--quiet'], {
+const PORT = 6006
+
+const buildSpawn = spawnSync('yarn', ['run', 'build-storybook', '--quiet'], {
+  stdio: 'inherit',
+  shell: true,
+})
+if (buildSpawn.status !== 0) {
+  process.exit(1)
+}
+const httpServer = spawn(
+  'yarn',
+  ['http-server', 'storybook-static', '--port', `${PORT}`, '--silent'],
+  {
     stdio: 'inherit',
     shell: true,
-  })
-  if (buildSpawn.status !== 0) {
-    process.exit(1)
-  }
-  const httpServer = spawn(
-    'yarn',
-    ['http-server', 'storybook-static', '--port', `${PORT}`, '--silent'],
-    {
-      stdio: 'inherit',
-      shell: true,
-    },
-  )
+  },
+)
 
-  await waitForLocalhost({ port: PORT, path: '/iframe.html' })
-
-  const browser = process.env.TESTCAFE_BROWSER || 'chrome'
-  const testcafeArgs = `"e2e/**/*.test.ts" --host localhost --skip-js-errors`
-  const testcafe = spawn('yarn', ['run', 'testcafe', browser, ...testcafeArgs.split(' ')], {
-    stdio: 'inherit',
-    shell: true,
-  })
-  testcafe.on('close', (code) => {
-    httpServer.kill()
-    process.exit(code || undefined)
-  })
-})()
+const browser = process.env.TESTCAFE_BROWSER || 'chrome'
+const testcafeArgs = `"e2e/**/*.test.ts" --host localhost --skip-js-errors`
+const testcafe = spawn('yarn', ['run', 'testcafe', browser, ...testcafeArgs.split(' ')], {
+  stdio: 'inherit',
+  shell: true,
+})
+testcafe.on('close', (code) => {
+  httpServer.kill()
+  process.exit(code || undefined)
+})
