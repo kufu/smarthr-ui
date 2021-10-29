6542a8b21f0a91c843ff9a259dc1ea3d516869f4
wmoai <wmoaism@gmail.com>
2021-10-29 18:24:04 +0900

    docs: add props descriptions of `CompactInformationPanel` (#2007)

--- src/components/CompactInformationPanel/CompactInformationPanel.tsx
+++ src/components/CompactInformationPanel/CompactInformationPanel.tsx
@@ -15,8 +15,11 @@ import {
 type IconType = 'info' | 'success' | 'warning' | 'error'
 
 type Props = {
+  /** 表示する情報の種類 */
   type?: IconType
+  /** コンポーネントに適用するクラス名 */
   className?: string
+  /** 表示する情報の内容 */
   children: ReactNode
 }
 
