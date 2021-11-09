3772cd1d3fad55c750eb0e23c46dbaa05dc9e37e
wmoai <wmoaism@gmail.com>
2021-11-09 11:37:18 +0900

    chore: change not to use shotrhand

--- src/components/Button/UnstyledButton.tsx
+++ src/components/Button/UnstyledButton.tsx
@@ -10,7 +10,13 @@ export const UnstyledButton = styled.button`
   border-style: none;
   border-width: medium;
   border-color: currentColor;
-  border-image: none 100% 1 0 stretch;
+  /* stylelint-disable declaration-block-no-redundant-longhand-properties */
+  border-image-source: none;
+  border-image-slice: 100%;
+  border-image-width: 1;
+  border-image-outset: 0;
+  border-image-repeat: stretch;
+  /* stylelint-enable declaration-block-no-redundant-longhand-properties */
   box-sizing: content-box;
   background-color: transparent;
   background-image: none;
