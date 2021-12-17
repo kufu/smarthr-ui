ecbbce2c10dfad4515331dd1fbaa16a614cd523f
AtsushiM <at.mizoue@gmail.com>
2021-12-17 11:54:53 +0900

    fix: add AccordionPanel style

--- scripts/build-stylesheet.ts
+++ scripts/build-stylesheet.ts
@@ -1,65 +1,74 @@
 const React = require('react')
 const { renderToString } = require('react-dom/server')
 const { ServerStyleSheet, StyleSheetManager } = require('styled-components')
+const path = require('path')
+const fs = require('fs')
+const AccordionPanel = require('../lib/components/AccordionPanel/style')
 const Button = require('../lib/components/Button/style')
+
+const Components = [AccordionPanel, Button]
 
 const compact = (ary) => ary.filter((i) => !!i)
 
-const sheet = new ServerStyleSheet()
 const styleTags = (() => {
-  let style
+  const generateStyle = (Component) => {
+    const sheet = new ServerStyleSheet()
 
-  try {
-    renderToString(
-      React.createElement(
-        StyleSheetManager,
-        { sheet: sheet.instance },
-        React.createElement(Button.Style, {}),
-      ),
-    )
-    style = sheet.getStyleTags()
-  } catch (error) {
-    console.error(error)
-  } finally {
-    sheet.seal()
-  }
-
-  style = style
-    // eslint-disable-next-line no-useless-escape
-    .replace(/\/\*\!sc\*\//g, '')
-    .replace(/^<style.*?>/, '')
-    .replace(/<\/style>$/, '')
-  style = `${style}\n`
-
-  const regexp = /data-styled\.(.+?})\n/g
-  const matcher = style.match(regexp)
-
-  matcher.forEach((m) => {
-    const [_line, _gid, id, content] = m.match(
-      /^data-styled(.+?)\[id="(.+?)"\]\{content:"(.+?)"\}\n?$/,
-    )
-    const nextStyleName = [
-      Button.PREFIX,
-      ...compact(id.split('-')).filter((_i, index) => index !== 1),
-    ].join('-')
-    const parsedContent = compact(content.split(','))
-
-    console.log(nextStyleName, parsedContent)
-
-    const styleNameFormatter = (key) => {
-      // eslint-disable-next-line no-useless-escape
-      style = style.replace(new RegExp(`\.${key}([\{|\.|\[|\: ])`, 'g'), `.${nextStyleName}$1`)
+    let style
+    try {
+      renderToString(
+        React.createElement(
+          StyleSheetManager,
+          { sheet: sheet.instance },
+          React.createElement(Component.Style, {}),
+        ),
+      )
+      style = sheet.getStyleTags()
+    } catch (error) {
+      console.error(error)
+    } finally {
+      sheet.seal()
     }
 
-    styleNameFormatter(parsedContent[0])
-    styleNameFormatter(id)
-  })
+    style = style
+      // eslint-disable-next-line no-useless-escape
+      .replace(/\/\*\!sc\*\//g, '')
+      .replace(/^<style.*?>/, '')
+      .replace(/<\/style>$/, '')
+    style = `${style}\n`
 
-  console.log('----------------------')
+    const regexp = /data-styled\.(.+?})\n/g
+    const matcher = style.match(regexp)
 
-  style = style.replace(regexp, '\n')
+    matcher.forEach((m) => {
+      const [_line, _gid, id, content] = m.match(
+        /^data-styled(.+?)\[id="(.+?)"\]\{content:"(.+?)"\}\n?$/,
+      )
+      const nextStyleName = [
+        Component.PREFIX,
+        ...compact(id.split('-')).filter((_i, index) => index !== 1),
+      ].join('-')
+      const parsedContent = compact(content.split(','))
 
-  return style
+      console.log(nextStyleName, parsedContent)
+
+      const styleNameFormatter = (key) => {
+        // eslint-disable-next-line no-useless-escape
+        style = style.replace(new RegExp(`\.${key}([\{|\.|\[|\: ])`, 'g'), `.${nextStyleName}$1`)
+      }
+
+      styleNameFormatter(parsedContent[0])
+      styleNameFormatter(id)
+    })
+
+    console.log('----------------------')
+
+    style = style.replace(regexp, '\n')
+    return style
+  }
+
+  return Components.map((C) => generateStyle(C)).join('\n')
 })()
 
 console.log(styleTags)
+fs.writeFileSync(path.resolve('lib', 'style.css'), styleTags)
