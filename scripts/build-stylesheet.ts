8bf52c60edefb66e73b0129a01bf50aaddabbd6d
AtsushiM <at.mizoue@gmail.com>
2021-12-17 10:38:38 +0900

    fix: export css format

--- scripts/build-stylesheet.ts
+++ scripts/build-stylesheet.ts
@@ -3,6 +3,9 @@ const { renderToString } = require('react-dom/server')
 const { ServerStyleSheet, StyleSheetManager } = require('styled-components')
 const { Style } = require('../lib/style')
 
+const compact = (ary) => ary.filter((i) => !!i)
+const idPrefix = 'static-style-temp-'
+
 const sheet = new ServerStyleSheet()
 const styleTags = (() => {
   let style
@@ -22,7 +25,36 @@ const styleTags = (() => {
     sheet.seal()
   }
 
-  return style.replace(/^<style.*?>/, '').replace(/<\/style>$/, '')
+  style = style
+    // eslint-disable-next-line no-useless-escape
+    .replace(/\/\*\!sc\*\//g, '')
+    .replace(/^<style.*?>/, '')
+    .replace(/<\/style>$/, '')
+  style = `${style}\n`
+
+  const regexp = /data-styled\.(.+?})\n/g
+  const matcher = style.match(regexp)
+
+  matcher.forEach((m) => {
+    const [_line, _gid, id, content] = m.match(
+      /^data-styled(.+?)\[id="(.+?)"\]\{content:"(.+?)"\}\n?$/,
+    )
+    const parsedId = compact(id.split(idPrefix))
+    const parsedContent = compact(content.split(','))
+
+    console.log(parsedId, parsedContent)
+
+    // eslint-disable-next-line no-useless-escape
+    const partRegex = `\.${parsedContent[0]}([\{|\.|\[|\: ])`
+    style = style.replace(new RegExp(partRegex, 'g'), `.${parsedId.join('')}$1`)
+  })
+
+  console.log('----------------------')
+
+  style = style.replace(regexp, '\n')
+  style = style.replace(new RegExp(`${idPrefix}`, 'g'), '')
+
+  return style
 })()
 
 console.log(styleTags)
