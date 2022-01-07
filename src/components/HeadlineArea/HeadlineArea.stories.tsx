718a5667c625d25e2c5de1b6ac89818965ce4cf6
wmoai <wmoaism@gmail.com>
2022-01-07 16:48:23 +0900

    docs: change story of HeadlineArea to CSF

--- src/components/HeadlineArea/HeadlineArea.stories.tsx
+++ src/components/HeadlineArea/HeadlineArea.stories.tsx
@@ -1,23 +1,28 @@
-import { storiesOf } from '@storybook/react'
+import { Story } from '@storybook/react'
 import * as React from 'react'
 
 import { HeadlineArea } from './HeadlineArea'
 
 import readme from './README.md'
 
+export default {
+  title: 'HeadlineArea',
+  component: HeadlineArea,
+  parameters: {
+    readme: {
+      sidebar: readme,
+    },
+  },
+}
+
 const heading = {
   children: 'HeadlineArea',
 }
 
-storiesOf('HeadlineArea', module)
-  .addParameters({
-    readme: {
-      sidebar: readme,
-    },
-  })
-  .add('all', () => (
-    <HeadlineArea
-      heading={heading}
-      description="画面説明テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
-    />
-  ))
+export const All: Story = () => (
+  <HeadlineArea
+    heading={heading}
+    description="画面説明テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+  />
+)
+All.storyName = 'all'
