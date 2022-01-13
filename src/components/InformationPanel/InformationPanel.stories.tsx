2c64b79e44939c3e488b398f370167c8216e64ce
wmoai <wmoaism@gmail.com>
2022-01-13 11:08:06 +0900

    docs: `InformationPanel` の Story メンテナンス (SHRUI-464) (#2179)
    * docs: change story of InformationPanel to CSF
    
    * docs: add description comment of props

--- src/components/InformationPanel/InformationPanel.stories.tsx
+++ src/components/InformationPanel/InformationPanel.stories.tsx
@@ -1,44 +1,49 @@
-import { storiesOf } from '@storybook/react'
+import { Story } from '@storybook/react'
 import React from 'react'
 import styled from 'styled-components'
 
 import { InformationPanel } from './InformationPanel'
 import readme from './README.md'
 
-storiesOf('InformationPanel', module)
-  .addParameters({
+export default {
+  title: 'InformationPanel',
+  component: InformationPanel,
+  parameters: {
     readme: {
       sidebar: readme,
     },
-  })
-  .add('all', () => (
-    <List>
-      <li>
-        <Wrapper>
-          <InformationPanel title="Panel Title" openButtonLabel="OPEN" closeButtonLabel="CLOSE">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </InformationPanel>
-        </Wrapper>
-      </li>
-      <li>
-        <Wrapper>
-          <InformationPanel title="Panel without a toggle button" togglable={false}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </InformationPanel>
-        </Wrapper>
-      </li>
-    </List>
-  ))
+  },
+}
+
+export const All: Story = () => (
+  <List>
+    <li>
+      <Wrapper>
+        <InformationPanel title="Panel Title" openButtonLabel="OPEN" closeButtonLabel="CLOSE">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </InformationPanel>
+      </Wrapper>
+    </li>
+    <li>
+      <Wrapper>
+        <InformationPanel title="Panel without a toggle button" togglable={false}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </InformationPanel>
+      </Wrapper>
+    </li>
+  </List>
+)
+All.storyName = 'all'
 
 const List = styled.ul`
   list-style: none;
