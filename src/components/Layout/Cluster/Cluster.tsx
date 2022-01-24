7fd4f289db4838866a7feae65ce16da2f5a82141
masuP9 <masuP9@gmail.com>
2022-01-24 15:28:06 +0900

    feat: add inline Props

--- src/components/Layout/Cluster/Cluster.tsx
+++ src/components/Layout/Cluster/Cluster.tsx
@@ -4,18 +4,20 @@ import { useSpacing } from '../../../hooks/useSpacing'
 import type { Gap, SeparateGap } from '../type'
 
 export const Cluster = styled.div<{
+  /** true の場合は inline-flex */
+  inline?: boolean
   /** 間隔の指定（基準フォントサイズの相対値または抽象値） */
   gap?: Gap | SeparateGap
   /** 垂直方向の揃え方（align-items） */
   align?: CSSProperties['alignItems']
   /** 水平方向の揃え方（justify-content） */
   justify?: CSSProperties['justifyContent']
-}>(({ gap = 0.5, align, justify }) => {
+}>(({ gap = 0.5, align, justify, inline }) => {
   const rowGap = gap instanceof Object ? useSpacing(gap.row) : useSpacing(gap)
   const columnGap = gap instanceof Object ? useSpacing(gap.column) : useSpacing(gap)
 
   return css`
-    display: flex;
+    display: ${inline ? 'inline-flex' : 'flex'};
     flex-wrap: wrap;
     ${align && `align-items: ${align};`}
     ${justify && `justify-content: ${justify};`}
