a75270cb680e5b0fa722e8fe766212b1cdd65128
masuP9 <masuP9@gmail.com>
2022-01-24 15:27:07 +0900

    feat: use CSSProperties type to align, justify props type

--- src/components/Layout/Cluster/Cluster.tsx
+++ src/components/Layout/Cluster/Cluster.tsx
@@ -1,17 +1,15 @@
+import type { CSSProperties } from 'react'
 import styled, { css } from 'styled-components'
 import { useSpacing } from '../../../hooks/useSpacing'
 import type { Gap, SeparateGap } from '../type'
-
-type alignMethod = 'normal' | 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch'
-type justifyMethod = 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around'
 
 export const Cluster = styled.div<{
   /** 間隔の指定（基準フォントサイズの相対値または抽象値） */
   gap?: Gap | SeparateGap
   /** 垂直方向の揃え方（align-items） */
-  align?: alignMethod
+  align?: CSSProperties['alignItems']
   /** 水平方向の揃え方（justify-content） */
-  justify?: justifyMethod
+  justify?: CSSProperties['justifyContent']
 }>(({ gap = 0.5, align, justify }) => {
   const rowGap = gap instanceof Object ? useSpacing(gap.row) : useSpacing(gap)
   const columnGap = gap instanceof Object ? useSpacing(gap.column) : useSpacing(gap)
