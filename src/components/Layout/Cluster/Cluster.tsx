bec21152b8b2df773aed453d9cfcc12e79ec1031
masuP9 <masuP9@gmail.com>
2022-01-24 15:25:10 +0900

    feat!: remove fallback flex gap, change Cluster to styled-components

--- src/components/Layout/Cluster/Cluster.tsx
+++ src/components/Layout/Cluster/Cluster.tsx
@@ -1,75 +1,18 @@
-import React, { HTMLAttributes } from 'react'
 import styled, { css } from 'styled-components'
-
-import { useClassNames } from './useClassNames'
 import { useSpacing } from '../../../hooks/useSpacing'
 import type { Gap, SeparateGap } from '../type'
 
 type alignMethod = 'normal' | 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch'
 type justifyMethod = 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around'
 
-type Props = {
+export const Cluster = styled.div<{
   /** 間隔の指定（基準フォントサイズの相対値または抽象値） */
   gap?: Gap | SeparateGap
   /** 垂直方向の揃え方（align-items） */
   align?: alignMethod
   /** 水平方向の揃え方（justify-content） */
   justify?: justifyMethod
-  /** ネガティブマージンを隠す要素の HTML タグ名 */
-  as?: React.ElementType
-  /** Cluster 本体の HTML タグ名 */
-  bodyAs?: React.ElementType
-  /** 均等に間隔を空けたい要素群 */
-  children?: React.ReactNode
-}
-type ElementProps = Omit<HTMLAttributes<HTMLDivElement>, keyof Props>
-
-/**
- * @param gap 間隔の指定（基準フォントサイズの相対値または抽象値）
- * @param rowGap 垂直方向の間隔の指定（基準フォントサイズの相対値または抽象値）
- * @param columnGap 水平方向の間隔の指定（基準フォントサイズの相対値または抽象値）
- * @param align 垂直方向の揃え方（align-items）
- * @param justify 水平方向の揃え方（justify-content）
- * @param as ネガティブマージンを隠す要素の HTML タグ名
- * @param bodyAs Cluster 本体の HTML タグ名
- * @param children 均等に間隔を空けたい要素群
- */
-export const Cluster: React.VFC<Props & ElementProps> = ({
-  gap = 0.5,
-  align,
-  justify,
-  as,
-  bodyAs,
-  children,
-  className,
-  ...props
-}) => {
-  const classNames = useClassNames()
-
-  return (
-    <Wrapper as={as} {...props} className={`${classNames.wrapper}${className && ` ${className}`}`}>
-      <Body as={bodyAs} gap={gap} align={align} justify={justify} className={`${classNames.body}`}>
-        {children}
-      </Body>
-    </Wrapper>
-  )
-}
-
-// gap のネガティブマージンを隠すための要素、gap が利用できる場合は不要
-// Wrapper to hide the negative margin of gap, not required if gap is available.
-const Wrapper = styled.div`
-  display: block; /* expect it to always be "block" */
-  overflow: hidden;
-
-  @supports (gap: 1px) {
-    overflow: revert;
-  }
-`
-const Body = styled.div<{
-  gap: Gap | SeparateGap
-  align?: alignMethod
-  justify?: justifyMethod
-}>(({ gap, align, justify }) => {
+}>(({ gap = 0.5, align, justify }) => {
   const rowGap = gap instanceof Object ? useSpacing(gap.row) : useSpacing(gap)
   const columnGap = gap instanceof Object ? useSpacing(gap.column) : useSpacing(gap)
 
@@ -78,20 +21,7 @@ const Body = styled.div<{
     flex-wrap: wrap;
     ${align && `align-items: ${align};`}
     ${justify && `justify-content: ${justify};`}
-    margin: calc(${rowGap} / 2 * -1) calc(${columnGap} / 2 * -1);
-
-    > * {
-      margin: calc(${rowGap} / 2) calc(${columnGap} / 2);
-    }
-
-    @supports (gap: 1px) {
-      margin: revert;
-      row-gap: ${rowGap};
-      column-gap: ${columnGap};
-
-      > * {
-        margin: revert;
-      }
-    }
+    row-gap: ${rowGap};
+    column-gap: ${columnGap};
   `
 })
