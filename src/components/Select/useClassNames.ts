ba597ce2120a53ced025b4b80264b0d7b4722682
wmoai <wmoaism@gmail.com>
2022-01-25 13:04:24 +0900

    chore: add type assertion

--- src/components/Select/useClassNames.ts
+++ src/components/Select/useClassNames.ts
@@ -1,10 +1,10 @@
-import { useMemo } from 'react'
+import { VFC, useMemo } from 'react'
 
 import { useClassNameGenerator } from '../../hooks/useClassNameGenerator'
 import { Select } from './Select'
 
 export function useClassNames() {
-  const generate = useClassNameGenerator(Select.displayName || 'Select')
+  const generate = useClassNameGenerator((Select as VFC).displayName || 'Select')
   return useMemo(
     () => ({
       wrapper: generate(),
