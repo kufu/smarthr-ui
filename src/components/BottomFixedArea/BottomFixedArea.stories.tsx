4b01e719037b2bcf28e05d4e65df0b2df4daf898
wmoai <wmoaism@gmail.com>
2022-02-18 14:50:53 +0900

    docs: add mobile viewport story

--- src/components/BottomFixedArea/BottomFixedArea.stories.tsx
+++ src/components/BottomFixedArea/BottomFixedArea.stories.tsx
@@ -28,3 +28,24 @@ export const _BottomFixedArea: Story = () => {
   )
 }
 _BottomFixedArea.storyName = 'BottomFixedArea'
+
+export const MobileViewport: Story = () => {
+  return (
+    <BottomFixedArea
+      description="This is description."
+      primaryButton={<PrimaryButton>Primary Button</PrimaryButton>}
+      secondaryButton={<SecondaryButton>Secondary Button</SecondaryButton>}
+      tertiaryLinks={[
+        { text: 'Tertiary_1', icon: FaTrashIcon, onClick: action('click_1') },
+        { text: 'Tertiary_2', icon: FaTrashIcon, onClick: action('click_2') },
+        { text: 'Tertiary_3', icon: FaTrashIcon, onClick: action('click_3') },
+        { text: 'Tertiary_4', icon: FaTrashIcon, onClick: action('click_4') },
+      ]}
+    />
+  )
+}
+MobileViewport.parameters = {
+  viewport: {
+    defaultViewport: 'iphone5',
+  },
+}
