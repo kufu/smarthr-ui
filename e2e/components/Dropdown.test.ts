417b14b13ef562c642e507e3519ed07cd8eb1ab3
wmoai <wmoaism@gmail.com>
2022-03-04 13:23:37 +0900

    test: update test

--- e2e/components/Dropdown.test.ts
+++ e2e/components/Dropdown.test.ts
@@ -13,10 +13,22 @@ test('ボタンがクリックされるとドロップダウンが開くこと',
     .ok()
 })
 
-test('ドロップダウンが展開されるとドロップダウン内のフォーカス可能な要素にフォーカスが移動すること', async (t) => {
+test('トリガとドロップダウン間でフォーカスの行き来ができること', async (t) => {
+  const trigger = Selector('#dropdown-button-1')
+  const firstFocusable = Selector('#dropdown-list-item-1')
   await t
-    .click(Selector('#dropdown-button-1'))
-    .expect(Selector('#dropdown-list-item-1').focused)
+    .click(trigger)
+    // Tab キーを押下すると、ドロップダウン内の最初のフォーカス可能要素にフォーカスが移動すること
+    .pressKey('tab')
+    .expect(firstFocusable.focused)
+    .ok()
+    // 最初のフォーカス可能要素がフォーカスされた状態で Shift + Tab キーを押下すると、トリガにフォーカスが移動すること
+    .pressKey('shift+tab')
+    .expect(trigger.focused)
+    .ok()
+    // ドロップダウンが展開されていてトリガがフォーカスされている状態で Tab キーを押下すると、ドロップダウン内の最初のフォーカス可能要素にフォーカスが移動すること
+    .pressKey('tab')
+    .expect(firstFocusable.focused)
     .ok()
 })
 
