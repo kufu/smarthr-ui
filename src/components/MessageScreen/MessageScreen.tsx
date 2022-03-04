281165c86a73edfb9895e8ce68e719cafd251be1
wmoai <wmoaism@gmail.com>
2022-03-04 18:25:32 +0900

    docs: add description comment of link

--- src/components/MessageScreen/MessageScreen.tsx
+++ src/components/MessageScreen/MessageScreen.tsx
@@ -13,8 +13,11 @@ type Props = {
   title?: ReactNode
   /** コンテンツの下に表示されるアンカー要素のリスト */
   links?: Array<{
+    /** アンカー要素のテキスト */
     label: string
+    /** アンカー要素の href */
     url: string
+    /** アンカー要素の target。`_blank` を設定すると外部リンクアイコンが表示されます。*/
     target?: string
   }>
   /** 表示するコンテンツ */
