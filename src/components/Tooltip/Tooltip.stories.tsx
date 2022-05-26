1eb3be2df824348eb2cab0d682487c7339c88f1a
wmoai <wmoaism@gmail.com>
2022-05-26 15:21:28 +0900

    docs: change Tooltip story to CSF

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -1,4 +1,4 @@
-import { storiesOf } from '@storybook/react'
+import { Story } from '@storybook/react'
 import * as React from 'react'
 import styled from 'styled-components'
 
@@ -13,208 +13,209 @@ import { Tooltip } from './Tooltip'
 
 import readme from './README.md'
 
-storiesOf('Tooltip', module)
-  .addParameters({
+export default {
+  title: 'Tooltip',
+  component: Tooltip,
+  parameters: {
     readme: {
       sidebar: readme,
     },
-  })
-  .add('all', () => (
-    <List>
-      <dt>Default</dt>
-      <dd>
-        <Tooltip message="LightBalloon">LightTooltip</Tooltip>
-      </dd>
-      <dt>horizontal & vertical</dt>
-      <dd>
-        <Tooltip
-          message="horizontal=left & vertical=bottom (default)"
-          horizontal="left"
-          vertical="bottom"
-        >
-          horizontal=left & vertical=bottom (default)
-        </Tooltip>
-      </dd>
-      <dd className="center">
-        <Tooltip
-          message="horizontal=center & vertical=bottom"
-          horizontal="center"
-          vertical="bottom"
-        >
-          horizontal=center & vertical=bottom
-        </Tooltip>
-      </dd>
-      <dd className="right">
-        <Tooltip message="horizontal=right & vertical=bottom" horizontal="right" vertical="bottom">
-          horizontal=right & vertical=bottom
-        </Tooltip>
-      </dd>
-      <dd>
-        <Tooltip message="horizontal=left & vertical=middle" horizontal="left" vertical="middle">
-          horizontal=left & vertical=middle
-        </Tooltip>
-      </dd>
-      <dd className="right">
-        <Tooltip message="horizontal=right & vertical=middle" horizontal="right" vertical="middle">
-          horizontal=right & vertical=middle
-        </Tooltip>
-      </dd>
-      <dd>
-        <Tooltip message="horizontal=left & vertical=top" horizontal="left" vertical="top">
-          horizontal=left & vertical=top
-        </Tooltip>
-      </dd>
-      <dd className="center">
-        <Tooltip message="horizontal=center & vertical=top" horizontal="center" vertical="top">
-          horizontal=center & vertical=top
-        </Tooltip>
-      </dd>
-      <dd className="right">
-        <Tooltip message="horizontal=right & vertical=top" horizontal="right" vertical="top">
-          horizontal=right & vertical=top
-        </Tooltip>
-      </dd>
-      <dt>ellipsisOnly</dt>
-      <dd className="limit">
-        <Tooltip message="invisible message" ellipsisOnly={true}>
-          ellipsisOnly: invisible
-        </Tooltip>
-      </dd>
-      <dd className="limit">
-        <Tooltip message="visible message" ellipsisOnly={true}>
-          <Text>
-            ellipsisOnly: visible: Pablo Diego José Francisco de Paula Juan Nepomuceno Cipriano de
-            la Santísima Trinidad Ruiz Picasso
-          </Text>
-        </Tooltip>
-      </dd>
-      <dt>multiLine</dt>
-      <dd className="limit">
-        <Tooltip
-          message="Pablo Diego José Francisco de Paula Juan Nepomuceno Cipriano de la Santísima Trinidad Ruiz Picasso"
-          multiLine={true}
-        >
+  },
+}
+
+export const All: Story = () => (
+  <List>
+    <dt>Default</dt>
+    <dd>
+      <Tooltip message="LightBalloon">LightTooltip</Tooltip>
+    </dd>
+    <dt>horizontal & vertical</dt>
+    <dd>
+      <Tooltip
+        message="horizontal=left & vertical=bottom (default)"
+        horizontal="left"
+        vertical="bottom"
+      >
+        horizontal=left & vertical=bottom (default)
+      </Tooltip>
+    </dd>
+    <dd className="center">
+      <Tooltip message="horizontal=center & vertical=bottom" horizontal="center" vertical="bottom">
+        horizontal=center & vertical=bottom
+      </Tooltip>
+    </dd>
+    <dd className="right">
+      <Tooltip message="horizontal=right & vertical=bottom" horizontal="right" vertical="bottom">
+        horizontal=right & vertical=bottom
+      </Tooltip>
+    </dd>
+    <dd>
+      <Tooltip message="horizontal=left & vertical=middle" horizontal="left" vertical="middle">
+        horizontal=left & vertical=middle
+      </Tooltip>
+    </dd>
+    <dd className="right">
+      <Tooltip message="horizontal=right & vertical=middle" horizontal="right" vertical="middle">
+        horizontal=right & vertical=middle
+      </Tooltip>
+    </dd>
+    <dd>
+      <Tooltip message="horizontal=left & vertical=top" horizontal="left" vertical="top">
+        horizontal=left & vertical=top
+      </Tooltip>
+    </dd>
+    <dd className="center">
+      <Tooltip message="horizontal=center & vertical=top" horizontal="center" vertical="top">
+        horizontal=center & vertical=top
+      </Tooltip>
+    </dd>
+    <dd className="right">
+      <Tooltip message="horizontal=right & vertical=top" horizontal="right" vertical="top">
+        horizontal=right & vertical=top
+      </Tooltip>
+    </dd>
+    <dt>ellipsisOnly</dt>
+    <dd className="limit">
+      <Tooltip message="invisible message" ellipsisOnly={true}>
+        ellipsisOnly: invisible
+      </Tooltip>
+    </dd>
+    <dd className="limit">
+      <Tooltip message="visible message" ellipsisOnly={true}>
+        <Text>
+          ellipsisOnly: visible: Pablo Diego José Francisco de Paula Juan Nepomuceno Cipriano de la
+          Santísima Trinidad Ruiz Picasso
+        </Text>
+      </Tooltip>
+    </dd>
+    <dt>multiLine</dt>
+    <dd className="limit">
+      <Tooltip
+        message="Pablo Diego José Francisco de Paula Juan Nepomuceno Cipriano de la Santísima Trinidad Ruiz Picasso"
+        multiLine={true}
+      >
+        MultiLineMessage: Pablo Diego José Francisco de Paula Juan Nepomuceno Cipriano de la
+        Santísima Trinidad Ruiz Picasso
+      </Tooltip>
+    </dd>
+    <dt>ReactNode message attribute</dt>
+    <dd>
+      <Tooltip
+        message={
+          <>
+            MultiLineMessage
+            <br />
+            MultiLine 1<br />
+            MultiLine 2<br />
+            MultiLine 3<br />
+            ...
+          </>
+        }
+      >
+        <Text>
           MultiLineMessage: Pablo Diego José Francisco de Paula Juan Nepomuceno Cipriano de la
           Santísima Trinidad Ruiz Picasso
-        </Tooltip>
-      </dd>
-      <dt>ReactNode message attribute</dt>
-      <dd>
-        <Tooltip
-          message={
-            <>
-              MultiLineMessage
-              <br />
-              MultiLine 1<br />
-              MultiLine 2<br />
-              MultiLine 3<br />
-              ...
-            </>
-          }
-        >
-          <Text>
-            MultiLineMessage: Pablo Diego José Francisco de Paula Juan Nepomuceno Cipriano de la
-            Santísima Trinidad Ruiz Picasso
-          </Text>
-        </Tooltip>
-      </dd>
-      <dt>triggerType</dt>
-      <dd className="center">
-        <Tooltip
-          message="horizontal=left & vertical=bottom (default)"
-          horizontal="left"
-          vertical="bottom"
-          triggerType="icon"
-        >
-          <FaArrowAltCircleUpIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
-        </Tooltip>
-        <Tooltip
-          message="horizontal=center & vertical=bottom"
-          horizontal="center"
-          vertical="bottom"
-          triggerType="icon"
-        >
-          <FaArrowAltCircleUpIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
-        </Tooltip>
-        <Tooltip
-          message="horizontal=right & vertical=bottom"
-          horizontal="right"
-          vertical="bottom"
-          triggerType="icon"
-        >
-          <FaArrowAltCircleUpIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
-        </Tooltip>
-        <Tooltip
-          message="horizontal=left & vertical=middle"
-          horizontal="left"
-          vertical="middle"
-          triggerType="icon"
-        >
-          <FaArrowAltCircleRightIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
-        </Tooltip>
-        <Tooltip
-          message="horizontal=right & vertical=middle"
-          horizontal="right"
-          vertical="middle"
-          triggerType="icon"
-        >
-          <FaArrowAltCircleLeftIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
-        </Tooltip>
-        <Tooltip
-          message="horizontal=left & vertical=top"
-          horizontal="left"
-          vertical="top"
-          triggerType="icon"
-        >
-          <FaArrowAltCircleDownIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
-        </Tooltip>
-        <Tooltip
-          message="horizontal=center & vertical=top"
-          horizontal="center"
-          vertical="top"
-          triggerType="icon"
-        >
-          <FaArrowAltCircleDownIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
-        </Tooltip>
-        <Tooltip
-          message="horizontal=right & vertical=top"
-          horizontal="right"
-          vertical="top"
-          triggerType="icon"
-        >
-          <FaArrowAltCircleDownIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
-        </Tooltip>
-      </dd>
-      <dt>disabled ボタン</dt>
-      <dd>
-        <Tooltip message="disabled なボタンに表示するツールチップです">
-          <SecondaryButton disabled>ボタン</SecondaryButton>
-        </Tooltip>
-      </dd>
-      <dt>自動位置決め</dt>
-      {[undefined, 'center', 'right'].map((className, i) => (
-        <dd className={className} key={i}>
-          <Tooltip
-            message="メッセージメッセージメッセージメッセージメッセージメッセージメッセージ"
-            horizontal="auto"
-            vertical="auto"
-          >
-            horizontal=auto & vertical=auto
-          </Tooltip>
-        </dd>
-      ))}
-      <dd style={{ marginBottom: '100vh' }}>
+        </Text>
+      </Tooltip>
+    </dd>
+    <dt>triggerType</dt>
+    <dd className="center">
+      <Tooltip
+        message="horizontal=left & vertical=bottom (default)"
+        horizontal="left"
+        vertical="bottom"
+        triggerType="icon"
+      >
+        <FaArrowAltCircleUpIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
+      </Tooltip>
+      <Tooltip
+        message="horizontal=center & vertical=bottom"
+        horizontal="center"
+        vertical="bottom"
+        triggerType="icon"
+      >
+        <FaArrowAltCircleUpIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
+      </Tooltip>
+      <Tooltip
+        message="horizontal=right & vertical=bottom"
+        horizontal="right"
+        vertical="bottom"
+        triggerType="icon"
+      >
+        <FaArrowAltCircleUpIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
+      </Tooltip>
+      <Tooltip
+        message="horizontal=left & vertical=middle"
+        horizontal="left"
+        vertical="middle"
+        triggerType="icon"
+      >
+        <FaArrowAltCircleRightIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
+      </Tooltip>
+      <Tooltip
+        message="horizontal=right & vertical=middle"
+        horizontal="right"
+        vertical="middle"
+        triggerType="icon"
+      >
+        <FaArrowAltCircleLeftIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
+      </Tooltip>
+      <Tooltip
+        message="horizontal=left & vertical=top"
+        horizontal="left"
+        vertical="top"
+        triggerType="icon"
+      >
+        <FaArrowAltCircleDownIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
+      </Tooltip>
+      <Tooltip
+        message="horizontal=center & vertical=top"
+        horizontal="center"
+        vertical="top"
+        triggerType="icon"
+      >
+        <FaArrowAltCircleDownIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
+      </Tooltip>
+      <Tooltip
+        message="horizontal=right & vertical=top"
+        horizontal="right"
+        vertical="top"
+        triggerType="icon"
+      >
+        <FaArrowAltCircleDownIcon visuallyHiddenText="フォーカスすると情報が表示されます" />
+      </Tooltip>
+    </dd>
+    <dt>disabled ボタン</dt>
+    <dd>
+      <Tooltip message="disabled なボタンに表示するツールチップです">
+        <SecondaryButton disabled>ボタン</SecondaryButton>
+      </Tooltip>
+    </dd>
+    <dt>自動位置決め</dt>
+    {[undefined, 'center', 'right'].map((className, i) => (
+      <dd className={className} key={i}>
         <Tooltip
           message="メッセージメッセージメッセージメッセージメッセージメッセージメッセージ"
           horizontal="auto"
           vertical="auto"
-          multiLine
         >
-          multiLine 指定時
+          horizontal=auto & vertical=auto
         </Tooltip>
       </dd>
-    </List>
-  ))
+    ))}
+    <dd style={{ marginBottom: '100vh' }}>
+      <Tooltip
+        message="メッセージメッセージメッセージメッセージメッセージメッセージメッセージ"
+        horizontal="auto"
+        vertical="auto"
+        multiLine
+      >
+        multiLine 指定時
+      </Tooltip>
+    </dd>
+  </List>
+)
+All.storyName = 'all'
 
 const List = styled.dl`
   margin: 50px 0;
