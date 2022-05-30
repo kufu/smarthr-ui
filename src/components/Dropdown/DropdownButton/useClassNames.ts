4b611905e8a2979e73252a71e6c11d5e72bc24fb
uknmr <y.kinmori@gmail.com>
2022-05-30 14:28:28 +0900

    fix: DropdownButton に className を渡せるように修正

---
+++ src/components/Dropdown/DropdownButton/useClassNames.ts
@@ -0,0 +1,16 @@
+import { useMemo } from 'react'
+
+import { useClassNameGenerator } from '../../../hooks/useClassNameGenerator'
+import { DropdownButton } from './DropdownButton'
+
+export function useClassNames() {
+  const generate = useClassNameGenerator(DropdownButton.displayName || 'DropdownButton')
+  return useMemo(
+    () => ({
+      wrapper: generate(),
+      trigger: generate('trigger'),
+      panel: generate('panel'),
+    }),
+    [generate],
+  )
+}
