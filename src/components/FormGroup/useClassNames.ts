262a71ce34838e6dccf8d2abcd59fb0b4ff33a3f
wmoai <wmoaism@gmail.com>
2022-06-10 09:18:29 +0900

    chore: 要素のデフォルト属性を渡せるようにする (SHRUI-510) (#2546)
    * chore: change Dropdown to allow passing elmeent props
    
    * chore: fix InputFile element props
    
    * chore: change SideNav to allow passing element props
    
    * chore: add escape hatch of FormGroup

---
+++ src/components/FormGroup/useClassNames.ts
@@ -0,0 +1,19 @@
+import { useMemo } from 'react'
+import { useClassNameGenerator } from '../../hooks/useClassNameGenerator'
+
+import { FormGroup } from './'
+
+export function useClassNames() {
+  const generate = useClassNameGenerator(FormGroup.displayName || 'FormGroup')
+  return useMemo(
+    () => ({
+      wrapper: generate(),
+      title: generate('title'),
+      label: generate('label'),
+      helpMessage: generate('helpMessage'),
+      errorMessage: generate('errorMessage'),
+      body: generate('body'),
+    }),
+    [generate],
+  )
+}
