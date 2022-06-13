8a81c42bf56512bc5e1733f5f708a633de2744b9
uknmr <y.kinmori@gmail.com>
2022-06-13 16:30:52 +0900

    test: StatusLabel の I/F 変更にテスト追従

--- src/components/StatusLabel/StatusLabel.test.tsx
+++ src/components/StatusLabel/StatusLabel.test.tsx
@@ -16,17 +16,17 @@ describe('StatusLabel', () => {
   })
   const hello = 'hello'
   it('should be match snapshot', () => {
-    const testRenderer = create(<StatusLabel type="success">{hello}</StatusLabel>)
+    const testRenderer = create(<StatusLabel>{hello}</StatusLabel>)
     expect(testRenderer.toJSON()).toMatchSnapshot()
   })
   it('should render given children', () => {
     act(() => {
-      createRoot(container).render(<StatusLabel type="success">{hello}</StatusLabel>)
+      createRoot(container).render(<StatusLabel>{hello}</StatusLabel>)
     })
     expect(container.textContent).toBe(hello)
   })
   it('should have given type', () => {
-    const testInstance = create(<StatusLabel type="success">{hello}</StatusLabel>).root
-    expect(testInstance.props.type).toBe('success')
+    const testInstance = create(<StatusLabel type="grey">{hello}</StatusLabel>).root
+    expect(testInstance.props.type).toBe('grey')
   })
 })
