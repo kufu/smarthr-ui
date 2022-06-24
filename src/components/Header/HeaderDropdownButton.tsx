a2f7e32df5a2001155b6c601c78566ed3f4fa33d
wmoai <wmoaism@gmail.com>
2022-06-24 16:08:18 +0900

    fix: `Header` 関連コンポーネントの theme 参照の修正 (#2604)
    * fix: change not to refer styled-components theme
    
    * fix: change HeaderDropdownButton theme reference

--- src/components/Header/HeaderDropdownButton.tsx
+++ src/components/Header/HeaderDropdownButton.tsx
@@ -1,20 +1,24 @@
 import styled, { css } from 'styled-components'
 
+import { useTheme } from '../../hooks/useTheme'
 import { DropdownButton } from '../Dropdown'
 
 export const HeaderDropdownButton = styled(DropdownButton)`
   > .smarthr-ui-DropdownButton-trigger {
-    ${({ theme: { color, spacingByChar } }) => css`
-      border-color: transparent;
-      background-color: transparent;
-      padding-inline: ${spacingByChar(0.25)};
-      color: ${color.TEXT_WHITE};
-      font-weight: normal;
+    ${() => {
+      const { color, spacingByChar } = useTheme()
+      return css`
+        border-color: transparent;
+        background-color: transparent;
+        padding-inline: ${spacingByChar(0.25)};
+        color: ${color.TEXT_WHITE};
+        font-weight: normal;
 
-      &:last-of-type {
-        /* ボタンの余白分だけ微調整 */
-        margin-inline-end: ${spacingByChar(-0.25)};
-      }
-    `}
+        &:last-of-type {
+          /* ボタンの余白分だけ微調整 */
+          margin-inline-end: ${spacingByChar(-0.25)};
+        }
+      `
+    }}
   }
 `
--- src/components/Header/HeaderLink.tsx
+++ src/components/Header/HeaderLink.tsx
@@ -1,19 +1,23 @@
 import styled, { css } from 'styled-components'
 
+import { useTheme } from '../../hooks/useTheme'
 import { TextLink } from '../TextLink'
 
 export const HeaderLink = styled(TextLink).attrs({ target: '_blank', suffix: null })`
-  ${({ theme: { color, shadow, spacingByChar } }) => css`
-    padding-inline: ${spacingByChar(0.25)};
-    color: ${color.TEXT_WHITE};
-    box-shadow: unset;
-
-    &:hover {
+  ${() => {
+    const { color, shadow, spacingByChar } = useTheme()
+    return css`
+      padding-inline: ${spacingByChar(0.25)};
       color: ${color.TEXT_WHITE};
-    }
+      box-shadow: unset;
 
-    &:focus-visible {
-      ${shadow.focusIndicatorStyles}
-    }
-  `}
+      &:hover {
+        color: ${color.TEXT_WHITE};
+      }
+
+      &:focus-visible {
+        ${shadow.focusIndicatorStyles}
+      }
+    `
+  }}
 `
