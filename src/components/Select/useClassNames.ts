0fd4b280b5f70ddf16439cedb876a823cac8ab47
KANAMORI Yu <y.kinmori@gmail.com>
2022-06-27 05:36:13 +0900

    feat: Select にサイズ小を追加 (#2600)
    * feat: Select にサイズ小を追加
    
    * docs: Select の Story を更新
    
    * fix: lint
    
    * chore: size に 'default' を追加
    
    * fix: useClassNames に引数を渡すのをやめ、コンポーネント内の関数でサイズ用のクラス名を生成する

--- src/components/Select/useClassNames.ts
+++ src/components/Select/useClassNames.ts
@@ -3,7 +3,7 @@ import { VFC, useMemo } from 'react'
 import { useClassNameGenerator } from '../../hooks/useClassNameGenerator'
 import { Select } from './Select'
 
-export function useClassNames() {
+export const useClassNames = () => {
   const generate = useClassNameGenerator((Select as VFC).displayName || 'Select')
   return useMemo(
     () => ({
