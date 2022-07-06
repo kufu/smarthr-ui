ae66feaf50792e976de7b73ae7cfb4b39978af55
Yuta Yamamoto <ftbae1@gmail.com>
2022-07-06 14:09:24 +0900

    feat: createRadiusに'L'と'FULL'を追加する (#2642)

--- src/themes/createRadius.ts
+++ src/themes/createRadius.ts
@@ -3,16 +3,22 @@ import { merge } from '../libs/lodash'
 export interface RadiusProperty {
   s?: string
   m?: string
+  l?: string
+  full?: string
 }
 
 export interface CreatedRadiusTheme {
   s: string
   m: string
+  l: string
+  full: string
 }
 
 export const defaultRadius: CreatedRadiusTheme = {
   s: '4px',
   m: '6px',
+  l: '8px',
+  full: '10000px',
 }
 
 export const createRadius = (userRadius: RadiusProperty = {}) => {
