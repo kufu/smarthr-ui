69095ebe90c0743aae78c29e69ad25fabf2d037e
AtsushiM <at.mizoue@gmail.com>
2022-08-23 09:15:33 +0900

    chore: eslint-plugin-smarthrのruleを導入する

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,18 @@ module.exports = {
     'jsx-a11y/anchor-is-valid': 'warn',
     'jsx-a11y/click-events-have-key-events': 'warn',
     'jsx-a11y/no-static-element-interactions': 'warn',
+
+    'smarthr/a11y-clickable-element-has-text': 'warn',
+    'smarthr/a11y-image-has-alt-attribute': 'warn',
+    'smarthr/a11y-trigger-has-button': 'warn',
+    'smarthr/best-practice-for-date': 'warn',
+    'smarthr/jsx-start-with-spread-attributes': [
+      'warn',
+      // {
+      //   fix: true,
+      // },
+    ],
+    'smarthr/prohibit-export-array-type': 'warn',
+    'smarthr/require-barrel-import': 'warn',
   },
 }
