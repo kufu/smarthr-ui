7724191c5a033e11a236aabc08612174654a058e
AtsushiM <at.mizoue@gmail.com>
2022-09-06 16:47:30 +0900

    feat: カレンダー選択可能範囲のデフォルトを1900/01/01からにする

--- src/components/Calendar/calendarHelper.ts
+++ src/components/Calendar/calendarHelper.ts
@@ -2,54 +2,53 @@ import dayjs from 'dayjs'
 
 export const daysInWeek = ['日', '月', '火', '水', '木', '金', '土']
 
-export function getFromDate(date?: Date): Date {
-  const defaultDate = new Date(1970, 0, 1)
-  const min = new Date(1900, 0, 1)
+export const minDate = new Date(1900, 0, 1)
+const minDatetime = minDate.getTime()
+const maxDate = new Date(9999, 11, 31)
+const maxDatetime = maxDate.getTime()
 
-  if (!date || isNaN(date.getTime())) {
-    return defaultDate
-  }
+export function getFromDate(date: Date): Date {
+  const time = date.getTime()
 
-  if (date.getTime() < min.getTime()) {
-    return min
-  }
-
-  return date
+  return isNaN(time) || time < minDatetime ? minDate : date
 }
 
 export function getToDate(date?: Date): Date {
-  if (!date || isNaN(date.getTime())) {
+  if (!date) {
     return dayjs().add(50, 'year').toDate()
   }
-  const max = new Date(9999, 11, 31)
-  if (date.getTime() > max.getTime()) {
-    return max
+
+  const time = date.getTime()
+
+  if (isNaN(time)) {
+    return dayjs().add(50, 'year').toDate()
   }
-  return date
+
+  return time > maxDatetime ? maxDate : date
 }
 
 export function getMonthArray(date: Date) {
   const startDay = dayjs(date).date(1).day()
   const lastDate = dayjs(date).add(1, 'month').date(0).date()
 
-  const numOfWeek = Math.ceil((lastDate + startDay) / 7)
-  return Array.from({ length: numOfWeek }).map((_, weekIndex) => {
+  return Array.from({ length: Math.ceil((lastDate + startDay) / 7) }).map((_, weekIndex) => {
     // 週毎の配列を形成
     const startDateInWeek = weekIndex * 7 - startDay + 1
     return Array.from({ length: 7 }).map((__, dateIndex) => {
       // 1週の配列を形成
       const dateNum = startDateInWeek + dateIndex
-      if (dateNum > 0 && dateNum <= lastDate) {
-        return dateNum
-      }
-      return null
+
+      return dateNum > 0 && dateNum <= lastDate ? dateNum : null
     })
   })
 }
 
 export function isBetween(date: Date, from: Date, to: Date) {
   const time = date.getTime()
-  const fromTime = new Date(from.getFullYear(), from.getMonth(), from.getDate()).getTime()
-  const toTime = new Date(to.getFullYear(), to.getMonth(), to.getDate() + 1).getTime()
-  return time >= fromTime && time < toTime
+
+  return (
+    time >= toRoundDate(from).getTime() && time < dayjs(toRoundDate(to)).add(1, 'day').valueOf()
+  )
 }
+
+const toRoundDate = (d: Date): Date => new Date(d.getFullYear(), d.getMonth(), d.getDate())
