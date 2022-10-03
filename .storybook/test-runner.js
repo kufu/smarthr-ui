5e42ee93af84b11d67cfb427a2e0ef88eaefacc6
yamish123 <32166731+yamish123@users.noreply.github.com>
2022-10-03 11:06:15 +0900

    chore: エラー内容を詳細に表示するようにする

--- .storybook/test-runner.js
+++ .storybook/test-runner.js
@@ -1,17 +1,45 @@
-// .storybook/test-runner.js
+const { injectAxe, getViolations } = require('axe-playwright')
 
-const { injectAxe, checkA11y } = require('axe-playwright')
+// エラー内容を詳細に表示するためのカスタムマッチャを作成
+expect.extend({
+  toValidA11y(violations) {
+    if (violations.length > 0) {
+      const violationsMessage = JSON.stringify(
+        violations.map(({ id, impact, description, help }) => ({
+          id,
+          impact,
+          description,
+          help,
+        })),
+        '\n',
+        '  ',
+      )
+
+      return {
+        message: () => `Accessibility violations detected.\n\n${violationsMessage}`,
+        pass: false,
+      }
+    } else {
+      return {
+        message: () => `No accessibility violations detected!`,
+        pass: true,
+      }
+    }
+  },
+})
 
 module.exports = {
-  async preRender(page, context) {
+  async preRender(page) {
     await injectAxe(page)
   },
-  async postRender(page, context) {
-    await checkA11y(page, '#root', {
+  async postRender(page) {
+    const violations = await getViolations(page, '#root', {
       detailedReport: true,
       detailedReportOptions: {
         html: true,
       },
     })
+
+    expect(violations).toValidA11y()
   },
 }
