78d55827f775a95cf67b77c75f7a2dcd98838878
uknmr <y.kinmori@gmail.com>
2022-10-12 16:23:22 +0900

    fix: レビュー指摘修正。
    - SearchInput の prefix を I/F から削除
    - SearchInput を label 付きで提供

--- src/components/Input/SearchInput/SearchInput.tsx
+++ src/components/Input/SearchInput/SearchInput.tsx
@@ -4,11 +4,13 @@ import { ComponentProps } from 'react'
 import { InputWithTooltip } from '../InputWithTooltip'
 import { FaSearchIcon } from '../../Icon'
 
-type Props = Omit<ComponentProps<typeof InputWithTooltip>, 'tooltipMessage'> & {
+type Props = Omit<ComponentProps<typeof InputWithTooltip>, 'tooltipMessage' | 'prefix'> & {
   /** 入力欄の説明を紐付けるツールチップに表示するメッセージ */
   tooltipMessage: React.ReactNode
 }
 
 export const SearchInput: React.FC<Props> = ({ ...props }) => (
-  <InputWithTooltip {...props} prefix={<FaSearchIcon alt="検索" color="TEXT_GREY" />} />
+  <label>
+    <InputWithTooltip {...props} prefix={<FaSearchIcon alt="検索" color="TEXT_GREY" />} />
+  </label>
 )
