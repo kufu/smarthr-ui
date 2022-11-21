76b8d7200892b1f7b01a65b7d2545471e136cad3
uknmr <y.kinmori@gmail.com>
2022-11-22 06:50:23 +0900

    feat: 入力系コンポーネントに ref を渡せるよう修正

--- src/components/Select/useClassNames.ts
+++ src/components/Select/useClassNames.ts
@@ -1,10 +1,10 @@
-import { VFC, useMemo } from 'react'
+import { useMemo } from 'react'
 
 import { useClassNameGenerator } from '../../hooks/useClassNameGenerator'
 import { Select } from './Select'
 
 export const useClassNames = () => {
-  const generate = useClassNameGenerator((Select as VFC).displayName || 'Select')
+  const generate = useClassNameGenerator(Select.displayName || 'Select')
   return useMemo(
     () => ({
       wrapper: generate(),
