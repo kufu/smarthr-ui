37af5e598ae5221eda1aada2ce91ddc4bafaf441
KANAMORI Yu <y.kinmori@gmail.com>
2022-11-22 10:33:07 +0900

    feat: 入力系コンポーネントに ref を渡せるよう修正 (#2933)

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,4 +1,12 @@
-import React, { TextareaHTMLAttributes, VFC, useCallback, useEffect, useRef, useState } from 'react'
+import React, {
+  TextareaHTMLAttributes,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react'
 import styled, { css } from 'styled-components'
 
 import { Theme, useTheme } from '../../hooks/useTheme'
@@ -39,87 +47,97 @@ const getStringLength = (value: string | number | readonly string[]) => {
   return formattedValue.length - (formattedValue.match(surrogatePairs) || []).length
 }
 
-export const Textarea: VFC<Props & ElementProps> = ({
-  autoFocus,
-  maxLength,
-  width,
-  className = '',
-  autoResize = false,
-  maxRows = Infinity,
-  rows = 2,
-  onInput,
-  ...props
-}) => {
-  const theme = useTheme()
-  const ref = useRef<HTMLTextAreaElement>(null)
-  const currentValue = props.defaultValue || props.value
-  const [interimRows, setInterimRows] = useState(rows)
-  const [count, setCount] = useState(currentValue ? getStringLength(currentValue) : 0)
-  const textAreaWidth = typeof width === 'number' ? `${width}px` : width
-
-  useEffect(() => {
-    if (autoFocus && ref && ref.current) {
-      ref.current.focus()
-    }
-  }, [autoFocus])
-
-  const handleKeyup = useCallback((event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    setCount(getStringLength(event.currentTarget.value))
-  }, [])
-  const handleInput = useCallback(
-    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-      if (!autoResize) {
-        return onInput && onInput(e)
-      }
-
-      const previousRows = e.target.rows
-      // 消したことを検知できないので必ず初期化
-      e.target.rows = rows
-
-      const currentRows = Math.floor(
-        e.target.scrollHeight / (defaultHtmlFontSize * theme.leading.NORMAL),
-      )
-
-      if (previousRows === currentRows) {
-        e.target.rows = currentRows
-      } else if (maxRows < currentRows) {
-        // 最大まで達したとき高さが潰れないように代入
-        e.target.rows = maxRows
-      }
-
-      setInterimRows(currentRows < maxRows ? currentRows : maxRows)
-      onInput && onInput(e)
+export const Textarea = forwardRef<HTMLTextAreaElement, Props & ElementProps>(
+  (
+    {
+      autoFocus,
+      maxLength,
+      width,
+      className = '',
+      autoResize = false,
+      maxRows = Infinity,
+      rows = 2,
+      onInput,
+      ...props
     },
-    [autoResize, maxRows, onInput, rows, theme.leading.NORMAL],
-  )
+    ref,
+  ) => {
+    const theme = useTheme()
+    const textareaRef = useRef<HTMLTextAreaElement>(null)
+    const currentValue = props.defaultValue || props.value
+    const [interimRows, setInterimRows] = useState(rows)
+    const [count, setCount] = useState(currentValue ? getStringLength(currentValue) : 0)
+    const textAreaWidth = typeof width === 'number' ? `${width}px` : width
 
-  const classNames = useClassNames()
+    useImperativeHandle<HTMLTextAreaElement | null, HTMLTextAreaElement | null>(
+      ref,
+      () => textareaRef.current,
+    )
 
-  return (
-    <>
-      <StyledTextarea
-        {...props}
-        {...(maxLength ? { onKeyUp: handleKeyup } : {})}
-        textAreaWidth={textAreaWidth}
-        ref={ref}
-        themes={theme}
-        aria-invalid={props.error || undefined}
-        className={`${className} ${classNames.textarea}`}
-        rows={interimRows}
-        onInput={handleInput}
-      />
-      {maxLength && (
-        <Counter themes={theme} className={classNames.counter}>
-          あと
-          <span className={maxLength && maxLength - count <= 0 ? 'error' : ''}>
-            {maxLength - count}
-          </span>
-          文字
-        </Counter>
-      )}
-    </>
-  )
-}
+    useEffect(() => {
+      if (autoFocus && textareaRef && textareaRef.current) {
+        textareaRef.current.focus()
+      }
+    }, [autoFocus])
+
+    const handleKeyup = useCallback((event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+      setCount(getStringLength(event.currentTarget.value))
+    }, [])
+    const handleInput = useCallback(
+      (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        if (!autoResize) {
+          return onInput && onInput(e)
+        }
+
+        const previousRows = e.target.rows
+        // 消したことを検知できないので必ず初期化
+        e.target.rows = rows
+
+        const currentRows = Math.floor(
+          e.target.scrollHeight / (defaultHtmlFontSize * theme.leading.NORMAL),
+        )
+
+        if (previousRows === currentRows) {
+          e.target.rows = currentRows
+        } else if (maxRows < currentRows) {
+          // 最大まで達したとき高さが潰れないように代入
+          e.target.rows = maxRows
+        }
+
+        setInterimRows(currentRows < maxRows ? currentRows : maxRows)
+        onInput && onInput(e)
+      },
+      [autoResize, maxRows, onInput, rows, theme.leading.NORMAL],
+    )
+
+    const classNames = useClassNames()
+
+    return (
+      <>
+        <StyledTextarea
+          {...props}
+          {...(maxLength ? { onKeyUp: handleKeyup } : {})}
+          textAreaWidth={textAreaWidth}
+          ref={textareaRef}
+          themes={theme}
+          aria-invalid={props.error || undefined}
+          className={`${className} ${classNames.textarea}`}
+          rows={interimRows}
+          onInput={handleInput}
+        />
+        {maxLength && (
+          <Counter themes={theme} className={classNames.counter}>
+            あと
+            <span className={maxLength && maxLength - count <= 0 ? 'error' : ''}>
+              {maxLength - count}
+            </span>
+            文字
+          </Counter>
+        )}
+      </>
+    )
+  },
+)
 
 const StyledTextarea = styled.textarea<Props & { themes: Theme; textAreaWidth?: string | number }>`
   ${({
