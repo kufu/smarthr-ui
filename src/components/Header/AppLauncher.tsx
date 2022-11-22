aa3d4bbd1bf6db843439db352c113bdfff57f869
daxelbook <nao.enomoto@smarthr.co.jp>
2022-11-22 14:46:17 +0900

    feat: 叩き完成

--- src/components/Header/AppLauncher.tsx
+++ src/components/Header/AppLauncher.tsx
@@ -1,6 +1,10 @@
-import React, { HTMLAttributes } from 'react'
-import { Text } from '../Text'
-import { Stack } from '../Layout'
+import React, { ComponentProps, HTMLAttributes } from 'react'
+import styled, { css } from 'styled-components'
+import { Cluster, Stack } from '../Layout'
+import { Theme, useTheme } from '../../hooks/useTheme'
+import { TextLink } from '../TextLink'
+import { Heading } from '../Heading'
+import { FaExternalLinkAlt } from 'react-icons/fa'
 
 type Props = {
   apps: {
@@ -16,10 +20,91 @@ type Props = {
 type ElementProps = Omit<HTMLAttributes<HTMLElement>, keyof Props>
 
 export const AppLauncher: React.FC<Props & ElementProps> = ({ apps, urlToShowAll }) => {
+  const { base, roumu, jinmane, renkei, plus } = apps
+  const themes = useTheme()
   return (
-    <Stack>
-      {apps.base && <Text>yes</Text>}
-      <Text>{urlToShowAll}</Text>
-    </Stack>
+    <Wrapper themes={themes}>
+      {base && (
+        <Stack gap={0.5}>
+          <Heading type="subBlockTitle">基本機能</Heading>
+          <Stack as="ul" gap={0.5}>
+            {base.map((item, index) => (
+              <li key={index}>
+                <TextLink href={item.url} target={item.target} suffix={<FaExternalLinkAlt />}>
+                  {item.label}
+                </TextLink>
+              </li>
+            ))}
+          </Stack>
+        </Stack>
+      )}
+      <Cluster gap={1.5}>
+        {roumu && (
+          <Stack gap={0.5}>
+            <Heading type="subBlockTitle">人事労務</Heading>
+            <Stack as="ul" gap={0.5}>
+              {roumu.map((item, index) => (
+                <li key={index}>
+                  <TextLink href={item.url} target={item.target} suffix={<FaExternalLinkAlt />}>
+                    {item.label}
+                  </TextLink>
+                </li>
+              ))}
+            </Stack>
+          </Stack>
+        )}
+        {jinmane && (
+          <Stack gap={0.5}>
+            <Heading type="subBlockTitle">人材マネジメント</Heading>
+            <Stack as="ul" gap={0.5}>
+              {jinmane.map((item, index) => (
+                <li key={index}>
+                  <TextLink href={item.url} target={item.target} suffix={<FaExternalLinkAlt />}>
+                    {item.label}
+                  </TextLink>
+                </li>
+              ))}
+            </Stack>
+          </Stack>
+        )}
+      </Cluster>
+      {renkei && (
+        <Stack gap={0.5}>
+          <Heading type="subBlockTitle">連携</Heading>
+          <Stack as="ul" gap={0.5}>
+            {renkei.map((item, index) => (
+              <li key={index}>
+                <TextLink href={item.url} target={item.target} suffix={<FaExternalLinkAlt />}>
+                  {item.label}
+                </TextLink>
+              </li>
+            ))}
+          </Stack>
+        </Stack>
+      )}
+      {plus && (
+        <Stack gap={0.5}>
+          <Heading type="subBlockTitle">SmartHR Plus</Heading>
+          <Stack as="ul" gap={0.5}>
+            {plus.map((item, index) => (
+              <li key={index}>
+                <TextLink href={item.url} target={item.target} suffix={<FaExternalLinkAlt />}>
+                  {item.label}
+                </TextLink>
+              </li>
+            ))}
+          </Stack>
+        </Stack>
+      )}
+      <TextLink href={urlToShowAll} suffix={<FaExternalLinkAlt />} style={{ width: 'fit-content' }}>
+        {urlToShowAll}
+      </TextLink>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled(Stack)<{ themes: Theme }>(
+  ({ themes: { space } }) => css`
+    padding: ${space(1)};
+  `,
+)
--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -36,34 +36,70 @@ const launcher = {
   apps: {
     base: [
       {
-        label: 'hogehoge',
-        url: 'hoge',
+        label: 'ホーム',
+        url: 'http://',
+      },
+      {
+        label: '従業員リスト',
+        url: 'http://',
+      },
+      {
+        label: '共通設定',
+        url: 'http://',
       },
     ],
     roumu: [
       {
-        label: 'hoge',
-        url: 'hoge',
+        label: '電子申請',
+        url: 'http://',
+      },
+      {
+        label: '給与明細',
+        url: 'http://',
+      },
+      {
+        label: 'マイナンバー',
+        url: 'http://',
+      },
+      {
+        label: '文書配布',
+        url: 'http://',
       },
     ],
     jinmane: [
       {
-        label: 'hoge',
-        url: 'hoge',
+        label: '人事評価',
+        url: 'http://',
+      },
+      {
+        label: '分析レポート',
+        url: 'http://',
+      },
+      {
+        label: '従業員サーベイ',
+        url: 'http://',
       },
     ],
     renkei: [
       {
-        label: 'hoge',
-        url: 'hoge',
+        label: 'おまかせ はたラクサポート連携',
+        url: 'http://',
       },
     ],
     plus: [
       {
-        label: 'hoge',
-        url: 'hoge',
+        label: 'Smart打刻',
+        url: 'http://',
+      },
+      {
+        label: 'KING OF TIME',
+        url: 'http://',
+      },
+      {
+        label: '検診予約・結果管理システム',
+        url: 'http://',
       },
     ],
   },
-  urlToShowAll: 'hogehoge',
+  urlToShowAll: 'すべて見る',
 }
