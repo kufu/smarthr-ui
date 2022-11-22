80a2c34b0ee1ec443cb9727b45b9c2c93ccd41fd
AtsushiM <at.mizoue@gmail.com>
2022-11-22 10:37:22 +0900

    chore: date pickerのplaceholder optionを非推奨に変更

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -33,6 +33,10 @@ type Props = {
   to?: Date
   /** フォームを無効にするかどうか */
   disabled?: boolean
+  /**
+   * @deprecated placeholder属性は非推奨です。別途ヒント用要素を設置するか、それらの領域を確保出来ない場合はTooltipコンポーネントの利用を検討してください。
+   */
+  placeholder?: string
   /** フォームにエラーがあるかどうか */
   error?: boolean
   /** コンポーネントに適用するクラス名 */
