ad32a93754246744ff6ff200f6e6d36fa51d4a17
yamish123 <32166731+yamish123@users.noreply.github.com>
2022-12-05 18:26:18 +0900

    fix: remove export and use props with children

--- src/components/Table/EmptyTableBody.tsx
+++ src/components/Table/EmptyTableBody.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React, { PropsWithChildren } from 'react'
 import styled, { css } from 'styled-components'
 
 import { useSpacing } from '../../hooks/useSpacing'
@@ -6,12 +6,10 @@ import { Center, Gap } from '../Layout'
 
 import { Td } from './Td'
 
-export type Props = {
-  /** セルの内容 */
-  children?: ReactNode
+type Props = PropsWithChildren<{
   /** 境界とコンテンツの間の余白 */
   padding?: Gap
-}
+}>
 
 export const EmptyTableBody: React.FC<Props> = ({ children, padding = 4 }) => {
   return (
