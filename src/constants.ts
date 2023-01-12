dff34e866c9d0ad076c2a7f6d8ca5609a3e7128a
KANAMORI Yu <y.kinmori@gmail.com>
2023-01-12 11:10:14 +0900

    feat: グラフ色を追加 (#3023)

--- src/constants.ts
+++ src/constants.ts
@@ -14,3 +14,16 @@ export const VISUALLY_HIDDEN_STYLE = css`
   overflow: hidden;
 `
 export const FONT_FAMILY = 'system-ui, sans-serif'
+export const CHART_COLORS = [
+  'rgb(0, 196, 204)',
+  'rgb(255, 205, 0)',
+  'rgb(255, 145, 0)',
+  'rgb(230, 85, 55)',
+  'rgb(45, 75, 155)',
+  'rgb(45, 125, 240)',
+  'rgb(105, 215, 255)',
+  'rgb(75, 180, 125)',
+  'rgb(5, 135, 140)',
+  'rgb(0, 90, 100)',
+]
+export const OTHER_CHART_COLOR = 'rgb(235, 235, 235)'
