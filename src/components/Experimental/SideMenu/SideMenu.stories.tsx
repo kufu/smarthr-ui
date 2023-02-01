2f473d902e1a3b9328db679541f3f6df9c326489
uknmr <y.kinmori@gmail.com>
2023-02-01 16:52:03 +0900

    docs: SideMenu の Story を実態に合わせて背景色を追加

--- src/components/Experimental/SideMenu/SideMenu.stories.tsx
+++ src/components/Experimental/SideMenu/SideMenu.stories.tsx
@@ -1,26 +1,40 @@
 import { Story } from '@storybook/react'
 import React from 'react'
+import styled, { css } from 'styled-components'
 
 import { SideMenu } from './SideMenu'
 
 export default {
   title: 'Experimental（実験的）/SideMenu',
   component: SideMenu,
+  parameters: {
+    layout: 'fullscreen',
+    withTheming: true,
+  },
 }
 
 export const Default: Story = () => (
-  <SideMenu>
-    <SideMenu.Group name="基本設定">
-      <SideMenu.Item href="#">評価シート</SideMenu.Item>
-      <SideMenu.Item href="#" current>
-        評価ロール
-      </SideMenu.Item>
-      <SideMenu.Item href="#">評価フロー</SideMenu.Item>
-    </SideMenu.Group>
-    <SideMenu.Group name="その他の設定">
-      <SideMenu.Item href="#">評価項目の入力必須設定</SideMenu.Item>
-      <SideMenu.Item href="#">評価ロールの閲覧・編集権限設定</SideMenu.Item>
-      <SideMenu.Item href="#">評価対象者の表示項目設定</SideMenu.Item>
-    </SideMenu.Group>
-  </SideMenu>
+  <Wrapper>
+    <SideMenu>
+      <SideMenu.Group name="基本設定">
+        <SideMenu.Item href="#">評価シート</SideMenu.Item>
+        <SideMenu.Item href="#" current>
+          評価ロール
+        </SideMenu.Item>
+        <SideMenu.Item href="#">評価フロー</SideMenu.Item>
+      </SideMenu.Group>
+      <SideMenu.Group name="その他の設定">
+        <SideMenu.Item href="#">評価項目の入力必須設定</SideMenu.Item>
+        <SideMenu.Item href="#">評価ロールの閲覧・編集権限設定</SideMenu.Item>
+        <SideMenu.Item href="#">評価対象者の表示項目設定</SideMenu.Item>
+      </SideMenu.Group>
+    </SideMenu>
+  </Wrapper>
 )
+
+const Wrapper = styled.div`
+  ${({ theme: { color, space } }) => css`
+    background-color: ${color.BACKGROUND};
+    padding: ${space(2)};
+  `}
+`
