56622a319c452afb97272153220da98179483e42
yamish123 <32166731+yamish123@users.noreply.github.com>
2023-05-01 13:52:46 +0900

    chore: preview.tsx を型安全にする

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import { Title, Subtitle, Description, Primary, ArgsTable, Stories } from '@storybook/blocks';
 
 import { Reset } from 'styled-reset'
-import { Parameters } from '@storybook/react'
+import { Preview } from '@storybook/react'
 
 import { createTheme, CreatedTheme } from '../src/themes/createTheme'
 import { ThemeProvider as ShrThemeProvider } from '../src/themes/ThemeProvider'
@@ -11,89 +11,91 @@ import { ThemeProvider as SCThemeProvider, createGlobalStyle } from 'styled-comp
 import CssBaseLine from 'smarthr-normalize-css'
 import { defaultLeading, defaultColor } from '../src/'
 
-export const globalTypes = {
-  reset: {
-    name: 'Reset',
-    defaultValue: 'smarthr-normalize',
-    toolbar: {
-      items: [
-        { value: 'smarthr-normalize', title: 'smarthr-normalize' },
-        { value: 'styled-reset', title: 'styled-reset' },
-        { value: null, title: 'off' },
-      ],
+const preview: Preview = {
+  globalTypes: {
+    reset: {
+      name: 'Reset',
+      defaultValue: 'smarthr-normalize',
+      toolbar: {
+        items: [
+          { value: 'smarthr-normalize', title: 'smarthr-normalize' },
+          { value: 'styled-reset', title: 'styled-reset' },
+          { value: null, title: 'off' },
+        ],
+      },
     },
   },
+  parameters: {
+    options: {
+      isFullscreen: false,
+      isToolshown: true,
+      storySort: {
+        // 並び替え方針がないためアルファベット順
+        order: [
+          'Buttons（ボタン）',
+          'Data Display（データ表示）',
+          'Dialog（ダイアログ）',
+          'Forms（フォーム）',
+          'Layouts（レイアウト）',
+          'Media（メディア）',
+          'Navigation（ナビゲーション）',
+          'Page Templates（ページテンプレート）',
+          'States（状態）',
+          'Text（テキスト）',
+          'Experimental（実験的）'
+        ],
+      },
+    },
+    viewport: { viewports: INITIAL_VIEWPORTS },
+    docs: {
+      // ArgsTable は deprecated で、subcomponentsで複数コンポーネントの props を見せる機能は非推奨になった
+      // ここでは、一旦v6.5->v7アップデート時に後方互換を保つために独自のpageを設定している
+      // 参考: https://github.com/storybookjs/storybook/issues/20782#issuecomment-1482771013
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <ArgsTable />
+          <Stories includePrimary={false} />
+        </>
+      ),
+    },
+  },
+  decorators: [
+    (Story, context) => {
+      const resetStyle =
+        context.globals.reset === 'smarthr-normalize' ? (
+          <SmartHRGlobalStyle />
+        ) : context.globals.reset === 'styled-reset' ? (
+          <Reset />
+        ) : undefined
+      const theme = createTheme()
+      const ThemeProvider = callThemeProvider(context.parameters.withTheming, theme)
+      return (
+        <ThemeProvider>
+          <ShrThemeProvider theme={theme}>
+            {resetStyle}
+            <Story />
+          </ShrThemeProvider>
+        </ThemeProvider>
+      )
+    },
+  ]
 }
 
-export const parameters: Parameters = {
-  options: {
-    isFullscreen: false,
-    isToolshown: true,
-    storySort: {
-      // 並び替え方針がないためアルファベット順
-      order: [
-        'Buttons（ボタン）',
-        'Data Display（データ表示）',
-        'Dialog（ダイアログ）',
-        'Forms（フォーム）',
-        'Layouts（レイアウト）',
-        'Media（メディア）',
-        'Navigation（ナビゲーション）',
-        'Page Templates（ページテンプレート）',
-        'States（状態）',
-        'Text（テキスト）',
-        'Experimental（実験的）'
-      ],
-    },
-  },
-  viewport: { viewports: INITIAL_VIEWPORTS },
-  docs: {
-    // ArgsTable は deprecated で、subcomponentsで複数コンポーネントの props を見せる機能は非推奨になった
-    // ここでは、一旦v6.5->v7アップデート時に後方互換を保つために独自のpageを設定している
-    // 参考: https://github.com/storybookjs/storybook/issues/20782#issuecomment-1482771013
-    page: () => (
-      <>
-        <Title />
-        <Subtitle />
-        <Description />
-        <Primary />
-        <ArgsTable />
-        <Stories includePrimary={false} />
-      </>
-    ),
-  },
-}
+export default preview
 
 const callThemeProvider =
   (withThemeProvider: boolean, theme: CreatedTheme) =>
-  ({ children }) => {
+  ({ children }: { children: ReactNode }) => {
     if (withThemeProvider) {
       return <SCThemeProvider theme={theme}>{children}</SCThemeProvider>
     }
 
     return <>{children}</>
   }
-
-export const decorators = [
-  (Story, context) => {
-    const resetStyle =
-      context.globals.reset === 'smarthr-normalize' ? (
-        <SmartHRGlobalStyle />
-      ) : context.globals.reset === 'styled-reset' ? (
-        <Reset />
-      ) : undefined
-    const theme = createTheme()
-    const ThemeProvider = callThemeProvider(context.parameters.withTheming, theme)
-    return (
-      <ThemeProvider>
-        <ShrThemeProvider theme={theme}>
-          {resetStyle}
-          <Story />
-        </ShrThemeProvider>
-      </ThemeProvider>
-    )
-  },
-]
 
 const SmartHRGlobalStyle = createGlobalStyle`
   ${CssBaseLine}
