f92d857fc02da7fb039a4300f5818893b0096ed5
Mizoue Atsushi <at.mizoue@gmail.com>
2023-05-15 14:36:38 +0900

    feat: FormDialog & RemoteTriggerFormDialog を実装 (#3332)
    * feat: FormDialogを実装
    
    * feat: RemoteTriggerFormDialogを実装

--- e2e/components/Dialog/ActionDialog.test.ts
+++ e2e/components/Dialog/ActionDialog.test.ts
@@ -24,19 +24,3 @@ test('ダイアログが開閉できること', async (t) => {
     .expect(trigger.focused)
     .ok()
 })
-
-test('開いた状態で DOM に投入されたダイアログにフォーカスが移動すること', async (t) => {
-  const trigger = Selector('[data-test=opened-dialog-trigger]')
-  const focusTarget = Selector('[data-test=opened-dialog-focus-target]')
-  const closer = Selector('[data-test=opened-dialog').find('.smarthr-ui-Dialog-closeButton')
-
-  await t
-    // ダイアログを開いたとき、ダイアログ内のフォーカスターゲットがフォーカスされること
-    .click(trigger)
-    .expect(focusTarget.focused)
-    .ok()
-    // ダイアログを閉じたとき、トリガがフォーカスされること
-    .click(closer)
-    .expect(trigger.focused)
-    .ok()
-})
