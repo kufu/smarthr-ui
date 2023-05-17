159e12a4b0eaf821ef0de1f10afbda17fcb78068
yamish123 <32166731+yamish123@users.noreply.github.com>
2023-05-17 15:46:43 +0900

    chore: コンポーネントを分割

---
+++ src/components/Dropdown/FilterDropdown/FillterDropdownContent.tsx
@@ -0,0 +1,131 @@
+import React, { ReactNode, useMemo } from 'react'
+import styled, { css } from 'styled-components'
+
+import { DecoratorType, DecoratorsType, ResponseMessageType } from '@/types/props'
+
+import { Theme, useTheme } from '../../../hooks/useTheme'
+import { Button } from '../../Button'
+import { FaCheckCircleIcon, FaExclamationCircleIcon, FaUndoAltIcon } from '../../Icon'
+import { Cluster, Stack } from '../../Layout'
+import { DropdownCloser } from '../DropdownCloser'
+import { DropdownContent } from '../DropdownContent'
+import { DropdownScrollArea } from '../DropdownScrollArea'
+
+type Props = {
+  onApply: React.MouseEventHandler<HTMLButtonElement>
+  onCancel?: React.MouseEventHandler<HTMLButtonElement>
+  onReset?: React.MouseEventHandler<HTMLButtonElement>
+  children: ReactNode
+  decorators?: DecoratorsType<'applyButton' | 'cancelButton' | 'resetButton'>
+  responseMessage?: ResponseMessageType
+}
+
+const APPLY_BUTTON_TEXT = '適用'
+const CANCEL_BUTTON_TEXT = 'キャンセル'
+const RESET_BUTTON_TEXT = '絞り込み条件を解除'
+
+const executeDecorator = (defaultText: string, decorator: DecoratorType | undefined) =>
+  decorator?.(defaultText) || defaultText
+
+export const FilterDropdownContent: React.FC<Props> = ({
+  children,
+  onApply,
+  decorators,
+  onCancel,
+  onReset,
+  responseMessage,
+}) => {
+  const themes = useTheme()
+
+  const applyButton: ReactNode = useMemo(
+    () => executeDecorator(APPLY_BUTTON_TEXT, decorators?.applyButton),
+    [decorators],
+  )
+  const cancelButton: ReactNode = useMemo(
+    () => executeDecorator(CANCEL_BUTTON_TEXT, decorators?.cancelButton),
+    [decorators],
+  )
+  const resetButton: ReactNode = useMemo(
+    () => executeDecorator(RESET_BUTTON_TEXT, decorators?.resetButton),
+    [decorators],
+  )
+
+  const isRequestProcessing =
+    responseMessage !== undefined && responseMessage.status === 'processing'
+
+  return (
+    <DropdownContent controllable>
+      <DropdownScrollArea>
+        <ContentLayout themes={themes}>{children}</ContentLayout>
+      </DropdownScrollArea>
+      <ActionArea themes={themes}>
+        <Cluster gap={1} align="center" justify="space-between">
+          {onReset && (
+            <ResetButtonLayout themes={themes}>
+              <Button
+                variant="text"
+                size="s"
+                prefix={<FaUndoAltIcon />}
+                onClick={onReset}
+                disabled={isRequestProcessing}
+              >
+                {resetButton}
+              </Button>
+            </ResetButtonLayout>
+          )}
+
+          <RightButtonLayout>
+            <DropdownCloser>
+              <Button onClick={onCancel} disabled={isRequestProcessing}>
+                {cancelButton}
+              </Button>
+            </DropdownCloser>
+            <Button variant="primary" onClick={onApply} loading={isRequestProcessing}>
+              {applyButton}
+            </Button>
+          </RightButtonLayout>
+        </Cluster>
+        {responseMessage?.status === 'success' && (
+          <Message>
+            <FaCheckCircleIcon color={themes.color.MAIN} text={responseMessage.text} role="alert" />
+          </Message>
+        )}
+        {responseMessage?.status === 'error' && (
+          <Message>
+            <FaExclamationCircleIcon
+              color={themes.color.DANGER}
+              text={responseMessage.text}
+              role="alert"
+            />
+          </Message>
+        )}
+      </ActionArea>
+    </DropdownContent>
+  )
+}
+
+const ContentLayout = styled.div<{ themes: Theme }>`
+  ${({ themes: { space } }) => css`
+    padding: ${space(1.5)};
+  `}
+`
+const ActionArea = styled(Stack).attrs({ gap: 0.5 })<{ themes: Theme }>`
+  ${({ themes: { space, border } }) => css`
+    border-block-start: ${border.shorthand};
+    padding: ${space(1)} ${space(1.5)};
+  `}
+`
+const ResetButtonLayout = styled.div<{ themes: Theme }>`
+  ${({ themes: { space } }) => css`
+    margin-inline-start: ${space(-0.5)};
+  `}
+`
+const RightButtonLayout = styled(Cluster).attrs({
+  gap: { column: 1, row: 0.5 },
+  justify: 'flex-end',
+})`
+  margin-inline-start: auto;
+`
+const Message = styled.div`
+  text-align: right;
+`
--- src/components/Dropdown/FilterDropdown/FilterDropdown.tsx
+++ src/components/Dropdown/FilterDropdown/FilterDropdown.tsx
@@ -1,21 +1,19 @@
 import React, { ButtonHTMLAttributes, FC, ReactNode, useMemo } from 'react'
 import innerText from 'react-innertext'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 import { Theme, useTheme } from '../../../hooks/useTheme'
 import { DecoratorType, DecoratorsType, ResponseMessageType } from '../../../types/props'
 import { Button } from '../../Button'
-import { FaCheckCircleIcon, FaExclamationCircleIcon, FaFilterIcon, FaUndoAltIcon } from '../../Icon'
-import { Cluster, Stack } from '../../Layout'
+import { FaCheckCircleIcon, FaFilterIcon } from '../../Icon'
 import { Dropdown } from '../Dropdown'
-import { DropdownCloser } from '../DropdownCloser'
-import { DropdownContent } from '../DropdownContent'
-import { DropdownScrollArea } from '../DropdownScrollArea'
 import { DropdownTrigger } from '../DropdownTrigger'
+
+import { FilterDropdownContent } from './FillterDropdownContent'
 
 type Props = {
   isFiltered?: boolean
-  onApply: React.MouseEventHandler<HTMLButtonElement>
+  onApply: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, close: () => void) => void
   onCancel?: React.MouseEventHandler<HTMLButtonElement>
   onReset?: React.MouseEventHandler<HTMLButtonElement>
   children: ReactNode
@@ -29,9 +27,6 @@ type ElementProps = Omit<ButtonHTMLAttributes<HTMLButtonElement>, keyof Props>
 
 const STATUS_FILTERED_TEXT = '適用中'
 const TRIGGER_BUTTON_TEXT = '絞り込み'
-const APPLY_BUTTON_TEXT = '適用'
-const CANCEL_BUTTON_TEXT = 'キャンセル'
-const RESET_BUTTON_TEXT = '絞り込み条件を解除'
 
 const executeDecorator = (defaultText: string, decorator: DecoratorType | undefined) =>
   decorator?.(defaultText) || defaultText
@@ -56,24 +51,10 @@ export const FilterDropdown: FC<Props & ElementProps> = ({
     () => executeDecorator(TRIGGER_BUTTON_TEXT, decorators?.triggerButton),
     [decorators],
   )
-  const applyButton: ReactNode = useMemo(
-    () => executeDecorator(APPLY_BUTTON_TEXT, decorators?.applyButton),
-    [decorators],
-  )
-  const cancelButton: ReactNode = useMemo(
-    () => executeDecorator(CANCEL_BUTTON_TEXT, decorators?.cancelButton),
-    [decorators],
-  )
-  const resetButton: ReactNode = useMemo(
-    () => executeDecorator(RESET_BUTTON_TEXT, decorators?.resetButton),
-    [decorators],
-  )
   const filteredIconAriaLabel = useMemo(
     () => (hasStatusText ? undefined : innerText(status)),
     [status, hasStatusText],
   )
-  const isRequestProcessing =
-    responseMessage !== undefined && responseMessage.status === 'processing'
 
   return (
     <Dropdown>
@@ -93,59 +74,15 @@ export const FilterDropdown: FC<Props & ElementProps> = ({
         </Button>
       </DropdownTrigger>
       {hasStatusText && isFiltered ? <StatusText themes={themes}>{status}</StatusText> : null}
-      <DropdownContent controllable>
-        <DropdownScrollArea>
-          <ContentLayout themes={themes}>{children}</ContentLayout>
-        </DropdownScrollArea>
-        <ActionArea themes={themes}>
-          <Cluster gap={1} align="center" justify="space-between">
-            {onReset && (
-              <ResetButtonLayout themes={themes}>
-                <Button
-                  variant="text"
-                  size="s"
-                  prefix={<FaUndoAltIcon />}
-                  onClick={onReset}
-                  disabled={isRequestProcessing}
-                >
-                  {resetButton}
-                </Button>
-              </ResetButtonLayout>
-            )}
-
-            <RightButtonLayout>
-              <DropdownCloser>
-                <Button onClick={onCancel} disabled={isRequestProcessing}>
-                  {cancelButton}
-                </Button>
-              </DropdownCloser>
-              <DropdownCloser>
-                <Button variant="primary" onClick={onApply} loading={isRequestProcessing}>
-                  {applyButton}
-                </Button>
-              </DropdownCloser>
-            </RightButtonLayout>
-          </Cluster>
-          {responseMessage?.status === 'success' && (
-            <Message>
-              <FaCheckCircleIcon
-                color={themes.color.MAIN}
-                text={responseMessage.text}
-                role="alert"
-              />
-            </Message>
-          )}
-          {responseMessage?.status === 'error' && (
-            <Message>
-              <FaExclamationCircleIcon
-                color={themes.color.DANGER}
-                text={responseMessage.text}
-                role="alert"
-              />
-            </Message>
-          )}
-        </ActionArea>
-      </DropdownContent>
+      <FilterDropdownContent
+        decorators={decorators}
+        responseMessage={responseMessage}
+        onApply={onApply}
+        onCancel={onCancel}
+        onReset={onReset}
+      >
+        {children}
+      </FilterDropdownContent>
     </Dropdown>
   )
 }
@@ -166,29 +103,4 @@ const IsFilteredIconWrapper = styled.span<{ isFiltered: boolean; themes: Theme }
 const StatusText = styled.span<{ themes: Theme }>`
   margin-left: ${({ themes }) => themes.spacing.XXS};
   font-size: ${({ themes }) => themes.fontSize.S};
-`
-const ContentLayout = styled.div<{ themes: Theme }>`
-  ${({ themes: { space } }) => css`
-    padding: ${space(1.5)};
-  `}
-`
-const ActionArea = styled(Stack).attrs({ gap: 0.5 })<{ themes: Theme }>`
-  ${({ themes: { space, border } }) => css`
-    border-block-start: ${border.shorthand};
-    padding: ${space(1)} ${space(1.5)};
-  `}
-`
-const ResetButtonLayout = styled.div<{ themes: Theme }>`
-  ${({ themes: { space } }) => css`
-    margin-inline-start: ${space(-0.5)};
-  `}
-`
-const RightButtonLayout = styled(Cluster).attrs({
-  gap: { column: 1, row: 0.5 },
-  justify: 'flex-end',
-})`
-  margin-inline-start: auto;
-`
-const Message = styled.div`
-  text-align: right;
 `
