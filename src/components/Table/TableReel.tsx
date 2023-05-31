c5d68b5e780b1163b1e86b39bb637fe7c54c5900
shinichi.kogiso <kuusai1998@gmail.com>
2023-05-31 14:56:16 +0900

    fix: className周りを修正

--- src/components/Table/TableReel.tsx
+++ src/components/Table/TableReel.tsx
@@ -7,13 +7,21 @@ import { useReelShadow } from './useReelShadow'
 
 type ElementProps = Omit<HTMLAttributes<HTMLDivElement>, keyof PropsWithChildren>
 
-export const TableReel: React.FC<PropsWithChildren & ElementProps> = ({ children, ...props }) => {
+export const TableReel: React.FC<PropsWithChildren & ElementProps> = ({
+  children,
+  className,
+  ...props
+}) => {
   const { showShadow, tableWrapperRef } = useReelCells()
   const classNames = useClassNames()
 
   return (
-    <Shadow showShadow={showShadow} className={`${className}`}>
-      <Wrapper {...props} ref={tableWrapperRef} className={classNames.tableReel.wrapper}>
+    <Shadow showShadow={showShadow} className={`${classNames.tableReel.wrapper}`}>
+      <Wrapper
+        {...props}
+        ref={tableWrapperRef}
+        className={`${className} ${classNames.tableReel.inner}`}
+      >
         {children}
       </Wrapper>
     </Shadow>
