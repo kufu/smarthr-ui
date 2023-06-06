fcdac1e116a7a4261b3ad618567dd9d897e4f2b2
taconasu <smi.git.se@gmail.com>
2023-06-06 14:08:55 +0900

    fix: まだ不要な型があったため削除

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,3 @@
 export type { Gap, SeparateGap } from './Gap'
 export type { DecoratorsType, DecoratorType } from './Decorator'
 export type { ResponseMessageType } from './ResponseMessage'
-export type { Size } from './Size'
