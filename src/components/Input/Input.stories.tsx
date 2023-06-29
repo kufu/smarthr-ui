58742cc9735c339cfea89548b9b4f1dce95666c0
yamish123 <32166731+yamish123@users.noreply.github.com>
2023-06-29 10:15:09 +0900

    test: FormControlを使うようにする

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -3,6 +3,7 @@ import { Story } from '@storybook/react'
 import React from 'react'
 import styled from 'styled-components'
 
+import { FormControl } from '../FormControl'
 import { FaSearchIcon } from '../Icon'
 import { Stack } from '../Layout'
 
@@ -22,75 +23,83 @@ export default {
 export const All: Story = () => {
   return (
     <List>
-      <ListItem>
-        <label htmlFor="text">text</label>
-        <Input id="text" name="text" type="text" defaultValue="string" />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="number">number</label>
-        <Input id="number" name="number" type="number" defaultValue="1" />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="password">password</label>
-        <Input id="password" name="password" type="password" defaultValue="password" />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="width_with_percent">width (with %)</label>
-        <Input
-          id="width_with_percent"
-          name="width_with_percent"
-          defaultValue="width: 100%"
-          width="100%"
-        />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="width_with_px">width (with px)</label>
-        <Input id="width_with_px" name="width_with_px" defaultValue="width: 100px" width="100px" />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="onChange">onChange</label>
-        <Input id="onChange" name="onChange" onChange={action('onChange!!')} />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="onBlur">onBlur</label>
-        <Input id="onBlur" name="onBlur" onBlur={action('onBlur!!')} />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="readOnly">readonly</label>
-        <Input id="readOnly" name="readOnly" value="これは read-only な input テキスト" readOnly />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="disabled">disabled</label>
-        <Input
-          id="disabled"
-          name="disabled"
-          disabled={true}
-          defaultValue="これは disabled なテキスト"
-        />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="error">error</label>
-        <Input id="error" name="error" error={true} />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="disabledAndError">disabled and error</label>
-        <p>
-          <code>disabled</code>は<code>error</code>よりも優先されます。
-        </p>
-        <Input id="disabledAndError" name="disabledAndError" disabled={true} error={true} />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="prefix">prefix</label>
-        <Input id="prefix" name="prefix" prefix={<FaSearchIcon />} />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="suffix">suffix</label>
-        <Input id="suffix" name="suffix" suffix={<FaSearchIcon />} />
-      </ListItem>
-      <ListItem>
-        <label htmlFor="extending_style">extending style (width 50%)</label>
-        <StyledInput id="extending_style" name="extending_style" />
-      </ListItem>
+      <li>
+        <FormControl title="text">
+          <Input name="text" type="text" defaultValue="string" />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="number">
+          <Input name="number" type="number" defaultValue="1" />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="password">
+          <Input name="password" type="password" defaultValue="password" />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="width (with %)">
+          <Input name="width_with_percent" defaultValue="width: 100%" width="100%" />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="width (with px)">
+          <Input name="width_with_px" defaultValue="width: 100px" width="100px" />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="onChange">
+          <Input name="onChange" onChange={action('onChange!!')} />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="onBlur">
+          <Input name="onBlur" onBlur={action('onBlur!!')} />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="readOnly">
+          <Input name="readOnly" value="これは read-only な input テキスト" readOnly />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="disabled">
+          <Input name="disabled" disabled={true} defaultValue="これは disabled なテキスト" />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="error">
+          <Input name="error" error={true} />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl
+          title="disabled and error"
+          helpMessage={
+            <>
+              <code>disabled</code>は<code>error</code>よりも優先されます。
+            </>
+          }
+        >
+          <Input name="disabledAndError" disabled={true} error={true} />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="prefix">
+          <Input name="prefix" prefix={<FaSearchIcon />} />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="suffix">
+          <Input name="suffix" suffix={<FaSearchIcon />} />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl htmlFor="extending_style" title="extending style (width 50%)">
+          <StyledInput id="extending_style" name="extending_style" />
+        </FormControl>
+      </li>
     </List>
   )
 }
@@ -100,29 +109,28 @@ export const Currency: Story = () => {
   const [value, setValue] = React.useState('1234567890')
   return (
     <List>
-      <ListItem>
-        <label htmlFor="currency">currency (add comma to integer every 3 digits)</label>
-        <CurrencyInput
-          id="currency"
-          name="currency"
-          value={value}
-          onChange={(e) => {
-            action('changed')(e)
-            setValue(e.target.value)
-          }}
-          onFormatValue={(formatted) => {
-            action('formatted')(formatted)
-            setValue(formatted)
-          }}
-        />
-      </ListItem>
+      <li>
+        <FormControl title="currency (add comma to integer every 3 digits)">
+          <CurrencyInput
+            name="currency"
+            value={value}
+            onChange={(e) => {
+              action('changed')(e)
+              setValue(e.target.value)
+            }}
+            onFormatValue={(formatted) => {
+              action('formatted')(formatted)
+              setValue(formatted)
+            }}
+          />
+        </FormControl>
+      </li>
     </List>
   )
 }
 Currency.storyName = 'CurrencyInput'
 
 const List = styled(Stack).attrs({ as: 'ul' })``
-const ListItem = styled(Stack).attrs({ gap: 0.5, as: 'li', align: 'flex-start' })``
 const StyledInput = styled(Input)`
   width: 50%;
 `
