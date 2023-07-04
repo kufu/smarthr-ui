bd52f64984bfb8546be0f2bbf2bc539597118f1f
shun <32166731+yamish123@users.noreply.github.com>
2023-07-04 13:50:03 +0900

    test: a11y自動テストのテスト範囲を広げる (#3498)
    * test: a11y自動テストの範囲を広げる
    
    * test: InputのStoryのアクセシビリティ改善
    
    * test: FormControlを使うようにする

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,12 @@ module.exports = {
     'jsx-a11y/anchor-is-valid': 'warn',
     'jsx-a11y/click-events-have-key-events': 'warn',
     'jsx-a11y/no-static-element-interactions': 'warn',
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        controlComponents: ['Input', 'InputWithTooltip'],
+      },
+    ],
     'smarthr/a11y-anchor-has-href-attribute': 'error',
     'smarthr/a11y-prohibit-input-placeholder': 'error',
     'smarthr/require-barrel-import': 0,
