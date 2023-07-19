25497609d6cf7a2025dd33e935302a138c95a6d6
Mizoue Atsushi <at.mizoue@gmail.com>
2023-07-19 11:36:16 +0900

    feat: RemoteTrigger系Dialogに onToggle, onOpen, onCloseオプションを追加する (#3565)

--- src/components/Dialog/useRemoteTrigger.ts
+++ src/components/Dialog/useRemoteTrigger.ts
@@ -5,11 +5,17 @@ export const TRIGGER_EVENT = 'smarthr-ui:remote-dialog-trigger-dispatch'
 export function useRemoteTrigger({
   onClickClose: orgOnClickClose,
   onPressEscape: orgOnPressEscape,
+  onToggle,
+  onOpen,
+  onClose,
   id,
 }: {
   id: string
   onClickClose?: (close: () => void) => void
   onPressEscape?: (close: () => void) => void
+  onToggle?: (isOpen: boolean) => void
+  onOpen?: () => void
+  onClose?: () => void
 }) {
   const [isOpen, setIsOpen] = useState(false)
 
@@ -46,6 +52,21 @@ export function useRemoteTrigger({
       document.removeEventListener(TRIGGER_EVENT, handler)
     }
   }, [id])
+
+  useEffect(() => {
+    if (onToggle) {
+      onToggle(isOpen)
+    }
+    if (onOpen && isOpen) {
+      onOpen()
+    }
+    if (onClose && !isOpen) {
+      onClose()
+    }
+    // HINT: 再レンダリング際にhookを利用していないonToggleなどが渡されると意図せず発火してしまう場合がありえるため
+    // onToggle, onOpen, onClose を depsに含めていません
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpen])
 
   return {
     isOpen,
