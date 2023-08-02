970a411b86156eda947dcd04775eacd31461ddd5
uknmr <y.kinmori@gmail.com>
2023-08-02 15:37:17 +0900

    chore: smarthr-ui-preset を追加

---
+++ smarthr-ui-preset.js
@@ -0,0 +1,16 @@
+// この preset を各プロダクトでも読み込んでもらう想定
+module.exports = {
+  presets: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// 必要そうなトークン from Text
+// font-size
+// font-weight
+// line-height
+// font-style
+// white-space
+// color
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('./smarthr-ui-preset.js')],
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 }
