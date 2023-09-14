1372b5dcae9520d0f733db61057ad69d7354de29
uknmr <y.kinmori@gmail.com>
2023-09-14 11:17:11 +0900

    fix: NewText を Text に置き換え

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,7 +1,7 @@
 import { StoryFn } from '@storybook/react'
 import React from 'react'
 
-import { NewText, Text } from './Text'
+import { Text } from './Text'
 
 export default {
   title: 'Text（テキスト）/Text',
@@ -10,30 +10,30 @@ export default {
 
 export const Default: StoryFn = () => (
   <>
-    <NewText as="h1" size="XXL" leading="TIGHT">
+    <Text as="h1" size="XXL" leading="TIGHT">
       &lt;Text&gt;
-    </NewText>
-    <NewText as="p">
+    </Text>
+    <Text as="p">
       デフォルトの出力要素は <code>span</code> です。
-    </NewText>
-    <NewText as="p">
+    </Text>
+    <Text as="p">
       styled-components と同じく <code>as</code> で要素を差し替えられます。
-    </NewText>
-    <NewText as="p">
-      <NewText color="TEXT_GREY">色</NewText>や<NewText bold>ウェイト</NewText>、
-      <NewText whiteSpace="nowrap">ホワイトスペース</NewText>を変えられます。
-    </NewText>
-    <NewText as="p">
-      <code>emphasis</code> を渡すとそのテキストは<NewText emphasis>強調</NewText>を示し、
+    </Text>
+    <Text as="p">
+      <Text color="TEXT_GREY">色</Text>や<Text bold>ウェイト</Text>、
+      <Text whiteSpace="nowrap">ホワイトスペース</Text>を変えられます。
+    </Text>
+    <Text as="p">
+      <code>emphasis</code> を渡すとそのテキストは<Text emphasis>強調</Text>を示し、
       <code>em</code>
       要素の太字装飾で出力します。
-    </NewText>
-    <NewText as="p">
-      <NewText emphasis>入れ子</NewText>もできますが、
-      <NewText color="TEXT_LINK" bold>
+    </Text>
+    <Text as="p">
+      <Text emphasis>入れ子</Text>もできますが、
+      <Text color="TEXT_LINK" bold>
         Valid
-      </NewText>
+      </Text>
       な HTML になるよう注意してください。
-    </NewText>
+    </Text>
   </>
 )
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,43 +1,5 @@
-import React, { CSSProperties, HTMLAttributes, PropsWithChildren } from 'react'
-import styled, { css } from 'styled-components'
+import React, { PropsWithChildren } from 'react'
 import { VariantProps, tv } from 'tailwind-variants'
-
-import { useTheme } from '../../hooks/useTheme'
-import { TextColors } from '../../themes/createColor'
-import { FontSizes } from '../../themes/createFontSize'
-import { Leadings } from '../../themes/createLeading'
-
-export type TextProps = {
-  size?: FontSizes
-  weight?: CSSProperties['fontWeight']
-  italic?: boolean
-  color?: TextColors | 'inherit'
-  leading?: Leadings
-  whiteSpace?: CSSProperties['whiteSpace']
-  emphasis?: boolean
-}
-type ElementProps = Omit<HTMLAttributes<HTMLElement>, keyof TextProps>
-
-export type Props = TextProps &
-  ElementProps & {
-    as?: string | React.ComponentType<any> | undefined
-    children: React.ReactNode
-  }
-
-/**
- * @param [size] フォントサイズの抽象値（font-size）
- * @param [weight] フォントウェイト（font-weight）
- * @param [italic] 斜体にするかどうかの真偽値（font-style: italic）
- * @param [color] 色。初期値は inherit（color、）
- * @param [leading] 行送りの抽象値（line-height）
- * @param [whiteSpace] ホワイトスペース（white-space）
- * @param [emphasis] 強調するかどうかの真偽値。指定すると em 要素になる
- * @param [as] テキストコンポーネントの HTML タグ名。初期値は span
- * @param [children]
- */
-export const Text: React.FC<Props> = ({ color, as = 'span', ...props }) => (
-  <Wrapper {...props} $color={color} as={props.emphasis ? 'em' : as} />
-)
 
 const text = tv({
   variants: {
@@ -82,45 +44,16 @@ const text = tv({
 })
 
 // VariantProps を使うとコメントが書けない〜🥹
-type NewTextVariants = VariantProps<typeof text>
-type NewTextProps = PropsWithChildren<
-  NewTextVariants & {
-    /** テキストコンポーネントの HTML タグ名。初期値は span */
-    as?: string | React.ComponentType<any> | undefined
-    /** 強調するかどうかの真偽値。指定すると em 要素になる */
-    emphasis?: boolean
-  }
->
+export type TextProps = VariantProps<typeof text> & {
+  /** テキストコンポーネントの HTML タグ名。初期値は span */
+  as?: string | React.ComponentType<any> | undefined
+  /** 強調するかどうかの真偽値。指定すると em 要素になる */
+  emphasis?: boolean
+}
 
-export const NewText: React.FC<NewTextProps> = ({
+export const Text: React.FC<PropsWithChildren<TextProps>> = ({
   bold,
   emphasis,
   as: Component = emphasis ? 'em' : 'span',
   ...props
 }) => <Component {...props} className={text({ ...props, bold: emphasis || bold })} />
-
-const Wrapper = styled.span<
-  Omit<TextProps, 'color'> & {
-    $color: TextProps['color']
-  }
->(
-  ({
-    size = 'M',
-    weight = 'normal',
-    italic,
-    $color = 'inherit',
-    leading = 'NORMAL',
-    whiteSpace,
-    emphasis,
-  }) => {
-    const { color: shrColor, fontSize, leading: shrLeading } = useTheme()
-    return css`
-      ${whiteSpace && `white-space: ${whiteSpace};`}
-      font-size: ${fontSize[size]};
-      line-height: ${shrLeading[leading]};
-      font-weight: ${emphasis ? 'bold' : weight};
-      ${italic && `font-style: italic;`}
-      color: ${$color === 'inherit' ? $color : shrColor[$color]};
-    `
-  },
-)
