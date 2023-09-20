363145385bbf9085ca44322f3613232591f04808
tomof <fffjt007@gmail.com>
2023-09-20 14:14:37 +0900

    chore: フォーカスがあたったボタンのテスト

--- src/components/Button/Button.reg.stories.tsx
+++ src/components/Button/Button.reg.stories.tsx
@@ -1,6 +1,6 @@
 import { action } from '@storybook/addon-actions'
 import { StoryFn } from '@storybook/react'
-import { userEvent } from '@storybook/testing-library'
+import { userEvent, within } from '@storybook/testing-library'
 import React from 'react'
 
 import { Cluster, Stack } from '../Layout'
@@ -37,4 +37,29 @@ _ButtonHover.parameters = {
 _ButtonHover.play = async ({ canvasElement }) => {
   const button = canvasElement.querySelector('.smarthr-ui-Button')
   await userEvent.hover(button!)
+}
+
+export const _ButtonFocused: StoryFn = () => (
+  <div>
+    <dt>Default</dt>
+    <dd>
+      <Stack>
+        <Cluster>
+          <Button variant="primary" onClick={action('clicked')}>
+            ボタン
+          </Button>
+        </Cluster>
+      </Stack>
+    </dd>
+  </div>
+)
+
+_ButtonFocused.parameters = {
+  controls: { hideNoControlsWarning: true },
+}
+
+_ButtonFocused.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  const button = canvas.getByRole('button')
+  await button.focus()
 }
