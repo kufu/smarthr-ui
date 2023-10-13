4e7e258b87e54427423f324a5a9a4cbf47f7482c
uknmr <y.kinmori@gmail.com>
2023-10-13 17:10:54 +0900

    fix: jsx-a11y/click-events-have-key-events と jsx-a11y/no-static-element-interactions を修正

--- src/components/Dropdown/DropdownTrigger.tsx
+++ src/components/Dropdown/DropdownTrigger.tsx
@@ -1,4 +1,12 @@
-import React, { ComponentProps, PropsWithChildren, useContext, useEffect, useMemo } from 'react'
+import React, {
+  ComponentProps,
+  MouseEvent,
+  PropsWithChildren,
+  ReactElement,
+  useContext,
+  useEffect,
+  useMemo,
+} from 'react'
 import { tv } from 'tailwind-variants'
 
 import { tabbable } from '../../libs/tabbable'
@@ -28,41 +36,32 @@ export const DropdownTrigger: React.FC<Props> = ({ children, className }) => {
     })
   }, [triggerElementRef, active, contentId])
 
+  let foundFirstElement = false
+
   return (
-    <div
-      ref={triggerElementRef}
-      onClick={(e) => {
-        // 引き金となる要素が disabled な場合は発火させない
-        if (includeDisabledTrigger(children)) {
-          return
+    <div ref={triggerElementRef} className={styles}>
+      {React.Children.map(children, (child) => {
+        if (foundFirstElement || !React.isValidElement(child)) {
+          return child
         }
 
-        const rect = e.currentTarget.getBoundingClientRect()
-        onClickTrigger({
-          top: rect.top,
-          right: rect.right,
-          bottom: rect.bottom,
-          left: rect.left,
+        foundFirstElement = true
+
+        return React.cloneElement(child as ReactElement, {
+          onClick: (e: MouseEvent) => {
+            // 引き金となる要素が disabled な場合は発火させない
+            if (includeDisabledTrigger(children)) {
+              return
+            }
+
+            const { top, right, bottom, left } = e.currentTarget.getBoundingClientRect()
+            onClickTrigger({ top, right, bottom, left })
+
+            if (child.props.onClick) {
+              child.props.onClick(e)
+            }
+          },
         })
-      }}
-      className={styles}
-    >
-      {React.Children.map(children, (child: any) => {
-        const props = child.props ? child.props : {}
-        const { className: classNameProps = '' } = props
-
-        switch (typeof child) {
-          case 'string':
-            return child
-
-          case 'object':
-            return React.cloneElement(child, {
-              className: `${active ? 'active' : ''} ${classNameProps}`,
-            })
-
-          default:
-            return null
-        }
       })}
     </div>
   )
