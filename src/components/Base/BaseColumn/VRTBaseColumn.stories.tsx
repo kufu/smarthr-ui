40e54240ca9087c2646fa8a3eee4f448788fdd39
tomof <fffjt007@gmail.com>
2023-10-25 13:54:58 +0900

    chore: BaseColumnのVRT用Storyを追加

---
+++ src/components/Base/BaseColumn/VRTBaseColumn.stories.tsx
@@ -0,0 +1,32 @@
+import { StoryFn } from '@storybook/react'
+import * as React from 'react'
+import styled from 'styled-components'
+
+import { InformationPanel } from '../../InformationPanel'
+
+import { BaseColumn } from './BaseColumn'
+import { All } from './BaseColumn.stories'
+
+export default {
+  title: 'Data Display（データ表示）/BaseColumn',
+  component: BaseColumn,
+  parameters: {
+    withTheming: true,
+  },
+}
+
+export const VRTBaseForcedColors: StoryFn = () => (
+  <>
+    <VRTInformationPanel title="VRT 用の Story です" togglable={false}>
+      Chromatic 上では強制カラーモードで表示されます
+    </VRTInformationPanel>
+    <All />
+  </>
+)
+VRTBaseForcedColors.parameters = {
+  chromatic: { forcedColors: 'active' },
+}
+
+const VRTInformationPanel = styled(InformationPanel)`
+  margin-bottom: 24px;
+`
