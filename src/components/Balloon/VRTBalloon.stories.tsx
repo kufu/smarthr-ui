855ce5447b5c197eae238b3cd9c77caa652345db
tomof <fffjt007@gmail.com>
2023-10-26 09:34:38 +0900

    chore: BalloonのVRT用Storyを追加 (#3895)

---
+++ src/components/Balloon/VRTBalloon.stories.tsx
@@ -0,0 +1,33 @@
+import { StoryFn } from '@storybook/react'
+import * as React from 'react'
+import styled from 'styled-components'
+
+import { InformationPanel } from '../InformationPanel'
+
+import { Balloon } from './Balloon'
+import { All } from './Balloon.stories'
+
+export default {
+  title: 'Data Display（データ表示）/Balloon',
+  component: Balloon,
+  parameters: {
+    withTheming: true,
+  },
+}
+
+export const VRTForcedColors: StoryFn = () => (
+  <>
+    <VRTInformationPanel title="VRT 用の Story です" togglable={false}>
+      Chromatic 上では強制カラーモードで表示されます
+    </VRTInformationPanel>
+    <All />
+  </>
+)
+
+VRTForcedColors.parameters = {
+  chromatic: { forcedColors: 'active' },
+}
+
+const VRTInformationPanel = styled(InformationPanel)`
+  margin-bottom: 24px;
+`
