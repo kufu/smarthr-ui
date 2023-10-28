4b203f1d05043b5f595a7a4cc2f1248c9c92fa3b
atzzCokeK <atsushi1230.sdi@gmail.com>
2023-10-28 14:40:44 +0900

    chore: fix

--- src/components/VisuallyHiddenText/VisuallyHiddenText.tsx
+++ src/components/VisuallyHiddenText/VisuallyHiddenText.tsx
@@ -1,13 +1,17 @@
-import React, { HTMLAttributes } from 'react'
+import React, { ComponentProps } from 'react'
 
-export const VisuallyHiddenText: React.FC<HTMLAttributes<HTMLSpanElement>> = ({ children }) => (
-  <span
+type Props = ComponentProps<'span'> & {
+  /** テキストコンポーネントの HTML タグ名。初期値は span */
+  as?: string | React.ComponentType<any>
+}
+
+export const VisuallyHiddenText: React.FC<Props> = ({ as: Component = 'span', ...others }) => (
+  <Component
+    {...others}
     style={{
       clipPath: 'inset(100%)',
       clip: 'rect(0 0 0 0)',
     }}
     className="shr-absolute shr-left-0 shr-top-[-1px] shr-h-[1px] shr-w-[1px] shr-overflow-hidden shr-whitespace-nowrap shr-border-0 shr-p-0"
-  >
-    {children}
-  </span>
+  />
 )
