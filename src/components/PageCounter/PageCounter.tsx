b5ad468e6dc5a41fe88684ef8681aca425e055da
atzzCokeK <atsushi1230.sdi@gmail.com>
2023-10-30 12:45:55 +0900

    chore: apply format

--- src/components/PageCounter/PageCounter.tsx
+++ src/components/PageCounter/PageCounter.tsx
@@ -12,24 +12,24 @@ type Props = {
 }
 
 export const PageCounter: React.FC<Props> = ({ start, end, total = 0 }) => (
-    <Cluster inline align="baseline" className={wrapperClassName}>
-      {total > 0 && (
-        <Cluster as="span" gap={0.25} inline align="baseline" className={wrapperClassName}>
-          <Text weight="bold" as="b">
-            {total.toLocaleString()}
-          </Text>
-          件中
-        </Cluster>
-      )}
+  <Cluster inline align="baseline" className={wrapperClassName}>
+    {total > 0 && (
       <Cluster as="span" gap={0.25} inline align="baseline" className={wrapperClassName}>
         <Text weight="bold" as="b">
-          {start.toLocaleString()}
+          {total.toLocaleString()}
         </Text>
-        –
-        <Text weight="bold" as="b">
-          {end.toLocaleString()}
-        </Text>
-        件
+        件中
       </Cluster>
+    )}
+    <Cluster as="span" gap={0.25} inline align="baseline" className={wrapperClassName}>
+      <Text weight="bold" as="b">
+        {start.toLocaleString()}
+      </Text>
+      –
+      <Text weight="bold" as="b">
+        {end.toLocaleString()}
+      </Text>
+      件
     </Cluster>
-  )
+  </Cluster>
+)
