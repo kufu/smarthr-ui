c994c83d370ea29095f44311928d2c0499d67307
uknmr <y.kinmori@gmail.com>
2023-11-05 19:53:18 +0900

    refactor: Base を Tailwind CSS 化

--- src/components/Base/Base.tsx
+++ src/components/Base/Base.tsx
@@ -1,25 +1,101 @@
-import React, { CSSProperties, HTMLAttributes, PropsWithChildren, forwardRef, useMemo } from 'react'
-import styled, { css } from 'styled-components'
-
-import { useSpacing } from '../../hooks/useSpacing'
-import { Theme, useTheme } from '../../hooks/useTheme'
-
-import { useClassNames } from './useClassNames'
+import React, { ComponentPropsWithRef, PropsWithChildren, forwardRef, useMemo } from 'react'
+import { VariantProps, tv } from 'tailwind-variants'
 
 import type { Gap } from '../../types'
 
-type Props = PropsWithChildren<{
-  /** 境界とコンテンツの間の余白 */
-  padding?: Gap | SeparatePadding
-  /** 角丸のサイズ */
-  radius?: 's' | 'm'
-  /** コンテンツが要素内に収まらない場合の処理方法 */
-  overflow?:
-    | CSSProperties['overflow']
-    | { x: CSSProperties['overflowX']; y: CSSProperties['overflowY'] }
-  /** レイヤの重なり方向の高さ（影の付き方に影響する） */
-  layer?: LayerKeys
-}>
+const base = tv({
+  base: 'smarthr-ui-Base shr-bg-white contrast-more:shr-border-highContrast',
+  variants: {
+    paddingBlock: {
+      0: 'shr-py-0',
+      0.25: 'shr-py-0.25',
+      0.5: 'shr-py-0.5',
+      0.75: 'shr-py-0.75',
+      1: 'shr-py-1',
+      1.25: 'shr-py-1.25',
+      1.5: 'shr-py-1.5',
+      2: 'shr-py-2',
+      2.5: 'shr-py-2.5',
+      3: 'shr-py-3',
+      3.5: 'shr-py-3.5',
+      4: 'shr-py-4',
+      8: 'shr-py-8',
+      X3S: 'shr-py-0.25',
+      XXS: 'shr-py-0.5',
+      XS: 'shr-py-1',
+      S: 'shr-py-1.5',
+      M: 'shr-py-2',
+      L: 'shr-py-2.5',
+      XL: 'shr-py-3',
+      XXL: 'shr-py-3.5',
+      X3L: 'shr-py-4',
+    } as { [key in Gap]: string },
+    paddingInline: {
+      0: 'shr-px-0',
+      0.25: 'shr-px-0.25',
+      0.5: 'shr-px-0.5',
+      0.75: 'shr-px-0.75',
+      1: 'shr-px-1',
+      1.25: 'shr-px-1.25',
+      1.5: 'shr-px-1.5',
+      2: 'shr-px-2',
+      2.5: 'shr-px-2.5',
+      3: 'shr-px-3',
+      3.5: 'shr-px-3.5',
+      4: 'shr-px-4',
+      8: 'shr-px-8',
+      X3S: 'shr-px-0.25',
+      XXS: 'shr-px-0.5',
+      XS: 'shr-px-1',
+      S: 'shr-px-1.5',
+      M: 'shr-px-2',
+      L: 'shr-px-2.5',
+      XL: 'shr-px-3',
+      XXL: 'shr-px-3.5',
+      X3L: 'shr-px-4',
+    } as { [key in Gap]: string },
+    radius: {
+      s: 'shr-rounded-m',
+      m: 'shr-rounded-l',
+    },
+    overflowBlock: {
+      visible: 'shr-overflow-y-visible',
+      hidden: 'shr-overflow-y-hidden',
+      clip: 'shr-overflow-y-clip',
+      scroll: 'shr-overflow-y-scroll',
+      auto: 'shr-overflow-y-auto',
+    },
+    overflowInline: {
+      visible: 'shr-overflow-x-visible',
+      hidden: 'shr-overflow-x-hidden',
+      clip: 'shr-overflow-x-clip',
+      scroll: 'shr-overflow-x-scroll',
+      auto: 'shr-overflow-x-auto',
+    },
+    layer: {
+      0: 'shr-shadow-layer-0',
+      1: 'shr-shadow-layer-1',
+      2: 'shr-shadow-layer-2',
+      3: 'shr-shadow-layer-3',
+      4: 'shr-shadow-layer-4',
+    },
+  },
+})
+
+type Overflow = 'visible' | 'hidden' | 'clip' | 'scroll' | 'auto'
+
+type Props = PropsWithChildren<
+  Omit<
+    VariantProps<typeof base>,
+    'paddingBlock' | 'paddingInline' | 'overflowBlock' | 'overflowInline'
+  > & {
+    /** 境界とコンテンツの間の余白 */
+    padding?: Gap | SeparatePadding
+    /** コンテンツが要素内に収まらない場合の処理方法 */
+    overflow?: Overflow | { x: Overflow; y: Overflow }
+    as?: string | React.ComponentType<any>
+  }
+>
 
 export type LayerKeys = keyof typeof layerMap
 
@@ -36,79 +112,31 @@ type SeparatePadding = {
   inline?: Gap
 }
 
-export type ElementProps = Omit<HTMLAttributes<HTMLDivElement>, keyof Props>
-
-const separatePadding = (padding: Props['padding']) => {
-  if (padding instanceof Object) {
-    return {
-      block: padding.block,
-      inline: padding.inline,
-    }
-  }
-
-  return {
-    block: padding,
-    inline: padding,
-  }
-}
+export type ElementProps = Omit<ComponentPropsWithRef<'div'>, keyof Props>
 
 export const Base = forwardRef<HTMLDivElement, Props & ElementProps>(
-  ({ padding, radius = 'm', overflow, layer = 1, className = '', ...props }, ref) => {
-    const themes = useTheme()
-    const classNames = useClassNames()
+  (
+    { padding, radius = 'm', overflow, layer = 1, as: Component = 'div', className, ...props },
+    ref,
+  ) => {
+    const styles = useMemo(() => {
+      const paddingBlock = padding instanceof Object ? padding.block : padding
+      const paddingInline = padding instanceof Object ? padding.inline : padding
 
-    const $padding = separatePadding(padding)
-    const $radius = useMemo(() => {
-      switch (radius) {
-        case 's':
-          return themes.radius.m
-        case 'm':
-          return themes.radius.l
-      }
-    }, [radius, themes.radius.l, themes.radius.m])
+      const overflowBlock = overflow instanceof Object ? overflow.y : overflow
+      const overflowInline = overflow instanceof Object ? overflow.x : overflow
 
-    return (
-      <Wrapper
-        {...props}
-        className={`${className} ${classNames.base.wrapper}`}
-        themes={themes}
-        $padding={$padding}
-        $radius={$radius}
-        $overflow={overflow}
-        $layer={layerMap[layer]}
-        ref={ref}
-      />
-    )
+      return base({
+        paddingBlock,
+        paddingInline,
+        radius,
+        overflowBlock,
+        overflowInline,
+        layer,
+        className,
+      })
+    }, [className, layer, overflow, padding, radius])
+
+    return <Component {...props} className={styles} ref={ref} />
   },
 )
-
-const Wrapper = styled.div<{
-  themes: Theme
-  $padding: { block?: Gap; inline?: Gap }
-  $radius: string
-  $overflow: Props['overflow']
-  $layer: (typeof layerMap)[LayerKeys]
-}>`
-  ${({ themes: { border, color, shadow }, $padding, $radius, $overflow, $layer }) => css`
-    box-shadow: ${shadow[$layer]};
-    border-radius: ${$radius};
-    background-color: ${color.WHITE};
-
-    @media (prefers-contrast: more) {
-      & {
-        border: ${border.highContrast};
-      }
-    }
-
-    ${$padding.block && `padding-block: ${useSpacing($padding.block)};`}
-    ${$padding.inline && `padding-inline: ${useSpacing($padding.inline)};`}
-    ${$overflow &&
-    ($overflow instanceof Object
-      ? css`
-          overflow: ${$overflow.x} ${$overflow.y};
-        `
-      : css`
-          overflow: ${$overflow};
-        `)}
-  `}
-`
