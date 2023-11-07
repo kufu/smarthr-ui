721c5ed072a527a73a8324c3e9ca0d2edb209b4d
uknmr <y.kinmori@gmail.com>
2023-11-07 10:09:17 +0900

    refactor: DialogContentOverlap を Tailwind CSS 化

--- src/components/Dialog/DialogOverlap.tsx
+++ src/components/Dialog/DialogOverlap.tsx
@@ -1,17 +1,35 @@
-import React, { FC, PropsWithChildren, ReactNode, useEffect, useState } from 'react'
+import React, { FC, PropsWithChildren, ReactNode, useEffect, useMemo, useState } from 'react'
 import { CSSTransition } from 'react-transition-group'
-import styled from 'styled-components'
-
-import { Theme, useTheme } from '../../hooks/useTheme'
+import { tv } from 'tailwind-variants'
 
 type Props = PropsWithChildren<{
   isOpen: boolean
 }>
 
-const transitionClassName = 'shr-dialog-transition'
+const dialogOverlap = tv({
+  base: [
+    'shr-absolute',
+    'shr-z-overlap-base',
+    '[&.shr-dialog-transition-appear]:shr-opacity-0',
+    '[&.shr-dialog-transition-appear-active]:shr-transition-[opacity]',
+    '[&.shr-dialog-transition-appear-active]:shr-duration-500',
+    '[&.shr-dialog-transition-appear-active]:shr-ease-in-out',
+    '[&.shr-dialog-transition-appear-active]:shr-opacity-100',
+    '[&.shr-dialog-transition-enter]:shr-opacity-0',
+    '[&.shr-dialog-transition-enter-active]:shr-transition-[opacity]',
+    '[&.shr-dialog-transition-enter-active]:shr-duration-300',
+    '[&.shr-dialog-transition-enter-active]:shr-ease-in-out',
+    '[&.shr-dialog-transition-enter-active]:shr-opacity-100',
+    '[&.shr-dialog-transition-exit]:shr-opacity-100',
+    '[&.shr-dialog-transition-exit-active]:shr-transition-[opacity]',
+    '[&.shr-dialog-transition-exit-active]:shr-duration-300',
+    '[&.shr-dialog-transition-exit-active]:shr-ease-in-out',
+    '[&.shr-dialog-transition-exit-active]:shr-opacity-0',
+  ],
+})
 
 export const DialogOverlap: FC<Props> = ({ isOpen, children }) => {
-  const theme = useTheme()
+  const styles = useMemo(() => dialogOverlap(), [])
   const [childrenBuffer, setChildrenBuffer] = useState<ReactNode>(null)
 
   useEffect(() => {
@@ -22,7 +40,7 @@ export const DialogOverlap: FC<Props> = ({ isOpen, children }) => {
 
   return (
     <CSSTransition
-      classNames={transitionClassName}
+      classNames="shr-dialog-transition"
       in={isOpen}
       timeout={{
         appear: 500,
@@ -32,34 +50,7 @@ export const DialogOverlap: FC<Props> = ({ isOpen, children }) => {
       appear
       unmountOnExit
     >
-      <Wrapper themes={theme}>{isOpen ? children : childrenBuffer}</Wrapper>
+      <div className={styles}>{isOpen ? children : childrenBuffer}</div>
     </CSSTransition>
   )
 }
-
-const Wrapper = styled.div<{ themes: Theme }>`
-  position: absolute;
-  z-index: ${({ themes }) => themes.zIndex.OVERLAP_BASE};
-
-  &.${transitionClassName}-appear {
-    opacity: 0;
-  }
-  &.${transitionClassName}-appear-active {
-    transition: opacity 500ms;
-    opacity: 1;
-  }
-  &.${transitionClassName}-enter {
-    opacity: 0;
-  }
-  &.${transitionClassName}-enter-active {
-    transition: opacity 300ms;
-    opacity: 1;
-  }
-  &.${transitionClassName}-exit {
-    opacity: 1;
-  }
-  &.${transitionClassName}-exit-active {
-    transition: opacity 300ms;
-    opacity: 0;
-  }
-`
