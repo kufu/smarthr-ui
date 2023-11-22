ced7676bebc14e24e58a4d54db561de8338d5782
Yu Watanabe <yuw27b@users.noreply.github.com>
2023-11-22 10:04:03 +0900

    chore: LoaderのVRT用Storyを追加 (#4005)
    Co-authored-by: Masakatsu Tokita <lily.yarn.5@gmail.com>

---
+++ src/components/Loader/VRTLoader.stories.tsx
@@ -0,0 +1,40 @@
+import { StoryFn } from '@storybook/react'
+import React from 'react'
+import styled from 'styled-components'
+
+import { InformationPanel } from '../InformationPanel'
+
+import { Loader } from './Loader'
+import { All } from './Loader.stories'
+
+export default {
+  title: 'States（状態）/Loader',
+  component: Loader,
+  parameters: {
+    withTheming: true,
+  },
+}
+
+export const VRTLoaderForcedColors: StoryFn = () => (
+  <Wrapper>
+    <VRTInformationPanel title="VRT 用の Story です" togglable={false}>
+      Chromatic 上では強制カラーモードで表示されます
+    </VRTInformationPanel>
+    <All />
+  </Wrapper>
+)
+
+VRTLoaderForcedColors.parameters = {
+  chromatic: { forcedColors: 'active' },
+}
+
+const Wrapper = styled.div`
+  height: 100vh;
+  box-sizing: border-box;
+  padding: 24px;
+  color: ${({ theme }) => theme.color.TEXT_BLACK};
+`
+
+const VRTInformationPanel = styled(InformationPanel)`
+  margin-bottom: 24px;
+`
