cdcbe305e9b8e675f1db4f45d47c5d111d404327
uknmr <y.kinmori@gmail.com>
2023-11-28 17:22:13 +0900

    fix: アニメーションを調整

--- src/components/Dialog/DialogOverlap.tsx
+++ src/components/Dialog/DialogOverlap.tsx
@@ -10,21 +10,16 @@ const dialogOverlap = tv({
   base: [
     'shr-absolute',
     'shr-z-overlap-base',
-    '[&.shr-dialog-transition-appear]:shr-opacity-0',
-    '[&.shr-dialog-transition-appear-active]:shr-transition-opacity',
-    '[&.shr-dialog-transition-appear-active]:shr-duration-500',
-    '[&.shr-dialog-transition-appear-active]:shr-ease-in-out',
-    '[&.shr-dialog-transition-appear-active]:shr-opacity-100',
     '[&.shr-dialog-transition-enter]:shr-opacity-0',
-    '[&.shr-dialog-transition-enter-active]:shr-transition-opacity',
-    '[&.shr-dialog-transition-enter-active]:shr-duration-300',
-    '[&.shr-dialog-transition-enter-active]:shr-ease-in-out',
-    '[&.shr-dialog-transition-enter-active]:shr-opacity-100',
+    '[&.shr-dialog-transition-enter-done]:shr-transition-opacity',
+    '[&.shr-dialog-transition-enter-done]:shr-duration-300',
+    '[&.shr-dialog-transition-enter-done]:shr-ease-in-out',
+    '[&.shr-dialog-transition-enter-done]:shr-opacity-100',
     '[&.shr-dialog-transition-exit]:shr-opacity-100',
+    '[&.shr-dialog-transition-exit-active]:shr-opacity-0',
     '[&.shr-dialog-transition-exit-active]:shr-transition-opacity',
     '[&.shr-dialog-transition-exit-active]:shr-duration-300',
     '[&.shr-dialog-transition-exit-active]:shr-ease-in-out',
-    '[&.shr-dialog-transition-exit-active]:shr-opacity-0',
   ],
 })
 
@@ -39,17 +34,7 @@ export const DialogOverlap: FC<Props> = ({ isOpen, children }) => {
   }, [isOpen, children])
 
   return (
-    <CSSTransition
-      classNames="shr-dialog-transition"
-      in={isOpen}
-      timeout={{
-        appear: 500,
-        enter: 300,
-        exit: 300,
-      }}
-      appear
-      unmountOnExit
-    >
+    <CSSTransition classNames="shr-dialog-transition" in={isOpen} timeout={300} unmountOnExit>
       <div className={styles}>{isOpen ? children : childrenBuffer}</div>
     </CSSTransition>
   )
