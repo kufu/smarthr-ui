21df229f2ef484bc6bf0192bfdf3ba177b126ba9
uknmr <y.kinmori@gmail.com>
2023-12-03 19:41:35 +0900

    docs: eslint の注意書きを追加

--- src/components/SectioningContent/useSectioningWrapper.ts
+++ src/components/SectioningContent/useSectioningWrapper.ts
@@ -9,5 +9,7 @@ const sectioningContents = ['article', 'aside', 'nav', 'section']
 const isSectioningContent = (as: ComponentType) =>
   typeof as === 'string' && sectioningContents.includes(as)
 
+/** NOTE: Layout コンポーネントに変更がある場合、必ず [smarthr/a11y-heading-in-sectioning-content](https://github.com/kufu/eslint-plugin-smarthr/tree/main/rules/a11y-heading-in-sectioning-content) を見直すこと
+ */
 export const useSectionWrapper = (as: ComponentType) =>
   useMemo(() => (isSectioningContent(as) ? SectioningFragment : React.Fragment), [as])
