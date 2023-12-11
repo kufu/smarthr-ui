73ed4cbb87e85e714ee7fbb37f7df77fab52fbd3
oti <yoshiaki.ito@smarthr.co.jp>
2023-12-11 11:22:49 +0900

    fix: src/components/Chip/Chip.stories.tsx

--- src/components/Chip/Chip.stories.tsx
+++ src/components/Chip/Chip.stories.tsx
@@ -1,9 +1,7 @@
 import { StoryFn } from '@storybook/react'
 import React from 'react'
-import styled from 'styled-components'
 
-import { Cluster, Stack } from '../Layout'
-import { Text } from '../Text'
+import { Stack } from '../Layout'
 
 import { Chip } from '.'
 
@@ -14,33 +12,11 @@ export default {
 
 export const All: StoryFn = () => (
   <Stack as="dl" gap={3}>
-    <Stack>
-      <DtText>Size</DtText>
-      <dd>
-        <Stack align="flex-start">
-          <Chip label="XXS Label" size="XXS" />
-          <Chip label="XS Label" size="XS" />
-          <Chip label="S Label" size="S" />
-          <div>
-            <Chip label="M Label" />
-            <span>（default）</span>
-          </div>
-          <Chip label="L Label" size="L" />
-          <Chip label="XL Label" size="XL" />
-          <Chip label="XXL Label" size="XXL" />
-        </Stack>
-      </dd>
-      <DtText>In Cluster</DtText>
-      <dd>
-        <Cluster>
-          <Chip label="S Label" size="S" />
-          <Chip label="S Label" size="S" />
-          <Chip label="S Label" size="S" />
-          <Chip label="S Label" size="S" />
-        </Cluster>
-      </dd>
+    <Stack align="flex-start">
+      <Chip label="ラベル" />
+      <Chip label="Label" />
+      <Chip label="label" />
+      <Chip label="Label ラベル" />
     </Stack>
   </Stack>
 )
-
-const DtText = styled(Text).attrs({ forwardedAs: 'dt', weight: 'bold', color: 'TEXT_GREY' })``
