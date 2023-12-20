510e0976d324d47bb1741c5e0f7c4082883324bd
uknmr <y.kinmori@gmail.com>
2023-12-20 17:30:02 +0900

    fix: styled-comopnents も確認しにいく

--- src/components/SectioningContent/useSectioningWrapper.ts
+++ src/components/SectioningContent/useSectioningWrapper.ts
@@ -1,4 +1,5 @@
 import React, { useMemo } from 'react'
+import { isStyledComponent } from 'styled-components'
 
 import { SectioningFragment } from '.'
 
@@ -6,8 +7,10 @@ type ComponentType = string | React.ComponentType<any>
 
 const sectioningContents = ['article', 'aside', 'nav', 'section']
 
-const isSectioningContent = (as: ComponentType) =>
-  typeof as === 'string' && sectioningContents.includes(as)
+const isSectioningContent = (type: ComponentType) => {
+  const type_ = isStyledComponent(type) ? type.target : type
+  return typeof type === 'string' && sectioningContents.includes(type_)
+}
 
 /** NOTE: Layout コンポーネントに変更がある場合、必ず [smarthr/a11y-heading-in-sectioning-content](https://github.com/kufu/eslint-plugin-smarthr/tree/main/rules/a11y-heading-in-sectioning-content) を見直すこと
  */
