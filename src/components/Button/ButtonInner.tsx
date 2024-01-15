22dbcb501c9042b66a30fc072c57f63a51f6b501
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2024-01-15 11:00:42 +0900

    fix: size="s" の Button でも、アイコンとテキストコンテントの縦位置が揃うようにする (#4182)
    * fix: size="s" のボタンでも、アイコンとテキストの縦位置が揃うようにする
    
    * fix: leading の影響を size="s" に絞る
    
    * fix: AnchorButton への適用漏れを修正
    
    ---------
    
    Co-authored-by: uknmr <y.kinmori@gmail.com>

--- src/components/Button/ButtonInner.tsx
+++ src/components/Button/ButtonInner.tsx
@@ -1,9 +1,10 @@
-import React, { FC, PropsWithChildren } from 'react'
+import React, { FC, PropsWithChildren, useMemo } from 'react'
 import { tv } from 'tailwind-variants'
 
 export type Props = PropsWithChildren<{
   prefix?: React.ReactNode
   suffix?: React.ReactNode
+  size: 'default' | 's'
 }>
 
 const buttonInner = tv({
@@ -11,12 +12,24 @@ const buttonInner = tv({
     /* LineClamp を併用する場合に、幅を計算してもらうために指定 */
     'shr-min-w-0',
   ],
+  variants: {
+    size: {
+      default: '',
+      s: [
+        /* SVG とテキストコンテンツの縦位置を揃えるために指定 */
+        'shr-leading-[0]',
+      ],
+    },
+  },
 })
 
-export const ButtonInner: FC<Props> = ({ prefix, suffix, ...props }) => (
-  <>
-    {prefix}
-    <span {...props} className={buttonInner()} />
-    {suffix}
-  </>
-)
+export const ButtonInner: FC<Props> = ({ prefix, suffix, size, ...props }) => {
+  const styles = useMemo(() => buttonInner({ size }), [size])
+  return (
+    <>
+      {prefix}
+      <span {...props} className={styles} />
+      {suffix}
+    </>
+  )
+}
