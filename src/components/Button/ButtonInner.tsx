54a115857482074e24adaa9d3367133af06582c7
uknmr <y.kinmori@gmail.com>
2024-01-15 10:01:41 +0900

    fix: leading の影響を size="s" に絞る

--- src/components/Button/ButtonInner.tsx
+++ src/components/Button/ButtonInner.tsx
@@ -1,24 +1,35 @@
-import React, { FC, PropsWithChildren } from 'react'
+import React, { FC, PropsWithChildren, useMemo } from 'react'
 import { tv } from 'tailwind-variants'
 
 export type Props = PropsWithChildren<{
   prefix?: React.ReactNode
   suffix?: React.ReactNode
+  size: 'default' | 's'
 }>
 
 const buttonInner = tv({
   base: [
     /* LineClamp を併用する場合に、幅を計算してもらうために指定 */
     'shr-min-w-0',
-    /* SVG とテキストコンテンツの縦位置を揃えるために指定 */
-    'shr-leading-[0]',
   ],
+  variants: {
+    size: {
+      default: '',
+      s: [
+        /* SVG とテキストコンテンツの縦位置を揃えるために指定 */
+        'shr-leading-[0]',
+      ],
+    },
+  },
 })
 
-export const ButtonInner: FC<Props> = ({ prefix, suffix, ...props }) => (
-  <>
-    {prefix}
-    <span {...props} className={buttonInner()} />
-    {suffix}
-  </>
-)
+export const ButtonInner: FC<Props> = ({ prefix, suffix, size, ...props }) => {
+  const styles = useMemo(() => buttonInner({ size }), [size])
+  return (
+    <>
+      {prefix}
+      <span {...props} className={styles} />
+      {suffix}
+    </>
+  )
+}
