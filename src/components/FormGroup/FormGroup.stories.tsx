2fe55e48f0e4ed9fa85ed7a118398752aec70d5e
AtsushiM <at.mizoue@gmail.com>
2024-01-25 10:09:49 +0900

    chore: FormGroupのstorybookを非推奨用に書き直す

--- src/components/FormGroup/FormGroup.stories.tsx
+++ src/components/FormGroup/FormGroup.stories.tsx
@@ -1,12 +1,7 @@
 import { StoryFn } from '@storybook/react'
 import React from 'react'
 
-import { Input } from '../Input'
-import { Cluster, Stack } from '../Layout'
-import { Text } from '../Text'
-
-import { FormGroup } from './FormGroup'
-import { Fieldset } from './NewFieldset'
+import { CompactInformationPanel } from '../CompactInformationPanel'
 
 export default {
   title: 'Forms（フォーム）/FormGroup（非推奨）',
@@ -14,111 +9,11 @@ export default {
 }
 
 export const All: StoryFn = () => (
-  <Stack gap={2} as="dl">
-    <Stack>
-      <Text italic color="TEXT_GREY" as="dt">
-        基本
-      </Text>
-      <dd>
-        <FormGroup title="フォームコントロール名">
-          <Input name="defaultInput" />
-        </FormGroup>
-      </dd>
-    </Stack>
-    <Stack>
-      <Text italic color="TEXT_GREY" as="dt">
-        すべてのオプション
-      </Text>
-      <dd>
-        <FormGroup
-          title="氏名"
-          statusLabelProps={{ type: 'grey', children: '任意' }}
-          helpMessage="氏名を入力してください。"
-          exampleMessage="例：草野栄一郎"
-          errorMessages={'氏名が入力されていません。'}
-          supplementaryMessage="※ 補足文はフォームコントロールの下に表示します。"
-        >
-          <Input name="fullname" />
-        </FormGroup>
-      </dd>
-    </Stack>
-    <Stack>
-      <Text italic color="TEXT_GREY" as="dt">
-        入れ子
-      </Text>
-      <dd>
-        <Fieldset
-          title="姓名"
-          statusLabelProps={{ type: 'grey', children: '任意' }}
-          helpMessage="姓名を入力してください。"
-          errorMessages="姓名が入力されていません。"
-        >
-          <Cluster gap={1}>
-            <FormGroup
-              title="姓"
-              titleType="subSubBlockTitle"
-              errorMessages="姓が入力されていません。"
-            >
-              <Input name="lastName" />
-            </FormGroup>
-            <FormGroup
-              title="名"
-              titleType="subSubBlockTitle"
-              errorMessages="名が入力されていません。"
-            >
-              <Input name="firstName" />
-            </FormGroup>
-          </Cluster>
-        </Fieldset>
-      </dd>
-    </Stack>
-    <Stack>
-      <Text italic color="TEXT_GREY" as="dt">
-        読み取り専用
-      </Text>
-      <dd>
-        <Fieldset title="姓名">
-          <Cluster gap={1}>
-            <FormGroup title="姓" titleType="subSubBlockTitle">
-              <Input name="lastName" value="草野" readOnly />
-            </FormGroup>
-            <FormGroup title="名" titleType="subSubBlockTitle">
-              <Input name="firstName" value="栄一郎" readOnly />
-            </FormGroup>
-          </Cluster>
-        </Fieldset>
-      </dd>
-    </Stack>
-    <Stack>
-      <Text italic color="TEXT_GREY" as="dt">
-        disabled
-      </Text>
-      <dd>
-        <Fieldset
-          title="disabled なフォームグループ"
-          helpMessage="このフォームグループは disabled です。内包するフォームグループを個別に disabled する必要はありません。"
-          errorMessages="すべてのフォームコントロールが disabled です。"
-          disabled
-        >
-          <Cluster gap={1}>
-            <FormGroup
-              title="姓"
-              titleType="subSubBlockTitle"
-              errorMessages="姓が入力されていません。"
-            >
-              <Input name="lastName" />
-            </FormGroup>
-            <FormGroup
-              title="名"
-              titleType="subSubBlockTitle"
-              errorMessages="名が入力されていません。"
-            >
-              <Input name="firstName" />
-            </FormGroup>
-          </Cluster>
-        </Fieldset>
-      </dd>
-    </Stack>
-  </Stack>
+  <CompactInformationPanel type="warning">
+    FormGroupコンポーネントは非推奨です。
+    <a href="/?path=/docs/forms（フォーム）-formcontrol--docs">FormControl</a>か
+    <a href="/?path=/docs/forms（フォーム）-fieldset--docs">Fieldset</a>
+    コンポーネントを使用してください。
+  </CompactInformationPanel>
 )
 All.storyName = 'all'
