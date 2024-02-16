6e2abbfb1a1d51378c2ce908a4edc00240820864
atzzCokeK <atsushi1230.sdi@gmail.com>
2024-02-16 15:16:21 +0900

    chore: styleの修正

--- src/components/Header/HeaderLink.tsx
+++ src/components/Header/HeaderLink.tsx
@@ -10,6 +10,6 @@ export const HeaderLink: React.FC<Props> = (props) => (
     href={props.href}
     target="_blank"
     suffix={null}
-    className="focus-visible:shr-focusIndicator shr-pe-0.25 shr-ps-0.25 shr-text-white hover:shr-text-white [&&&]:shr-shadow-none"
+    className="smarthr-ui-header-link shr-pe-0.25 shr-ps-0.25 shr-text-white shr-shadow-none focus-visible:shr-focus-indicator hover:shr-text-white [&:not(focus-visible)]:shr-shadow-none"
   />
 )
