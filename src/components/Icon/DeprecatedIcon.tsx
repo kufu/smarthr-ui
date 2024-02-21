def4afd1ec3f698d45a039b9236c9ddfb03359de
tosiiu <toshihiro.ura@smarthr.co.jp>
2024-02-21 17:08:26 +0900

    feat: 旧名称アイコンをdeprecatedに

---
+++ src/components/Icon/DeprecatedIcon.tsx
@@ -0,0 +1,274 @@
+import {
+  FaAnglesDown,
+  FaAnglesLeft,
+  FaAnglesRight,
+  FaArrowDown19,
+  FaArrowDownAZ,
+  FaArrowDownWideShort,
+  FaArrowRotateLeft,
+  FaArrowRotateRight,
+  FaArrowUp19,
+  FaArrowUpAZ,
+  FaArrowUpWideShort,
+  FaArrowsRotate,
+  FaBackwardStep,
+  FaBagShopping,
+  FaBarsStaggered,
+  FaBasketShopping,
+  FaBoxArchive,
+  FaBoxesStacked,
+  FaBusSimple,
+  FaCakeCandles,
+  FaCalendarDays,
+  FaCalendarXmark,
+  FaCarRear,
+  FaCartShopping,
+  FaCircleArrowDown,
+  FaCircleCheck,
+  FaCircleChevronDown,
+  FaCircleChevronLeft,
+  FaCircleChevronRight,
+  FaCircleChevronUp,
+  FaCircleDot,
+  FaCircleDown,
+  FaCircleExclamation,
+  FaCircleInfo,
+  FaCircleLeft,
+  FaCircleMinus,
+  FaCirclePlay,
+  FaCirclePlus,
+  FaCircleQuestion,
+  FaCircleRight,
+  FaCircleStop,
+  FaCircleUp,
+  FaCircleUser,
+  FaCircleXmark,
+  FaClockRotateLeft,
+  FaCloudArrowDown,
+  FaCloudArrowUp,
+  FaDownLeftAndUpRightToCenter,
+  FaDownLong,
+  FaEllipsis,
+  FaEllipsisVertical,
+  FaFileArrowDown,
+  FaFileArrowUp,
+  FaFileLines,
+  FaFileZipper,
+  FaFloppyDisk,
+  FaForwardStep,
+  FaGauge,
+  FaGear,
+  FaGears,
+  FaGrip,
+  FaHand,
+  FaHospital,
+  FaHouse,
+  FaIdCardClip,
+  FaImagePortrait,
+  FaLeftLong,
+  FaLeftRight,
+  FaLinkSlash,
+  FaListCheck,
+  FaMagnifyingGlass,
+  FaMagnifyingGlassMinus,
+  FaMagnifyingGlassPlus,
+  FaMaximize,
+  FaMessage,
+  FaMobileScreenButton,
+  FaMoneyBill1,
+  FaMoneyBill1Wave,
+  FaMoneyCheckDollar,
+  FaNoteSticky,
+  FaPenToSquare,
+  FaPencil,
+  FaRectangleList,
+  FaRectangleXmark,
+  FaRightFromBracket,
+  FaRightLeft,
+  FaRightLong,
+  FaRightToBracket,
+  FaRotate,
+  FaRotateLeft,
+  FaRotateRight,
+  FaScrewdriverWrench,
+  FaShareFromSquare,
+  FaShareNodes,
+  FaShieldHalved,
+  FaShuffle,
+  FaSliders,
+  FaSquareCaretDown,
+  FaSquareCaretLeft,
+  FaSquareCaretRight,
+  FaSquareCaretUp,
+  FaSquareCheck,
+  FaSquareMinus,
+  FaSquarePlus,
+  FaSquarePollHorizontal,
+  FaSquarePollVertical,
+  FaSuitcaseMedical,
+  FaTableCells,
+  FaTableCellsLarge,
+  FaTableColumns,
+  FaTableList,
+  FaTabletScreenButton,
+  FaTrainSubway,
+  FaTrashArrowUp,
+  FaTrashCan,
+  FaTrashCanArrowUp,
+  FaTriangleExclamation,
+  FaTruckRampBox,
+  FaUnlockKeyhole,
+  FaUpDown,
+  FaUpDownLeftRight,
+  FaUpLong,
+  FaUpRightAndDownLeftFromCenter,
+  FaUpRightFromSquare,
+  FaUserGear,
+  FaUserLarge,
+  FaUserLargeSlash,
+  FaUserPen,
+  FaUsersGear,
+  FaVolumeHigh,
+  FaVolumeLow,
+  FaVolumeXmark,
+  FaXmark,
+} from 'react-icons/fa6'
+
+import { createIcon } from './generateIcon'
+
+/**
+ * @deprecated DeprecatedIconは旧名称のため非推奨です。Icon.tsxより新名称アイコンを利用してください。
+ */
+export const FaAngleDoubleDownIcon = /*#__PURE__*/ createIcon(FaAnglesDown)
+export const FaAngleDoubleLeftIcon = /*#__PURE__*/ createIcon(FaAnglesLeft)
+export const FaAngleDoubleRightIcon = /*#__PURE__*/ createIcon(FaAnglesRight)
+export const FaArchiveIcon = /*#__PURE__*/ createIcon(FaBoxArchive)
+export const FaArrowCircleDownIcon = /*#__PURE__*/ createIcon(FaCircleArrowDown)
+export const FaArrowAltCircleDownIcon = /*#__PURE__*/ createIcon(FaCircleDown)
+export const FaArrowAltCircleLeftIcon = /*#__PURE__*/ createIcon(FaCircleLeft)
+export const FaArrowAltCircleRightIcon = /*#__PURE__*/ createIcon(FaCircleRight)
+export const FaArrowAltCircleUpIcon = /*#__PURE__*/ createIcon(FaCircleUp)
+export const FaArrowsAltIcon = /*#__PURE__*/ createIcon(FaUpDownLeftRight)
+export const FaArrowsAltHIcon = /*#__PURE__*/ createIcon(FaLeftRight)
+export const FaArrowsAltVIcon = /*#__PURE__*/ createIcon(FaUpDown)
+export const FaBirthdayCakeIcon = /*#__PURE__*/ createIcon(FaCakeCandles)
+export const FaBoxesIcon = /*#__PURE__*/ createIcon(FaBoxesStacked)
+export const FaBusAltIcon = /*#__PURE__*/ createIcon(FaBusSimple)
+export const FaCalendarAltIcon = /*#__PURE__*/ createIcon(FaCalendarDays)
+export const FaCalendarTimesIcon = /*#__PURE__*/ createIcon(FaCalendarXmark)
+export const FaCarAltIcon = /*#__PURE__*/ createIcon(FaCarRear)
+export const FaCaretSquareDownIcon = /*#__PURE__*/ createIcon(FaSquareCaretDown)
+export const FaCaretSquareLeftIcon = /*#__PURE__*/ createIcon(FaSquareCaretLeft)
+export const FaCaretSquareRightIcon = /*#__PURE__*/ createIcon(FaSquareCaretRight)
+export const FaCaretSquareUpIcon = /*#__PURE__*/ createIcon(FaSquareCaretUp)
+export const FaCheckCircleIcon = /*#__PURE__*/ createIcon(FaCircleCheck)
+export const FaCheckSquareIcon = /*#__PURE__*/ createIcon(FaSquareCheck)
+export const FaChevronCircleDownIcon = /*#__PURE__*/ createIcon(FaCircleChevronDown)
+export const FaChevronCircleLeftIcon = /*#__PURE__*/ createIcon(FaCircleChevronLeft)
+export const FaChevronCircleRightIcon = /*#__PURE__*/ createIcon(FaCircleChevronRight)
+export const FaChevronCircleUpIcon = /*#__PURE__*/ createIcon(FaCircleChevronUp)
+export const FaCloudDownloadAltIcon = /*#__PURE__*/ createIcon(FaCloudArrowDown)
+export const FaCloudUploadAltIcon = /*#__PURE__*/ createIcon(FaCloudArrowUp)
+export const FaCogIcon = /*#__PURE__*/ createIcon(FaGear)
+export const FaCogsIcon = /*#__PURE__*/ createIcon(FaGears)
+export const FaColumnsIcon = /*#__PURE__*/ createIcon(FaTableColumns)
+export const FaCommentAltIcon = /*#__PURE__*/ createIcon(FaMessage)
+export const FaCompressAltIcon = /*#__PURE__*/ createIcon(FaDownLeftAndUpRightToCenter)
+export const FaEditIcon = /*#__PURE__*/ createIcon(FaPenToSquare)
+export const FaEllipsisHIcon = /*#__PURE__*/ createIcon(FaEllipsis)
+export const FaEllipsisVIcon = /*#__PURE__*/ createIcon(FaEllipsisVertical)
+export const FaExchangeAltIcon = /*#__PURE__*/ createIcon(FaRightLeft)
+export const FaExclamationCircleIcon = /*#__PURE__*/ createIcon(FaCircleExclamation)
+export const FaExclamationTriangleIcon = /*#__PURE__*/ createIcon(FaTriangleExclamation)
+export const FaExpandAltIcon = /*#__PURE__*/ createIcon(FaUpRightAndDownLeftFromCenter)
+export const FaExpandArrowsAltIcon = /*#__PURE__*/ createIcon(FaMaximize)
+export const FaExternalLinkAltIcon = /*#__PURE__*/ createIcon(FaUpRightFromSquare)
+export const FaFileAltIcon = /*#__PURE__*/ createIcon(FaFileLines)
+export const FaFileArchiveIcon = /*#__PURE__*/ createIcon(FaFileZipper)
+export const FaFileDownloadIcon = /*#__PURE__*/ createIcon(FaFileArrowDown)
+export const FaFileUploadIcon = /*#__PURE__*/ createIcon(FaFileArrowUp)
+export const FaGripHorizontalIcon = /*#__PURE__*/ createIcon(FaGrip)
+export const FaHandPaperIcon = /*#__PURE__*/ createIcon(FaHand)
+export const FaHistoryIcon = /*#__PURE__*/ createIcon(FaClockRotateLeft)
+export const FaHomeIcon = /*#__PURE__*/ createIcon(FaHouse)
+export const FaHospitalAltIcon = /*#__PURE__*/ createIcon(FaHospital)
+export const FaIdCardAltIcon = /*#__PURE__*/ createIcon(FaIdCardClip)
+export const FaInfoCircleIcon = /*#__PURE__*/ createIcon(FaCircleInfo)
+export const FaListAltIcon = /*#__PURE__*/ createIcon(FaRectangleList)
+export const FaLongArrowAltDownIcon = /*#__PURE__*/ createIcon(FaDownLong)
+export const FaLongArrowAltLeftIcon = /*#__PURE__*/ createIcon(FaLeftLong)
+export const FaLongArrowAltRightIcon = /*#__PURE__*/ createIcon(FaRightLong)
+export const FaLongArrowAltUpIcon = /*#__PURE__*/ createIcon(FaUpLong)
+export const FaMedkitIcon = /*#__PURE__*/ createIcon(FaSuitcaseMedical)
+export const FaMinusCircleIcon = /*#__PURE__*/ createIcon(FaCircleMinus)
+export const FaMinusSquareIcon = /*#__PURE__*/ createIcon(FaSquareMinus)
+export const FaMobileAltIcon = /*#__PURE__*/ createIcon(FaMobileScreenButton)
+export const FaMoneyBillAltIcon = /*#__PURE__*/ createIcon(FaMoneyBill1)
+export const FaMoneyBillWaveAltIcon = /*#__PURE__*/ createIcon(FaMoneyBill1Wave)
+export const FaMoneyCheckAltIcon = /*#__PURE__*/ createIcon(FaMoneyCheckDollar)
+export const FaPencilAltIcon = /*#__PURE__*/ createIcon(FaPencil)
+export const FaPlayCircleIcon = /*#__PURE__*/ createIcon(FaCirclePlay)
+export const FaPlusCircleIcon = /*#__PURE__*/ createIcon(FaCirclePlus)
+export const FaPlusSquareIcon = /*#__PURE__*/ createIcon(FaSquarePlus)
+export const FaPollIcon = /*#__PURE__*/ createIcon(FaSquarePollVertical)
+export const FaPollHIcon = /*#__PURE__*/ createIcon(FaSquarePollHorizontal)
+export const FaPortraitIcon = /*#__PURE__*/ createIcon(FaImagePortrait)
+export const FaQuestionCircleIcon = /*#__PURE__*/ createIcon(FaCircleQuestion)
+export const FaRandomIcon = /*#__PURE__*/ createIcon(FaShuffle)
+export const FaRedoIcon = /*#__PURE__*/ createIcon(FaArrowRotateRight)
+export const FaRedoAltIcon = /*#__PURE__*/ createIcon(FaRotateRight)
+export const FaRegDotCircleIcon = /*#__PURE__*/ createIcon(FaCircleDot)
+export const FaSaveIcon = /*#__PURE__*/ createIcon(FaFloppyDisk)
+export const FaSearchIcon = /*#__PURE__*/ createIcon(FaMagnifyingGlass)
+export const FaSearchMinusIcon = /*#__PURE__*/ createIcon(FaMagnifyingGlassMinus)
+export const FaSearchPlusIcon = /*#__PURE__*/ createIcon(FaMagnifyingGlassPlus)
+export const FaShareAltIcon = /*#__PURE__*/ createIcon(FaShareNodes)
+export const FaShareSquareIcon = /*#__PURE__*/ createIcon(FaShareFromSquare)
+export const FaShieldAltIcon = /*#__PURE__*/ createIcon(FaShieldHalved)
+export const FaShoppingBagIcon = /*#__PURE__*/ createIcon(FaBagShopping)
+export const FaShoppingBasketIcon = /*#__PURE__*/ createIcon(FaBasketShopping)
+export const FaShoppingCartIcon = /*#__PURE__*/ createIcon(FaCartShopping)
+export const FaSignInAltIcon = /*#__PURE__*/ createIcon(FaRightToBracket)
+export const FaSignOutAltIcon = /*#__PURE__*/ createIcon(FaRightFromBracket)
+export const FaSlidersHIcon = /*#__PURE__*/ createIcon(FaSliders)
+export const FaSortAlphaDownIcon = /*#__PURE__*/ createIcon(FaArrowDownAZ)
+export const FaSortAlphaUpIcon = /*#__PURE__*/ createIcon(FaArrowUpAZ)
+export const FaSortAmountDownIcon = /*#__PURE__*/ createIcon(FaArrowDownWideShort)
+export const FaSortAmountUpIcon = /*#__PURE__*/ createIcon(FaArrowUpWideShort)
+export const FaSortNumericDownIcon = /*#__PURE__*/ createIcon(FaArrowDown19)
+export const FaSortNumericUpIcon = /*#__PURE__*/ createIcon(FaArrowUp19)
+export const FaStepBackwardIcon = /*#__PURE__*/ createIcon(FaBackwardStep)
+export const FaStepForwardIcon = /*#__PURE__*/ createIcon(FaForwardStep)
+export const FaStickyNoteIcon = /*#__PURE__*/ createIcon(FaNoteSticky)
+export const FaStopCircleIcon = /*#__PURE__*/ createIcon(FaCircleStop)
+export const FaStreamIcon = /*#__PURE__*/ createIcon(FaBarsStaggered)
+export const FaSubwayIcon = /*#__PURE__*/ createIcon(FaTrainSubway)
+export const FaSyncIcon = /*#__PURE__*/ createIcon(FaArrowsRotate)
+export const FaSyncAltIcon = /*#__PURE__*/ createIcon(FaRotate)
+export const FaTabletAltIcon = /*#__PURE__*/ createIcon(FaTabletScreenButton)
+export const FaTachometerAltIcon = /*#__PURE__*/ createIcon(FaGauge)
+export const FaTasksIcon = /*#__PURE__*/ createIcon(FaListCheck)
+export const FaThIcon = /*#__PURE__*/ createIcon(FaTableCells)
+export const FaThLargeIcon = /*#__PURE__*/ createIcon(FaTableCellsLarge)
+export const FaThListIcon = /*#__PURE__*/ createIcon(FaTableList)
+export const FaTimesIcon = /*#__PURE__*/ createIcon(FaXmark)
+export const FaTimesCircleIcon = /*#__PURE__*/ createIcon(FaCircleXmark)
+export const FaToolsIcon = /*#__PURE__*/ createIcon(FaScrewdriverWrench)
+export const FaTrashAltIcon = /*#__PURE__*/ createIcon(FaTrashCan)
+export const FaTrashRestoreIcon = /*#__PURE__*/ createIcon(FaTrashArrowUp)
+export const FaTrashRestoreAltIcon = /*#__PURE__*/ createIcon(FaTrashCanArrowUp)
+export const FaTruckLoadingIcon = /*#__PURE__*/ createIcon(FaTruckRampBox)
+export const FaUndoIcon = /*#__PURE__*/ createIcon(FaArrowRotateLeft)
+export const FaUndoAltIcon = /*#__PURE__*/ createIcon(FaRotateLeft)
+export const FaUnlinkIcon = /*#__PURE__*/ createIcon(FaLinkSlash)
+export const FaUnlockAltIcon = /*#__PURE__*/ createIcon(FaUnlockKeyhole)
+export const FaUserAltIcon = /*#__PURE__*/ createIcon(FaUserLarge)
+export const FaUserAltSlashIcon = /*#__PURE__*/ createIcon(FaUserLargeSlash)
+export const FaUserCircleIcon = /*#__PURE__*/ createIcon(FaCircleUser)
+export const FaUserCogIcon = /*#__PURE__*/ createIcon(FaUserGear)
+export const FaUserEditIcon = /*#__PURE__*/ createIcon(FaUserPen)
+export const FaUsersCogIcon = /*#__PURE__*/ createIcon(FaUsersGear)
+export const FaVolumeDownIcon = /*#__PURE__*/ createIcon(FaVolumeLow)
+export const FaVolumeMuteIcon = /*#__PURE__*/ createIcon(FaVolumeXmark)
+export const FaVolumeUpIcon = /*#__PURE__*/ createIcon(FaVolumeHigh)
+export const FaWindowCloseIcon = /*#__PURE__*/ createIcon(FaRectangleXmark)
