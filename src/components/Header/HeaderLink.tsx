d75a223bca98d354a7eefa1b9a1cefe3bbfd65e5
atzzCokeK <atsushi1230.sdi@gmail.com>
2024-02-21 13:49:43 +0900

    fix: reviewの指摘を修正

--- src/components/Header/HeaderLink.tsx
+++ src/components/Header/HeaderLink.tsx
@@ -1,15 +1,15 @@
-import React, { ComponentProps, PropsWithChildren } from 'react'
+import React, { ComponentProps } from 'react'
 
 import { TextLink } from '../TextLink'
 
-type Props = PropsWithChildren<ComponentProps<typeof TextLink>>
+type Props = ComponentProps<typeof TextLink>
 
 export const HeaderLink: React.FC<Props> = (props) => (
+  // eslint-disable-next-line smarthr/a11y-anchor-has-href-attribute
   <TextLink
     {...props}
-    href={props.href}
     target="_blank"
     suffix={null}
-    className="smarthr-ui-header-link shr-pe-0.25 shr-ps-0.25 shr-text-white shr-shadow-none focus-visible:shr-focus-indicator hover:shr-text-white [&:not(focus-visible)]:shr-shadow-none"
+    className="shr-px-0.25 shr-text-white focus-visible:shr-focus-indicator hover:shr-text-white [&:not(focus-visible)]:shr-shadow-none"
   />
 )
