c485c4696ae19f416cd09899dc4331ced73268a4
uknmr <y.kinmori@gmail.com>
2024-02-23 11:44:12 +0900

    Revert "fix: HeaderLink に className を渡せるように修正"
    715c315c34667d74fceb4bbe13e219125429bee2

--- src/components/Header/HeaderLink.tsx
+++ src/components/Header/HeaderLink.tsx
@@ -1,22 +1,15 @@
 import React, { ComponentProps } from 'react'
-import { tv } from 'tailwind-variants'
 
 import { TextLink } from '../TextLink'
 
 type Props = ComponentProps<typeof TextLink>
 
-const headerLink = tv({
-  base: [
-    'shr-px-0.25 shr-text-white [&]:shr-shadow-none',
-    'hover:shr-text-white',
-    'focus-visible:shr-focus-indicator',
-  ],
-})
-
-export const HeaderLink: React.FC<Props> = ({ className, ...props }) => {
-  const style = headerLink({ className })
-  return (
-    // eslint-disable-next-line smarthr/a11y-anchor-has-href-attribute
-    <TextLink {...props} target="_blank" suffix={null} className={style} />
-  )
-}
+export const HeaderLink: React.FC<Props> = (props) => (
+  // eslint-disable-next-line smarthr/a11y-anchor-has-href-attribute
+  <TextLink
+    {...props}
+    target="_blank"
+    suffix={null}
+    className="shr-px-0.25 shr-text-white focus-visible:shr-focus-indicator hover:shr-text-white [&]:shr-shadow-none"
+  />
+)
