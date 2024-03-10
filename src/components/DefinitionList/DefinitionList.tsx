e7837161651bc6807d24af860c901c2bcce5b3f6
KANAMORI Yu <y.kinmori@gmail.com>
2024-03-11 00:13:55 +0900

    feat: DefiinitionList の用語の見た目を変える termStyleType を追加 (#4357)

--- src/components/DefinitionList/DefinitionList.tsx
+++ src/components/DefinitionList/DefinitionList.tsx
@@ -5,11 +5,15 @@ import { Cluster } from '../Layout'
 
 import { DefinitionListItem } from './DefinitionListItem'
 
+type ItemType = ComponentProps<typeof DefinitionListItem>
+
 type Props = {
   /** 定義リストのアイテムの配列 */
-  items: Array<ComponentProps<typeof DefinitionListItem>>
+  items: Array<Omit<ItemType, 'termStyleType'>>
   /** 最大列数 */
   maxColumns?: number
+  /** 用語の見た目の種類 */
+  termStyleType?: ItemType['termStyleType']
 }
 type ElementProps = Omit<ComponentProps<'dl'>, keyof Props>
 
@@ -17,13 +21,23 @@ const definitionList = tv({
   base: 'smarthr-ui-DefinitionList shr-my-[initial]',
 })
 
-export const DefinitionList: FC<Props & ElementProps> = ({ items, maxColumns, className }) => {
+export const DefinitionList: FC<Props & ElementProps> = ({
+  items,
+  maxColumns,
+  termStyleType = 'subBlockTitle',
+  className,
+}) => {
   const styles = useMemo(() => definitionList({ className }), [className])
 
   return (
     <Cluster as="dl" gap={1.5} className={styles}>
       {items.map((item, index) => (
-        <DefinitionListItem {...item} key={index} maxColumns={maxColumns} />
+        <DefinitionListItem
+          {...item}
+          key={index}
+          maxColumns={maxColumns}
+          termStyleType={termStyleType}
+        />
       ))}
     </Cluster>
   )
