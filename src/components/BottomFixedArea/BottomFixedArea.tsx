c06cd0cd0873dca828ff6683fe811a93f473ca81
uknmr <y.kinmori@gmail.com>
2024-03-13 16:12:39 +0900

    fix: BottomFiexArea を Tailwind CSS 化し、normalize 依存を修正

--- src/components/BottomFixedArea/BottomFixedArea.tsx
+++ src/components/BottomFixedArea/BottomFixedArea.tsx
@@ -1,22 +1,22 @@
 import React, {
   ComponentProps,
+  ComponentPropsWithRef,
+  ComponentType,
+  FC,
   FunctionComponentElement,
-  HTMLAttributes,
+  MouseEventHandler,
   ReactNode,
-  VFC,
   useEffect,
+  useMemo,
 } from 'react'
-import styled, { css } from 'styled-components'
+import { tv } from 'tailwind-variants'
 
-import { Theme, useTheme } from '../../hooks/useTheme'
-import { Base as BaseComponent } from '../Base'
+import { Base } from '../Base'
 import { AnchorButton, Button } from '../Button'
+import { ComponentProps as IconProps } from '../Icon'
 import { Cluster, Stack } from '../Layout'
-import { Text } from '../Text'
 
-import { TertiaryLink } from './TertiaryLink'
 import { validateElement } from './bottomFixedAreaHelper'
-import { useClassNames } from './useClassNames'
 
 export type Primary =
   | FunctionComponentElement<ComponentProps<typeof Button>>
@@ -26,7 +26,7 @@ export type Secondary =
   | FunctionComponentElement<ComponentProps<typeof Button>>
   | FunctionComponentElement<ComponentProps<typeof AnchorButton>>
 
-type ElementProps = Omit<HTMLAttributes<HTMLDivElement>, keyof Props>
+type ElementProps = Omit<ComponentPropsWithRef<'div'>, keyof Props>
 
 type Props = {
   /** この領域の説明 */
@@ -36,73 +36,90 @@ type Props = {
   /** 表示する `Button` または `AnchorButton` （`variant="secondary"` である必要がある）*/
   secondaryButton?: Secondary
   /** 表示する tertialy link のプロパティの配列 */
-  tertiaryLinks?: Array<React.ComponentProps<typeof TertiaryLink>>
+  tertiaryLinks?: Array<
+    ComponentPropsWithRef<'button'> & {
+      text: ReactNode
+      icon?: ComponentType<IconProps>
+      type?: 'button' | 'reset'
+      onClick?: MouseEventHandler<HTMLButtonElement>
+    }
+  >
   /** コンポーネントに適用する z-index 値 */
   zIndex?: number
-  /** コンポーネントに適用するクラス名 */
-  className?: string
 }
 
-export const BottomFixedArea: VFC<Props & ElementProps> = ({
+const bottomFixedArea = tv({
+  slots: {
+    wrapper: [
+      'smarthr-ui-BottomFixedArea',
+      '',
+      'shr-fixed shr-bottom-0 shr-z-fixed-menu shr-box-border shr-w-full shr-rounded-none shr-p-1.5 shr-text-center',
+      // Layer 3 だが、上方向への指定のためベタ書き： https://smarthr.design/products/design-tokens/shadow/
+      '[box-shadow:_0_-4px_8px_2px_rgba(0,_0,_0,_0.24)]',
+      '[&_ul]:shr-list-none',
+    ],
+    tertiaryButton: [
+      'smarthr-ui-BottomFixedArea-tertiaryLink',
+      '-shr-mb-0.5 shr-font-normal shr-text-main',
+    ],
+  },
+})
+
+export const BottomFixedArea: FC<Props & ElementProps> = ({
   description,
   primaryButton,
   secondaryButton,
   tertiaryLinks,
-  zIndex = 500,
-  className = '',
+  zIndex,
+  className,
   ...props
 }) => {
-  const theme = useTheme()
-  const classNames = useClassNames()
+  const { wrapperStyleProps, tertiaryButtonStyle } = useMemo(() => {
+    const { wrapper, tertiaryButton } = bottomFixedArea()
+    return {
+      wrapperStyleProps: { className: wrapper({ className }), style: { zIndex } },
+      tertiaryButtonStyle: tertiaryButton(),
+    }
+  }, [className, zIndex])
 
   useEffect(() => {
     validateElement(primaryButton, secondaryButton)
   }, [primaryButton, secondaryButton])
 
   return (
-    <Base
-      {...props}
-      $themes={theme}
-      $zIndex={zIndex}
-      className={`${className} ${classNames.wrapper}`}
-    >
+    <Base {...props} {...wrapperStyleProps}>
       <Stack>
-        {description && (
-          <Text as="p" className={classNames.description}>
-            {description}
-          </Text>
-        )}
-        {(secondaryButton || primaryButton) && (
-          <Cluster as="ul" justify="center" gap={{ row: 0.5, column: 1 }}>
-            {secondaryButton && <li className={classNames.secondaryButton}>{secondaryButton}</li>}
-            {primaryButton && <li className={classNames.primaryButton}>{primaryButton}</li>}
-          </Cluster>
-        )}
-        {tertiaryLinks && tertiaryLinks.length > 0 && (
-          <Cluster as="ul" justify="center" gap={{ row: 0.5, column: 1 }}>
-            {tertiaryLinks.map((tertiaryLink, index) => (
-              <li key={index} className={classNames.tertiaryListItem}>
-                {/* eslint-disable-next-line smarthr/a11y-anchor-has-href-attribute */}
-                <TertiaryLink {...tertiaryLink} />
-              </li>
-            ))}
-          </Cluster>
-        )}
+        {description && <p className="smarthr-ui-BottomFixedArea-description">{description}</p>}
+        <Stack gap={0.25}>
+          {(secondaryButton || primaryButton) && (
+            <Cluster as="ul" justify="center" gap={{ row: 0.5, column: 1 }}>
+              {secondaryButton && (
+                <li className="smarthr-ui-BottomFixedArea-secondaryButton">{secondaryButton}</li>
+              )}
+              {primaryButton && (
+                <li className="smarthr-ui-BottomFixedArea-primaryButton">{primaryButton}</li>
+              )}
+            </Cluster>
+          )}
+          {tertiaryLinks && tertiaryLinks.length > 0 && (
+            <Cluster as="ul" justify="center" gap={{ row: 0.5, column: 0 }}>
+              {tertiaryLinks.map(({ text, icon: Icon, onClick, ...tertiaryProps }, index) => (
+                <li key={index} className="smarthr-ui-BottomFixedArea-tertiaryListItem">
+                  <Button
+                    {...tertiaryProps}
+                    variant="text"
+                    prefix={Icon && <Icon />}
+                    onClick={onClick}
+                    className={tertiaryButtonStyle}
+                  >
+                    {text}
+                  </Button>
+                </li>
+              ))}
+            </Cluster>
+          )}
+        </Stack>
       </Stack>
     </Base>
   )
 }
-
-const Base = styled(BaseComponent)<{ $themes: Theme; $zIndex: number }>`
-  ${({ $themes: { spacingByChar }, $zIndex }) => css`
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    padding: ${spacingByChar(1.5)};
-    text-align: center;
-    z-index: ${$zIndex};
-    box-shadow: 0 -4px 8px 2px rgba(0, 0, 0, 0.24);
-    border-radius: 0;
-    box-sizing: border-box;
-  `}
-`
