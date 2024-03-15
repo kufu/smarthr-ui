b5850863aacbc58b7a7a62ad248e33790f5bc99c
atzzCokeK <atsushi1230.sdi@gmail.com>
2024-03-16 07:29:11 +0900

    refactor: HeaderDropdownMenuButtonをTailwindCSS化

--- src/components/Header/HeaderDropdownMenuButton.tsx
+++ src/components/Header/HeaderDropdownMenuButton.tsx
@@ -1,22 +1,42 @@
-import styled, { css } from 'styled-components'
+import React, { ComponentPropsWithRef, useMemo } from 'react'
+import { tv } from 'tailwind-variants'
 
-import { useTheme } from '../../hooks/useTheme'
 import { DropdownMenuButton } from '../Dropdown'
 
-export const HeaderDropdownMenuButton = styled(DropdownMenuButton)(() => {
-  const { color, space } = useTheme()
-  return css`
-    > .smarthr-ui-DropdownMenuButton-trigger {
-      border-color: transparent;
-      background-color: transparent;
-      padding-inline: ${space(0.25)};
-      color: ${color.TEXT_WHITE};
-      font-weight: normal;
-
-      &:last-of-type {
-        /* ボタンの余白分だけ微調整 */
-        margin-inline-end: ${space(-0.25)};
-      }
-    }
-  `
+const headerDropdownMenuButton = tv({
+  base: [
+    '[&>.smarthr-ui-DropdownMenuButton-trigger]:shr-ps-0.25] [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-border-transparent [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-bg-transparent [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-pe-0.25 [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-ps-0.25 [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-font-normal [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-text-white',
+    // ボタンの余白分だけ微調整
+    '[&>.smarthr-ui-DropdownMenuButton-trigger]:last-of-type:-shr-me-0.25',
+  ],
 })
+
+export const HeaderDropdownMenuButton = ({
+  className,
+  ...rest
+}: ComponentPropsWithRef<typeof DropdownMenuButton>) => {
+  const headerDropdownMenuButtonStyle = useMemo(
+    () => headerDropdownMenuButton({ className }),
+    [className],
+  )
+
+  return <DropdownMenuButton {...rest} className={headerDropdownMenuButtonStyle} />
+}
+
+// export const HeaderDropdownMenuButton = styled(DropdownMenuButton)(() => {
+//   const { color, space } = useTheme()
+//   return css`
+//     > .smarthr-ui-DropdownMenuButton-trigger {
+//       border-color: transparent;
+//       background-color: transparent;
+//       padding-inline: ${space(0.25)};
+//       color: ${color.TEXT_WHITE};
+//       font-weight: normal;
+
+//       &:last-of-type {
+//         /* ボタンの余白分だけ微調整 */
+//         margin-inline-end: ${space(-0.25)};
+//       }
+//     }
+//   `
+// })
