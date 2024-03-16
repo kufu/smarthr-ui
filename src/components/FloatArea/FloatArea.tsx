2f7e4dfc775fcb13e12fe3225b35252ce52b9e25
atzzCokeK <atsushi1230.sdi@gmail.com>
2024-03-16 13:52:00 +0900

    refactor: FloatAreaをTailwindCSS化

--- src/components/FloatArea/FloatArea.tsx
+++ src/components/FloatArea/FloatArea.tsx
@@ -1,15 +1,69 @@
-import React, { FC, HTMLAttributes, ReactNode } from 'react'
-import styled, { css } from 'styled-components'
+import React, { FC, HTMLAttributes, ReactNode, useMemo } from 'react'
+import { tv } from 'tailwind-variants'
 
-import { useSpacing } from '../../hooks/useSpacing'
-import { Theme, useTheme } from '../../hooks/useTheme'
 import { AbstractSize, CharRelativeSize } from '../../themes/createSpacing'
-import { ResponseMessageType } from '../../types'
-import { Base as shrBase } from '../Base'
+import { Gap, ResponseMessageType } from '../../types'
+import { Base } from '../Base'
 import { Cluster } from '../Layout'
 import { ResponseMessage } from '../ResponseMessage'
 
-import { useClassNames } from './useClassNames'
+const floatArea = tv({
+  base: 'smarthr-ui-FloatArea',
+  variants: {
+    fixed: {
+      true: 'shr-fixed',
+      false: 'shr-sticky',
+    },
+    top: {
+      0: 'shr-top-0',
+      0.25: 'shr-top-0.25',
+      0.5: 'shr-top-0.5',
+      0.75: 'shr-top-0.75',
+      1: 'shr-top-1',
+      1.25: 'shr-top-1.25',
+      1.5: 'shr-top-1.5',
+      2: 'shr-top-2',
+      2.5: 'shr-top-2.5',
+      3: 'shr-top-3',
+      3.5: 'shr-top-3.5',
+      4: 'shr-top-4',
+      8: 'shr-top-8',
+      X3S: 'shr-top-0.25',
+      XXS: 'shr-top-0.5',
+      XS: 'shr-top-1',
+      S: 'shr-top-1.5',
+      M: 'shr-top-2',
+      L: 'shr-top-2.5',
+      XL: 'shr-top-3',
+      XXL: 'shr-top-3.5',
+      X3L: 'shr-top-4',
+    } as { [key in Gap]: string },
+    bottom: {
+      0: 'shr-bottom-0',
+      0.25: 'shr-bottom-0.25',
+      0.5: 'shr-bottom-0.5',
+      0.75: 'shr-bottom-0.75',
+      1: 'shr-bottom-1',
+      1.25: 'shr-bottom-1.25',
+      1.5: 'shr-bottom-1.5',
+      2: 'shr-bottom-2',
+      2.5: 'shr-bottom-2.5',
+      3: 'shr-bottom-3',
+      3.5: 'shr-bottom-3.5',
+      4: 'shr-bottom-4',
+      8: 'shr-bottom-8',
+      X3S: 'shr-bottom-0.25',
+      XXS: 'shr-bottom-0.5',
+      XS: 'shr-bottom-1',
+      S: 'shr-bottom-1.5',
+      M: 'shr-bottom-2',
+      L: 'shr-bottom-2.5',
+      XL: 'shr-bottom-3',
+      XXL: 'shr-bottom-3.5',
+      X3L: 'shr-bottom-4',
+    } as { [key in Gap]: string },
+  },
+})
 
 type StyleProps = {
   /** コンポーネントの上端から、包含ブロックの上端までの間隔（基準フォントサイズの相対値または抽象値） */
@@ -44,23 +98,32 @@ export const FloatArea: FC<Props & ElementProps> = ({
   responseMessage,
   fixed = false,
   className = '',
+  top,
+  bottom,
   width,
+  zIndex,
   ...props
 }) => {
-  const theme = useTheme()
-  const classNames = useClassNames()
+  const { wrapperProps } = useMemo(
+    () => ({
+      wrapperProps: {
+        className: floatArea({
+          fixed,
+          className: `${className} ${zIndex ? `shr-z-[${zIndex}]` : 'shr-z-[500]'}`,
+          top: top || top === 0 ? top : undefined,
+          bottom: bottom || bottom === 0 ? bottom : undefined,
+        }),
+        style: { width: width ? width : undefined },
+      },
+    }),
+    [className],
+  )
 
   return (
-    <Base
-      {...props}
-      $themes={theme}
-      className={`${className} ${classNames.wrapper}`}
-      $width={width}
-      fixed={fixed}
-    >
+    <Base {...wrapperProps} {...props} layer={3} padding={1}>
       <Cluster gap={1}>
         {tertiaryButton && tertiaryButton}
-        <RightSide>
+        <div className="shr-ms-auto">
           <Cluster gap={1} align="center">
             {(responseMessage?.status === 'success' || responseMessage?.status === 'error') && (
               <ResponseMessage type={responseMessage.status}>
@@ -72,24 +135,24 @@ export const FloatArea: FC<Props & ElementProps> = ({
               {primaryButton && primaryButton}
             </Cluster>
           </Cluster>
-        </RightSide>
+        </div>
       </Cluster>
     </Base>
   )
 }
 
-const Base = styled(shrBase).attrs({ layer: 3 })<
-  StyleProps & { $themes: Theme; $width: Props['width']; fixed: Props['fixed'] }
->`
-  ${({ $themes: { space }, top, bottom, $width, fixed, zIndex = 500 }) => css`
-    position: ${fixed ? 'fixed' : 'sticky'};
-    ${(top || top === 0) && `top: ${useSpacing(top)};`}
-    ${(bottom || bottom === 0) && `bottom: ${useSpacing(bottom)};`}
-      z-index: ${zIndex};
-    padding: ${space(1)};
-    ${$width && `width: ${$width};`}
-  `}
-`
-const RightSide = styled.div`
-  margin-inline-start: auto;
-`
+// const Base = styled(shrBase).attrs({ layer: 3 })<
+//   StyleProps & { $themes: Theme; $width: Props['width']; fixed: Props['fixed'] }
+// >`
+//   ${({ $themes: { space }, top, bottom, $width, fixed, zIndex = 500 }) => css`
+//     position: ${fixed ? 'fixed' : 'sticky'};
+//     ${(top || top === 0) && `top: ${useSpacing(top)};`}
+//     ${(bottom || bottom === 0) && `bottom: ${useSpacing(bottom)};`}
+//       z-index: ${zIndex};
+//     padding: ${space(1)};
+//     ${$width && `width: ${$width};`}
+//   `}
+// `
+// const RightSide = styled.div`
+//   margin-inline-start: auto;
+// `
