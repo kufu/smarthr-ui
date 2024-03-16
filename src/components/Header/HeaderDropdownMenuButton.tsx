c82f2688f38a4f907dd8b5fe39c10007fee68399
atzzCokeK <atsushi1230.sdi@gmail.com>
2024-03-16 12:23:57 +0900

    chore: delete comment

--- src/components/Header/HeaderDropdownMenuButton.tsx
+++ src/components/Header/HeaderDropdownMenuButton.tsx
@@ -22,21 +22,3 @@ export const HeaderDropdownMenuButton = ({
 
   return <DropdownMenuButton {...rest} className={headerDropdownMenuButtonStyle} />
 }
-
-// export const HeaderDropdownMenuButton = styled(DropdownMenuButton)(() => {
-//   const { color, space } = useTheme()
-//   return css`
-//     > .smarthr-ui-DropdownMenuButton-trigger {
-//       border-color: transparent;
-//       background-color: transparent;
-//       padding-inline: ${space(0.25)};
-//       color: ${color.TEXT_WHITE};
-//       font-weight: normal;
-
-//       &:last-of-type {
-//         /* ボタンの余白分だけ微調整 */
-//         margin-inline-end: ${space(-0.25)};
-//       }
-//     }
-//   `
-// })
