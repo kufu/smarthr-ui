73058dc7d032b24bbdbcf57ceb4a2e5f4fe39239
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2024-03-21 13:47:24 +0900

    Update src/smarthr-ui-preset.ts
    Co-authored-by: KANAMORI Yu <y.kinmori@gmail.com>

--- src/smarthr-ui-preset.ts
+++ src/smarthr-ui-preset.ts
@@ -26,7 +26,7 @@ export default {
       link: defaultColor.TEXT_LINK,
       background: defaultColor.BACKGROUND,
       border: theme('colors.grey.20'),
-      brand: defaultColor.BRAND,
+      brand: theme('colors.brand'),
       column: defaultColor.COLUMN,
       'base-grey': defaultColor.BASE_GREY,
       'over-background': defaultColor.OVER_BACKGROUND,
