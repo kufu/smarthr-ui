7e6f9098e495efbe2c38a15295173009f06958ea
uknmr <y.kinmori@gmail.com>
2024-03-22 02:28:51 +0900

    chore!: 使用していないアイコンを消す

--- src/components/Icon/DeprecatedIcon.tsx
+++ src/components/Icon/DeprecatedIcon.tsx
@@ -138,10 +138,6 @@ import {
 import { createIcon } from './generateIcon'
 
 /**
- * @deprecated FaAngleDoubleDownIconは旧名称のため非推奨です。FaAnglesDownIconを利用してください。
- */
-export const FaAngleDoubleDownIcon = /*#__PURE__*/ createIcon(FaAnglesDown)
-/**
  * @deprecated FaAngleDoubleLeftIconは旧名称のため非推奨です。FaAnglesLeftIconを利用してください。
  */
 export const FaAngleDoubleLeftIcon = /*#__PURE__*/ createIcon(FaAnglesLeft)
@@ -153,30 +149,6 @@ export const FaAngleDoubleRightIcon = /*#__PURE__*/ createIcon(FaAnglesRight)
  * @deprecated FaArchiveIconは旧名称のため非推奨です。FaBoxArchiveIconを利用してください。
  */
 export const FaArchiveIcon = /*#__PURE__*/ createIcon(FaBoxArchive)
-/**
- * @deprecated FaArrowCircleDownIconは旧名称のため非推奨です。FaCircleArrowDownIconを利用してください。
- */
-export const FaArrowCircleDownIcon = /*#__PURE__*/ createIcon(FaCircleArrowDown)
-/**
- * @deprecated FaArrowAltCircleDownIconは旧名称のため非推奨です。FaCircleDownIconを利用してください。
- */
-export const FaArrowAltCircleDownIcon = /*#__PURE__*/ createIcon(FaCircleDown)
-/**
- * @deprecated FaArrowAltCircleLeftIconは旧名称のため非推奨です。FaCircleLeftIconを利用してください。
- */
-export const FaArrowAltCircleLeftIcon = /*#__PURE__*/ createIcon(FaCircleLeft)
-/**
- * @deprecated FaArrowAltCircleRightIconは旧名称のため非推奨です。FaCircleRightIconを利用してください。
- */
-export const FaArrowAltCircleRightIcon = /*#__PURE__*/ createIcon(FaCircleRight)
-/**
- * @deprecated FaArrowAltCircleUpIconは旧名称のため非推奨です。FaCircleUpIconを利用してください。
- */
-export const FaArrowAltCircleUpIcon = /*#__PURE__*/ createIcon(FaCircleUp)
-/**
- * @deprecated FaArrowsAltIconは旧名称のため非推奨です。FaUpDownLeftRightIconを利用してください。
- */
-export const FaArrowsAltIcon = /*#__PURE__*/ createIcon(FaUpDownLeftRight)
 /**
  * @deprecated FaArrowsAltHIconは旧名称のため非推奨です。FaLeftRightIconを利用してください。
  */
@@ -190,14 +162,6 @@ export const FaArrowsAltVIcon = /*#__PURE__*/ createIcon(FaUpDown)
  */
 export const FaBirthdayCakeIcon = /*#__PURE__*/ createIcon(FaCakeCandles)
 /**
- * @deprecated FaBoxesIconは旧名称のため非推奨です。FaBoxesStackedIconを利用してください。
- */
-export const FaBoxesIcon = /*#__PURE__*/ createIcon(FaBoxesStacked)
-/**
- * @deprecated FaBusAltIconは旧名称のため非推奨です。FaBusSimpleIconを利用してください。
- */
-export const FaBusAltIcon = /*#__PURE__*/ createIcon(FaBusSimple)
-/**
  * @deprecated FaCalendarAltIconは旧名称のため非推奨です。FaCalendarDaysIconを利用してください。
  */
 export const FaCalendarAltIcon = /*#__PURE__*/ createIcon(FaCalendarDays)
@@ -206,49 +170,9 @@ export const FaCalendarAltIcon = /*#__PURE__*/ createIcon(FaCalendarDays)
  */
 export const FaCalendarTimesIcon = /*#__PURE__*/ createIcon(FaCalendarXmark)
 /**
- * @deprecated FaCarAltIconは旧名称のため非推奨です。FaCarRearIconを利用してください。
- */
-export const FaCarAltIcon = /*#__PURE__*/ createIcon(FaCarRear)
-/**
- * @deprecated FaCaretSquareDownIconは旧名称のため非推奨です。FaSquareCaretDownIconを利用してください。
- */
-export const FaCaretSquareDownIcon = /*#__PURE__*/ createIcon(FaSquareCaretDown)
-/**
- * @deprecated FaCaretSquareLeftIconは旧名称のため非推奨です。FaSquareCaretLeftIconを利用してください。
- */
-export const FaCaretSquareLeftIcon = /*#__PURE__*/ createIcon(FaSquareCaretLeft)
-/**
- * @deprecated FaCaretSquareRightIconは旧名称のため非推奨です。FaSquareCaretRightIconを利用してください。
- */
-export const FaCaretSquareRightIcon = /*#__PURE__*/ createIcon(FaSquareCaretRight)
-/**
- * @deprecated FaCaretSquareUpIconは旧名称のため非推奨です。FaSquareCaretUpIconを利用してください。
- */
-export const FaCaretSquareUpIcon = /*#__PURE__*/ createIcon(FaSquareCaretUp)
-/**
  * @deprecated FaCheckCircleIconは旧名称のため非推奨です。FaCircleCheckIconを利用してください。
  */
 export const FaCheckCircleIcon = /*#__PURE__*/ createIcon(FaCircleCheck)
-/**
- * @deprecated FaCheckSquareIconは旧名称のため非推奨です。FaSquareCheckIconを利用してください。
- */
-export const FaCheckSquareIcon = /*#__PURE__*/ createIcon(FaSquareCheck)
-/**
- * @deprecated FaChevronCircleDownIconは旧名称のため非推奨です。FaCircleChevronDownIconを利用してください。
- */
-export const FaChevronCircleDownIcon = /*#__PURE__*/ createIcon(FaCircleChevronDown)
-/**
- * @deprecated FaChevronCircleLeftIconは旧名称のため非推奨です。FaCircleChevronLeftIconを利用してください。
- */
-export const FaChevronCircleLeftIcon = /*#__PURE__*/ createIcon(FaCircleChevronLeft)
-/**
- * @deprecated FaChevronCircleRightIconは旧名称のため非推奨です。FaCircleChevronRightIconを利用してください。
- */
-export const FaChevronCircleRightIcon = /*#__PURE__*/ createIcon(FaCircleChevronRight)
-/**
- * @deprecated FaChevronCircleUpIconは旧名称のため非推奨です。FaCircleChevronUpIconを利用してください。
- */
-export const FaChevronCircleUpIcon = /*#__PURE__*/ createIcon(FaCircleChevronUp)
 /**
  * @deprecated FaCloudDownloadAltIconは旧名称のため非推奨です。FaCloudArrowDownIconを利用してください。
  */
@@ -261,18 +185,6 @@ export const FaCloudUploadAltIcon = /*#__PURE__*/ createIcon(FaCloudArrowUp)
  * @deprecated FaCogIconは旧名称のため非推奨です。FaGearIconを利用してください。
  */
 export const FaCogIcon = /*#__PURE__*/ createIcon(FaGear)
-/**
- * @deprecated FaCogsIconは旧名称のため非推奨です。FaGearsIconを利用してください。
- */
-export const FaCogsIcon = /*#__PURE__*/ createIcon(FaGears)
-/**
- * @deprecated FaColumnsIconは旧名称のため非推奨です。FaTableColumnsIconを利用してください。
- */
-export const FaColumnsIcon = /*#__PURE__*/ createIcon(FaTableColumns)
-/**
- * @deprecated FaCommentAltIconは旧名称のため非推奨です。FaMessageIconを利用してください。
- */
-export const FaCommentAltIcon = /*#__PURE__*/ createIcon(FaMessage)
 /**
  * @deprecated FaCompressAltIconは旧名称のため非推奨です。FaDownLeftAndUpRightToCenterIconを利用してください。
  */
@@ -306,10 +218,6 @@ export const FaExclamationTriangleIcon = /*#__PURE__*/ createIcon(FaTriangleExcl
  */
 export const FaExpandAltIcon = /*#__PURE__*/ createIcon(FaUpRightAndDownLeftFromCenter)
 /**
- * @deprecated FaExpandArrowsAltIconは旧名称のため非推奨です。FaMaximizeIconを利用してください。
- */
-export const FaExpandArrowsAltIcon = /*#__PURE__*/ createIcon(FaMaximize)
-/**
  * @deprecated FaExternalLinkAltIconは旧名称のため非推奨です。FaUpRightFromSquareIconを利用してください。
  */
 export const FaExternalLinkAltIcon = /*#__PURE__*/ createIcon(FaUpRightFromSquare)
@@ -318,25 +226,13 @@ export const FaExternalLinkAltIcon = /*#__PURE__*/ createIcon(FaUpRightFromSquar
  */
 export const FaFileAltIcon = /*#__PURE__*/ createIcon(FaFileLines)
 /**
- * @deprecated FaFileArchiveIconは旧名称のため非推奨です。FaFileZipperIconを利用してください。
- */
-export const FaFileArchiveIcon = /*#__PURE__*/ createIcon(FaFileZipper)
-/**
  * @deprecated FaFileDownloadIconは旧名称のため非推奨です。FaFileArrowDownIconを利用してください。
  */
 export const FaFileDownloadIcon = /*#__PURE__*/ createIcon(FaFileArrowDown)
 /**
- * @deprecated FaFileUploadIconは旧名称のため非推奨です。FaFileArrowUpIconを利用してください。
- */
-export const FaFileUploadIcon = /*#__PURE__*/ createIcon(FaFileArrowUp)
-/**
  * @deprecated FaGripHorizontalIconは旧名称のため非推奨です。FaGripIconを利用してください。
  */
 export const FaGripHorizontalIcon = /*#__PURE__*/ createIcon(FaGrip)
-/**
- * @deprecated FaHandPaperIconは旧名称のため非推奨です。FaHandIconを利用してください。
- */
-export const FaHandPaperIcon = /*#__PURE__*/ createIcon(FaHand)
 /**
  * @deprecated FaHistoryIconは旧名称のため非推奨です。FaClockRotateLeftIconを利用してください。
  */
@@ -346,25 +242,9 @@ export const FaHistoryIcon = /*#__PURE__*/ createIcon(FaClockRotateLeft)
  */
 export const FaHomeIcon = /*#__PURE__*/ createIcon(FaHouse)
 /**
- * @deprecated FaHospitalAltIconは旧名称のため非推奨です。FaHospitalIconを利用してください。
- */
-export const FaHospitalAltIcon = /*#__PURE__*/ createIcon(FaHospital)
-/**
- * @deprecated FaIdCardAltIconは旧名称のため非推奨です。FaIdCardClipIconを利用してください。
- */
-export const FaIdCardAltIcon = /*#__PURE__*/ createIcon(FaIdCardClip)
-/**
  * @deprecated FaInfoCircleIconは旧名称のため非推奨です。FaCircleInfoIconを利用してください。
  */
 export const FaInfoCircleIcon = /*#__PURE__*/ createIcon(FaCircleInfo)
-/**
- * @deprecated FaListAltIconは旧名称のため非推奨です。FaRectangleListIconを利用してください。
- */
-export const FaListAltIcon = /*#__PURE__*/ createIcon(FaRectangleList)
-/**
- * @deprecated FaLongArrowAltDownIconは旧名称のため非推奨です。FaDownLongIconを利用してください。
- */
-export const FaLongArrowAltDownIcon = /*#__PURE__*/ createIcon(FaDownLong)
 /**
  * @deprecated FaLongArrowAltLeftIconは旧名称のため非推奨です。FaLeftLongIconを利用してください。
  */
@@ -374,10 +254,6 @@ export const FaLongArrowAltLeftIcon = /*#__PURE__*/ createIcon(FaLeftLong)
  */
 export const FaLongArrowAltRightIcon = /*#__PURE__*/ createIcon(FaRightLong)
 /**
- * @deprecated FaLongArrowAltUpIconは旧名称のため非推奨です。FaUpLongIconを利用してください。
- */
-export const FaLongArrowAltUpIcon = /*#__PURE__*/ createIcon(FaUpLong)
-/**
  * @deprecated FaMedkitIconは旧名称のため非推奨です。FaSuitcaseMedicalIconを利用してください。
  */
 export const FaMedkitIcon = /*#__PURE__*/ createIcon(FaSuitcaseMedical)
@@ -386,33 +262,9 @@ export const FaMedkitIcon = /*#__PURE__*/ createIcon(FaSuitcaseMedical)
  */
 export const FaMinusCircleIcon = /*#__PURE__*/ createIcon(FaCircleMinus)
 /**
- * @deprecated FaMinusSquareIconは旧名称のため非推奨です。FaSquareMinusIconを利用してください。
- */
-export const FaMinusSquareIcon = /*#__PURE__*/ createIcon(FaSquareMinus)
-/**
- * @deprecated FaMobileAltIconは旧名称のため非推奨です。FaMobileScreenButtonIconを利用してください。
- */
-export const FaMobileAltIcon = /*#__PURE__*/ createIcon(FaMobileScreenButton)
-/**
- * @deprecated FaMoneyBillAltIconは旧名称のため非推奨です。FaMoneyBill1Iconを利用してください。
- */
-export const FaMoneyBillAltIcon = /*#__PURE__*/ createIcon(FaMoneyBill1)
-/**
- * @deprecated FaMoneyBillWaveAltIconは旧名称のため非推奨です。FaMoneyBill1WaveIconを利用してください。
- */
-export const FaMoneyBillWaveAltIcon = /*#__PURE__*/ createIcon(FaMoneyBill1Wave)
-/**
- * @deprecated FaMoneyCheckAltIconは旧名称のため非推奨です。FaMoneyCheckDollarIconを利用してください。
- */
-export const FaMoneyCheckAltIcon = /*#__PURE__*/ createIcon(FaMoneyCheckDollar)
-/**
  * @deprecated FaPencilAltIconは旧名称のため非推奨です。FaPencilIconを利用してください。
  */
 export const FaPencilAltIcon = /*#__PURE__*/ createIcon(FaPencil)
-/**
- * @deprecated FaPlayCircleIconは旧名称のため非推奨です。FaCirclePlayIconを利用してください。
- */
-export const FaPlayCircleIcon = /*#__PURE__*/ createIcon(FaCirclePlay)
 /**
  * @deprecated FaPlusCircleIconは旧名称のため非推奨です。FaCirclePlusIconを利用してください。
  */
@@ -426,29 +278,13 @@ export const FaPlusSquareIcon = /*#__PURE__*/ createIcon(FaSquarePlus)
  */
 export const FaPollIcon = /*#__PURE__*/ createIcon(FaSquarePollVertical)
 /**
- * @deprecated FaPollHIconは旧名称のため非推奨です。FaSquarePollHorizontalIconを利用してください。
- */
-export const FaPollHIcon = /*#__PURE__*/ createIcon(FaSquarePollHorizontal)
-/**
- * @deprecated FaPortraitIconは旧名称のため非推奨です。FaImagePortraitIconを利用してください。
- */
-export const FaPortraitIcon = /*#__PURE__*/ createIcon(FaImagePortrait)
-/**
  * @deprecated FaQuestionCircleIconは旧名称のため非推奨です。FaCircleQuestionIconを利用してください。
  */
 export const FaQuestionCircleIcon = /*#__PURE__*/ createIcon(FaCircleQuestion)
 /**
- * @deprecated FaRandomIconは旧名称のため非推奨です。FaShuffleIconを利用してください。
- */
-export const FaRandomIcon = /*#__PURE__*/ createIcon(FaShuffle)
-/**
  * @deprecated FaRedoIconは旧名称のため非推奨です。FaArrowRotateRightIconを利用してください。
  */
 export const FaRedoIcon = /*#__PURE__*/ createIcon(FaArrowRotateRight)
-/**
- * @deprecated FaRedoAltIconは旧名称のため非推奨です。FaRotateRightIconを利用してください。
- */
-export const FaRedoAltIcon = /*#__PURE__*/ createIcon(FaRotateRight)
 /**
  * @deprecated FaRegDotCircleIconは旧名称のため非推奨です。FaCircleDotIconを利用してください。
  */
@@ -474,26 +310,6 @@ export const FaSearchPlusIcon = /*#__PURE__*/ createIcon(FaMagnifyingGlassPlus)
  */
 export const FaShareAltIcon = /*#__PURE__*/ createIcon(FaShareNodes)
 /**
- * @deprecated FaShareSquareIconは旧名称のため非推奨です。FaShareFromSquareIconを利用してください。
- */
-export const FaShareSquareIcon = /*#__PURE__*/ createIcon(FaShareFromSquare)
-/**
- * @deprecated FaShieldAltIconは旧名称のため非推奨です。FaShieldHalvedIconを利用してください。
- */
-export const FaShieldAltIcon = /*#__PURE__*/ createIcon(FaShieldHalved)
-/**
- * @deprecated FaShoppingBagIconは旧名称のため非推奨です。FaBagShoppingIconを利用してください。
- */
-export const FaShoppingBagIcon = /*#__PURE__*/ createIcon(FaBagShopping)
-/**
- * @deprecated FaShoppingBasketIconは旧名称のため非推奨です。FaBasketShoppingIconを利用してください。
- */
-export const FaShoppingBasketIcon = /*#__PURE__*/ createIcon(FaBasketShopping)
-/**
- * @deprecated FaShoppingCartIconは旧名称のため非推奨です。FaCartShoppingIconを利用してください。
- */
-export const FaShoppingCartIcon = /*#__PURE__*/ createIcon(FaCartShopping)
-/**
  * @deprecated FaSignInAltIconは旧名称のため非推奨です。FaRightToBracketIconを利用してください。
  */
 export const FaSignInAltIcon = /*#__PURE__*/ createIcon(FaRightToBracket)
@@ -502,61 +318,13 @@ export const FaSignInAltIcon = /*#__PURE__*/ createIcon(FaRightToBracket)
  */
 export const FaSignOutAltIcon = /*#__PURE__*/ createIcon(FaRightFromBracket)
 /**
- * @deprecated FaSlidersHIconは旧名称のため非推奨です。FaSlidersIconを利用してください。
- */
-export const FaSlidersHIcon = /*#__PURE__*/ createIcon(FaSliders)
-/**
- * @deprecated FaSortAlphaDownIconは旧名称のため非推奨です。FaArrowDownAZIconを利用してください。
- */
-export const FaSortAlphaDownIcon = /*#__PURE__*/ createIcon(FaArrowDownAZ)
-/**
- * @deprecated FaSortAlphaUpIconは旧名称のため非推奨です。FaArrowUpAZIconを利用してください。
- */
-export const FaSortAlphaUpIcon = /*#__PURE__*/ createIcon(FaArrowUpAZ)
-/**
- * @deprecated FaSortAmountDownIconは旧名称のため非推奨です。FaArrowDownWideShortIconを利用してください。
- */
-export const FaSortAmountDownIcon = /*#__PURE__*/ createIcon(FaArrowDownWideShort)
-/**
- * @deprecated FaSortAmountUpIconは旧名称のため非推奨です。FaArrowUpWideShortIconを利用してください。
- */
-export const FaSortAmountUpIcon = /*#__PURE__*/ createIcon(FaArrowUpWideShort)
-/**
- * @deprecated FaSortNumericDownIconは旧名称のため非推奨です。FaArrowDown19Iconを利用してください。
- */
-export const FaSortNumericDownIcon = /*#__PURE__*/ createIcon(FaArrowDown19)
-/**
  * @deprecated FaSortNumericUpIconは旧名称のため非推奨です。FaArrowUp19Iconを利用してください。
  */
 export const FaSortNumericUpIcon = /*#__PURE__*/ createIcon(FaArrowUp19)
 /**
- * @deprecated FaStepBackwardIconは旧名称のため非推奨です。FaBackwardStepIconを利用してください。
- */
-export const FaStepBackwardIcon = /*#__PURE__*/ createIcon(FaBackwardStep)
-/**
- * @deprecated FaStepForwardIconは旧名称のため非推奨です。FaForwardStepIconを利用してください。
- */
-export const FaStepForwardIcon = /*#__PURE__*/ createIcon(FaForwardStep)
-/**
- * @deprecated FaStickyNoteIconは旧名称のため非推奨です。FaNoteStickyIconを利用してください。
- */
-export const FaStickyNoteIcon = /*#__PURE__*/ createIcon(FaNoteSticky)
-/**
- * @deprecated FaPaintBrushIconは旧名称のため非推奨です。FaPaintbrushIconを利用してください。
- */
-export const FaPaintBrushIcon = /*#__PURE__*/ createIcon(FaPaintbrush)
-/**
- * @deprecated FaStopCircleIconは旧名称のため非推奨です。FaCircleStopIconを利用してください。
- */
-export const FaStopCircleIcon = /*#__PURE__*/ createIcon(FaCircleStop)
-/**
  * @deprecated FaStreamIconは旧名称のため非推奨です。FaBarsStaggeredIconを利用してください。
  */
 export const FaStreamIcon = /*#__PURE__*/ createIcon(FaBarsStaggered)
-/**
- * @deprecated FaSubwayIconは旧名称のため非推奨です。FaTrainSubwayIconを利用してください。
- */
-export const FaSubwayIcon = /*#__PURE__*/ createIcon(FaTrainSubway)
 /**
  * @deprecated FaSyncIconは旧名称のため非推奨です。FaArrowsRotateIconを利用してください。
  */
@@ -566,25 +334,9 @@ export const FaSyncIcon = /*#__PURE__*/ createIcon(FaArrowsRotate)
  */
 export const FaSyncAltIcon = /*#__PURE__*/ createIcon(FaRotate)
 /**
- * @deprecated FaTabletAltIconは旧名称のため非推奨です。FaTabletScreenButtonIconを利用してください。
- */
-export const FaTabletAltIcon = /*#__PURE__*/ createIcon(FaTabletScreenButton)
-/**
  * @deprecated FaTachometerAltIconは旧名称のため非推奨です。FaGaugeIconを利用してください。
  */
 export const FaTachometerAltIcon = /*#__PURE__*/ createIcon(FaGauge)
-/**
- * @deprecated FaTasksIconは旧名称のため非推奨です。FaListCheckIconを利用してください。
- */
-export const FaTasksIcon = /*#__PURE__*/ createIcon(FaListCheck)
-/**
- * @deprecated FaThIconは旧名称のため非推奨です。FaTableCellsIconを利用してください。
- */
-export const FaThIcon = /*#__PURE__*/ createIcon(FaTableCells)
-/**
- * @deprecated FaThLargeIconは旧名称のため非推奨です。FaTableCellsLargeIconを利用してください。
- */
-export const FaThLargeIcon = /*#__PURE__*/ createIcon(FaTableCellsLarge)
 /**
  * @deprecated FaThListIconは旧名称のため非推奨です。FaTableListIconを利用してください。
  */
@@ -598,21 +350,9 @@ export const FaTimesIcon = /*#__PURE__*/ createIcon(FaXmark)
  */
 export const FaTimesCircleIcon = /*#__PURE__*/ createIcon(FaCircleXmark)
 /**
- * @deprecated FaToolsIconは旧名称のため非推奨です。FaScrewdriverWrenchIconを利用してください。
- */
-export const FaToolsIcon = /*#__PURE__*/ createIcon(FaScrewdriverWrench)
-/**
  * @deprecated FaTrashAltIconは旧名称のため非推奨です。FaTrashCanIconを利用してください。
  */
 export const FaTrashAltIcon = /*#__PURE__*/ createIcon(FaTrashCan)
-/**
- * @deprecated FaTrashRestoreIconは旧名称のため非推奨です。FaTrashArrowUpIconを利用してください。
- */
-export const FaTrashRestoreIcon = /*#__PURE__*/ createIcon(FaTrashArrowUp)
-/**
- * @deprecated FaTrashRestoreAltIconは旧名称のため非推奨です。FaTrashCanArrowUpIconを利用してください。
- */
-export const FaTrashRestoreAltIcon = /*#__PURE__*/ createIcon(FaTrashCanArrowUp)
 /**
  * @deprecated FaTruckLoadingIconは旧名称のため非推奨です。FaTruckRampBoxIconを利用してください。
  */
@@ -626,50 +366,10 @@ export const FaUndoIcon = /*#__PURE__*/ createIcon(FaArrowRotateLeft)
  */
 export const FaUndoAltIcon = /*#__PURE__*/ createIcon(FaRotateLeft)
 /**
- * @deprecated FaUnlinkIconは旧名称のため非推奨です。FaLinkSlashIconを利用してください。
- */
-export const FaUnlinkIcon = /*#__PURE__*/ createIcon(FaLinkSlash)
-/**
- * @deprecated FaUnlockAltIconは旧名称のため非推奨です。FaUnlockKeyholeIconを利用してください。
- */
-export const FaUnlockAltIcon = /*#__PURE__*/ createIcon(FaUnlockKeyhole)
-/**
  * @deprecated FaUserAltIconは旧名称のため非推奨です。FaUserLargeIconを利用してください。
  */
 export const FaUserAltIcon = /*#__PURE__*/ createIcon(FaUserLarge)
 /**
- * @deprecated FaUserAltSlashIconは旧名称のため非推奨です。FaUserLargeSlashIconを利用してください。
- */
-export const FaUserAltSlashIcon = /*#__PURE__*/ createIcon(FaUserLargeSlash)
-/**
  * @deprecated FaUserCircleIconは旧名称のため非推奨です。FaCircleUserIconを利用してください。
  */
 export const FaUserCircleIcon = /*#__PURE__*/ createIcon(FaCircleUser)
-/**
- * @deprecated FaUserCogIconは旧名称のため非推奨です。FaUserGearIconを利用してください。
- */
-export const FaUserCogIcon = /*#__PURE__*/ createIcon(FaUserGear)
-/**
- * @deprecated FaUserEditIconは旧名称のため非推奨です。FaUserPenIconを利用してください。
- */
-export const FaUserEditIcon = /*#__PURE__*/ createIcon(FaUserPen)
-/**
- * @deprecated FaUsersCogIconは旧名称のため非推奨です。FaUsersGearIconを利用してください。
- */
-export const FaUsersCogIcon = /*#__PURE__*/ createIcon(FaUsersGear)
-/**
- * @deprecated FaVolumeDownIconは旧名称のため非推奨です。FaVolumeLowIconを利用してください。
- */
-export const FaVolumeDownIcon = /*#__PURE__*/ createIcon(FaVolumeLow)
-/**
- * @deprecated FaVolumeMuteIconは旧名称のため非推奨です。FaVolumeXmarkIconを利用してください。
- */
-export const FaVolumeMuteIcon = /*#__PURE__*/ createIcon(FaVolumeXmark)
-/**
- * @deprecated FaVolumeUpIconは旧名称のため非推奨です。FaVolumeHighIconを利用してください。
- */
-export const FaVolumeUpIcon = /*#__PURE__*/ createIcon(FaVolumeHigh)
-/**
- * @deprecated FaWindowCloseIconは旧名称のため非推奨です。FaRectangleXmarkIconを利用してください。
- */
-export const FaWindowCloseIcon = /*#__PURE__*/ createIcon(FaRectangleXmark)
