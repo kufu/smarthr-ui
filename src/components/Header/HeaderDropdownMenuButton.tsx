97bef24115ca3ba86cfeec177d3c92e3c405120f
uknmr <y.kinmori@gmail.com>
2024-03-22 15:50:36 +0900

    fix: 不要な記述を修正

--- src/components/Header/HeaderDropdownMenuButton.tsx
+++ src/components/Header/HeaderDropdownMenuButton.tsx
@@ -5,7 +5,7 @@ import { DropdownMenuButton } from '../Dropdown'
 
 const headerDropdownMenuButton = tv({
   base: [
-    '[&>.smarthr-ui-DropdownMenuButton-trigger]:shr-ps-0.25] [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-border-transparent [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-bg-transparent [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-pe-0.25 [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-ps-0.25 [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-font-normal [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-text-white',
+    '[&>.smarthr-ui-DropdownMenuButton-trigger]:shr-border-transparent [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-bg-transparent [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-px-0.25 [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-font-normal [&>.smarthr-ui-DropdownMenuButton-trigger]:shr-text-white',
     // ボタンの余白分だけ微調整
     '[&>.smarthr-ui-DropdownMenuButton-trigger]:last-of-type:-shr-me-0.25',
   ],
