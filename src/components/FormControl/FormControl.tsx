2dedb621b8f06b6c59dea4e0d1b44a64e908ba2d
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2024-04-17 16:54:56 +0900

    feat: FormControl でも dangerouslyTitleHidden オプションを許容する (#4583)

--- src/components/FormControl/FormControl.tsx
+++ src/components/FormControl/FormControl.tsx
@@ -2,7 +2,7 @@ import React, { ComponentProps } from 'react'
 
 import { FormGroup } from '../FormGroup'
 
-type Props = Omit<ComponentProps<typeof FormGroup>, 'as' | 'dangerouslyTitleHidden'>
+type Props = Omit<ComponentProps<typeof FormGroup>, 'as'>
 
 export const FormControl: React.FC<Props> = FormGroup
 // 一部スタイリングが内部的に FormGroup という名前に依存しているため置き換え
