6795b6616fc1a962aa71027d420ed726e3057dc7
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-05-07 19:46:42 +0900

    fix: 強制カラーモード時にフォーカスリングが表示されるようにする

--- packages/smarthr-ui/src/components/Input/SearchInput/SearchInput.tsx
+++ packages/smarthr-ui/src/components/Input/SearchInput/SearchInput.tsx
@@ -1,4 +1,5 @@
 import React, { ComponentProps, forwardRef, useCallback, useMemo } from 'react'
+import { tv } from 'tailwind-variants'
 
 import { UnstyledButton } from '../../Button'
 import { FaCircleXmarkIcon, FaMagnifyingGlassIcon } from '../../Icon'
@@ -16,12 +17,37 @@ type Props = Omit<ComponentProps<typeof InputWithTooltip>, 'tooltipMessage' | 'p
 const ICON_ALT = '検索'
 const CLEAR_BUTTON_ALT = '削除'
 
+const searchInput = tv({
+  slots: {
+    clearButton: [
+      'smarthr-ui-SearchInput-clearButton',
+      'shr-group/clearButton',
+      'shr-me-0.5 shr-cursor-pointer',
+      'focus-visible:shr-shadow-none',
+    ],
+    clearButtonIcon: [
+      'shr-block',
+      'group-focus-visible/clearButton:shr-focus-indicator group-focus-visible/clearButton:shr-rounded-full',
+    ],
+  },
+})
+
 export const SearchInput = forwardRef<HTMLInputElement, Props>(({ decorators, ...props }, ref) => {
+  const { clearButton, clearButtonIcon } = searchInput()
+  const { clearButtonStyle, clearButtonIconStyle } = useMemo(
+    () => ({
+      clearButtonStyle: clearButton(),
+      clearButtonIconStyle: clearButtonIcon(),
+    }),
+    [clearButton, clearButtonIcon],
+  )
+
   const iconAlt = useMemo(() => decorators?.iconAlt?.(ICON_ALT) || ICON_ALT, [decorators])
   const clearButtonAlt = useMemo(
     () => decorators?.iconAlt?.(CLEAR_BUTTON_ALT) || CLEAR_BUTTON_ALT,
     [decorators],
   )
+
   const onClickClear = useCallback(
     (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => props.onClickClear?.(e),
     [props],
@@ -35,8 +61,12 @@ export const SearchInput = forwardRef<HTMLInputElement, Props>(({ decorators, ..
         prefix={<FaMagnifyingGlassIcon alt={iconAlt} color="TEXT_GREY" />}
         suffix={
           props.onClickClear && (
-            <UnstyledButton onClick={(e) => onClickClear(e)}>
-              <FaCircleXmarkIcon color="TEXT_BLACK" alt={clearButtonAlt} />
+            <UnstyledButton onClick={(e) => onClickClear(e)} className={clearButtonStyle}>
+              <FaCircleXmarkIcon
+                color="TEXT_BLACK"
+                alt={clearButtonAlt}
+                className={clearButtonIconStyle}
+              />
             </UnstyledButton>
           )
         }
