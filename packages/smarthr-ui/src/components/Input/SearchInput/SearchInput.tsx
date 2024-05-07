f0dc8ab62d21c9dd496a0812b8d372b108b222eb
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-05-07 19:32:00 +0900

    feat: onClickClear を実装する

--- packages/smarthr-ui/src/components/Input/SearchInput/SearchInput.tsx
+++ packages/smarthr-ui/src/components/Input/SearchInput/SearchInput.tsx
@@ -1,6 +1,7 @@
-import React, { ComponentProps, forwardRef, useMemo } from 'react'
+import React, { ComponentProps, forwardRef, useCallback, useMemo } from 'react'
 
-import { FaSearchIcon } from '../../Icon'
+import { UnstyledButton } from '../../Button'
+import { FaCircleXmarkIcon, FaMagnifyingGlassIcon } from '../../Icon'
 import { InputWithTooltip } from '../InputWithTooltip'
 
 import type { DecoratorsType } from '../../../types'
@@ -8,20 +9,37 @@ import type { DecoratorsType } from '../../../types'
 type Props = Omit<ComponentProps<typeof InputWithTooltip>, 'tooltipMessage' | 'prefix'> & {
   /** 入力欄の説明を紐付けるツールチップに表示するメッセージ */
   tooltipMessage: React.ReactNode
-  decorators?: DecoratorsType<'iconAlt'>
+  decorators?: DecoratorsType<'iconAlt' | 'clearButtonAlt'>
+  onClickClear?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
 const ICON_ALT = '検索'
+const CLEAR_BUTTON_ALT = '削除'
 
 export const SearchInput = forwardRef<HTMLInputElement, Props>(({ decorators, ...props }, ref) => {
   const iconAlt = useMemo(() => decorators?.iconAlt?.(ICON_ALT) || ICON_ALT, [decorators])
+  const clearButtonAlt = useMemo(
+    () => decorators?.iconAlt?.(CLEAR_BUTTON_ALT) || CLEAR_BUTTON_ALT,
+    [decorators],
+  )
+  const onClickClear = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => props.onClickClear?.(e),
+    [props],
+  )
 
   return (
     <label>
       <InputWithTooltip
         {...props}
         ref={ref}
-        prefix={<FaSearchIcon alt={iconAlt} color="TEXT_GREY" />}
+        prefix={<FaMagnifyingGlassIcon alt={iconAlt} color="TEXT_GREY" />}
+        suffix={
+          props.onClickClear && (
+            <UnstyledButton onClick={(e) => onClickClear(e)}>
+              <FaCircleXmarkIcon color="TEXT_BLACK" alt={clearButtonAlt} />
+            </UnstyledButton>
+          )
+        }
       />
     </label>
   )
