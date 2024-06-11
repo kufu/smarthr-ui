959b6d9800eed8090cad6584a4aa72c49b3bbfdc
oti <yoshiaki.ito@smarthr.co.jp>
2024-06-11 18:36:07 +0900

    feat: ErrorScreen の Storybook の `full` を `all` にして内容を調整した

--- packages/smarthr-ui/src/components/ErrorScreen/ErrorScreen.stories.tsx
+++ packages/smarthr-ui/src/components/ErrorScreen/ErrorScreen.stories.tsx
@@ -1,6 +1,8 @@
 import { StoryFn } from '@storybook/react'
 import React from 'react'
 
+import { Text } from '../Text'
+
 import { Footer } from './Footer'
 
 import { ErrorScreen } from '.'
@@ -13,27 +15,26 @@ export default {
   },
 }
 
-export const Full: StoryFn = () => (
+export const All: StoryFn = () => (
   <ErrorScreen
     title="SmartHR は現在メンテナンス中です"
     links={[
       {
-        label: 'SmartHR お知らせ',
-        url: 'http://example.com',
+        label: 'お知らせ',
+        url: 'https://support.smarthr.jp/ja/info/status/page/1/',
         target: '_blank',
       },
     ]}
   >
-    <p>
+    <Text as="p" className="shr-text-center">
       いつも SmartHR をご利用いただきありがとうございます。
       <br />
       ただいまメンテナンスのため、一時サービスを停止しております。
       <br />
       ご迷惑をおかけいたしますが、ご理解のほどよろしくお願いいたします。
-    </p>
+    </Text>
   </ErrorScreen>
 )
-Full.storyName = 'full'
 
 export const WithoutChildren: StoryFn = () => (
   <ErrorScreen
@@ -52,3 +53,4 @@ export const WithFooter: StoryFn = () => (
   <ErrorScreen links={[{ label: 'ホームへ', url: 'http://example.com' }]} footer={<Footer />} />
 )
 WithFooter.storyName = 'with footer'
+All.storyName = 'all'
