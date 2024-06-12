55de4fac6869188c56c7b84c2d78615aca3c1022
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-06-12 20:25:41 +0900

    fix: 英語のほうも lorem ipsum にする

--- packages/smarthr-ui/src/components/Balloon/Balloon.stories.tsx
+++ packages/smarthr-ui/src/components/Balloon/Balloon.stories.tsx
@@ -43,7 +43,15 @@ export const All: StoryFn = () => (
     </li>
     <li>
       <Balloon horizontal="left" vertical="middle">
-        <Txt>{'long text '.repeat(30)} </Txt>
+        <Txt>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+          been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer
+          took a galley of type and scrambled it to make a type specimen book. It has survived not
+          only five centuries, but also the leap into electronic typesetting, remaining essentially
+          unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+          PageMaker including versions of Lorem Ipsum.
+        </Txt>
       </Balloon>
       <Balloon horizontal="right" vertical="middle">
         <Txt>
