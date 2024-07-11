f2dc9100f8d52b13368f9070c112ef7ed13ae464
Qs-F <qsf@de-liker.com>
2024-07-11 19:50:36 +0900

    fix(LineClamp): fix to use tailwind-variants slots option

--- packages/smarthr-ui/src/components/LineClamp/LineClamp.tsx
+++ packages/smarthr-ui/src/components/LineClamp/LineClamp.tsx
@@ -14,19 +14,35 @@ import { Tooltip } from '../Tooltip'
 type Props = PropsWithChildren<VariantProps<typeof lineClamp>>
 type ElementProps = Omit<ComponentPropsWithRef<'span'>, keyof Props>
 
-const root = tv({
-  base: 'smarthr-ui-LineClamp shr-relative',
-})
-
 const lineClamp = tv({
+  slots: {
+    base: 'smarthr-ui-LineClamp shr-relative',
+    clampedLine: '',
+    shadowElementWrapper:
+      'shr-absolute shr-overflow-hidden shr-w-full shr-h-full shr-opacity-0 shr-invisible shr-left-0 shr-top-0 shr-whitespace-normal',
+    shadowElement: 'shr-absolute shr-w-full shr-top-0 shr-left-0',
+  },
   variants: {
     maxLines: {
-      1: 'shr-inline-block shr-w-full shr-overflow-hidden shr-overflow-ellipsis shr-whitespace-nowrap shr-align-middle',
-      2: 'shr-line-clamp-[2]',
-      3: 'shr-line-clamp-[3]',
-      4: 'shr-line-clamp-[4]',
-      5: 'shr-line-clamp-[5]',
-      6: 'shr-line-clamp-[6]',
+      1: {
+        clampedLine:
+          'shr-inline-block shr-w-full shr-overflow-hidden shr-overflow-ellipsis shr-whitespace-nowrap shr-align-middle',
+      },
+      2: {
+        clampedLine: 'shr-line-clamp-[2]',
+      },
+      3: {
+        clampedLine: 'shr-line-clamp-[3]',
+      },
+      4: {
+        clampedLine: 'shr-line-clamp-[4]',
+      },
+      5: {
+        clampedLine: 'shr-line-clamp-[5]',
+      },
+      6: {
+        clampedLine: 'shr-line-clamp-[6]',
+      },
     },
   },
 })
@@ -57,20 +73,31 @@ export const LineClamp: FC<Props & ElementProps> = ({
     throw new Error('"maxLines" cannot be less than 0.')
   }
 
-  const lineClampStyles = useMemo(() => lineClamp({ maxLines }), [maxLines])
-  const rootStyles = useMemo(() => root({ className }), [className])
+  const { baseStyle, clampedLineStyle, shadowElementWrapperStyle, shadowElementStyle } =
+    useMemo(() => {
+      const { base, clampedLine, shadowElementWrapper, shadowElement } = lineClamp({ maxLines })
+      return {
+        baseStyle: base({ className }),
+        clampedLineStyle: clampedLine(),
+        shadowElementWrapperStyle: shadowElementWrapper(),
+        shadowElementStyle: shadowElement(),
+      }
+    }, [maxLines, className])
 
   const ActualLineClamp = () => (
-    <span {...props} className={rootStyles}>
-      <span className={lineClampStyles} ref={ref}>
+    <span {...props} className={baseStyle}>
+      <span className={clampedLineStyle} ref={ref}>
         {children}
       </span>
-      {/* 切り取られていないテキストの高さを取得するための要素 */}
-      <span
-        aria-hidden
-        className="shr-absolute shr-overflow-hidden shr-w-full shr-h-full shr-opacity-0 shr-invisible shr-left-0 shr-top-0 shr-whitespace-normal"
-      >
-        <span className="shr-absolute shr-w-full shr-top-0 shr-left-0" ref={shadowRef}>
+      {/*
+        切り取られていないテキストの高さを取得するための要素
+        -webkit-line-clamp を使った要素ではjsのscrollHeightとclientHeightがフォントによっては
+        予期しない余白分が発生してしまい適切な高さが取得できないため
+        この要素で取得した高さを元にTooltipの表示を切り替えている
+        参考: https://github.com/kufu/smarthr-ui/pull/4710
+      */}
+      <span aria-hidden className={shadowElementWrapperStyle}>
+        <span className={shadowElementStyle} ref={shadowRef}>
           {children}
         </span>
       </span>
