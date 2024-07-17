8a4269e77a3c461c2202f177aed41b077309a542
KANAMORI Yu <y.kinmori@gmail.com>
2024-07-17 13:57:55 +0900

    feat: useDevice を追加 (#4781)
    * feat: useDevice を追加
    
    * fix: breakPoint.NARROW を消し Chromatic を追加

---
+++ packages/smarthr-ui/src/hooks/useDevice/useDevice.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+
+import { defaultBreakpoint } from '../../themes/createBreakpoint'
+
+const mediaQuery = {
+  narrow: `(max-width: ${defaultBreakpoint.SP}px)`,
+}
+
+export const useDevice = () => {
+  const [isNarrowView, setIsNarrowView] = useState(matchMedia(mediaQuery.narrow).matches)
+  const handleChange = (e: MediaQueryListEvent) => {
+    setIsNarrowView(e.matches)
+  }
+
+  useEffect(() => {
+    const matchQueryList = matchMedia(mediaQuery.narrow)
+
+    matchQueryList.addEventListener('change', handleChange)
+
+    return () => {
+      matchQueryList.removeEventListener('change', handleChange)
+    }
+  }, [])
+
+  return {
+    isNarrowView,
+  }
+}
