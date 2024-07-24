ca07255b85a7ab8e05327733b3afe514d5d27ae6
uknmr <y.kinmori@gmail.com>
2024-07-24 11:53:38 +0900

    feat: TabItem に suffix を追加

--- packages/smarthr-ui/src/components/TabBar/TabItem.tsx
+++ packages/smarthr-ui/src/components/TabBar/TabItem.tsx
@@ -5,17 +5,27 @@ import { isTouchDevice } from '../../libs/ua'
 import { UnstyledButton } from '../Button'
 
 const tabItem = tv({
-  base: [
-    'smarthr-ui-TabItem',
-    'shr-cursor-pointer shr-appearance-none shr-border-none shr-bg-transparent shr-px-1 shr-py-0 shr-font-bold hover:shr-bg-column hover:shr-text-black [&]:shr-box-border [&]:shr-text-grey',
-    'shr-h-[43px]', // TODO 高さを指定しないようにする
-    'disabled:shr-cursor-not-allowed disabled:shr-bg-transparent disabled:shr-text-grey/50',
-    'aria-selected:shr-border-b-shorthand aria-selected:shr-relative aria-selected:shr-border-b-[3px] aria-selected:shr-border-main aria-selected:shr-text-black',
-    'aria-selected:shr-h-[40px]', // TODO 高さを指定しないようにする
-  ],
+  slots: {
+    wrapper: [
+      'smarthr-ui-TabItem',
+      'shr-relative shr-cursor-pointer shr-px-1 shr-py-0.75 shr-font-bold shr-text-grey shr-inline-flex shr-items-center shr-gap-0.5 shr-leading-none',
+      'hover:shr-bg-white-darken hover:shr-text-black',
+      'disabled:shr-cursor-not-allowed disabled:shr-bg-transparent disabled:shr-text-grey/50',
+      'aria-selected:shr-text-black',
+      'aria-selected:before:shr-absolute aria-selected:before:shr-inset-x-0 aria-selected:before:shr-bottom-0 aria-selected:before:shr-content-[""] aria-selected:before:shr-block aria-selected:before:shr-h-0.25 aria-selected:before:shr-bg-main aria-selected:before:shr-z-1',
+    ],
+    suffixWrapper: [
+      // Badge など内包要素に依って高さが変わらないようにするため、ネガティブマージンを指定
+      '-shr-my-0.25',
+      // 内包アイコンの leading を詰める
+      '[&_.smarthr-ui-Icon]:shr-block',
+    ],
+  },
   variants: {
     isTouchDevice: {
-      false: 'shr-transition-colors',
+      false: {
+        wrapper: 'shr-transition-colors',
+      },
     },
   },
 })
@@ -23,6 +33,8 @@ const tabItem = tv({
 type Props = PropsWithChildren<{
   /** タブの ID */
   id: string
+  /** ボタン内の末尾に表示する内容 */
+  suffix?: React.ReactNode
   /** `true` のとき、タブが選択状態のスタイルになる */
   selected?: boolean
   /** `true` のとき、タブを無効状態にしてクリック不能にする */
@@ -37,13 +49,21 @@ type ElementProps = Omit<
 
 export const TabItem: FC<Props & ElementProps> = ({
   id,
+  children,
+  suffix,
   onClick,
   selected = false,
   className,
   disabled = false,
   ...props
 }) => {
-  const tabItemStyle = useMemo(() => tabItem({ className, isTouchDevice }), [className])
+  const { wrapperStyle, suffixStyle } = useMemo(() => {
+    const { wrapper, suffixWrapper } = tabItem({ isTouchDevice })
+    return {
+      wrapperStyle: wrapper({ className }),
+      suffixStyle: suffixWrapper(),
+    }
+  }, [className])
 
   return (
     <UnstyledButton
@@ -51,10 +71,13 @@ export const TabItem: FC<Props & ElementProps> = ({
       id={id}
       role="tab"
       aria-selected={selected}
-      className={tabItemStyle}
+      className={wrapperStyle}
       onClick={() => onClick(id)}
       disabled={disabled}
       type="button"
-    />
+    >
+      {children}
+      {suffix && <span className={suffixStyle}>{suffix}</span>}
+    </UnstyledButton>
   )
 }
