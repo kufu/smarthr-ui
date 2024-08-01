fc4c6bec20617497a594d5063463d8b110382758
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-08-01 14:48:23 +0900

    feat! SearchInput コンポーネントから onClickClear オプションを削除
    This reverts commit 48be8b414a97dfd0d917ac6ace05a6f58d46af36.

--- packages/smarthr-ui/src/components/Input/SearchInput/SearchInput.tsx
+++ packages/smarthr-ui/src/components/Input/SearchInput/SearchInput.tsx
@@ -1,9 +1,6 @@
-import React, { ComponentProps, forwardRef, useId, useMemo } from 'react'
-import { tv } from 'tailwind-variants'
+import React, { ComponentProps, forwardRef, useMemo } from 'react'
 
-import { UnstyledButton } from '../../Button'
-import { FaCircleXmarkIcon, FaMagnifyingGlassIcon } from '../../Icon'
-import { VisuallyHiddenText } from '../../VisuallyHiddenText'
+import { FaSearchIcon } from '../../Icon'
 import { InputWithTooltip } from '../InputWithTooltip'
 
 import type { DecoratorsType } from '../../../types'
@@ -11,72 +8,21 @@ import type { DecoratorsType } from '../../../types'
 type Props = Omit<ComponentProps<typeof InputWithTooltip>, 'tooltipMessage' | 'prefix'> & {
   /** 入力欄の説明を紐付けるツールチップに表示するメッセージ */
   tooltipMessage: React.ReactNode
-  decorators?: DecoratorsType<'iconAlt' | 'clearButtonAlt'>
-  onClickClear?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  decorators?: DecoratorsType<'iconAlt'>
 }
 
 const ICON_ALT = '検索'
-const CLEAR_BUTTON_ALT = '削除'
 
-const searchInput = tv({
-  slots: {
-    clearButton: [
-      'smarthr-ui-SearchInput-clearButton',
-      'shr-group/clearButton',
-      'shr-cursor-pointer',
-      'focus-visible:shr-shadow-none',
-    ],
-    clearButtonIcon: [
-      'shr-block',
-      'group-focus-visible/clearButton:shr-focus-indicator group-focus-visible/clearButton:shr-rounded-full',
-    ],
-  },
+export const SearchInput = forwardRef<HTMLInputElement, Props>(({ decorators, ...props }, ref) => {
+  const iconAlt = useMemo(() => decorators?.iconAlt?.(ICON_ALT) || ICON_ALT, [decorators])
+
+  return (
+    <label>
+      <InputWithTooltip
+        {...props}
+        ref={ref}
+        prefix={<FaSearchIcon alt={iconAlt} color="TEXT_GREY" />}
+      />
+    </label>
+  )
 })
-
-export const SearchInput = forwardRef<HTMLInputElement, Props>(
-  ({ decorators, onClickClear, ...props }, ref) => {
-    const { clearButton, clearButtonIcon } = searchInput()
-    const { clearButtonStyle, clearButtonIconStyle } = useMemo(
-      () => ({
-        clearButtonStyle: clearButton(),
-        clearButtonIconStyle: clearButtonIcon(),
-      }),
-      [clearButton, clearButtonIcon],
-    )
-
-    const iconAlt = useMemo(() => decorators?.iconAlt?.(ICON_ALT) || ICON_ALT, [decorators])
-    const clearButtonAlt = useMemo(
-      () => decorators?.clearButtonAlt?.(CLEAR_BUTTON_ALT) || CLEAR_BUTTON_ALT,
-      [decorators],
-    )
-
-    const defaultInptuId = useId()
-    const inputId = props.id || defaultInptuId
-
-    return (
-      <span>
-        <label htmlFor={inputId}>
-          <VisuallyHiddenText>{iconAlt}</VisuallyHiddenText>
-        </label>
-        <InputWithTooltip
-          {...props}
-          id={inputId}
-          ref={ref}
-          prefix={<FaMagnifyingGlassIcon color="TEXT_GREY" />}
-          suffix={
-            onClickClear &&
-            props.value && (
-              <UnstyledButton onClick={(e) => onClickClear(e)} className={clearButtonStyle}>
-                <FaCircleXmarkIcon
-                  color="TEXT_BLACK"
-                  alt={clearButtonAlt}
-                  className={clearButtonIconStyle}
-                />
-              </UnstyledButton>
-            )
-          }
-        />
-      </span>
-    )
-  },
-)
