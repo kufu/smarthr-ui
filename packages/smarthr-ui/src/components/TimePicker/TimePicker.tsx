08dc46e933c09debfa07978a3f27118ac6e55053
uknmr <y.kinmori@gmail.com>
2024-08-05 23:40:41 +0900

    fix: 強制カラーモードで余計な装飾が出ないように修正

--- packages/smarthr-ui/src/components/TimePicker/TimePicker.tsx
+++ packages/smarthr-ui/src/components/TimePicker/TimePicker.tsx
@@ -12,7 +12,7 @@ const timePicker = tv({
       'focus-within:shr-focus-indicator',
     ],
     input:
-      'shr-border-none shr-text-base shr-text-black shr-outline-none shr-p-[unset] shr-py-0.75 shr-h-[theme(fontSize.base)] shr-tabular-nums',
+      'shr-border-none shr-text-base shr-text-black shr-outline-none shr-outline-0 shr-p-[unset] shr-py-0.75 shr-h-[theme(fontSize.base)] shr-tabular-nums',
   },
 })
 
