3ad380d02390cb35282838e64ff03cc32982acda
uknmr <y.kinmori@gmail.com>
2024-08-06 16:17:56 +0900

    fix: 不要な slots を消す

--- packages/smarthr-ui/src/components/UpwardLink/UpwardLink.tsx
+++ packages/smarthr-ui/src/components/UpwardLink/UpwardLink.tsx
@@ -5,16 +5,11 @@ import { FaArrowLeftIcon } from '../Icon'
 import { TextLink } from '../TextLink'
 
 const upwardLink = tv({
-  slots: {
-    wrapper: 'shr-leading-none',
-    link: '',
-  },
+  base: 'shr-leading-none',
   variants: {
     indent: {
-      true: {
-        wrapper: '-shr-translate-x-1.25',
-      },
-      false: {},
+      true: '-shr-translate-x-1.25',
+      false: '',
     },
   },
 })
@@ -23,11 +18,11 @@ type Props = Omit<ComponentProps<typeof TextLink>, 'prefix' | 'suffix'> &
   VariantProps<typeof upwardLink>
 
 export const UpwardLink: React.FC<Props> = ({ indent = true, className, ...rest }) => {
-  const { wrapper, link } = upwardLink({ indent })
+  const style = upwardLink({ indent, className })
   return (
-    <div className={wrapper({ className })}>
+    <div className={style}>
       {/* eslint-disable-next-line smarthr/a11y-anchor-has-href-attribute */}
-      <TextLink {...rest} prefix={<FaArrowLeftIcon />} className={link()} />
+      <TextLink {...rest} prefix={<FaArrowLeftIcon />} />
     </div>
   )
 }
