fa4a206175547bb19518043cf060785c19a8ce54
Misako Tateiwa <misako.tateiwa@smarthr.co.jp>
2024-08-22 09:29:30 +0900

    chore: Chromaticチェック用差分のコミット

--- packages/smarthr-ui/.storybook/preview.tsx
+++ packages/smarthr-ui/.storybook/preview.tsx
@@ -19,6 +19,7 @@ const isProduction = process.env.STORYBOOK_NODE_ENV === 'production'
 if (isProduction) {
   ReactGA.initialize('G-65N1S3NF5R')
 }
+// Chromatic 確認用差分
 
 const preview: Preview = {
   parameters: {
