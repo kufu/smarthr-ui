02fd6696dd72683ad03f3f5168f7e54d17397206
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-09-16 19:56:13 +0900

    chore: Jest を Vitest に置き換え、一部単体テストを整理

--- packages/smarthr-ui/src/components/ComboBox/__tests__/useActiveOption.test.ts
+++ packages/smarthr-ui/src/components/ComboBox/__tests__/useActiveOption.test.ts
@@ -2,7 +2,7 @@ import { act, renderHook } from '@testing-library/react'
 
 import { useActiveOption } from '../useActiveOption'
 
-describe('useActiveOption', () => {
+describe.only('useActiveOption', () => {
   const initialProps = {
     options: [
       { id: 'id1', selected: false, isNew: false, item: { label: 'label1', value: 'value1' } },
