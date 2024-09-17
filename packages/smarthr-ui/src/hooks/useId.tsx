38bd8ee2c73b4a16ca03271823f405ece1f2edcd
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-09-17 12:03:04 +0900

    chore: Hooks のベストプラクティスに沿って修正

--- packages/smarthr-ui/src/hooks/useId.tsx
+++ packages/smarthr-ui/src/hooks/useId.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export const useId = (defaultId?: string): string => {
-  if (defaultId) return defaultId
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  return React.useId()
+  const id = React.useId()
+  return defaultId || id
 }
