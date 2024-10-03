66d34cc23bf36c50ba622c9cc18dc7a0fa06e77c
AtsushiM <at.mizoue@gmail.com>
2024-10-03 16:04:49 +0900

    chore: storybookのInputのerror表示にミスがあったため調整する

--- packages/smarthr-ui/src/components/Input/Input.stories.tsx
+++ packages/smarthr-ui/src/components/Input/Input.stories.tsx
@@ -68,7 +68,7 @@ export const All: StoryFn = () => (
       </FormControl>
     </li>
     <li>
-      <FormControl title="error">
+      <FormControl title="error" autoBindErrorInput={true}>
         <Input name="error" error={true} />
       </FormControl>
     </li>
@@ -80,6 +80,7 @@ export const All: StoryFn = () => (
             <code>disabled</code>は<code>error</code>よりも優先されます。
           </>
         }
+        autoBindErrorInput={true}
       >
         <Input name="disabledAndError" disabled={true} error={true} />
       </FormControl>
