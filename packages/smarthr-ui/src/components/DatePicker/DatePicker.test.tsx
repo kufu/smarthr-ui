45e810e6f11486fb0ce93c3c16d06aed9f336d1a
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2024-10-11 12:42:31 +0900

    chore: DatePicker の E2E テストを単体テストで書き直す (#4998)

---
+++ packages/smarthr-ui/src/components/DatePicker/DatePicker.test.tsx
@@ -0,0 +1,51 @@
+import { userEvent } from '@storybook/test'
+import { render, screen } from '@testing-library/react'
+import React, { act } from 'react'
+
+import { FormControl } from '../FormControl'
+
+import { DatePicker } from './DatePicker'
+
+describe('DatePicker', () => {
+  beforeEach(() => {
+    vi.spyOn(window, 'requestAnimationFrame').mockImplementation((cb) => {
+      cb(0)
+      return 0
+    })
+  })
+
+  it('カレンダーから日付を選択することができる', async () => {
+    render(
+      <FormControl title="DatePicker">
+        <DatePicker value="2024-09-25" name="date" onChangeDate={vi.fn()} />
+      </FormControl>,
+    )
+    const textbox = screen.getByRole('textbox', { name: 'DatePicker' })
+
+    // カレンダーを開く
+    act(() => textbox.click())
+
+    // カレンダーが開かれていることを確認
+    expect(textbox).toHaveProperty('ariaExpanded', 'true')
+
+    // 日にちを選択する
+    act(() => screen.getByRole('button', { name: '4' }).click())
+
+    // 日付がテキストボックスに入ることを確認
+    expect(textbox).toHaveProperty('value', '2024/09/04')
+
+    // 選択後はカレンダーが閉じられていることを確認
+    expect(textbox).toHaveProperty('ariaExpanded', 'false')
+  })
+
+  it('フォーカスを失うときにテキストボックスの内容がフォーマットされること', async () => {
+    render(
+      <FormControl title="DatePicker">
+        <DatePicker name="date" onChangeDate={vi.fn()} />
+      </FormControl>,
+    )
+    const textbox = screen.getByRole('textbox', { name: 'DatePicker' })
+    await act(() => userEvent.type(textbox, '平成4年5月29日{tab}'))
+    expect(textbox).toHaveProperty('value', '1992/05/29')
+  })
+})
