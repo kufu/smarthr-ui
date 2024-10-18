b0ac5a64e9800756af3673c097546427895f7b83
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-10-18 19:41:41 +0900

    docs: CheckBox の Story を見直し

---
+++ packages/smarthr-ui/src/components/CheckBox/stories/VRTCheckBox.stories.tsx
@@ -0,0 +1,73 @@
+import { fireEvent, within } from '@storybook/test'
+import React from 'react'
+
+import { Cluster, Stack } from '../../Layout'
+import { CheckBox } from '../CheckBox'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Forms（フォーム）/CheckBox/VRT',
+  // ペアワイズ法は使わずに総当りする
+  render: (args) => {
+    const mixed = [true, false]
+    const error = [true, false]
+    const disabled = [true, false]
+    const checked = [true, false]
+    return (
+      <Cluster>
+        {mixed.map((isMixed) =>
+          error.map((isError) =>
+            disabled.map((isDisabled) =>
+              checked.map((isChecked) => (
+                <CheckBox
+                  key={`${isMixed}-${isError}-${isDisabled}-${isChecked}`}
+                  name={`${isMixed}-${isError}-${isDisabled}-${isChecked}`}
+                  mixed={isMixed}
+                  error={isError}
+                  disabled={isDisabled}
+                  checked={isChecked}
+                />
+              )),
+            ),
+          ),
+        )}
+      </Cluster>
+    )
+  },
+  parameters: {
+    chromatic: { disableSnapshot: false },
+  },
+  tags: ['!autodocs'],
+} as Meta<typeof CheckBox>
+
+export const VRT = {}
+
+export const VRTHover = {
+  ...VRT,
+  parameters: {
+    pseudo: {
+      hover: ['.smarthr-ui-CheckBox-checkBox'],
+    },
+    // MEMO: VRT として機能していないので、解決するまでスナップショットを無効化
+    chromatic: { disableSnapshot: true },
+  },
+}
+
+export const VRTFocusVisible = {
+  ...VRT,
+  parameters: {
+    pseudo: {
+      focusVisible: ['.smarthr-ui-CheckBox-checkBox'],
+    },
+    // MEMO: VRT として機能していないので、解決するまでスナップショットを無効化
+    chromatic: { disableSnapshot: true },
+  },
+}
+
+export const VRTForcedColors: StoryObj = {
+  ...VRT,
+  parameters: {
+    chromatic: { forcedColors: 'active' },
+  },
+}
