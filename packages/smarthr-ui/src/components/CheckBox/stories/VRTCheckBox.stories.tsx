c07d19d6c1ebf28964ef945cb679aaa7edd65b01
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-10-21 10:25:14 +0900

    docs: VRTのストーリーをまとめる

--- packages/smarthr-ui/src/components/CheckBox/stories/VRTCheckBox.stories.tsx
+++ packages/smarthr-ui/src/components/CheckBox/stories/VRTCheckBox.stories.tsx
@@ -1,65 +1,68 @@
+import { Heading } from '@storybook/blocks'
 import { fireEvent, within } from '@storybook/test'
 import React from 'react'
 
 import { Cluster, Stack } from '../../Layout'
+import { Section } from '../../SectioningContent'
 import { CheckBox } from '../CheckBox'
 
 import type { Meta, StoryObj } from '@storybook/react'
 
+// ペアワイズ法は使わずに総当りする
+const mixed = [true, false]
+const error = [true, false]
+const disabled = [true, false]
+const checked = [true, false]
+const Template = (
+  <Cluster className="shr-p-1">
+    {mixed.map((isMixed) =>
+      error.map((isError) =>
+        disabled.map((isDisabled) =>
+          checked.map((isChecked) => (
+            <CheckBox
+              key={`${isMixed}-${isError}-${isDisabled}-${isChecked}`}
+              name={`${isMixed}-${isError}-${isDisabled}-${isChecked}`}
+              mixed={isMixed}
+              error={isError}
+              disabled={isDisabled}
+              checked={isChecked}
+            />
+          )),
+        ),
+      ),
+    )}
+  </Cluster>
+)
+
 export default {
   title: 'Forms（フォーム）/CheckBox/VRT',
-  // ペアワイズ法は使わずに総当りする
-  render: (args) => {
-    const mixed = [true, false]
-    const error = [true, false]
-    const disabled = [true, false]
-    const checked = [true, false]
-    return (
-      <Cluster className="shr-p-1">
-        {mixed.map((isMixed) =>
-          error.map((isError) =>
-            disabled.map((isDisabled) =>
-              checked.map((isChecked) => (
-                <CheckBox
-                  {...args}
-                  key={`${isMixed}-${isError}-${isDisabled}-${isChecked}`}
-                  name={`${isMixed}-${isError}-${isDisabled}-${isChecked}`}
-                  mixed={isMixed}
-                  error={isError}
-                  disabled={isDisabled}
-                  checked={isChecked}
-                />
-              )),
-            ),
-          ),
-        )}
-      </Cluster>
-    )
-  },
   parameters: {
     chromatic: { disableSnapshot: false },
   },
   tags: ['!autodocs'],
 } as Meta<typeof CheckBox>
 
-export const VRT = {}
-
-export const VRTHover = {
-  ...VRT,
+export const VRT = {
+  render: () => (
+    <Stack>
+      <Section>
+        <Heading>VRT</Heading>
+        <div id="vrt">{Template}</div>
+      </Section>
+      <Section>
+        <Heading>VRT Hover</Heading>
+        <div id="vrt-hover">{Template}</div>
+      </Section>
+      <Section>
+        <Heading>VRT Focus Visible</Heading>
+        <div id="vrt-focus-visible">{Template}</div>
+      </Section>
+    </Stack>
+  ),
   parameters: {
     pseudo: {
-      hover: ['.smarthr-ui-CheckBox-checkBox'],
-    },
-    // MEMO: VRT として機能していないので、解決するまでスナップショットを無効化
-    chromatic: { disableSnapshot: true },
-  },
-}
-
-export const VRTFocusVisible = {
-  ...VRT,
-  parameters: {
-    pseudo: {
-      focusVisible: ['.smarthr-ui-CheckBox-checkBox'],
+      hover: ['#vrt-hover .smarthr-ui-CheckBox-checkBox'],
+      focusVisible: ['#vrt-focus-visible .smarthr-ui-CheckBox-checkBox'],
     },
   },
 }
@@ -67,6 +70,7 @@ export const VRTFocusVisible = {
 export const VRTForcedColors: StoryObj = {
   ...VRT,
   parameters: {
+    ...VRT.parameters,
     chromatic: { forcedColors: 'active' },
   },
 }
