fd825d5d9c927bd30daa42c224ad40f5b6b5ccce
Misako Tateiwa <misako.tateiwa@smarthr.co.jp>
2024-10-23 09:57:52 +0900

    chore: gapのStoryをVRTと共通化

--- packages/smarthr-ui/src/components/Layout/Stack/stories/VRTStack.stories.tsx
+++ packages/smarthr-ui/src/components/Layout/Stack/stories/VRTStack.stories.tsx
@@ -1,25 +1,13 @@
 import { Meta } from '@storybook/react'
-import React from 'react'
 
-import { Cluster, Stack } from '../../../..'
-import { Gap } from '../../../../types'
-import { ColorBox } from '../../ComponentsForStories'
+import { Stack } from '../../../..'
+
+import { GapStory } from './Stack.stories'
 
 export default {
   title: 'Layouts（レイアウト）/Stack/VRT',
   component: Stack,
-  render: (args) => (
-    <Cluster>
-      {([0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 2, 2.5, 3, 4, 8] as Gap[]).map((gap) => (
-        <Stack {...args} key={gap} gap={gap}>
-          <ColorBox />
-          <ColorBox />
-          <ColorBox />
-          <ColorBox />
-        </Stack>
-      ))}
-    </Cluster>
-  ),
+  render: GapStory.render,
   parameters: {
     chromatic: { disableSnapshot: false },
   },
