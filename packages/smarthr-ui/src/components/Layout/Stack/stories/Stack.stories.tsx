178f9feb0b7a855b237155a804164f9ff16beeb8
Misako Tateiwa <misako.tateiwa@smarthr.co.jp>
2024-10-23 09:29:21 +0900

    chore: gapの指定修正

--- packages/smarthr-ui/src/components/Layout/Stack/stories/Stack.stories.tsx
+++ packages/smarthr-ui/src/components/Layout/Stack/stories/Stack.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/react'
 import React from 'react'
 
 import { Stack } from '..'
+import { Gap } from '../../../../types'
 import { Cluster } from '../../Cluster'
 import { ColorBox } from '../../ComponentsForStories'
 
@@ -21,95 +22,37 @@ export default {
 
 export const Playground: StoryObj<typeof Stack> = {}
 
-export const Inline: StoryObj<typeof Stack> = {
+export const InlineStory: StoryObj<typeof Stack> = {
   name: 'inline',
   args: {
     inline: true,
   },
 }
 
-export const Gap: StoryObj<typeof Stack> = {
+export const GapStory: StoryObj<typeof Stack> = {
   name: 'gap',
   render: (args) => (
     <Cluster>
-      <Stack {...args} gap={0.25}>
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </Stack>
-      <Stack {...args} gap={0.5}>
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </Stack>
-      <Stack {...args} gap={0.75}>
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </Stack>
-      <Stack {...args} gap={1}>
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </Stack>
-      <Stack {...args} gap={1.25}>
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </Stack>
-      <Stack {...args} gap={1.5}>
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </Stack>
-      <Stack {...args} gap={2}>
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </Stack>
-      <Stack {...args} gap={2.5}>
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </Stack>
-      <Stack {...args} gap={3}>
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </Stack>
-      <Stack {...args} gap={4}>
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </Stack>
-      <Stack {...args} gap={8}>
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </Stack>
+      {([0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 2, 2.5, 3, 4, 8] as Gap[]).map((gap) => (
+        <Stack {...args} key={gap} gap={gap}>
+          <ColorBox />
+          <ColorBox />
+          <ColorBox />
+          <ColorBox />
+        </Stack>
+      ))}
     </Cluster>
   ),
 }
 
-export const Align: StoryObj<typeof Stack> = {
+export const AlignStory: StoryObj<typeof Stack> = {
   name: 'align',
   args: {
     align: 'center',
   },
 }
 
-export const As: StoryObj<typeof Stack> = {
+export const AsStory: StoryObj<typeof Stack> = {
   name: 'as',
   args: {
     as: 'section',
