5ac08562f8672064f0181fbbe698d68fff7e8901
schktjm <schktjm@gmail.com>
2024-10-31 12:07:46 +0900

    feat: Dropdownからフォーカスが外れたときのテスト実装

--- packages/smarthr-ui/src/components/Dropdown/Dropdown.test.tsx
+++ packages/smarthr-ui/src/components/Dropdown/Dropdown.test.tsx
@@ -58,4 +58,20 @@ describe('Dropdown', () => {
     act(() => document.body.click())
     expect(screen.queryByRole('button', { name: 'Button1' })).toBeNull()
   })
+
+  it('ドロップダウンからフォーカスが外れるとドロップダウンが閉じること', async () => {
+    render(template)
+
+    act(() => screen.getByRole('button', { name: 'Trigger', expanded: false }).click())
+    expect(screen.getByRole('button', { name: 'Button1' })).toBeVisible()
+
+    await userEvent.tab()
+    expect(screen.getByRole('button', { name: 'Button1' })).toHaveFocus()
+    await userEvent.tab()
+    expect(screen.getByRole('button', { name: 'Button2' })).toHaveFocus()
+    await userEvent.tab()
+    expect(screen.getByRole('button', { name: 'Button3' })).toHaveFocus()
+    await userEvent.tab()
+    expect(screen.queryByRole('button', { name: 'Button1' })).toBeNull()
+  })
 })
