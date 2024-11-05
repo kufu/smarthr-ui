2d0dc7eac16035b0eca6c90a986b2842c0faa3be
uknmr <y.kinmori@gmail.com>
2024-11-05 21:16:27 +0900

    fix(HeaderLink): 新ヘッダ時の装飾を修正

--- packages/smarthr-ui/src/components/Header/HeaderLink.tsx
+++ packages/smarthr-ui/src/components/Header/HeaderLink.tsx
@@ -1,19 +1,25 @@
 import React, { ComponentProps } from 'react'
-import { tv } from 'tailwind-variants'
+import { VariantProps, tv } from 'tailwind-variants'
 
 import { TextLink } from '../TextLink'
 
-type Props = ComponentProps<typeof TextLink>
+type Props = ComponentProps<typeof TextLink> & VariantProps<typeof headerLink>
 
 const headerLink = tv({
   base: [
-    'shr-px-0.25 shr-text-white [&]:shr-shadow-none',
+    'shr-px-0.25 shr-text-white shr-shadow-none',
     'hover:shr-text-white',
     'focus-visible:shr-focus-indicator',
+    '[.smarthr-ui-Header-enable-new_&]:shr-text-black',
   ],
+  variants: {
+    enableNew: {
+      true: ['shr-px-0.5', 'hover:shr-shadow-underline'],
+    },
+  },
 })
 
-export const HeaderLink: React.FC<Props> = ({ className, ...props }) => {
-  const style = headerLink({ className })
+export const HeaderLink: React.FC<Props> = ({ enableNew, className, ...props }) => {
+  const style = headerLink({ enableNew, className })
   return <TextLink {...props} target="_blank" suffix={null} className={style} />
 }
