e241799c29cf672dc9250ef15c6f8550b23824dc
uknmr <y.kinmori@gmail.com>
2024-11-05 17:20:00 +0900

    fix(LanguageSwitcher): 新ヘッダ時の装飾を変更

--- packages/smarthr-ui/src/components/Header/LanguageSwitcher/LanguageSwitcher.tsx
+++ packages/smarthr-ui/src/components/Header/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,16 +1,15 @@
 import React, { HTMLAttributes, useMemo } from 'react'
-import { tv } from 'tailwind-variants'
+import { VariantProps, tv } from 'tailwind-variants'
 
 import { tabbable } from '../../../libs/tabbable'
 import { Button } from '../../Button'
 import { Dropdown, DropdownContent, DropdownTrigger } from '../../Dropdown'
-import { FaCaretDownIcon, FaCheckIcon, FaGlobeIcon } from '../../Icon'
+import { FaCaretDownIcon, FaCheckIcon, FaGlobeIcon, LanguageIcon } from '../../Icon'
 
 import type { DecoratorsType, LocaleMap } from '../../../types'
 
 export type Props = {
   narrow?: boolean
-  invert?: boolean
   localeMap: LocaleMap
   locale?: string
   defaultLocale?: string
@@ -18,7 +17,7 @@ export type Props = {
   decorators?: DecoratorsType<'triggerLabel' | 'checkIconAlt'>
   /** 言語切替UIで言語を選択した時に発火するコールバック関数 */
   onLanguageSelect?: (code: string) => void
-}
+} & VariantProps<typeof appLauncher>
 
 type ElementProps = Omit<HTMLAttributes<HTMLElement>, keyof Props>
 
@@ -28,23 +27,32 @@ const CHECK_ICON_ALT = '選択中'
 const appLauncher = tv({
   slots: {
     switchButton: [
-      'shr-border-transparent shr-font-normal shr-text-white shr-transition-transform shr-duration-100 [&]:shr-bg-transparent [&]:shr-px-0.25',
-      'hover:shr-border-transparent hover:[&]:shr-bg-transparent',
-      'focus-visible:shr-border-transparent focus-visible:[&]:shr-bg-transparent',
-      '[&>svg]:aria-[expanded=true]:shr-rotate-180',
+      'shr-border-transparent shr-font-normal shr-text-white shr-transition-transform shr-duration-100 shr-bg-transparent shr-px-0.25',
+      'hover:shr-border-transparent hover:shr-bg-transparent',
+      'focus-visible:shr-border-transparent focus-visible:shr-bg-transparent',
+      '[&>svg]:aria-expanded:shr-rotate-180',
     ],
     languageItemsList: ['shr-p-0.5'],
     languageItem: ['shr-flex shr-items-center'],
     languageButton: [
-      'shr-border-transparent shr-font-normal [&]:shr-justify-start [&]:shr-px-0.5',
-      '[&:not(:has(svg))]:shr-ps-[32px]',
+      'shr-border-transparent shr-font-normal shr-justify-start shr-px-0.5',
+      '[&:not(:has(svg))]:shr-ps-2',
       'hover:shr-border-transparent',
     ],
   },
   variants: {
     invert: {
       true: {
-        switchButton: '[&&&]:shr-text-black',
+        switchButton: [
+          'shr-text-black',
+          'hover:shr-bg-white-darken',
+          'focus-visible:shr-bg-white-darken',
+        ],
+      },
+    },
+    enableNew: {
+      true: {
+        switchButton: 'shr-px-0.5',
       },
     },
   },
@@ -52,13 +60,14 @@ const appLauncher = tv({
 
 export const LanguageSwitcher: React.FC<Props & ElementProps> = ({
   narrow,
-  invert,
+  enableNew,
+  invert = enableNew,
   decorators,
   localeMap,
   locale,
   defaultLocale,
   onLanguageSelect,
-  ...props
+  ...rest
 }) => {
   const locales = useMemo(() => Object.entries(localeMap), [localeMap])
   const triggerLabel = useMemo(
@@ -109,23 +118,23 @@ export const LanguageSwitcher: React.FC<Props & ElementProps> = ({
   }
 
   const NarrowTrigger = (
-    <Button square suffix={<FaCaretDownIcon />} className={switchButton({ invert })}>
-      <FaGlobeIcon alt={triggerLabel} />
+    <Button square suffix={<FaCaretDownIcon />} className={switchButton({ invert, enableNew })}>
+      {invert ? <LanguageIcon alt={triggerLabel} /> : <FaGlobeIcon alt={triggerLabel} />}
     </Button>
   )
 
   const Trigger = (
     <Button
-      prefix={<FaGlobeIcon />}
+      prefix={invert ? <LanguageIcon /> : <FaGlobeIcon />}
       suffix={<FaCaretDownIcon />}
-      className={switchButton({ invert })}
+      className={switchButton({ invert, enableNew })}
     >
       {triggerLabel}
     </Button>
   )
 
   return (
-    <Dropdown {...props}>
+    <Dropdown {...rest}>
       <DropdownTrigger>{narrow ? NarrowTrigger : Trigger}</DropdownTrigger>
       <DropdownContent onKeyDown={handleKeyDown} role="presentation">
         <ul className={languageItemsList()}>
