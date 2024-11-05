225eb8fd48da077694a25000d29dff63ac86112a
uknmr <y.kinmori@gmail.com>
2024-11-05 17:18:17 +0900

    fix(tailwind.config): shadow の twMerge 設定を調整

--- packages/smarthr-ui/src/smarthr-ui-preset.ts
+++ packages/smarthr-ui/src/smarthr-ui-preset.ts
@@ -13,6 +13,21 @@ import type { Config } from 'tailwindcss'
 defaultConfig.twMergeConfig = {
   prefix: 'shr-',
   classGroups: {
+    boxShadow: [
+      {
+        shadow: [
+          'layer-0',
+          'layer-1',
+          'layer-2',
+          'layer-3',
+          'layer-4',
+          'outline',
+          'underline',
+          'input-hover',
+          'none',
+        ],
+      },
+    ],
     'border-shorthand': [
       'border-shorthand',
       'border-t-shorthand',
