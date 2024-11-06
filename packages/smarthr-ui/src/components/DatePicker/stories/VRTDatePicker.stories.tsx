30aa30fa9bd25e6b93942eae728c9045fd2acb67
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-11-06 11:31:31 +0900

    docs(DatePicker): Story を見直し

---
+++ packages/smarthr-ui/src/components/DatePicker/stories/VRTDatePicker.stories.tsx
@@ -0,0 +1,112 @@
+/* eslint-disable smarthr/a11y-input-has-name-attribute */
+/* eslint-disable smarthr/a11y-prohibit-input-placeholder */
+/* eslint-disable smarthr/a11y-input-in-form-control */
+import { userEvent, within } from '@storybook/test'
+import dayjs from 'dayjs'
+import React from 'react'
+
+import { Cluster, Stack } from '../../Layout'
+import { DatePicker } from '../DatePicker'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Forms（フォーム）/DatePicker/VRT',
+  /* ペアワイズ法による網羅 */
+  component: DatePicker,
+  render: (args) => {
+    const value = '2024/11/06'
+    const placeholder = '日付を入力してください'
+    const width = '100%'
+    const showAlternative = (_: Date | null) => <div>alt</div>
+    const formatDate = (date: Date | null) => dayjs(date).format('YYYY年MM月DD')
+
+    const matrics = [
+      { error: false, disabled: false },
+      { error: false, disabled: true },
+      { error: true, disabled: false },
+      { error: true, disabled: true },
+    ]
+
+    return (
+      <Cluster>
+        {matrics.map((m) => (
+          <>
+            <DatePicker {...args} error={m.error} disabled={m.disabled} />
+            <DatePicker error={m.error} disabled={m.disabled} placeholder={placeholder} />
+            <DatePicker error={m.error} disabled={m.disabled} value={value} />
+            <DatePicker error={m.error} disabled={m.disabled} width={width} />
+            <DatePicker
+              error={m.error}
+              disabled={m.disabled}
+              value={value}
+              showAlternative={showAlternative}
+            />
+            <DatePicker
+              error={m.error}
+              disabled={m.disabled}
+              value={value}
+              formatDate={formatDate}
+            />
+          </>
+        ))}
+      </Cluster>
+    )
+  },
+  parameters: {
+    chromatic: { disableSnapshot: false },
+  },
+  tags: ['!autodocs'],
+} satisfies Meta<typeof DatePicker>
+
+export const VRT = {}
+
+export const VRTForcedColors: StoryObj = {
+  ...VRT,
+  parameters: {
+    chromatic: { forcedColors: 'active' },
+  },
+}
+
+export const VRTExpanded: StoryObj = {
+  render: (args) => <DatePicker {...args} />,
+  args: {
+    value: '2024/11/06',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    await userEvent.click(canvas.getByRole('textbox'))
+  },
+}
+
+export const VRTExpandedForcedColor: StoryObj = {
+  ...VRTExpanded,
+  parameters: {
+    chromatic: { forcedColors: 'active' },
+  },
+}
+
+export const VRTExpandedFromTo: StoryObj = {
+  render: (args) => <DatePicker {...args} />,
+  args: {
+    value: '2024/11/06',
+    from: dayjs('2024/11/03').toDate(),
+    to: dayjs('2024/11/09').toDate(),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    await userEvent.click(canvas.getByRole('textbox'))
+  },
+}
+
+export const VRTExpandedFromToForcedColor: StoryObj = {
+  ...VRTExpandedFromTo,
+  parameters: {
+    chromatic: { forcedColors: 'active' },
+  },
+}
+
+export const VRTExpandedBottom: StoryObj = {
+  ...VRTExpanded,
+  render: (args) => <DatePicker {...args} className="shr-fixed shr-bottom-0" />,
+}
