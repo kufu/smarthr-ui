abe4a11db905e6a5c006213537e34347e53a996f
KANAMORI Yu <y.kinmori@gmail.com>
2024-11-06 17:54:38 +0900

    docs(Chip): Story を見直し (#5078)

--- packages/smarthr-ui/src/components/Chip/Chip.tsx
+++ packages/smarthr-ui/src/components/Chip/Chip.tsx
@@ -5,21 +5,24 @@ type Props = PropsWithChildren<VariantProps<typeof chip> & ComponentPropsWithout
 
 const chip = tv({
   base: [
-    'smarthr-ui-Chip shr-border-shorthand shr-rounded-full shr-leading-none',
+    'smarthr-ui-Chip',
+    'shr-border-shorthand shr-rounded-full shr-bg-white shr-text-black shr-leading-none',
     'contrast-more:shr-border-high-contrast',
   ],
   variants: {
     size: {
-      s: ['shr-text-sm', 'shr-px-0.5', 'shr-py-0.25'],
+      s: 'shr-text-sm shr-px-0.5 shr-py-0.25',
     },
     disabled: {
       true: 'shr-bg-white/50 shr-text-disabled',
-      false: 'shr-bg-white shr-text-black',
     },
+  },
+  defaultVariants: {
+    size: 's',
   },
 })
 
-export const Chip: FC<Props> = ({ className, size = 's', disabled, ...props }) => {
+export const Chip: FC<Props> = ({ size, disabled, className, ...props }) => {
   const styles = useMemo(() => chip({ size, disabled, className }), [size, disabled, className])
   return <span {...props} className={styles} />
 }
