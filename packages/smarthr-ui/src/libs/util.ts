d94d7183c2c109115e50b495fe873e1637a2a475
Qs-F <qsf@de-liker.com>
2024-11-07 14:09:35 +0900

    fix: typescript <v5.1での型エラーを修正

--- packages/smarthr-ui/src/libs/util.ts
+++ packages/smarthr-ui/src/libs/util.ts
@@ -1,6 +1,6 @@
 import React, {
+  type FC,
   type PropsWithoutRef,
-  type ReactNode,
   type Ref,
   type RefAttributes,
   forwardRef,
@@ -11,8 +11,10 @@ export const includeDisabledTrigger = (trigger: React.ReactNode) =>
     (bool: boolean) => bool,
   )
 
+type Element = ReturnType<FC>
+
 /** forwardRef でジェネリクスを使うためのラッパー
  * via https://www.totaltypescript.com/forwardref-with-generic-components */
 export const genericsForwardRef = <T, P = object>(
-  render: (props: PropsWithoutRef<P>, ref: Ref<T>) => ReactNode,
-): ((props: P & RefAttributes<T>) => ReactNode) => forwardRef(render) as any
+  render: (props: PropsWithoutRef<P>, ref: Ref<T>) => Element,
+): ((props: P & RefAttributes<T>) => Element) => forwardRef(render) as any
