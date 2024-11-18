5eb35514ec0ed142eec772d5fedd8e699f0ba9a6
AtsushiM <at.mizoue@gmail.com>
2024-11-19 08:16:02 +0900

    chore: TimePicker.stories.tsxを修正

--- packages/smarthr-ui/src/components/Picker/TimePicker.stories.tsx
+++ packages/smarthr-ui/src/components/Picker/TimePicker.stories.tsx
@@ -11,33 +11,35 @@ export default {
 }
 
 const _Template: StoryFn = (args) => (
-  <ul className="shr-list-none shr-space-y-1">
-    <li>
-      <FormControl title="時刻">
-        <TimePicker {...args} name="time" />
-      </FormControl>
-    </li>
-    <li>
-      <FormControl title="非活性">
-        <TimePicker {...args} disabled={true} name="disabled" />
-      </FormControl>
-    </li>
-    <li>
-      <FormControl title="エラーあり" autoBindErrorInput={false}>
-        <TimePicker {...args} error={true} name="error" />
-      </FormControl>
-    </li>
-    <li>
-      <FormControl title="エラーあり with FormControl" errorMessages={['エラーメッセージ']}>
-        <TimePicker {...args} name="error_With_formcontrol" />
-      </FormControl>
-    </li>
-    <li>
-      <FormControl title="読み取り専用">
-        <TimePicker {...args} readOnly={true} name="read_only" />
-      </FormControl>
-    </li>
-  </ul>
+  <form>
+    <ul className="shr-list-none shr-space-y-1">
+      <li>
+        <FormControl title="時刻">
+          <TimePicker {...args} name="time" />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="非活性">
+          <TimePicker {...args} disabled={true} name="disabled" />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="エラーあり" autoBindErrorInput={false}>
+          <TimePicker {...args} error={true} name="error" />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="エラーあり with FormControl" errorMessages={['エラーメッセージ']}>
+          <TimePicker {...args} name="error_With_formcontrol" />
+        </FormControl>
+      </li>
+      <li>
+        <FormControl title="読み取り専用">
+          <TimePicker {...args} readOnly={true} name="read_only" />
+        </FormControl>
+      </li>
+    </ul>
+  </form>
 )
 
 export const Default = _Template.bind({})
