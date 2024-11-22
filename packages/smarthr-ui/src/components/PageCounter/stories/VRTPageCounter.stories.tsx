485ef35ec9c23b61486eb85ff16444f3c444ed76
uknmr <y.kinmori@gmail.com>
2024-11-22 14:56:14 +0900

    docs(VRTPageCounter): total がない場合と start / end の桁が多い場合を追加

--- packages/smarthr-ui/src/components/PageCounter/stories/VRTPageCounter.stories.tsx
+++ packages/smarthr-ui/src/components/PageCounter/stories/VRTPageCounter.stories.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
 
+import { Stack } from '../../Layout'
 import { PageCounter } from '../PageCounter'
 
 import type { Meta, StoryObj } from '@storybook/react'
 
+const cases = [
+  { start: 1, end: 50 },
+  { start: 1, end: 50, total: 5000 },
+  { start: 4951, end: 5000, total: 5000 },
+]
+
 export default {
   title: 'Data Display（データ表示）/PageCounter/VRT',
-  render: (args) => <PageCounter {...args} />,
-  args: {
-    start: 1,
-    end: 50,
-    total: 5000,
-  },
+  render: (args) => (
+    <Stack>
+      {cases.map((props, i) => (
+        <PageCounter {...props} {...args} key={i} />
+      ))}
+    </Stack>
+  ),
   parameters: {
     chromatic: { disableSnapshot: false },
   },
