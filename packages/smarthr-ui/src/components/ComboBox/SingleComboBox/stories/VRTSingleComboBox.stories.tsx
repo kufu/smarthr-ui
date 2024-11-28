f3662606a83f30c7226eec7b1744b1cf70ac1616
Misako Tateiwa <misako.tateiwa@smarthr.co.jp>
2024-11-28 22:02:02 +0900

    fix: 指摘修正

--- packages/smarthr-ui/src/components/ComboBox/SingleComboBox/stories/VRTSingleComboBox.stories.tsx
+++ packages/smarthr-ui/src/components/ComboBox/SingleComboBox/stories/VRTSingleComboBox.stories.tsx
@@ -1,58 +1,12 @@
+/* eslint-disable smarthr/a11y-input-in-form-control */
 import { Meta, StoryObj } from '@storybook/react'
 import { userEvent, within } from '@storybook/test'
 import React from 'react'
 
-import { FormControl } from '../../../FormControl'
-import { FaCirclePlusIcon } from '../../../Icon'
 import { Stack } from '../../../Layout'
 import { SingleComboBox } from '../SingleComboBox'
 
-const defaultItems = {
-  'option 1': {
-    label: 'option 1',
-    value: 'value-1',
-    data: {
-      name: 'test',
-      age: 23,
-    },
-  },
-  'option 2': {
-    label: 'option 2',
-    value: 'value-2',
-    data: {
-      name: 'test 2',
-      age: 34,
-    },
-  },
-  'option 3': {
-    label: 'option 3',
-    value: 'value-3',
-    disabled: true,
-  },
-  'option 4': {
-    label: 'option 4',
-    value: 'value-4',
-  },
-  'option 5': {
-    label: 'option 5',
-    value: 'value-5',
-  },
-  'アイテムのラベルが長い場合（ダミーテキストダミーテキストダミーテキストダミーテキスト）': {
-    label: 'アイテムのラベルが長い場合（ダミーテキストダミーテキストダミーテキストダミーテキスト）',
-    value: 'value-6',
-  },
-  アイテムのラベルがReactNodeの場合: {
-    label: (
-      <Stack as="span" gap={0.25}>
-        <span>アイテムのラベルがReactNodeの場合</span>
-        <span>（ダミーテキストダミーテキストダミーテキストダミーテキスト）</span>
-      </Stack>
-    ),
-    value: 'value-7',
-  },
-}
-
-const prefixes = { なし: '', あり: <FaCirclePlusIcon /> }
+import { defaultItems, prefixes } from './SingleComboBox.stories'
 
 /* pict singleComboBox.pict
  * disabled        error   width   prefix  selectedItem
@@ -94,35 +48,35 @@ const _cases: Array<Omit<Parameters<typeof SingleComboBox>[0], 'items'>> = [
   },
 ]
 
+const playSingle = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+  const canvas = within(canvasElement)
+  const textboxes = await canvas.findAllByRole('combobox')
+  await textboxes[textboxes.length - 1].focus()
+  const body = canvasElement.ownerDocument.body
+  const option = await within(body).findByText('option 1')
+  await userEvent.hover(option)
+  const helpMessage = await within(body).findAllByText('入力でフィルタリングできます。')
+  await userEvent.click(helpMessage[0]) // カーソルの点滅によるVRTのフレーキーを避けるためにフォーカスを移動する
+}
+
 export default {
   title: 'Forms（フォーム）/SingleComboBox/VRT',
   component: SingleComboBox,
   render: (args) => (
-    <Stack align="flex-start" gap={2}>
-      <form>
-        <FormControl className="shr-mb-[15rem]" title="デフォルト" dangerouslyTitleHidden>
-          <SingleComboBox
-            name="default"
-            items={Object.values(defaultItems)}
-            dropdownHelpMessage="入力でフィルタリングできます。"
-            selectedItem={null}
-          />
-        </FormControl>
-      </form>
-      <Stack align="flex-start">
-        {_cases.map((props, i) => (
-          <form key={i}>
-            <FormControl title="VRT 用の Story です" dangerouslyTitleHidden>
-              <SingleComboBox {...args} {...props} items={Object.values(defaultItems)} />
-            </FormControl>
-          </form>
-        ))}
-      </Stack>
+    <Stack align="flex-start" gap={2} className="shr-h-screen">
+      {_cases.map((props, i) => (
+        <SingleComboBox {...args} {...props} items={Object.values(defaultItems)} key={i} />
+      ))}
+      <SingleComboBox
+        {...args}
+        name="default"
+        items={Object.values(defaultItems)}
+        dropdownHelpMessage="入力でフィルタリングできます。"
+        selectedItem={null}
+      />
     </Stack>
   ),
-  args: {
-    items: Object.values(defaultItems),
-  },
+  play: playSingle,
   parameters: {
     withTheming: true,
     chromatic: { disableSnapshot: false },
@@ -131,19 +85,6 @@ export default {
 } as Meta<typeof SingleComboBox>
 
 export const VRT: StoryObj<typeof SingleComboBox> = {}
-
-const playSingle = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
-  const canvas = within(canvasElement)
-  const textboxes = await canvas.findAllByRole('combobox')
-  await textboxes[0].focus()
-  const body = canvasElement.ownerDocument.body
-  const option = await within(body).findByText('option 1')
-  await userEvent.hover(option)
-  const helpMessage = await within(body).findAllByText('入力でフィルタリングできます。')
-  await userEvent.click(helpMessage[0]) // カーソルの点滅によるVRTのフレーキーを避けるためにフォーカスを移動する
-}
-
-VRT.play = playSingle
 
 export const VRTForcedColors: StoryObj<typeof SingleComboBox> = {
   ...VRT,
