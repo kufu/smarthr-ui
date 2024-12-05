9e6b9e35f77dca09841c04c961e9f3504c2abc08
Misako Tateiwa <misako.tateiwa@smarthr.co.jp>
2024-12-05 14:02:19 +0900

    docs(MultiComboBox): Story を見直し

--- packages/smarthr-ui/src/components/ComboBox/MultiComboBox/stories/MultiCombobox.stories.tsx
+++ packages/smarthr-ui/src/components/ComboBox/MultiComboBox/stories/MultiCombobox.stories.tsx
@@ -1,23 +1,13 @@
-import { action } from '@storybook/addon-actions'
-import { StoryFn } from '@storybook/react'
-import React, { ReactNode, useCallback, useState } from 'react'
+/* eslint-disable smarthr/a11y-input-in-form-control */
+import { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
 
-import { FormControl } from '../../../FormControl'
 import { Stack } from '../../../Layout'
 import { MultiComboBox } from '../MultiComboBox'
 
-export default {
-  title: 'Forms（フォーム）/MultiComboBox',
-  component: MultiComboBox,
-  parameters: {
-    docs: {
-      source: { type: 'dynamic' },
-    },
-  },
-}
-
-const defaultItems = [
-  {
+// eslint-disable-next-line storybook/prefer-pascal-case
+export const defaultItems = {
+  'option 1': {
     label: 'option 1',
     value: 'value-1',
     data: {
@@ -25,7 +15,7 @@ const defaultItems = [
       age: 23,
     },
   },
-  {
+  'option 2': {
     label: 'option 2',
     value: 'value-2',
     data: {
@@ -33,24 +23,24 @@ const defaultItems = [
       age: 34,
     },
   },
-  {
+  'option 3': {
     label: 'option 3',
     value: 'value-3',
     disabled: true,
   },
-  {
+  'option 4': {
     label: 'option 4',
     value: 'value-4',
   },
-  {
+  'option 5': {
     label: 'option 5',
     value: 'value-5',
   },
-  {
+  'アイテムのラベルが長い場合（ダミーテキストダミーテキストダミーテキストダミーテキスト）': {
     label: 'アイテムのラベルが長い場合（ダミーテキストダミーテキストダミーテキストダミーテキスト）',
     value: 'value-6',
   },
-  {
+  アイテムのラベルがReactNodeの場合: {
     label: (
       <Stack as="span" gap={0.25}>
         <span>アイテムのラベルがReactNodeの場合</span>
@@ -59,236 +49,82 @@ const defaultItems = [
     ),
     value: 'value-7',
   },
-]
+}
 
-const manyItems = Array.from({ length: 2000 }).map((_, i) => ({
-  label: `option ${i}`,
-  value: `option ${i}`,
-}))
-
-type Item = { label: ReactNode; value: string; disabled?: boolean; data?: any }
-
-export const MultiCombobox: StoryFn = () => {
-  const [items, setItems] = useState<Item[]>(defaultItems)
-  const [selectedItems, setSelectedItems] = useState<Item[]>([])
-  const [seq, setSeq] = useState(0)
-  const [controlledInputValue, setControlledInputValue] = useState<string>('')
-
-  const handleSelectItem = useCallback(
-    (item: Item) => {
-      action('onSelect')(item)
-      setSelectedItems([...selectedItems, item])
+export default {
+  title: 'Forms（フォーム）/MultiComboBox',
+  component: MultiComboBox,
+  args: {
+    items: Object.values(defaultItems),
+    selectedItems: [],
+  },
+  argTypes: {
+    items: { control: 'object' },
+    selectedItems: { control: 'object' },
+  },
+  parameters: {
+    parameters: {
+      chromatic: { disableSnapshot: true },
     },
-    [selectedItems],
-  )
-  const handleDelete = useCallback(
-    (deleted: Item) => {
-      action('onDelete')()
-      setSelectedItems(selectedItems.filter((item) => item.value !== deleted.value))
-    },
-    [selectedItems],
-  )
-  const handleAddItem = useCallback(
-    (label: string) => {
-      action('onAdd')(label)
-      const newItem = {
-        label,
-        value: label,
-      }
-      setItems([...items, newItem])
-      setSelectedItems([...selectedItems, newItem])
-      setSeq(seq + 1)
-    },
-    [items, selectedItems, seq],
-  )
+  },
+  excludeStories: ['defaultItems'],
+} as Meta<typeof MultiComboBox>
 
-  return (
-    <Stack>
-      <FormControl title="デフォルト">
-        <MultiComboBox
-          name="default"
-          items={items}
-          selectedItems={selectedItems}
-          dropdownHelpMessage="入力でフィルタリングできます。"
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-          onChangeSelected={(selected) => {
-            action('onChangeSelected')(selected)
-            setSelectedItems(selected)
-          }}
-          onFocus={action('onFocus')}
-          onBlur={action('onBlur')}
-          data-test="multi-combobox-default"
-        />
-      </FormControl>
-      <FormControl title="アイテム追加可能">
-        <MultiComboBox
-          name="onAdd"
-          items={items}
-          selectedItems={selectedItems}
-          dropdownHelpMessage="新しいアイテムを追加できます。"
-          creatable
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-          onAdd={handleAddItem}
-          data-test="multi-combobox-creatable"
-        />
-      </FormControl>
-      <FormControl title="Disabled">
-        <MultiComboBox
-          name="disabled"
-          items={items}
-          selectedItems={selectedItems}
-          dropdownHelpMessage="Disabled なコンボボックス"
-          disabled
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-          data-test="multi-combobox-disabled"
-        />
-      </FormControl>
-      <FormControl title="必須">
-        <MultiComboBox
-          name="required"
-          items={items}
-          selectedItems={selectedItems}
-          dropdownHelpMessage="Required なコンボボックス"
-          required
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-          data-test="multi-combobox-disabled"
-        />
-      </FormControl>
-      <FormControl title="その他属性">
-        <MultiComboBox
-          name="inputAttributes"
-          items={items}
-          selectedItems={selectedItems}
-          aria-label="inputAttributes"
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-          data-test="multi-combobox-disabled"
-        />
-      </FormControl>
-      <FormControl title="エラー表示" autoBindErrorInput={false}>
-        <MultiComboBox
-          name="error"
-          items={items}
-          selectedItems={selectedItems}
-          dropdownHelpMessage="入力でフィルタリングできます。"
-          error
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-        />
-      </FormControl>
-      <FormControl title="エラー表示 with FormControl" errorMessages={['エラーメッセージ']}>
-        <MultiComboBox
-          name="error"
-          items={items}
-          selectedItems={selectedItems}
-          dropdownHelpMessage="入力でフィルタリングできます。"
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-        />
-      </FormControl>
-      <FormControl title="選択済みアイテムを省略表示 + ツールチップ">
-        <MultiComboBox
-          name="selectedItemEllipsis"
-          items={items}
-          selectedItems={selectedItems}
-          dropdownHelpMessage="入力でフィルタリングできます。"
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-          selectedItemEllipsis
-        />
-      </FormControl>
-      <FormControl title="読込中">
-        <MultiComboBox
-          name="isLoading"
-          items={items}
-          selectedItems={selectedItems}
-          dropdownHelpMessage="入力でフィルタリングできます。"
-          isLoading
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-        />
-      </FormControl>
-      <FormControl title="文言変更">
-        <MultiComboBox
-          name="decorator"
-          items={items}
-          selectedItems={selectedItems}
-          dropdownHelpMessage="入力でフィルタリングできます"
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-          decorators={{
-            noResultText: (text) => `no result.(${text})`,
-            destroyButtonIconAlt: (text) => `destroy.(${text})`,
-            selectedListAriaLabel: (text) => `selected item list.(${text})`,
-          }}
-        />
-      </FormControl>
-      <FormControl title="選択解除ボタンを非表示">
-        <MultiComboBox
-          name="invisible_unselected_button"
-          items={items}
-          selectedItems={selectedItems.map((item) => ({ ...item, deletable: false }))}
-          dropdownHelpMessage="入力でフィルタリングできます。"
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-          data-test="multi-combobox-undeletable"
-        />
-      </FormControl>
-      <FormControl title="テキストボックスの挙動を制御">
-        <MultiComboBox
-          name="textbox_controllable"
-          items={items}
-          selectedItems={selectedItems}
-          dropdownHelpMessage="入力でフィルタリングできます。"
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-          onChangeSelected={(selected) => {
-            action('onChangeSelected')(selected)
-            setSelectedItems(selected)
-          }}
-          inputValue={controlledInputValue}
-          onChangeInput={(e) => {
-            setControlledInputValue(e.target.value)
-          }}
-          onBlur={() => setControlledInputValue('')}
-        />
-      </FormControl>
-      <FormControl title="100%幅">
-        <MultiComboBox
-          name="widthFull"
-          items={items}
-          selectedItems={selectedItems}
-          width="100%"
-          dropdownHelpMessage="入力でフィルタリングできます。"
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-        />
-      </FormControl>
-      <FormControl title="ドロップダウンリストの幅を指定(38文字)">
-        <MultiComboBox
-          name="dropdownWidth38em"
-          items={items}
-          selectedItems={selectedItems}
-          dropdownWidth="38em"
-          dropdownHelpMessage="入力でフィルタリングできます"
-          onDelete={handleDelete}
-          onSelect={handleSelectItem}
-        />
-      </FormControl>
-      <FormControl title="アイテム数が多い時">
-        <MultiComboBox
-          name="manyItems"
-          items={manyItems}
-          selectedItems={[]}
-          dropdownHelpMessage="入力でフィルタリングできます。"
-          onSelect={action('onSelect')}
-          data-test="multi-combobox-many"
-        />
-      </FormControl>
-    </Stack>
-  )
+export const Playground: StoryObj<typeof MultiComboBox> = {}
+
+export const SelectedItems: StoryObj<typeof MultiComboBox> = {
+  name: 'selectedItems',
+  args: {
+    selectedItems: [defaultItems['option 1'], defaultItems['option 4']],
+  },
+}
+
+export const Disabled: StoryObj<typeof MultiComboBox> = {
+  name: 'disabled',
+  args: {
+    disabled: true,
+  },
+}
+
+export const Error: StoryObj<typeof MultiComboBox> = {
+  name: 'error',
+  args: {
+    error: true,
+  },
+}
+
+export const Creatable: StoryObj<typeof MultiComboBox> = {
+  name: 'creatable',
+  args: {
+    creatable: true,
+    dropdownHelpMessage: '新しいアイテムを追加できます。',
+  },
+}
+
+export const IsLoading: StoryObj<typeof MultiComboBox> = {
+  name: 'isLoading',
+  args: {
+    isLoading: true,
+  },
+}
+
+export const Width: StoryObj<typeof MultiComboBox> = {
+  name: 'width',
+  args: {
+    width: '20rem',
+  },
+}
+
+export const DropdownHelpMessage: StoryObj<typeof MultiComboBox> = {
+  name: 'dropdownHelpMessage',
+  args: {
+    dropdownHelpMessage: 'ヘルプメッセージ',
+  },
+}
+
+export const DropdownWidth: StoryObj<typeof MultiComboBox> = {
+  name: 'dropdownWidth',
+  args: {
+    dropdownWidth: '30rem',
+  },
 }
--- packages/smarthr-ui/src/components/ComboBox/MultiComboBox/stories/VRTMultiCombobox.stories.tsx
+++ packages/smarthr-ui/src/components/ComboBox/MultiComboBox/stories/VRTMultiCombobox.stories.tsx
@@ -1,30 +1,53 @@
-import { StoryFn } from '@storybook/react'
+/* eslint-disable smarthr/a11y-input-in-form-control */
+import { Meta, StoryObj } from '@storybook/react'
 import { userEvent, within } from '@storybook/test'
-import React from 'react'
+import React, { useState } from 'react'
 
-import { InformationPanel } from '../../../InformationPanel'
 import { Stack } from '../../../Layout'
+import { ComboBoxItem } from '../../types'
 import { MultiComboBox } from '../MultiComboBox'
 
-import { MultiCombobox as StoriesMultiComboBox } from './MultiCombobox.stories'
+import { defaultItems } from './MultiCombobox.stories'
 
-export default {
-  title: 'Forms（フォーム）/MultiComboBox',
-  component: MultiComboBox,
-  parameters: {
-    withTheming: true,
+/*
+ * pict multiComboBox.pict
+ * disabled        error   width   selectedItems
+ * false   true    なし    なし
+ * false   false   あり    複数
+ * true    true    あり    一つ
+ * false   false   なし    一つ
+ * true    true    なし    複数
+ * true    false   あり    なし
+ */
+
+const _cases: Array<Omit<Parameters<typeof MultiComboBox>[0], 'items'>> = [
+  { disabled: false, error: true, width: undefined, selectedItems: [] },
+  {
+    disabled: false,
+    error: false,
+    width: '15em',
+    selectedItems: [defaultItems['option 1'], defaultItems['option 4']],
   },
-}
-
-export const VRTMultiCombobox: StoryFn = () => (
-  <Stack>
-    <InformationPanel title="VRT 用の Story です">
-      Multiコンボボックスのリストを展開して1つ目と2つ目の項目を選択した状態で表示されます
-    </InformationPanel>
-    {/* eslint-disable-next-line smarthr/a11y-input-has-name-attribute */}
-    <StoriesMultiComboBox />
-  </Stack>
-)
+  { disabled: true, error: true, width: '15em', selectedItems: [defaultItems['option 3']] },
+  {
+    disabled: false,
+    error: false,
+    width: undefined,
+    selectedItems: [defaultItems['アイテムのラベルがReactNodeの場合']],
+  },
+  {
+    disabled: true,
+    error: true,
+    width: undefined,
+    selectedItems: [
+      defaultItems['option 2'],
+      defaultItems[
+        'アイテムのラベルが長い場合（ダミーテキストダミーテキストダミーテキストダミーテキスト）'
+      ],
+    ],
+  },
+  { disabled: true, error: false, width: '15em', selectedItems: [] },
+]
 
 const waitForRAF = () =>
   new Promise<void>((resolve) => {
@@ -35,29 +58,53 @@ const waitForRAF = () =>
 const playMulti = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
   const canvas = within(canvasElement)
   const comboboxes = await canvas.findAllByRole('combobox')
-  comboboxes[0].focus()
+  comboboxes[comboboxes.length - 1].focus()
   const body = canvasElement.ownerDocument.body
-  const option1 = await within(body).findByText('option 1')
+  const option1 = await within(body).findByRole('button', { name: 'option 1' })
   await userEvent.click(option1)
   await waitForRAF()
-  const option2 = await within(body).findByText('option 2')
+  const option2 = await within(body).findByRole('button', { name: 'option 2' })
   await userEvent.click(option2)
   await waitForRAF()
   const helpMessage = await within(body).findAllByText('入力でフィルタリングできます。')
   await userEvent.click(helpMessage[0]) // カーソルの点滅によるVRTのフレーキーを避けるためにフォーカスを移動する
 }
-VRTMultiCombobox.play = playMulti
 
-export const VRTForcedColorsMultiCombobox: StoryFn = () => (
-  <Stack>
-    <InformationPanel title="VRT 用の Story です">
-      Chromatic 上では強制カラーモードで表示されます{' '}
-    </InformationPanel>
-    {/* eslint-disable-next-line smarthr/a11y-input-has-name-attribute */}
-    <StoriesMultiComboBox />
-  </Stack>
-)
-VRTForcedColorsMultiCombobox.play = playMulti
-VRTForcedColorsMultiCombobox.parameters = {
-  chromatic: { forcedColors: 'active' },
+export default {
+  title: 'Forms（フォーム）/MultiComboBox/VRT',
+  component: MultiComboBox,
+  render: (args) => {
+    const items = Object.values(defaultItems)
+    const [selectedItems, setSelectedItems] = useState<Array<ComboBoxItem<unknown>>>([])
+    return (
+      <Stack align="flex-start" gap={2} className="shr-h-screen">
+        {_cases.map((props, i) => (
+          <MultiComboBox {...args} {...props} items={items} key={i} />
+        ))}
+        <MultiComboBox
+          {...args}
+          name="default"
+          items={items}
+          dropdownHelpMessage="入力でフィルタリングできます。"
+          selectedItems={selectedItems}
+          onChangeSelected={(its) => setSelectedItems(its)}
+        />
+      </Stack>
+    )
+  },
+  play: playMulti,
+  parameters: {
+    withTheming: true,
+    chromatic: { disableSnapshot: false },
+  },
+  tags: ['!autodocs', 'skip-test-runner'],
+} as Meta<typeof MultiComboBox>
+
+export const VRT: StoryObj<typeof MultiComboBox> = {}
+
+export const VRTForcedColors: StoryObj<typeof MultiComboBox> = {
+  ...VRT,
+  parameters: {
+    chromatic: { forcedColors: 'active' },
+  },
 }
