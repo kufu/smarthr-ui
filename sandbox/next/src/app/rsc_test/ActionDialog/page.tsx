5e45c9bfd32cee88510673c80d0de107ab1e8b96
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2024-12-05 10:15:34 +0900

    Update sandbox/next/src/app/rsc_test/ActionDialog/page.tsx

--- sandbox/next/src/app/rsc_test/ActionDialog/page.tsx
+++ sandbox/next/src/app/rsc_test/ActionDialog/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ActionDialog } from 'smarthr-ui'
 
-import { RSCChecker } from '../components/RSCChecker'
+import { RSCChecker } from '../components/RSCChecker';
 export default function ActionDialogPage() {
   return (
     <>
