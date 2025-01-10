eb6ac4569f24f362890684a00d6851cd91f189d0
AtsushiM <at.mizoue@gmail.com>
2025-01-10 16:01:08 +0900

    chore: SideNavItemButtonのisSelectedの処理を最適化する

--- packages/smarthr-ui/src/components/SideNav/SideNavItemButton.tsx
+++ packages/smarthr-ui/src/components/SideNav/SideNavItemButton.tsx
@@ -51,23 +51,20 @@ const sideNavItem = tv({
   },
 })
 
-export const SideNavItemButton: FC<Props> = ({
-  id,
-  title,
-  prefix,
-  isSelected = false,
-  size,
-  onClick,
-}) => {
-  const handleClick = useMemo(() => (
-    onClick ? ((e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => onClick(e, e.currentTarget.value)) : undefined
-  ), [onClick])
+export const SideNavItemButton: FC<Props> = ({ id, title, prefix, isSelected, size, onClick }) => {
+  const handleClick = useMemo(
+    () =>
+      onClick
+        ? (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => onClick(e, e.currentTarget.value)
+        : undefined,
+    [onClick],
+  )
 
   const { wrapperStyle, buttonStyle, buttonInnerStyle } = useMemo(() => {
     const { wrapper, button, buttonInner } = sideNavItem()
 
     return {
-      wrapperStyle: wrapper({ selected: isSelected }),
+      wrapperStyle: wrapper({ selected: isSelected || false }),
       buttonStyle: button({ size }),
       buttonInnerStyle: buttonInner(),
     }
