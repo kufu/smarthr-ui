c26bc17c731fa22dee32e706b3b17bd8285aebe9
AtsushiM <at.mizoue@gmail.com>
2025-01-14 11:15:03 +0900

    chore: PaginationItemButtonの属性設定ロジックを整理、disabledがあるためonClickは常に設定するようにする

--- packages/smarthr-ui/src/components/Pagination/PaginationItemButton.tsx
+++ packages/smarthr-ui/src/components/Pagination/PaginationItemButton.tsx
@@ -9,17 +9,27 @@ type Props = {
 }
 
 export const PaginationItemButton: React.FC<Props> = ({ page, currentPage, onClick }) => {
-  const isCurrent = useMemo(() => page === currentPage, [page, currentPage])
+  const attrs = useMemo(() => {
+    if (page === currentPage) {
+      return {
+        'aria-current': 'page',
+        disabled: true,
+      }
+    }
+
+    return {
+      disabled: false,
+    }
+  }, [currentPage, page])
 
   return (
     <Button
+      {...attrs}
+      aria-label={`${page}ページ目`}
+      onClick={() => onClick(page)}
       square
       size="s"
       className="shr-rounded-s aria-current-page:shr-cursor-default aria-current-page:shr-border-solid aria-current-page:shr-border-main aria-current-page:shr-bg-main aria-current-page:shr-text-white"
-      aria-current={isCurrent ? 'page' : undefined}
-      aria-label={`${page}ページ目`}
-      onClick={isCurrent ? undefined : () => onClick(page)}
-      disabled={isCurrent}
     >
       {page}
     </Button>
