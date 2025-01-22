42ca1f393e3ba957703c430a27db83a4ff6ef3b3
AtsushiM <at.mizoue@gmail.com>
2025-01-22 09:52:42 +0900

    chore: useBodyScrollLockの無意味に分割されているuseEffectを結合

--- packages/smarthr-ui/src/components/Dialog/useBodyScrollLock.ts
+++ packages/smarthr-ui/src/components/Dialog/useBodyScrollLock.ts
@@ -1,20 +1,14 @@
 import { useEffect, useState } from 'react'
 
 export const useBodyScrollLock = (isOpen: boolean) => {
-  const [scrollBarWidth, setScrollBarWidth] = useState<number | null>(null)
   const [paddingRight, setPaddingRight] = useState<number | null>(null)
 
   useEffect(() => {
-    setScrollBarWidth(window.innerWidth - document.body.clientWidth)
-  }, [])
-
-  useEffect(() => {
-    if (scrollBarWidth === null) {
-      return
-    }
+    const scrollBarWidth = window.innerWidth - document.body.clientWidth
     const originalPaddingRight = getComputedStyle(document.body).getPropertyValue('padding-right')
+
     setPaddingRight(scrollBarWidth + parseInt(originalPaddingRight, 10))
-  }, [scrollBarWidth])
+  }, [])
 
   useEffect(() => {
     if (!isOpen) return
@@ -22,6 +16,7 @@ export const useBodyScrollLock = (isOpen: boolean) => {
     if (paddingRight !== null) {
       document.body.style.paddingInlineEnd = `${paddingRight}px`
     }
+
     document.body.style.overflow = 'hidden'
 
     return () => {
