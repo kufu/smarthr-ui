56446baaec9eb29d90775b2d5ec2e39edcfc0d7d
AtsushiM <at.mizoue@gmail.com>
2025-01-23 09:26:20 +0900

    chore: FormDialogのactionDisabledはFormDialogContentInnerでも初期化されているので削除

--- packages/smarthr-ui/src/components/Dialog/FormDialog/FormDialog.tsx
+++ packages/smarthr-ui/src/components/Dialog/FormDialog/FormDialog.tsx
@@ -24,7 +24,7 @@ export const FormDialog: React.FC<Props & ElementProps> = ({
   onClickClose,
   onPressEscape = onClickClose,
   responseMessage,
-  actionDisabled = false,
+  actionDisabled,
   closeDisabled,
   subActionArea,
   className,
