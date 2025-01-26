a53f6c219f522ce673d84b822972a8d782220fbe
AtsushiM <at.mizoue@gmail.com>
2025-01-27 08:56:20 +0900

    chore: ComboBoxのuseFocusControlのfocusNextDeletionButtonを最適化

--- packages/smarthr-ui/src/components/ComboBox/useFocusControl.ts
+++ packages/smarthr-ui/src/components/ComboBox/useFocusControl.ts
@@ -13,36 +13,38 @@ export function useFocusControl(selectedItemLength: number) {
     if (selectedItemLength === 0) {
       return
     }
+
     if (focusedIndex === null) {
       if (inputRef.current?.selectionStart === 0) {
         const nextIndex = deletionButtonRefs.length - 1
+
         deletionButtonRefs[nextIndex].current?.focus()
         setFocusedIndex(nextIndex)
       }
     } else {
       const nextIndex = Math.max(focusedIndex - 1, 0)
+
       deletionButtonRefs[nextIndex].current?.focus()
       setFocusedIndex(nextIndex)
     }
   }, [deletionButtonRefs, focusedIndex, selectedItemLength])
 
   const focusNextDeletionButton = useCallback(() => {
-    if (deletionButtonRefs.length === 0) {
+    if (deletionButtonRefs.length === 0 || focusedIndex === null) {
       return
     }
 
-    if (focusedIndex !== null) {
-      const nextIndex = focusedIndex + 1
-      if (nextIndex < deletionButtonRefs.length) {
-        deletionButtonRefs[nextIndex].current?.focus()
-        setFocusedIndex(nextIndex)
-      } else {
-        setFocusedIndex(null)
-        // キー入力が input に影響しないようにフォーカスタイミングを遅らせる
-        setTimeout(() => {
-          inputRef.current?.focus()
-        })
-      }
+    const nextIndex = focusedIndex + 1
+
+    if (nextIndex < deletionButtonRefs.length) {
+      deletionButtonRefs[nextIndex].current?.focus()
+      setFocusedIndex(nextIndex)
+    } else {
+      setFocusedIndex(null)
+      // キー入力が input に影響しないようにフォーカスタイミングを遅らせる
+      setTimeout(() => {
+        inputRef.current?.focus()
+      })
     }
   }, [deletionButtonRefs, focusedIndex])
 
