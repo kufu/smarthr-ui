c8fe4f081c0c0c407dd1dc25c3ffb5f51cd3f050
AtsushiM <at.mizoue@gmail.com>
2025-01-30 10:49:23 +0900

    chore: BrowserColumnをListItemを切り出す

--- packages/smarthr-ui/src/components/Browser/BrowserColumn.tsx
+++ packages/smarthr-ui/src/components/Browser/BrowserColumn.tsx
@@ -23,23 +23,34 @@ type Props = {
 export const BrowserColumn: FC<Props> = ({ items, index: columnIndex, value, onSelectItem }) => (
   <div className="last:shr-flex-1 [&:not(:last-child)]:shr-w-[218px] [&:not(:last-child)]:shr-border-r-shorthand">
     <ul className="shr-list-none shr-px-0.25 shr-py-0.5" id={getColumnId(columnIndex)}>
-      {items.map((item, rowIndex) => {
-        const selected = item.value === value
-        const ariaOwns =
-          selected && item.children.length > 0 ? getColumnId(columnIndex + 1) : undefined
-
-        return (
-          <li key={rowIndex} aria-owns={ariaOwns}>
-            <BrowserItem
-              selected={selected}
-              item={item}
-              columnIndex={columnIndex}
-              tabIndex={getTabIndex(selected, columnIndex, rowIndex, value)}
-              onSelectItem={onSelectItem}
-            />
-          </li>
-        )
-      })}
+      {items.map((item, rowIndex) => (
+        <ListItem
+          item={item}
+          value={value}
+          columnIndex={columnIndex}
+          rowIndex={rowIndex}
+          onSelectItem={onSelectItem}
+        />
+      ))}
     </ul>
   </div>
 )
+
+const ListItem = React.memo<
+  Pick<Props, 'value' | 'columnIndex' | 'onSelectItem'> & { item: ItemNode; rowIndex: number }
+>(({ item, value, columnIndex, rowIndex, onSelectItem }) => {
+  const selected = item.value === value
+  const ariaOwns = selected && item.children.length > 0 ? getColumnId(columnIndex + 1) : undefined
+
+  return (
+    <li key={rowIndex} aria-owns={ariaOwns}>
+      <BrowserItem
+        selected={selected}
+        item={item}
+        columnIndex={columnIndex}
+        tabIndex={getTabIndex(selected, columnIndex, rowIndex, value)}
+        onSelectItem={onSelectItem}
+      />
+    </li>
+  )
+})
