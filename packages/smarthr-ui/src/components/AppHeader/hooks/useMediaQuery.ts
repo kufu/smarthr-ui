5fb303095c343b9e5d2be756397f6c6c7878dc2c
AtsushiM <at.mizoue@gmail.com>
2025-01-31 08:50:39 +0900

    chore: AppHeaderのuseMediaQueryのロジックを整理

--- packages/smarthr-ui/src/components/AppHeader/hooks/useMediaQuery.ts
+++ packages/smarthr-ui/src/components/AppHeader/hooks/useMediaQuery.ts
@@ -5,25 +5,32 @@ export const mediaQuery = {
   mobile: 'max-width: 751px',
 } as const
 
+const NOOP = () => undefined
+const RETURN_FALSE = () => false
+
 export const useMediaQuery = (query: string) => {
   const mediaQueryList = useMemo(
     () => (typeof window === 'undefined' ? null : matchMedia(`(${query})`)),
     [query],
   )
 
-  const subscribe = useCallback(
-    (callback: () => void) => {
-      mediaQueryList?.addEventListener('change', callback)
-      return () => {
-        mediaQueryList?.removeEventListener('change', callback)
-      }
-    },
+  const syncArgs = useMemo(
+    () => ({
+      subscribe: (callback: () => void) => {
+        if (!mediaQueryList) {
+          return NOOP
+        }
+
+        mediaQueryList.addEventListener('change', callback)
+
+        return () => {
+          mediaQueryList.removeEventListener('change', callback)
+        }
+      },
+      getSnapshot: () => mediaQueryList?.matches ?? false,
+    }),
     [mediaQueryList],
   )
 
-  return useSyncExternalStore(
-    subscribe,
-    () => mediaQueryList?.matches ?? false,
-    () => false,
-  )
+  return useSyncExternalStore(syncArgs.subscribe, syncArgs.getSnapshot, RETURN_FALSE)
 }
