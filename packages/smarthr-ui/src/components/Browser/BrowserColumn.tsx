d9f176bbb028a0bef3c1277de69db6876d57e89f
AtsushiM <at.mizoue@gmail.com>
2025-01-30 10:59:11 +0900

    chore: BrowserColumnをListItem内のuseMemoを解除(やっていることが依存関係チェックとほぼ同じため)

--- packages/smarthr-ui/src/components/Browser/BrowserColumn.tsx
+++ packages/smarthr-ui/src/components/Browser/BrowserColumn.tsx
@@ -32,21 +32,8 @@ const ListItem = React.memo<
   Pick<Props, 'value' | 'columnIndex' | 'onSelectItem'> & { item: ItemNode; rowIndex: number }
 >(({ item, value, columnIndex, rowIndex, onSelectItem }) => {
   const selected = item.value === value
-  const ariaOwns = useMemo(
-    () => (selected && item.children.length > 0 ? getColumnId(columnIndex + 1) : undefined),
-    [selected, item.children.length, columnIndex],
-  )
-
-  // HINT: valueのままmemo化の依存関係にすると、キャッシュ効率が悪いため
-  // booleanに変更してなるべく変化がない状態にする
-  const nullValue = !value
-  const tabIndex = useMemo(() => {
-    if (selected || (nullValue && columnIndex === 0 && rowIndex === 0)) {
-      return 0
-    }
-
-    return -1
-  }, [nullValue, selected, columnIndex, rowIndex])
+  const ariaOwns = selected && item.children.length > 0 ? getColumnId(columnIndex + 1) : undefined
+  const tabIndex = selected || (!value && columnIndex === 0 && rowIndex === 0) ? 0 : -1
 
   return (
     <li key={rowIndex} aria-owns={ariaOwns}>
