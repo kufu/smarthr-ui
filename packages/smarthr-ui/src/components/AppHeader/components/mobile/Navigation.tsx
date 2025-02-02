a52f101cd1e6fd818eb618052266fbe323c91fdf
AtsushiM <at.mizoue@gmail.com>
2025-02-03 08:10:01 +0900

    chore: Navigation内のstyleをmemo

--- packages/smarthr-ui/src/components/AppHeader/components/mobile/Navigation.tsx
+++ packages/smarthr-ui/src/components/AppHeader/components/mobile/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { FC, Fragment } from 'react'
+import React, { FC, Fragment, useMemo } from 'react'
 import { tv } from 'tailwind-variants'
 
 import { Text } from '../../../Text'
@@ -16,39 +16,43 @@ const separator = tv({
   base: ['[&&]:shr-mx-0 [&&]:shr-my-0.5 [&&]:shr-border-b-shorthand'],
 })
 
-export const Navigation: FC<Props> = ({ navigations, onClickNavigation }) => (
-  <div>
-    {navigations.map((navigation, i) => {
-      if (isChildNavigationGroup(navigation)) {
-        const { childNavigations } = navigation
+export const Navigation: FC<Props> = ({ navigations, onClickNavigation }) => {
+  const separatorStyle = useMemo(() => separator(), [])
+
+  return (
+    <div>
+      {navigations.map((navigation, i) => {
+        if (isChildNavigationGroup(navigation)) {
+          const { childNavigations } = navigation
+
+          return (
+            <Fragment key={i}>
+              <Text styleType="subSubBlockTitle" as="p" className="shr-py-0.5">
+                {navigation.title}
+              </Text>
+
+              {childNavigations.map((childNavigation, j) => (
+                <NavigationItem
+                  key={j}
+                  navigation={childNavigation}
+                  onClickNavigation={onClickNavigation}
+                />
+              ))}
+
+              {i + 1 !== navigations.length && <hr className={separatorStyle} />}
+            </Fragment>
+          )
+        }
+
+        const nextNavigation = navigations[i + 1]
 
         return (
           <Fragment key={i}>
-            <Text styleType="subSubBlockTitle" as="p" className="shr-py-0.5">
-              {navigation.title}
-            </Text>
-
-            {childNavigations.map((childNavigation, j) => (
-              <NavigationItem
-                key={j}
-                navigation={childNavigation}
-                onClickNavigation={onClickNavigation}
-              />
-            ))}
-
-            {i + 1 !== navigations.length && <hr className={separator()} />}
+            <NavigationItem navigation={navigation} onClickNavigation={onClickNavigation} />
+            {isChildNavigationGroup(nextNavigation) && <hr className={separatorStyle} />}
           </Fragment>
         )
-      }
-
-      const nextNavigation = navigations[i + 1]
-
-      return (
-        <Fragment key={i}>
-          <NavigationItem navigation={navigation} onClickNavigation={onClickNavigation} />
-          {isChildNavigationGroup(nextNavigation) && <hr className={separator()} />}
-        </Fragment>
-      )
-    })}
-  </div>
-)
+      })}
+    </div>
+  )
+}
