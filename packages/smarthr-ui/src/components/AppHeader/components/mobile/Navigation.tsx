f5e5426218f9f5624d12da07c461256c07df025b
AtsushiM <at.mizoue@gmail.com>
2025-02-03 08:14:44 +0900

    chore: NavigationのMemoizedNavigationItemを切り出す

--- packages/smarthr-ui/src/components/AppHeader/components/mobile/Navigation.tsx
+++ packages/smarthr-ui/src/components/AppHeader/components/mobile/Navigation.tsx
@@ -21,38 +21,48 @@ export const Navigation: FC<Props> = ({ navigations, onClickNavigation }) => {
 
   return (
     <div>
-      {navigations.map((navigation, i) => {
-        if (isChildNavigationGroup(navigation)) {
-          const { childNavigations } = navigation
-
-          return (
-            <Fragment key={i}>
-              <Text styleType="subSubBlockTitle" as="p" className="shr-py-0.5">
-                {navigation.title}
-              </Text>
-
-              {childNavigations.map((childNavigation, j) => (
-                <NavigationItem
-                  key={j}
-                  navigation={childNavigation}
-                  onClickNavigation={onClickNavigation}
-                />
-              ))}
-
-              {i + 1 !== navigations.length && <hr className={separatorStyle} />}
-            </Fragment>
-          )
-        }
-
-        const nextNavigation = navigations[i + 1]
-
-        return (
-          <Fragment key={i}>
-            <NavigationItem navigation={navigation} onClickNavigation={onClickNavigation} />
-            {isChildNavigationGroup(nextNavigation) && <hr className={separatorStyle} />}
-          </Fragment>
-        )
-      })}
+      {navigations.map((navigation, i) => (
+        <MemoizedNavigationItem
+          key={i}
+          navigation={navigation}
+          onClickNavigation={onClickNavigation}
+        />
+      ))}
     </div>
   )
 }
+
+const MemoizedNavigationItem = React.memo<
+  Pick<Props, 'onClickNavigation'> & { navigation: NavigationType }
+>(({ navigation, onClickNavigation }) => {
+  if (isChildNavigationGroup(navigation)) {
+    const { childNavigations } = navigation
+
+    return (
+      <Fragment>
+        <Text styleType="subSubBlockTitle" as="p" className="shr-py-0.5">
+          {navigation.title}
+        </Text>
+
+        {childNavigations.map((childNavigation, j) => (
+          <NavigationItem
+            key={j}
+            navigation={childNavigation}
+            onClickNavigation={onClickNavigation}
+          />
+        ))}
+
+        {i + 1 !== navigations.length && <hr className={separatorStyle} />}
+      </Fragment>
+    )
+  }
+
+  const nextNavigation = navigations[i + 1]
+
+  return (
+    <Fragment>
+      <NavigationItem navigation={navigation} onClickNavigation={onClickNavigation} />
+      {isChildNavigationGroup(nextNavigation) && <hr className={separatorStyle} />}
+    </Fragment>
+  )
+})
