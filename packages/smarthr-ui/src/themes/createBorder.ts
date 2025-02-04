8717032aeab5116ac7aac5cfd2de1e2a255146e0
AtsushiM <at.mizoue@gmail.com>
2025-02-05 08:18:18 +0900

    chore: createBorderは単純な一階層のObjectを生成するため、lodash.mergeは不要

--- packages/smarthr-ui/src/themes/createBorder.ts
+++ packages/smarthr-ui/src/themes/createBorder.ts
@@ -1,11 +1,8 @@
-import { merge } from '../libs/lodash'
-
 import { ColorProperty, defaultColor } from './createColor'
 
 const defaultLineWidth = '1px'
 const defaultLineStyle = 'solid'
-const defaultLineColor = defaultColor.BORDER
-const highContrastBorderColor = defaultColor.GREY_100
+const defaultLinePrefix = `${defaultLineWidth} ${defaultLineStyle} `
 
 export type BorderProperty = {
   lineWidth?: string
@@ -13,29 +10,25 @@ export type BorderProperty = {
   shorthand?: string
 }
 
-export type CreatedBorderTheme = {
-  lineWidth: string
-  lineStyle: string
-  shorthand: string
+export type CreatedBorderTheme = Required<BorderProperty> & {
   highContrast: string
 }
 
 export const defaultBorder: CreatedBorderTheme = {
   lineWidth: defaultLineWidth,
   lineStyle: defaultLineStyle,
-  shorthand: `${defaultLineWidth} ${defaultLineStyle} ${defaultLineColor}`,
-  highContrast: `${defaultLineWidth} ${defaultLineStyle} ${highContrastBorderColor}`,
+  shorthand: `${defaultLinePrefix}${defaultColor.BORDER}`,
+  highContrast: `${defaultLinePrefix}${defaultColor.GREY_100}`,
 }
 
 export const createBorder = (userBorder: BorderProperty = {}, userColor: ColorProperty = {}) => {
-  const color = userColor.BORDER || defaultColor.BORDER
-  const created: CreatedBorderTheme = merge(
-    {
-      ...defaultBorder,
-      shorthand: `${defaultLineWidth} ${defaultLineStyle} ${color}`,
-    },
-    userBorder,
-  )
+  const userShorthand = userColor.BORDER
+    ? { shorthand: `${defaultLinePrefix}${userColor.BORDER}` }
+    : {}
 
-  return created
+  return {
+    ...defaultBorder,
+    ...userShorthand,
+    ...userBorder,
+  } as CreatedBorderTheme
 }
