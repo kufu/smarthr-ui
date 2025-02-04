a9d74b7c266080ef9e34fc9aa7d36c0c6c7f922e
AtsushiM <at.mizoue@gmail.com>
2025-02-04 09:51:32 +0900

    chore: useHandleEscapeのロジックを整理

--- packages/smarthr-ui/src/hooks/useHandleEscape.ts
+++ packages/smarthr-ui/src/hooks/useHandleEscape.ts
@@ -1,18 +1,17 @@
-import { useCallback, useEffect } from 'react'
+import { useEffect } from 'react'
 
 export const useHandleEscape = (cb: () => void) => {
-  const handleKeyPress = useCallback(
-    (e: KeyboardEvent) => {
+  useEffect(() => {
+    const handleKeyPress = (e: KeyboardEvent) => {
       // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
       // Esc is a IE/Edge specific value
       if (e.key === 'Escape' || e.key === 'Esc') {
         cb()
       }
-    },
-    [cb],
-  )
-  useEffect(() => {
+    }
+
     document.addEventListener('keydown', handleKeyPress)
+
     return () => document.removeEventListener('keydown', handleKeyPress)
-  }, [handleKeyPress])
+  }, [cb])
 }
