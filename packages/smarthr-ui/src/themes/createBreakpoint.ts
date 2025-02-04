736ea9255559f7c68b5aac21cd4e663d6d9b8d3d
AtsushiM <at.mizoue@gmail.com>
2025-02-05 08:10:46 +0900

    chore: createBreakpointは単純な一階層のObjectを生成するため、lodash.mergeは不要

--- packages/smarthr-ui/src/themes/createBreakpoint.ts
+++ packages/smarthr-ui/src/themes/createBreakpoint.ts
@@ -1,18 +1,11 @@
-import { merge } from '../libs/lodash'
-
 export type BreakpointProperty = {
   SP?: number
   TABLET?: number
 }
 
-export type CreatedBreakpointTheme = {
-  SP: number
-  TABLET: number
-}
+export type CreatedBreakpointTheme = Required<BreakpointProperty>
 
 export const defaultBreakpoint = { SP: 599, TABLET: 959 }
 
-export const createBreakpoint = (userBreakpoint: BreakpointProperty = {}) => {
-  const created: CreatedBreakpointTheme = merge({ ...defaultBreakpoint }, userBreakpoint)
-  return created
-}
+export const createBreakpoint = (userBreakpoint: BreakpointProperty = {}) =>
+  ({ ...defaultBreakpoint, ...userBreakpoint }) as CreatedBreakpointTheme
