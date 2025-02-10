020c359f6957c14253c69a772c2d38a15e274f4c
AtsushiM <at.mizoue@gmail.com>
2025-02-10 13:26:31 +0900

    chore: HorizontalStepItem自体をmemo化

--- packages/smarthr-ui/src/components/Stepper/HorizontalStepItem.tsx
+++ packages/smarthr-ui/src/components/Stepper/HorizontalStepItem.tsx
@@ -1,4 +1,4 @@
-import React, { type FC, useMemo } from 'react'
+import React, { useMemo } from 'react'
 import { tv } from 'tailwind-variants'
 
 import { Heading } from '../Heading'
@@ -70,46 +70,42 @@ type Props = HorizontalStep & {
   isPrevStepCompleted: boolean
 }
 
-export const HorizontalStepItem: FC<Props> = ({
-  stepNumber,
-  label,
-  status,
-  current,
-  isPrevStepCompleted,
-}) => {
-  const classNames = useMemo(() => {
-    const statusType = typeof status === 'object' ? status.type : status
-    const { wrapper, headingWrapper, stepCounterWrapper, beforeLine, afterLine, heading } =
-      horizontalStepItem({
-        status: statusType,
-        current,
-        isPrevStepCompleted,
-      })
+export const HorizontalStepItem = React.memo<Props>(
+  ({ stepNumber, label, status, current, isPrevStepCompleted }) => {
+    const classNames = useMemo(() => {
+      const statusType = typeof status === 'object' ? status.type : status
+      const { wrapper, headingWrapper, stepCounterWrapper, beforeLine, afterLine, heading } =
+        horizontalStepItem({
+          status: statusType,
+          current,
+          isPrevStepCompleted,
+        })
 
-    return {
-      wrapper: wrapper(),
-      headingWrapper: headingWrapper(),
-      stepCounterWrapper: stepCounterWrapper(),
-      beforeLine: beforeLine(),
-      afterLine: afterLine(),
-      heading: heading(),
-    }
-  }, [current, isPrevStepCompleted, status])
+      return {
+        wrapper: wrapper(),
+        headingWrapper: headingWrapper(),
+        stepCounterWrapper: stepCounterWrapper(),
+        beforeLine: beforeLine(),
+        afterLine: afterLine(),
+        heading: heading(),
+      }
+    }, [current, isPrevStepCompleted, status])
 
-  return (
-    <li aria-current={current} className={classNames.wrapper}>
-      <SectioningFragment>
-        <div className={classNames.headingWrapper}>
-          <div className={classNames.stepCounterWrapper}>
-            <span className={classNames.beforeLine} />
-            <StepCounter status={status} current={current} stepNumber={stepNumber} />
-            <span className={classNames.afterLine} />
+    return (
+      <li aria-current={current} className={classNames.wrapper}>
+        <SectioningFragment>
+          <div className={classNames.headingWrapper}>
+            <div className={classNames.stepCounterWrapper}>
+              <span className={classNames.beforeLine} />
+              <StepCounter status={status} current={current} stepNumber={stepNumber} />
+              <span className={classNames.afterLine} />
+            </div>
+            <Heading type="sectionTitle" className={classNames.heading}>
+              {label}
+            </Heading>
           </div>
-          <Heading type="sectionTitle" className={classNames.heading}>
-            {label}
-          </Heading>
-        </div>
-      </SectioningFragment>
-    </li>
-  )
-}
+        </SectioningFragment>
+      </li>
+    )
+  },
+)
