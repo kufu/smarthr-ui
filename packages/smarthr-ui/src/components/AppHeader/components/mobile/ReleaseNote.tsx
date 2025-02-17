e83c0325c9a9098ca30c9fe7fe5902996aa39daa
AtsushiM <at.mizoue@gmail.com>
2025-02-17 16:13:43 +0900

    chore: classNamesを調整

--- packages/smarthr-ui/src/components/AppHeader/components/mobile/ReleaseNote.tsx
+++ packages/smarthr-ui/src/components/AppHeader/components/mobile/ReleaseNote.tsx
@@ -1,4 +1,4 @@
-import React, { type FC, memo, useContext } from 'react'
+import React, { type FC, memo, useContext, useMemo } from 'react'
 import { tv } from 'tailwind-variants'
 
 import { FaUpRightFromSquareIcon } from '../../../Icon'
@@ -11,7 +11,7 @@ import { Translate } from '../common/Translate'
 
 import { ReleaseNoteContext } from './ReleaseNoteContext'
 
-const releaseNoteStyle = tv({
+const classNameGenerator = tv({
   slots: {
     anchor: ['shr-text-base shr-text-link [&&]:shr-underline', '[&&]:hover:shr-no-underline'],
     icon: ['shr-ms-0.5'],
@@ -33,7 +33,17 @@ const ActualReleaseNote: FC<{
   data: HeaderProps['releaseNote']
 }> = () => {
   const translate = useTranslate()
-  const { anchor, icon, indexLinkWrapper, indexLinkAnchor } = releaseNoteStyle()
+
+  const classNames = useMemo(() => {
+    const { anchor, icon, indexLinkWrapper, indexLinkAnchor } = classNameGenerator()
+
+    return {
+      anchor: anchor(),
+      icon: icon(),
+      indexLinkWrapper: indexLinkWrapper(),
+      indexLinkAnchor: indexLinkAnchor(),
+    }
+  }, [])
 
   return (
     <div>
@@ -49,24 +59,29 @@ const ActualReleaseNote: FC<{
         <Stack>
           {data.links.slice(0, 5).map((link) => (
             <div key={link.url}>
-              <a href={link.url} target="_blank" rel="noopener noreferrer" className={anchor()}>
+              <a
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={classNames.anchor}
+              >
                 {link.title}
-                <FaUpRightFromSquareIcon className={icon()} />
+                <FaUpRightFromSquareIcon className={classNames.icon} />
               </a>
             </div>
           ))}
         </Stack>
       )}
 
-      <div className={indexLinkWrapper()}>
+      <div className={classNames.indexLinkWrapper}>
         <a
           href={data.indexUrl}
           target="_blank"
           rel="noopener noreferrer"
-          className={indexLinkAnchor()}
+          className={classNames.indexLinkAnchor}
         >
           <Translate>{translate('common/seeAllReleaseNotes')}</Translate>
-          <FaUpRightFromSquareIcon className={icon()} />
+          <FaUpRightFromSquareIcon className={classNames.icon} />
         </a>
       </div>
     </div>
