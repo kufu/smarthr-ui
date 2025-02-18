de8331fbe9ae461e38e6bf60d53b95b2c24a1cc9
AtsushiM <at.mizoue@gmail.com>
2025-02-18 16:15:21 +0900

    chore: 関数をcustom hookに押し込める

--- packages/smarthr-ui/src/components/AppHeader/components/mobile/AppLauncher.tsx
+++ packages/smarthr-ui/src/components/AppHeader/components/mobile/AppLauncher.tsx
@@ -43,8 +43,9 @@ export const AppLauncher: FC<Props> = ({ features: baseFeatures }) => {
     sortType,
     searchQuery,
     changePage,
-    setSortType,
-    changeSearchQuery,
+    onSelectSortType,
+    onChangeSearchQuery,
+    onClickClearSearchQuery,
   } = useAppLauncher(baseFeatures)
 
   const { wrapper, searchArea, headArea, scrollArea, bottomArea } = appLauncher()
@@ -60,19 +61,12 @@ export const AppLauncher: FC<Props> = ({ features: baseFeatures }) => {
           value={searchQuery}
           suffix={
             mode === 'search' && (
-              <UnstyledButton
-                onClick={() => {
-                  // 別のキューにしないとドロップダウンが閉じてしまう
-                  setTimeout(() => {
-                    changeSearchQuery('')
-                  }, 0)
-                }}
-              >
+              <UnstyledButton onClick={onClickClearSearchQuery}>
                 <FaCircleXmarkIcon />
               </UnstyledButton>
             )
           }
-          onChange={(e) => changeSearchQuery(e.target.value)}
+          onChange={onChangeSearchQuery}
         />
       </div>
 
@@ -82,14 +76,11 @@ export const AppLauncher: FC<Props> = ({ features: baseFeatures }) => {
             <Translate>{translate('Launcher/searchResultText')}</Translate>
           </Text>
         ) : (
-          <AppLauncherFilterDropdown page={page} onSelectPage={(p) => changePage(p)} />
+          <AppLauncherFilterDropdown page={page} onSelectPage={changePage} />
         )}
 
         {(mode === 'search' || page === 'all') && (
-          <AppLauncherSortDropdown
-            sortType={sortType}
-            onSelectSortType={(value) => setSortType(value)}
-          />
+          <AppLauncherSortDropdown sortType={sortType} onSelectSortType={setSortType} />
         )}
       </Cluster>
 
