82362b2c80a779c66f18222a3fb51161ac8b443d
AtsushiM <at.mizoue@gmail.com>
2025-03-03 14:58:18 +0900

    chore: 型調整

--- packages/smarthr-ui/src/components/Textarea/Textarea.tsx
+++ packages/smarthr-ui/src/components/Textarea/Textarea.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import React, {
-  ChangeEvent,
-  ComponentPropsWithRef,
-  ReactNode,
+  type ChangeEvent,
+  type ComponentPropsWithRef,
+  type ReactNode,
   forwardRef,
   startTransition,
   useCallback,
@@ -205,7 +205,7 @@ export const Textarea = forwardRef<HTMLTextAreaElement, Props & ElementProps>(
     )
 
     const handleChange = useCallback(
-      (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      (e: ChangeEvent<HTMLTextAreaElement>) => {
         if (maxLetters) {
           const inputValue = e.currentTarget.value
           debouncedUpdateCount(inputValue)
