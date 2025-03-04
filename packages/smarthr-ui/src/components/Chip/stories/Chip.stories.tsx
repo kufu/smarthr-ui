605ced1adaa104c25ce4dc4d9cf437d661873dc8
oti <yoshiaki.ito@smarthr.co.jp>
2025-03-04 11:54:58 +0900

    refactor: props にないものは story に示さないよう変更した

--- packages/smarthr-ui/src/components/Chip/stories/Chip.stories.tsx
+++ packages/smarthr-ui/src/components/Chip/stories/Chip.stories.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import { UnstyledButton } from '../../Button'
-import { FaCircleCheckIcon, FaCircleXmarkIcon } from '../../Icon'
 import { Stack } from '../../Layout'
 import { Chip, classNameGenerator } from '../Chip'
 
@@ -54,31 +52,5 @@ export const Disabled: StoryObj<typeof Chip> = {
   name: 'disabled',
   args: {
     disabled: true,
-  },
-}
-
-export const SuffixIcon: StoryObj<typeof Chip> = {
-  name: 'suffixIcon',
-  args: {
-    children: (
-      <span className="shr-inline-flex shr-gap-0.5 shr-items-center shr-mr-[-3px]">
-        ラベル
-        <FaCircleCheckIcon className="shr-align-bottom" />
-      </span>
-    ),
-  },
-}
-
-export const SuffixIconButton: StoryObj<typeof Chip> = {
-  name: 'suffixIconButton',
-  args: {
-    children: (
-      <span className="shr-inline-flex shr-gap-0.5 shr-items-center shr-mr-[-3px]">
-        ラベル
-        <UnstyledButton className="shr-rounded-full shr-leading-none" aria-label="削除する">
-          <FaCircleXmarkIcon className="shr-align-bottom" />
-        </UnstyledButton>
-      </span>
-    ),
   },
 }
