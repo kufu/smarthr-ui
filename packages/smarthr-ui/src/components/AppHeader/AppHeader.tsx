f1cc6f8c76b19513c163c92f432706145a5a039f
AtsushiM <at.mizoue@gmail.com>
2025-03-13 15:43:52 +0900

    chore: remove comment

--- packages/smarthr-ui/src/components/AppHeader/AppHeader.tsx
+++ packages/smarthr-ui/src/components/AppHeader/AppHeader.tsx
@@ -10,9 +10,6 @@ import { mediaQuery, useMediaQuery } from './hooks/useMediaQuery'
 import type { HeaderProps } from './types'
 
 export const AppHeader: FC<HeaderProps> = ({ locale, children, ...props }) => {
-  // NOTE: ヘッダーの出し分けは CSS によって行われているので、useMediaQuery による children の出し分けは本来不要ですが、
-  //  wovn の言語切替カスタム UI の挿入対象となる DOM ("wovn-embedded-widget-anchor" クラスを持った div) が複数描画されていると、
-  //  wovn のスクリプトの仕様上1つ目の DOM にしか UI が挿入されないため、やむを得ず children のみ React のレンダリングレベルでの出し分けをしています。
   const isDesktop = useMediaQuery(mediaQuery.desktop)
   const Header = isDesktop ? DesktopHeader : MobileHeader
 
