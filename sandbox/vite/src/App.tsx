642636107710cda23e1bbd4f89045c46c1c3a50d
Qs-F <qsf@de-liker.com>
2025-03-14 20:35:58 +0900

    fix: docs sample

--- sandbox/vite/src/App.tsx
+++ sandbox/vite/src/App.tsx
@@ -1,28 +1,40 @@
-import { Link } from 'react-router-dom'
+import { Link, Routes, Route } from 'react-router-dom'
 import { Button, TextLink, FaArrowRightIcon, FaAddressCardIcon } from 'smarthr-ui'
 import 'smarthr-ui/smarthr-ui.css'
 
-function App() {
-  return (
-    <main>
-      <Button variant="primary">Hello, Next.</Button>
-      <ol>
-        <li>
-          <Link to="/about">React Router Link</Link>
-        </li>
-        <li>
-          <TextLink elementAs={Link} to="/about" suffix={<FaArrowRightIcon />}>
-            smarthr-ui with next/link
-          </TextLink>
-        </li>
-        <li>
-          <TextLink elementAs={Link} to="/about" prefix={<FaAddressCardIcon />}>
-            smarthr-ui with next/link
-          </TextLink>
-        </li>
-      </ol>
-    </main>
-  )
-}
+const About = () => (
+  <div>
+    <h1>About Page</h1>
+    <TextLink elementAs={Link} to="/">
+      Back to Home
+    </TextLink>
+  </div>
+)
+
+const App = () =>  (
+  <Routes>
+    <Route
+      path="/"
+      element={
+        <main>
+          <Button variant="primary">こんにちは</Button>
+          <ol>
+            <li>
+              <TextLink elementAs={Link} to="/about" suffix={<FaArrowRightIcon />}>
+                smarthr-ui with Router Link
+              </TextLink>
+            </li>
+            <li>
+              <TextLink elementAs={Link} to="/about" prefix={<FaAddressCardIcon />}>
+                smarthr-ui with Router Link
+              </TextLink>
+            </li>
+          </ol>
+        </main>
+      }
+    />
+    <Route path="/about" element={<About />} />
+  </Routes>
+)
 
 export default App
--- sandbox/vite/src/main.tsx
+++ sandbox/vite/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
