1cc660d1090277355db90118f1a7d073834fe5ad
Qs-F <qsf@de-liker.com>
2025-03-17 11:08:00 +0900

    fix: Remove favicon

--- sandbox/vite/src/app/root.tsx
+++ sandbox/vite/src/app/root.tsx
@@ -18,7 +18,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
