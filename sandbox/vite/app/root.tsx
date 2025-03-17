561d87d4a51e6beb51aa17299d0d4b617693815d
Qs-F <qsf@de-liker.com>
2025-03-17 11:12:56 +0900

    fix: Update

--- sandbox/vite/app/root.tsx
+++ sandbox/vite/app/root.tsx
@@ -11,32 +11,13 @@ import {
   DeviceProvider,
   ErrorScreen,
 } from "smarthr-ui";
-import "smarthr-ui/smarthr-ui.css";
+import "smarthr-ui/smarthr-ui.css"
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
