fdb1db3ef5eace764e1742eab93d8b40de5cd8ec
AtsushiM <at.mizoue@gmail.com>
2025-03-18 09:43:05 +0900

    chore: Nav -> nav

--- packages/smarthr-ui/src/components/Experimental/SideMenu/SideMenu.tsx
+++ packages/smarthr-ui/src/components/Experimental/SideMenu/SideMenu.tsx
@@ -7,7 +7,6 @@ import React, {
 import { tv } from 'tailwind-variants'
 
 import { Base } from '../../Base'
-import { Nav } from '../../SectioningContent'
 
 const classNameGenerator = tv({
   base: 'smarthr-ui-SideMenu shr-list-none shr-py-0.5',
@@ -26,9 +25,10 @@ export const SideMenu: FC<Props> = ({ elementAs = 'ul', className, ...rest }) =>
   const actualClassName = useMemo(() => classNameGenerator({ className }), [className])
 
   return (
+    // HINT: nav直下にheadingが存在しないため、レベル自動計算が置きないよう、navを直接利用する
     // eslint-disable-next-line smarthr/a11y-heading-in-sectioning-content
-    <Nav>
+    <nav>
       <Base {...rest} as={elementAs} className={actualClassName} />
-    </Nav>
+    </nav>
   )
 }
