232c6c2eb88785555f8dc6a24fa812fa2cdd7845
Mizoue Atsushi <at.mizoue@gmail.com>
2025-03-26 07:51:28 +0900

    chore: SideMenuのマークアップを調整する (#5486)

--- packages/smarthr-ui/src/components/Experimental/SideMenu/SideMenu.tsx
+++ packages/smarthr-ui/src/components/Experimental/SideMenu/SideMenu.tsx
@@ -2,6 +2,8 @@ import { type ComponentPropsWithoutRef, type FC, type PropsWithChildren, useMemo
 import { tv } from 'tailwind-variants'
 
 import { Base } from '../../Base'
+import { Heading } from '../../Heading'
+import { Nav } from '../../SectioningContent'
 
 const classNameGenerator = tv({
   base: 'smarthr-ui-SideMenu shr-list-none shr-py-0.5',
@@ -19,5 +21,10 @@ type Props = PropsWithChildren<
 export const SideMenu: FC<Props> = ({ elementAs = 'ul', className, ...rest }) => {
   const actualClassName = useMemo(() => classNameGenerator({ className }), [className])
 
-  return <Base {...rest} as={elementAs} className={actualClassName} />
+  return (
+    <Nav>
+      <Heading visuallyHidden={true}>サイドメニュー</Heading>
+      <Base {...rest} as={elementAs} className={actualClassName} />
+    </Nav>
+  )
 }
