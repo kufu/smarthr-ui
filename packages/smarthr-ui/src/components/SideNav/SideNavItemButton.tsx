f5ef60bb87c40992ab3b1b621b28eeb3b120ba74
AtsushiM <at.mizoue@gmail.com>
2025-04-04 07:25:30 +0900

    fix!: SideNavのisSelectedをcurrentにrename

--- packages/smarthr-ui/src/components/SideNav/SideNavItemButton.tsx
+++ packages/smarthr-ui/src/components/SideNav/SideNavItemButton.tsx
@@ -14,7 +14,7 @@ type Props = {
   /** タイトルのプレフィックスの内容。通常、StatusLabel の配置に用います。 */
   prefix?: ReactNode
   /** 選択されているアイテムかどうか */
-  isSelected?: boolean
+  current?: boolean
   /** アイテムの大きさ */
   size?: SideNavSizeType
   /** アイテムを押下したときに発火するコールバック関数 */
@@ -25,9 +25,9 @@ const classNameGenerator = tv({
   slots: {
     wrapper: [
       'smarthr-ui-SideNav-item',
-      'data-[selected=true]:shr-relative data-[selected=true]:shr-bg-main data-[selected=true]:shr-text-white',
-      'data-[selected=true]:after:shr-absolute data-[selected=true]:after:-shr-right-0.25 data-[selected=true]:after:shr-top-1/2 data-[selected=true]:after:-shr-translate-y-1/2 data-[selected=true]:after:shr-translate-x-0 data-[selected=true]:after:shr-border-b-4 data-[selected=true]:after:shr-border-l-4 data-[selected=true]:after:shr-border-r-0 data-[selected=true]:after:shr-border-t-4 data-[selected=true]:after:shr-border-solid data-[selected=true]:after:shr-border-b-transparent data-[selected=true]:after:shr-border-l-main data-[selected=true]:after:shr-border-r-transparent data-[selected=true]:after:shr-border-t-transparent data-[selected=true]:after:shr-content-[""]',
-      'data-[selected=false]:hover:shr-bg-column-darken',
+      'data-[current=true]:shr-relative data-[current=true]:shr-bg-main data-[current=true]:shr-text-white',
+      'data-[current=true]:after:shr-absolute data-[current=true]:after:-shr-right-0.25 data-[current=true]:after:shr-top-1/2 data-[current=true]:after:-shr-translate-y-1/2 data-[current=true]:after:shr-translate-x-0 data-[current=true]:after:shr-border-b-4 data-[current=true]:after:shr-border-l-4 data-[current=true]:after:shr-border-r-0 data-[current=true]:after:shr-border-t-4 data-[current=true]:after:shr-border-solid data-[current=true]:after:shr-border-b-transparent data-[current=true]:after:shr-border-l-main data-[current=true]:after:shr-border-r-transparent data-[current=true]:after:shr-border-t-transparent data-[current=true]:after:shr-content-[""]',
+      'data-[current=false]:hover:shr-bg-column-darken',
     ],
     button: ['shr-w-full shr-leading-none [&]:shr-box-border', 'focus-visible:shr-focus-indicator'],
     buttonInner: 'smarthr-ui-SideNav-itemTitle',
@@ -44,7 +44,7 @@ const classNameGenerator = tv({
   },
 })
 
-export const SideNavItemButton: FC<Props> = ({ id, title, prefix, isSelected, size, onClick }) => {
+export const SideNavItemButton: FC<Props> = ({ id, title, prefix, current, size, onClick }) => {
   const classNames = useMemo(() => {
     const { wrapper, button, buttonInner } = classNameGenerator()
 
@@ -56,7 +56,7 @@ export const SideNavItemButton: FC<Props> = ({ id, title, prefix, isSelected, si
   }, [size])
 
   return (
-    <li data-selected={!!isSelected} className={classNames.wrapper}>
+    <li data-current={!!current} className={classNames.wrapper}>
       <UnstyledButton className={classNames.button} onClick={onClick} value={id}>
         <ButtonBodyCluster prefix={prefix} title={title} titleClassName={classNames.buttonInner} />
       </UnstyledButton>
