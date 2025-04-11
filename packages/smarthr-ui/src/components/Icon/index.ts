5cfe4c58d0a244333339ef50b1f0e334e152b926
Qs-F <qsf@de-liker.com>
2025-04-11 20:41:44 +0900

    fix: add type prefix for export

--- packages/smarthr-ui/src/components/Icon/index.ts
+++ packages/smarthr-ui/src/components/Icon/index.ts
@@ -1,3 +1,3 @@
 export * from './Icon'
 export * from './DeprecatedIcon'
-export { generateIcon, Props as ComponentProps } from './generateIcon'
+export { generateIcon, type Props as ComponentProps } from './generateIcon'
