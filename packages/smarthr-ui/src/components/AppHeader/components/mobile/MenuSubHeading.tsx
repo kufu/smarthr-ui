d95b8528254e75a9952d020d4a4d389605cfc169
AtsushiM <at.mizoue@gmail.com>
2025-04-14 08:36:26 +0900

    chore: MenuSubHeadingをmemo化

--- packages/smarthr-ui/src/components/AppHeader/components/mobile/MenuSubHeading.tsx
+++ packages/smarthr-ui/src/components/AppHeader/components/mobile/MenuSubHeading.tsx
@@ -1,29 +1,31 @@
+import { type ReactNode, memo, useMemo } from 'react'
+
 import { Button } from '../../../Button'
 import { Heading } from '../../../Heading'
 import { FaArrowLeftIcon } from '../../../Icon'
 import { useTranslate } from '../../hooks/useTranslate'
 import { Translate } from '../common/Translate'
 
-import type { FC, ReactNode } from 'react'
-
 type Props = {
   title: ReactNode
   onClickBack: () => void
 }
 
-export const MenuSubHeading: FC<Props> = ({ title, onClickBack }) => {
+export const MenuSubHeading = memo<Props>(({ title, onClickBack }) => {
   const translate = useTranslate()
+  const backButtonAriaLabel = useMemo(
+    () => translate('MobileHeader/MenuSubHeading/back'),
+    [translate],
+  )
 
   return (
     <>
       <Button size="s" onClick={onClickBack}>
-        <FaArrowLeftIcon alt={translate('MobileHeader/MenuSubHeading/back')} />
+        <FaArrowLeftIcon alt={backButtonAriaLabel} />
       </Button>
-
-      {/* eslint-disable-next-line smarthr/a11y-heading-in-sectioning-content */}
       <Heading type="blockTitle">
         <Translate>{title}</Translate>
       </Heading>
     </>
   )
-}
+})
