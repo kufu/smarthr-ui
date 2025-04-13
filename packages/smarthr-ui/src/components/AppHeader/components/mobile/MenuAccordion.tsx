2db5e25dd893a0e78f4bdfc0f88f983b9b8ee557
AtsushiM <at.mizoue@gmail.com>
2025-04-14 07:50:03 +0900

    chore: AccordionHeadingを切り出す

--- packages/smarthr-ui/src/components/AppHeader/components/mobile/MenuAccordion.tsx
+++ packages/smarthr-ui/src/components/AppHeader/components/mobile/MenuAccordion.tsx
@@ -1,4 +1,13 @@
-import { type Dispatch, type FC, type PropsWithChildren, type ReactNode, useId } from 'react'
+import {
+  type Dispatch,
+  type FC,
+  type PropsWithChildren,
+  type ReactNode,
+  memo,
+  useCallback,
+  useId,
+  useMemo,
+} from 'react'
 
 import { Button } from '../../../Button'
 import { Heading } from '../../../Heading'
@@ -23,12 +32,31 @@ export const MenuAccordion: FC<Props> = ({ title, children, ...rest }) =>
     children
   )
 
-const ActualMenuAccordion: FC<Props> = ({ isOpen, setIsOpen, title, children }) => {
-  const translate = useTranslate()
+const ActualMenuAccordion: FC<Props> = ({ isOpen, children, ...rest }) => {
   const id = useId()
 
   return (
     <Section>
+      <AccordionHeading {...rest} isOpen={isOpen} id={id} />
+      <div id={id}>{isOpen && <div className="shr-mt-0.5">{children}</div>}</div>
+    </Section>
+  )
+}
+
+const AccordionHeading = memo<Omit<Props, 'children'> & { id: string }>(
+  ({ isOpen, setIsOpen, title, id }) => {
+    const onClickButton = useCallback(() => setIsOpen((prev) => !prev), [setIsOpen])
+
+    const translate = useTranslate()
+    const translated = useMemo(
+      () => ({
+        close: translate('MobileHeader/MenuAccordion/close'),
+        open: translate('MobileHeader/MenuAccordion/open'),
+      }),
+      [translate],
+    )
+
+    return (
       <Cluster justify="space-between" align="center">
         <Heading type="subSubBlockTitle">
           <Translate>{title}</Translate>
@@ -39,17 +67,15 @@ const ActualMenuAccordion: FC<Props> = ({ isOpen, setIsOpen, title, children }) 
           aria-expanded={isOpen}
           aria-controls={id}
           className="[&&]:shr-p-0.25 [&&]:shr-min-h-0"
-          onClick={() => setIsOpen((prev) => !prev)}
+          onClick={onClickButton}
         >
           {isOpen ? (
-            <FaCaretUpIcon alt={translate('MobileHeader/MenuAccordion/close')} />
+            <FaCaretUpIcon alt={translated.close} />
           ) : (
-            <FaCaretDownIcon alt={translate('MobileHeader/MenuAccordion/open')} />
+            <FaCaretDownIcon alt={translated.open} />
           )}
         </Button>
       </Cluster>
-
-      <div id={id}>{isOpen && <div className="shr-mt-0.5">{children}</div>}</div>
-    </Section>
-  )
-}
+    )
+  },
+)
