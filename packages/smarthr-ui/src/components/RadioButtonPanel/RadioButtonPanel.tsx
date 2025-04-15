c5f704d7e46f3f38229deb425a6f45bafa6b3224
schktjm <schktjm@gmail.com>
2025-04-15 22:02:16 +0900

    feat: RadioButtonPanelにdescriptionの枠を作成

--- packages/smarthr-ui/src/components/RadioButtonPanel/RadioButtonPanel.tsx
+++ packages/smarthr-ui/src/components/RadioButtonPanel/RadioButtonPanel.tsx
@@ -4,7 +4,9 @@ import {
   type ComponentProps,
   type ComponentType,
   type FC,
+  type ReactNode,
   useCallback,
+  useId,
   useMemo,
   useRef,
 } from 'react'
@@ -15,25 +17,48 @@ import { RadioButton } from '../RadioButton'
 
 type Props = ComponentProps<typeof RadioButton> & {
   as?: string | ComponentType<any>
+  description?: ReactNode
 }
 
 const NONE_ROLE_TAG_REGEX = /^(div|span)$/
 
 const classNameGenerator = tv({
-  base: [
-    'smarthr-ui-RadioButtonPanel',
-    'shr-border-shorthand shr-list-none shr-shadow-none',
-    // なぜか :has が動作しないので重ねて書いている
-    'has-[:focus-visible]:shr-focus-indicator [&:has(:focus-visible)]:shr-focus-indicator',
-    '[&_.smarthr-ui-RadioButton-radioButton:focus-visible_+_span]:shr-shadow-none',
-    '[&_.smarthr-ui-RadioButton-label]:shr-ms-0.75',
-    'shr-cursor-pointer has-[:not(:disabled)]:[&_.smarthr-ui-RadioButton-label]:shr-cursor-pointer',
-    'has-[:disabled]:shr-cursor-default has-[:disabled]:[&_.smarthr-ui-RadioButton-label]:shr-cursor-default',
-  ],
+  slots: {
+    base: [
+      'smarthr-ui-RadioButtonPanel',
+      'shr-border-shorthand shr-list-none shr-shadow-none',
+      // なぜか :has が動作しないので重ねて書いている
+      'has-[:focus-visible]:shr-focus-indicator [&:has(:focus-visible)]:shr-focus-indicator',
+      '[&_.smarthr-ui-RadioButton-radioButton:focus-visible_+_span]:shr-shadow-none',
+      '[&_.smarthr-ui-RadioButton-label]:shr-ms-0.75',
+      'shr-cursor-pointer has-[:not(:disabled)]:[&_.smarthr-ui-RadioButton-label]:shr-cursor-pointer',
+      'has-[:disabled]:shr-cursor-default has-[:disabled]:[&_.smarthr-ui-RadioButton-label]:shr-cursor-default',
+    ],
+    description: 'shr-ms-[1.75em] shr-mt-0.5',
+  },
+  variants: {
+    hasDescription: {
+      true: 'shr-flex shr-flex-col',
+      false: '',
+    },
+  },
 })
 
-export const RadioButtonPanel: FC<Props> = ({ onClick, as, className, ...props }) => {
-  const actualClassName = useMemo(() => classNameGenerator({ className }), [className])
+export const RadioButtonPanel: FC<Props> = ({
+  onClick,
+  as,
+  className,
+  description,
+  'aria-describedby': ariaDescribedbyProp,
+  ...props
+}) => {
+  const classNames = useMemo(() => {
+    const { base, description: descriptionStyle } = classNameGenerator({
+      className,
+      hasDescription: !!description,
+    })
+    return { base: base(), description: descriptionStyle() }
+  }, [className, description])
   const role = useMemo(
     () => (typeof as === 'string' && NONE_ROLE_TAG_REGEX.test(as) ? 'presentation' : undefined),
     [as],
@@ -45,10 +70,21 @@ export const RadioButtonPanel: FC<Props> = ({ onClick, as, className, ...props }
     innerRef.current?.click()
   }, [])
 
+  const descriptionId = useId()
+  const ariaDescribedby = useMemo(
+    () => [description && descriptionId, ariaDescribedbyProp].filter(Boolean).join(' '),
+    [description, descriptionId, ariaDescribedbyProp],
+  )
+
   return (
     // eslint-disable-next-line smarthr/a11y-delegate-element-has-role-presentation
-    <Base padding={1} role={role} onClick={handleOuterClick} as={as} className={actualClassName}>
-      <RadioButton {...props} ref={innerRef} />
+    <Base padding={1} role={role} onClick={handleOuterClick} as={as} className={classNames.base}>
+      <RadioButton {...props} ref={innerRef} aria-describedby={ariaDescribedby} />
+      {description && (
+        <span id={descriptionId} className={classNames.description}>
+          {description}
+        </span>
+      )}
     </Base>
   )
 }
