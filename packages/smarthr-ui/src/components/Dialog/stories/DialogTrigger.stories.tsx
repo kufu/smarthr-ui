94f0aa97c64aff81d301f3bd3dd532fa6f4451cb
uknmr <y.kinmori@gmail.com>
2025-05-13 10:59:17 +0900

    docs(Dialog): Story を整理

---
+++ packages/smarthr-ui/src/components/Dialog/stories/DialogTrigger.stories.tsx
@@ -0,0 +1,29 @@
+import { Button } from '../../Button'
+import { DialogCloser } from '../DialogCloser'
+import { DialogContent } from '../DialogContent'
+import { DialogTrigger } from '../DialogTrigger'
+import { DialogWrapper } from '../DialogWrapper'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Dialog（ダイアログ）/Dialog/DialogTrigger',
+  component: DialogTrigger,
+  render: (args) => (
+    <DialogWrapper>
+      <DialogTrigger {...args}>
+        <Button>ダイアログを開く</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogCloser>
+          <Button>閉じる</Button>
+        </DialogCloser>
+      </DialogContent>
+    </DialogWrapper>
+  ),
+  parameters: {
+    chromatic: { disableSnapshot: true },
+  },
+} satisfies Meta<typeof DialogTrigger>
+
+export const Playground: StoryObj<typeof DialogTrigger> = {}
---
+++ packages/smarthr-ui/src/components/Dialog/stories/DialogWrapper.stories.tsx
@@ -0,0 +1,29 @@
+import { Button } from '../../Button'
+import { DialogCloser } from '../DialogCloser'
+import { DialogContent } from '../DialogContent'
+import { DialogTrigger } from '../DialogTrigger'
+import { DialogWrapper } from '../DialogWrapper'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Dialog（ダイアログ）/Dialog/DialogWrapper',
+  component: DialogWrapper,
+  render: (args) => (
+    <DialogWrapper {...args}>
+      <DialogTrigger>
+        <Button>ダイアログを開く</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogCloser>
+          <Button>閉じる</Button>
+        </DialogCloser>
+      </DialogContent>
+    </DialogWrapper>
+  ),
+  parameters: {
+    chromatic: { disableSnapshot: true },
+  },
+} satisfies Meta<typeof DialogWrapper>
+
+export const Playground: StoryObj<typeof DialogWrapper> = {}
