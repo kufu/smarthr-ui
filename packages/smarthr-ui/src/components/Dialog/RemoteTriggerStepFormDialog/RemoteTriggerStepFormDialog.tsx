69d3d1806f392d5c802ccf30e947b522b4b7eaea
Mizoue Atsushi <at.mizoue@gmail.com>
2025-05-14 11:09:12 +0900

    feat: RemoteTriggerStepFormDialogを追加 SHRUI-1262 (#5606)

---
+++ packages/smarthr-ui/src/components/Dialog/RemoteTriggerStepFormDialog/RemoteTriggerStepFormDialog.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { StepFormDialog } from '../StepFormDialog'
+import { useRemoteTrigger } from '../useRemoteTrigger'
+
+import type { ComponentProps, FC } from 'react'
+
+type Props = Omit<ComponentProps<typeof StepFormDialog>, 'isOpen' | 'onClickClose' | 'id'> &
+  Parameters<typeof useRemoteTrigger>[0]
+
+export const RemoteTriggerStepFormDialog: FC<Props> = ({
+  id,
+  onClickClose,
+  onToggle,
+  onOpen,
+  onClose,
+  onPressEscape,
+  ...props
+}) => {
+  const {
+    isOpen,
+    onClickClose: actualOnClickClose,
+    onPressEscape: actualOnPressEscape,
+  } = useRemoteTrigger({
+    id,
+    onClickClose,
+    onPressEscape,
+    onToggle,
+    onOpen,
+    onClose,
+  })
+
+  return (
+    <StepFormDialog
+      {...props}
+      id={id}
+      isOpen={isOpen}
+      onClickClose={actualOnClickClose}
+      onPressEscape={actualOnPressEscape}
+    />
+  )
+}
--- packages/smarthr-ui/src/index.ts
+++ packages/smarthr-ui/src/index.ts
@@ -37,6 +37,7 @@ export {
   RemoteDialogTrigger,
   RemoteTriggerActionDialog,
   RemoteTriggerFormDialog,
+  RemoteTriggerStepFormDialog,
   RemoteTriggerMessageDialog,
   StepFormDialog,
   StepFormDialogItem,
