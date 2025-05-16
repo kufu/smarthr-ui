49a77a2b81d9773f16ba1137f842ff21753dfcc7
uknmr <y.kinmori@gmail.com>
2025-05-16 16:36:20 +0900

    feat(Table): border の種類とスタイル、角丸を追加

--- packages/smarthr-ui/src/components/Table/Table.tsx
+++ packages/smarthr-ui/src/components/Table/Table.tsx
@@ -1,33 +1,108 @@
-import { type ComponentProps, type FC, type PropsWithChildren, useMemo } from 'react'
+import { type ComponentProps, type FC, Fragment, type PropsWithChildren, useMemo } from 'react'
 import { type VariantProps, tv } from 'tailwind-variants'
+
+import { Base } from '../Base'
+
+import { TableReel } from './TableReel'
 
 type Props = PropsWithChildren<VariantProps<typeof classNameGenerator>>
 type ElementProps = Omit<ComponentProps<'table'>, keyof Props>
 
 const classNameGenerator = tv({
-  base: [
-    'smarthr-ui-Table',
-    'shr-border-collapse shr-w-full',
-    '[&_tbody]:shr-bg-white',
-    '[&_th]:contrast-more:shr-border-shorthand [&_th]:shr-bg-head [&_th]:contrast-more:shr-border-high-contrast',
-    '[&_td]:contrast-more:shr-border-shorthand [&_td]:contrast-more:shr-border-high-contrast',
-    'contrast-more:shr-border-shorthand contrast-more:shr-border-high-contrast',
-  ],
+  slots: {
+    wrapper: '',
+    table: [
+      'smarthr-ui-Table',
+      'shr-border-collapse shr-w-full',
+      '[&_tbody]:shr-bg-white',
+      '[&_th]:contrast-more:shr-border-shorthand [&_th]:shr-bg-head [&_th]:contrast-more:shr-border-high-contrast',
+      '[&_td]:contrast-more:shr-border-shorthand [&_td]:contrast-more:shr-border-high-contrast',
+      'contrast-more:shr-border-shorthand contrast-more:shr-border-high-contrast',
+    ],
+  },
   variants: {
     borderType: {
-      horizontal: 'shr-table-border-horizontal',
-      both: 'shr-table-border-vertical shr-table-border-horizontal',
+      vertical: {},
+      horizontal: {
+        table: 'shr-table-border-horizontal',
+      },
+      both: {
+        table: 'shr-table-border-horizontal',
+      },
+      outer: {
+        table: 'shr-border-shorthand',
+      },
+      all: {
+        table: 'shr-table-border-horizontal shr-border-shorthand',
+      },
+    },
+    borderStyle: {
+      solid: {
+        table: '[&_:is(.smarthr-ui-Th,.smarthr-ui-Td)]:shr-border-solid',
+      },
+      dotted: {
+        table: '[&_:is(.smarthr-ui-Th,.smarthr-ui-Td)]:shr-border-dotted',
+      },
+      dashed: {
+        table: '[&_:is(.smarthr-ui-Th,.smarthr-ui-Td)]:shr-border-dashed',
+      },
+    },
+    rounded: {
+      true: {},
     },
     layout: {
-      auto: '',
-      fixed: 'shr-table-fixed',
+      auto: {},
+      fixed: {
+        table: 'shr-table-fixed',
+      },
     },
     fixedHead: {
-      true: '[&_thead]:shr-sticky [&_thead]:shr-start-0 [&_thead]:shr-top-0 [&_thead]:shr-z-[2] [&_tbody]:shr-relative [&_tbody]:shr-z-1',
+      true: {
+        table:
+          '[&_thead]:shr-sticky [&_thead]:shr-start-0 [&_thead]:shr-top-0 [&_thead]:shr-z-[2] [&_tbody]:shr-relative [&_tbody]:shr-z-1',
+      },
     },
   },
+  compoundVariants: [
+    {
+      // rounded のとき Wrapper である Base に装飾するため、Table には装飾しない
+      borderType: ['outer', 'all'],
+      rounded: true,
+      className: {
+        table: 'shr-border-none',
+        wrapper: 'shr-border-shorthand',
+      },
+    },
+    {
+      borderType: ['vertical', 'both', 'all'],
+      className: {
+        table: [
+          '[&_:is(.smarthr-ui-Th:not(:first-child),.smarthr-ui-Td:not(:first-child))]:shr-border-l',
+          '[&_:is(.smarthr-ui-Th:not(:first-child),.smarthr-ui-Td:not(:first-child))]:shr-border-l-default',
+        ],
+      },
+    },
+    {
+      borderType: ['horizontal', 'both', 'all'],
+      className: {
+        table: [
+          // thead がある場合は、thead 配下以外の th と td に border-t を適用
+          [
+            '[&:has(thead)_tr:not(:where(thead_tr))_:is(.smarthr-ui-Th,.smarthr-ui-Td)]:shr-border-t',
+            '[&:has(thead)_tr:not(:where(thead_tr))_:is(.smarthr-ui-Th,.smarthr-ui-Td)]:shr-border-t-default',
+          ],
+          // thead がない場合は、最初以外の tr 配下の th と td に border-t を適用
+          [
+            '[&:not(:has(thead))_tr:not(:first-of-type)_:is(.smarthr-ui-Th,.smarthr-ui-Td)]:shr-border-t',
+            '[&:not(:has(thead))_tr:not(:first-of-type)_:is(.smarthr-ui-Th,.smarthr-ui-Td)]:shr-border-t-default',
+          ],
+        ],
+      },
+    },
+  ],
   defaultVariants: {
     borderType: 'horizontal',
+    borderStyle: 'solid',
     layout: 'auto',
     fixedHead: false,
   },
@@ -35,15 +110,38 @@ const classNameGenerator = tv({
 
 export const Table: FC<Props & ElementProps> = ({
   borderType,
+  borderStyle,
   fixedHead,
   layout,
+  rounded,
   className,
-  ...props
+  ...rest
 }) => {
-  const actualClassName = useMemo(
-    () => classNameGenerator({ borderType, fixedHead, layout, className }),
-    [borderType, className, fixedHead, layout],
+  const actualClassName = useMemo(() => {
+    const { table, wrapper } = classNameGenerator({
+      borderType,
+      borderStyle,
+      fixedHead,
+      layout,
+      rounded,
+      className,
+    })
+    return { table: table(), wrapper: wrapper() }
+  }, [borderType, borderStyle, className, fixedHead, layout, rounded])
+  const [Wrapper, wrapperProps] = useMemo(
+    () => (rounded ? [RoundedWrapper, { className: actualClassName.wrapper }] : [Fragment, {}]),
+    [rounded, actualClassName.wrapper],
   )
 
-  return <table {...props} className={actualClassName} />
+  return (
+    <Wrapper {...wrapperProps}>
+      <table {...rest} className={actualClassName.table} />
+    </Wrapper>
+  )
 }
+
+const RoundedWrapper = ({ children, ...rest }: PropsWithChildren) => (
+  <Base {...rest} overflow="hidden" layer={0}>
+    <TableReel>{children}</TableReel>
+  </Base>
+)
