404a30ee89036ee336d81052b74c3eaebbdf7d78
yt-ymmt <ftbae1@gmail.com>
2025-06-02 19:32:04 +0900

    chore: ESLint設定を更新し、無視リストに新しいパターンを追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,55 +4,57 @@ import storybook from 'eslint-plugin-storybook'
 /**
  * @type {import('eslint').Linter.Config[]}
  */
-export default [...smarthr, ...storybook.configs['flat/recommended'], {
-  rules: {
-    'jsx-a11y/anchor-is-valid': 'error',
-    'jsx-a11y/click-events-have-key-events': 'error',
-    'jsx-a11y/no-static-element-interactions': 'error',
-    'jsx-a11y/label-has-associated-control': [
-      'error',
-      {
-        controlComponents: ['Input', 'InputWithTooltip'],
-      },
-    ],
-    'jsx-a11y/role-has-required-aria-props': 'error',
-    'jsx-a11y/mouse-events-have-key-events': 'error',
-    'jsx-a11y/no-noninteractive-element-interactions': 'error',
-    '@typescript-eslint/consistent-type-definitions': [
-      'error',
-      'type',
-    ],
-    'react-hooks/exhaustive-deps': 'error',
-    'smarthr/require-barrel-import': 'off',
-    'smarthr/a11y-anchor-has-href-attribute': [
-      'error',
-      {
-        checkType: 'allow-spread-attributes',
-      }
-    ],
-    'smarthr/a11y-input-has-name-attribute': [
-      'error',
-      {
-        checkType: 'allow-spread-attributes',
-      }
-    ],
-    'smarthr/a11y-prohibit-sectioning-content-in-form': 'error',
-    '@typescript-eslint/consistent-type-imports': [
-      'error',
-      {
-        fixStyle: 'inline-type-imports',
-      }
-    ],
-    '@typescript-eslint/no-import-type-side-effects': 'error',
+export default [
+  ...smarthr,
+  ...storybook.configs['flat/recommended'],
+  {
+    rules: {
+      'jsx-a11y/anchor-is-valid': 'error',
+      'jsx-a11y/click-events-have-key-events': 'error',
+      'jsx-a11y/no-static-element-interactions': 'error',
+      'jsx-a11y/label-has-associated-control': [
+        'error',
+        {
+          controlComponents: ['Input', 'InputWithTooltip'],
+        },
+      ],
+      'jsx-a11y/role-has-required-aria-props': 'error',
+      'jsx-a11y/mouse-events-have-key-events': 'error',
+      'jsx-a11y/no-noninteractive-element-interactions': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      'react-hooks/exhaustive-deps': 'error',
+      'smarthr/require-barrel-import': 'off',
+      'smarthr/a11y-anchor-has-href-attribute': [
+        'error',
+        {
+          checkType: 'allow-spread-attributes',
+        },
+      ],
+      'smarthr/a11y-input-has-name-attribute': [
+        'error',
+        {
+          checkType: 'allow-spread-attributes',
+        },
+      ],
+      'smarthr/a11y-prohibit-sectioning-content-in-form': 'error',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+      '@typescript-eslint/no-import-type-side-effects': 'error',
+    },
   },
-}, {
-  ignores: [
-    '**/*.{mjs,js}',
-    '**/*.stories.tsx',
-    'sandbox/',
-    'storybook-static/',
-    'packages/smarthr-ui/esm/',
-    'packages/smarthr-ui/lib/',
-    'packages/smarthr-ui/.storybook',
-  ]
-}, ...storybook.configs["flat/recommended"]];
+  {
+    ignores: [
+      '**/*.{mjs,js}',
+      '**/*.stories.tsx',
+      'sandbox/',
+      'storybook-static/',
+      'packages/smarthr-ui/esm/',
+      'packages/smarthr-ui/lib/',
+      'packages/smarthr-ui/.storybook',
+    ],
+  },
+]
