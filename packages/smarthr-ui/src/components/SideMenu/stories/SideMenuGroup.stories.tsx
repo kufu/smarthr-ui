445471abe2a4428063e69cb602431a2b2bd2047f
uknmr <y.kinmori@gmail.com>
2025-06-03 14:57:52 +0900

    docs(SideMenu): Story を修正

---
+++ packages/smarthr-ui/src/components/SideMenu/stories/SideMenuGroup.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { SideMenu, SideMenuGroup, SideMenuItem } from '..'
+
+export default {
+  title: 'Navigation（ナビゲーション）/SideMenu/SideMenuGroup',
+  component: SideMenuGroup,
+  render: ({ title, ...args }) => (
+    <SideMenu>
+      <SideMenuGroup {...args} title={title || 'グループタイトル'}>
+        <SideMenuItem href="#">メニュー1</SideMenuItem>
+        <SideMenuItem href="#">メニュー2</SideMenuItem>
+      </SideMenuGroup>
+    </SideMenu>
+  ),
+  parameters: {
+    chromatic: { disableSnapshot: true },
+  },
+} satisfies Meta<typeof SideMenuGroup>
+
+export const Playground: StoryObj<typeof SideMenuGroup> = {}
+
+export const Title: StoryObj<typeof SideMenuGroup> = {
+  args: {
+    title: 'タイトル',
+  },
+}
+
+export const TitleElementAs: StoryObj<typeof SideMenuGroup> = {
+  args: {
+    titleElementAs: 'span',
+  },
+}
+
+export const ListElementAs: StoryObj<typeof SideMenuGroup> = {
+  args: {
+    listElementAs: 'ol',
+  },
+}
