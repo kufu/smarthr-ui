33fdf91aa990b9ed35a528afa1539e93ef2f8cfd
Qs-F <qsf@de-liker.com>
2025-06-12 20:51:53 +0900

    chore(SortDropdown): intlで多言語化

--- packages/smarthr-ui/src/intl/locales/ja.ts
+++ packages/smarthr-ui/src/intl/locales/ja.ts
@@ -8,5 +8,11 @@ export const locale = {
   'smarthr-ui/DropdownMenuButton/triggerActive': '候補を閉じる',
   'smarthr-ui/DropdownMenuButton/triggerInactive': '候補を開く',
   'smarthr-ui/RequiredLabel/text': '必須',
+  'smarthr-ui/SortDropdown/applyButtonLabel': '適用',
+  'smarthr-ui/SortDropdown/ascLabel': '昇順',
+  'smarthr-ui/SortDropdown/cancelButtonLabel': 'キャンセル',
+  'smarthr-ui/SortDropdown/descLabel': '降順',
+  'smarthr-ui/SortDropdown/sortFieldLabel': '並べ替え項目',
+  'smarthr-ui/SortDropdown/sortOrderLabel': '並び順',
   'smarthr-ui/TextLink/openInNewTab': '別タブで開く',
 } as const
