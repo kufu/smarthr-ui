16b3aee4ef3374632b6a61aa011a8f59053f061e
Qs-F <qsf@de-liker.com>
2025-06-23 17:44:48 +0900

    chore(NotificationBar): intlで多言語化

--- packages/smarthr-ui/src/intl/locales/vi_vn.ts
+++ packages/smarthr-ui/src/intl/locales/vi_vn.ts
@@ -21,6 +21,7 @@ export const locale = {
   'smarthr-ui/InformationPanel/closeButtonLabel': '',
   'smarthr-ui/InformationPanel/openButtonLabel': '',
   'smarthr-ui/InputFile/destroy': '',
+  'smarthr-ui/NotificationBar/closeButtonIconAlt': '',
   'smarthr-ui/RequiredLabel/text': 'Bắt buộc',
   'smarthr-ui/SortDropdown/applyButtonLabel': '',
   'smarthr-ui/SortDropdown/ascLabel': '',
