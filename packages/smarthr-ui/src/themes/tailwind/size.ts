8ae49333d4f19bbfc8685bb91cc52f6f1a46d566
KANAMORI Yu <y.kinmori@gmail.com>
2025-06-25 13:15:44 +0900

    feat: 幅のトークンと Container を追加 (#5704)

---
+++ packages/smarthr-ui/src/themes/tailwind/size.ts
@@ -0,0 +1,9 @@
+export const dialogSize = {
+  XS: 'shr-w-col3',
+  S: 'shr-w-col4',
+  M: 'shr-w-col5',
+  L: 'shr-w-col6',
+  XL: 'shr-w-col7',
+  XXL: 'shr-w-col8',
+  FULL: 'shr-w-full',
+} as const
