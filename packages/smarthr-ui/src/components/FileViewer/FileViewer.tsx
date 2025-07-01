3442d8517b69f8cd8e89e29f32b8873f9ea23baf
otakky <kazuki.takigawa@smarthr.co.jp>
2025-07-01 17:01:15 +0900

    feat(FileView): PDFViewer でパスワード付き PDF を開くときの操作を利用側で制御できるようにする (#5762)

--- packages/smarthr-ui/src/components/FileViewer/FileViewer.tsx
+++ packages/smarthr-ui/src/components/FileViewer/FileViewer.tsx
@@ -1,7 +1,16 @@
 'use client'
 
 import Decimal from 'decimal.js'
-import { type FC, memo, useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import {
+  type ComponentProps,
+  type FC,
+  memo,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 
 import {
   Button,
@@ -21,7 +30,6 @@ import { PDFViewer } from './PDFViewer'
 import type { FileForViewer } from './types'
 
 const defaultScaleStep = new Decimal(0.2)
-
 const defaultScaleSteps = [0.2, 0.6, 1, 1.6, 2, 3]
 
 type Props = {
@@ -34,9 +42,16 @@ type Props = {
   scaleSteps?: number[]
 
   scaleStep?: number
+  onPassword?: ComponentProps<typeof PDFViewer>['onPassword']
 }
 
-export const FileViewer: FC<Props> = ({ file, scaleStep, scaleSteps, width: fixedWidth }) => {
+export const FileViewer: FC<Props> = ({
+  file,
+  scaleStep,
+  scaleSteps,
+  width: fixedWidth,
+  onPassword,
+}) => {
   const ref = useRef<HTMLDivElement>(null)
   const [scale, setScale] = useState(1)
   const [loaded, setLoaded] = useState(false)
@@ -109,6 +124,7 @@ export const FileViewer: FC<Props> = ({ file, scaleStep, scaleSteps, width: fixe
               file={file}
               width={width}
               onLoad={handleLoaded}
+              onPassword={onPassword}
             />
           ) : file.contentType.startsWith('image/') ? (
             <ImageViewer
