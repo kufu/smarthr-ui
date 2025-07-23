60743805f75a9592f5b925dad8963a22fd8ae927
Misako Tateiwa <misako.tateiwa@smarthr.co.jp>
2025-07-23 09:11:29 +0900

    fix: valueに値は入らないので修正

--- packages/smarthr-ui/src/components/InputFile/InputFile.tsx
+++ packages/smarthr-ui/src/components/InputFile/InputFile.tsx
@@ -207,7 +207,7 @@ export const InputFile = forwardRef<HTMLInputElement, Props & ElementProps>(
         updateFiles(newFiles)
 
         // 削除後、同一ファイルを再選択可能にするためinput.valueをリセット
-        inputRef.current.value = newFiles[0]?.name || ''
+        inputRef.current.value = ''
       },
       [files, updateFiles],
     )
