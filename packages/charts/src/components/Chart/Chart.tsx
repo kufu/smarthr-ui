8eb89cfcf4bb6225aef88379522973de2d802507
schktjm <schktjm@gmail.com>
2025-07-31 19:11:01 +0900

    chore: デフォルトサイズを設定し、上書きできるようにした

--- packages/charts/src/components/Chart/Chart.tsx
+++ packages/charts/src/components/Chart/Chart.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import { useMemo } from 'react'
+import { tv } from 'tailwind-variants'
+
 import { registerChartComponents } from '../../config'
 import { BarChart } from '../BarChart'
 
 import type { ChartData } from 'chart.js'
 
-// Chart.jsのコンポーネントをモジュールレベルで登録
 registerChartComponents()
 
 type ChartType = 'bar'
@@ -14,9 +16,24 @@ type Props = {
   type: ChartType
   data: ChartData<ChartType>
   title: string
+  className?: string
 }
 
-export const Chart: React.FC<Props> = ({ type, data, title }) => {
+const classNameGenerator = tv({
+  base: 'h-[500px]',
+})
+
+export const Chart: React.FC<Props> = ({ type, data, title, className }) => {
+  const classNames = useMemo(() => classNameGenerator({ className }), [className])
+
+  return (
+    <div className={classNames}>
+      <InnerChart type={type} data={data} title={title} />
+    </div>
+  )
+}
+
+const InnerChart: React.FC<Props> = ({ type, data, title }) => {
   switch (type) {
     case 'bar':
       return <BarChart data={data} title={title} />
