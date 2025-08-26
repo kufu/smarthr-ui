c28b7eff7e4f54d93ce5f64f307dee4019e3802d
Ryo Igarashi <n33t5hin@gmail.com>
2025-08-26 14:42:26 +0900

    fix: RangeSeparatorを非推奨化する

--- packages/smarthr-ui/src/components/RangeSeparator/RangeSeparator.tsx
+++ packages/smarthr-ui/src/components/RangeSeparator/RangeSeparator.tsx
@@ -1,7 +1,6 @@
 import { memo } from 'react'
 
 import { type DecoratorsType, useDecorators } from '../../hooks/useDecorators'
-import { VisuallyHiddenText } from '../VisuallyHiddenText'
 
 type Props = {
   decorators?: DecoratorsType<DecoratorKeyTypes>
@@ -9,19 +8,13 @@ type Props = {
 
 const DECORATOR_DEFAULT_TEXTS = {
   text: '〜',
-  visuallyHiddenText: 'から',
 } as const
 type DecoratorKeyTypes = keyof typeof DECORATOR_DEFAULT_TEXTS
 
+/**
+ * @deprecated このコンポーネントはスクリーンリーダー「NVDA」のバグを迂回するために導入されましたが、当該のバグは解消されているため、今後削除される予定です。代わりにプレーンな文字列 `〜` を使ってください。（[参考: nvdajp/nvdajp#440](https://github.com/nvdajp/nvdajp/issues/440)）
+ */
 export const RangeSeparator = memo<Props>(({ decorators }) => {
   const decorated = useDecorators<DecoratorKeyTypes>(DECORATOR_DEFAULT_TEXTS, decorators)
-
-  return (
-    <>
-      <span aria-hidden="true">{decorated.text}</span>
-      <VisuallyHiddenText className="shr-select-none">
-        {decorated.visuallyHiddenText}
-      </VisuallyHiddenText>
-    </>
-  )
+  return decorated.text
 })
