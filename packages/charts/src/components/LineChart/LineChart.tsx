2f1d4f47fae53f423a4a0bc538dbb4da42bb7bb2
schktjm <schktjm@gmail.com>
2025-09-25 19:21:01 +0900

    fix: LineChartの場合はポイントを変更するようにした

--- packages/charts/src/components/LineChart/LineChart.tsx
+++ packages/charts/src/components/LineChart/LineChart.tsx
@@ -21,7 +21,7 @@ type Props = {
 export const LineChart: React.FC<Props> = ({ data, title }) => {
   const chartId = useId()
   const chartRef = useRef<Chart<'line'>>(null)
-  const chartColors = getChartColors<'line'>(data.datasets.length)
+  const chartColors = getChartColors('line', data.datasets.length)
 
   const ariaLabel = useMemo(() => {
     const datasetCount = data.datasets.length
