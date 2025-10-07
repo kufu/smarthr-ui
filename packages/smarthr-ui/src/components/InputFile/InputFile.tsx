9ed52d7b8a29fbade15b578a75b2fb47fedb7f96
Misako Tateiwa <misako.tateiwa@smarthr.co.jp>
2025-10-07 13:33:50 +0900

    feat: InputFileNativeを返せるようにする

--- packages/smarthr-ui/src/components/InputFile/InputFile.tsx
+++ packages/smarthr-ui/src/components/InputFile/InputFile.tsx
@@ -2,12 +2,14 @@
 
 import { forwardRef } from 'react'
 
-import { InputFileMultiplyAppendable, type Props } from './InputFileMultiplyAppendable'
+import { InputFileMultiplyAppendable } from './InputFileMultiplyAppendable'
+import { InputFileNative } from './InputFileNative'
 
-import type { ElementProps } from './types'
+import type { ElementProps, Props } from './types'
 
 export const InputFile = forwardRef<HTMLInputElement, Props & ElementProps>((props, ref) => {
   if (props.multiple && props.multiplyAppendable) {
     return <InputFileMultiplyAppendable {...props} ref={ref} />
   }
+  return <InputFileNative {...props} ref={ref} />
 })
