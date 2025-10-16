63a7fa5137508f12a8ab58142c23a5618fe09339
Hiroki Miyaji <hiroki.miyaji@smarthr.co.jp>
2025-10-16 09:20:56 +0900

    perf: storiesディレクトリをバンドルから削除した

--- packages/smarthr-ui/rollup.esm.config.js
+++ packages/smarthr-ui/rollup.esm.config.js
@@ -1,13 +1,13 @@
-import typescript from '@rollup/plugin-typescript';
-import preserveDirectives from "rollup-plugin-preserve-directives";
-import renameNodeModules from "rollup-plugin-rename-node-modules";
-import replace from '@rollup/plugin-replace';
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { globSync } from 'glob';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import packageJson from './package.json' with { type: "json" }
+import typescript from '@rollup/plugin-typescript'
+import preserveDirectives from 'rollup-plugin-preserve-directives'
+import renameNodeModules from 'rollup-plugin-rename-node-modules'
+import replace from '@rollup/plugin-replace'
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { globSync } from 'glob'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import packageJson from './package.json' with { type: 'json' }
 
 const peerDependencies = packageJson.peerDependencies
   ? Object.keys(packageJson.peerDependencies)
@@ -16,6 +16,7 @@ const peerDependencies = packageJson.peerDependencies
 const entryPoints = globSync('src/**/*.{ts,tsx}', {
   ignore: [
     '**/*.stories.{ts,tsx}',
+    '**/stories/*.{ts,tsx}',
     '**/*.test.{ts,tsx}',
     '**/__tests__/*.{ts,tsx}',
   ],
@@ -23,16 +24,16 @@ const entryPoints = globSync('src/**/*.{ts,tsx}', {
 
 /** @type {import('rollup').RollupOptions} */
 export default {
-	input: Object.fromEntries(
-		entryPoints.map(file => [
-			path.relative(
-				'src',
+  input: Object.fromEntries(
+    entryPoints.map((file) => [
+      path.relative(
+        'src',
         // 拡張子を除去し書き出し時に正しいファイル名になるようにしている
-				file.slice(0, file.length - path.extname(file).length)
-			),
-			fileURLToPath(new URL(file, import.meta.url))
-		])
-	),
+        file.slice(0, file.length - path.extname(file).length),
+      ),
+      fileURLToPath(new URL(file, import.meta.url)),
+    ]),
+  ),
   output: {
     format: 'es',
     sourcemap: true,
@@ -57,7 +58,7 @@ export default {
     commonjs(),
     nodeResolve(),
     // node_modulesのままだとimportできないケースがあったので、vendorにrenameしている
-    renameNodeModules("vendor"),
+    renameNodeModules('vendor'),
     // reactの影響でprocess is not definedになってしまうので、"production"に置き換えている
     // import.meta.env等に置き換えるほうが本当は好ましいが、あまり良い方法がない
     replace({
@@ -69,9 +70,9 @@ export default {
     // picocolorsのみprocessをglobalThis.processに置き換えて、process is not definedを回避する
     replace({
       values: {
-        'process': 'globalThis.process',
+        process: 'globalThis.process',
       },
-      include: "**/picocolors.*",
+      include: '**/picocolors.*',
       delimiters: ['\\s', '\\s(?!\\.)'],
       preventAssignment: true,
     }),
