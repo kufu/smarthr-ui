64da4311b8fe62a952ead100b8a91307bb4a4c82
Qs-F <qsf@de-liker.com>
2025-10-22 20:53:08 +0900

    feat: add PropsWithHTMLAttributes type

--- packages/smarthr-ui/src/types/ComponentTypes.ts
+++ packages/smarthr-ui/src/types/ComponentTypes.ts
@@ -1,5 +1,10 @@
-import type { ComponentPropsWithRef, ElementType } from 'react'
+import type { ComponentPropsWithRef, ComponentPropsWithoutRef, ElementType, FC } from 'react'
 
 export type ElementRef<T extends ElementType> = ComponentPropsWithRef<T>['ref']
 
 export type ElementRefProps<T extends ElementType> = { ref?: ElementRef<T> }
+
+export type PropsWithHTMLAttributes<
+  Props extends Parameters<FC>[0],
+  E extends ElementType,
+> = Props & Omit<ComponentPropsWithoutRef<E>, keyof Props>
