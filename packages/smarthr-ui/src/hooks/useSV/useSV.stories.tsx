65761dd5cabd0a340be045b2db10650e29fba93a
たふみ / Tafumi <qsf@de-liker.com>
2025-12-25 07:40:06 +0900

    feat: useEnvironment/EnvironmentProviderとmediaQueryトークンを追加 (#5954)

---
+++ packages/smarthr-ui/src/hooks/useSV/useSV.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryFn } from '@storybook/react'
+import { defineSV, useSV, VariantProps } from './useSV'
+import { EnvironmentProvider } from '../useEnvironment'
+import { FC } from 'react'
+
+const sv = defineSV(({ mobile }) => ({
+  base: {
+    box: ['shr-size-8', mobile && 'shr-size-4 shr-rounded-full'],
+  },
+  variants: {
+    color: {
+      default: {
+        box: 'shr-bg-column',
+      },
+      main: {
+        box: 'shr-bg-main',
+      },
+    },
+  },
+}))
+
+type Props = VariantProps<typeof sv>
+
+const Component: FC<Props> = ({ color = 'main' }) => {
+  const { box } = useSV(sv, { color })
+  return <div className={box} />
+}
+
+export default {
+  title: 'Hooks/useSV',
+  decorators: [
+    (Story) => (
+      <EnvironmentProvider>
+        <Story />
+      </EnvironmentProvider>
+    ),
+  ],
+} satisfies Meta<typeof useSV>
+
+export const Default: StoryFn = () => <Component />
