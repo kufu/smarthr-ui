353da2b67dc427c48d75345c587eb552442d22b8
AtsushiM <at.mizoue@gmail.com>
2025-12-26 14:48:09 +0900

    chore: eslint ruleの適用を調整

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,16 +9,22 @@ export default [
   ...storybook.configs['flat/recommended'],
   {
     rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+      '@typescript-eslint/no-import-type-side-effects': 'error',
       'jsx-a11y/anchor-is-valid': 'error',
       'jsx-a11y/click-events-have-key-events': 'error',
-      'jsx-a11y/no-static-element-interactions': 'error',
       'jsx-a11y/label-has-associated-control': [
         'error',
         {
           controlComponents: ['Input', 'InputWithTooltip'],
         },
       ],
-      'jsx-a11y/role-has-required-aria-props': 'error',
       'jsx-a11y/mouse-events-have-key-events': 'error',
       'jsx-a11y/no-noninteractive-element-interactions': 'error',
       'jsx-a11y/no-noninteractive-element-to-interactive-role': [
@@ -27,9 +33,9 @@ export default [
           menu: ['menu'],
         },
       ],
-      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      'jsx-a11y/no-static-element-interactions': 'error',
+      'jsx-a11y/role-has-required-aria-props': 'error',
       'react-hooks/exhaustive-deps': 'error',
-      'smarthr/require-barrel-import': 'off',
       'smarthr/a11y-anchor-has-href-attribute': [
         'error',
         {
@@ -42,15 +48,11 @@ export default [
           checkType: 'allow-spread-attributes',
         },
       ],
+      'smarthr/best-practice-for-optional-chaining': 'error',
       'smarthr/best-practice-for-prohibit-import-smarthr-ui-local': 'off',
       'smarthr/best-practice-for-rest-parameters': 'error',
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        {
-          fixStyle: 'inline-type-imports',
-        },
-      ],
-      '@typescript-eslint/no-import-type-side-effects': 'error',
+      'smarthr/best-practice-for-unnesessary-early-return': 'error',
+      'smarthr/require-barrel-import': 'off',
     },
   },
   {
