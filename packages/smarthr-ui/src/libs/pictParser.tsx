f07e70472170907cee3f49699621675c476c8917
AtsushiM <at.mizoue@gmail.com>
2026-02-13 08:31:39 +0900

    chore: pictParserを更新

--- packages/smarthr-ui/src/libs/pictParser.tsx
+++ packages/smarthr-ui/src/libs/pictParser.tsx
@@ -1,27 +1,29 @@
 import { FaAddressBookIcon } from '../components/Icon'
 
-export const pictParser = <T,>(pict: string, formatter: (vs: { [key: string]: any }) => T) => {
+type AnyObjectType = { [key: string]: any }
+
+export const pictParser = <T extends AnyObjectType>(
+  pict: string,
+  formatter: (vs: { [key: string]: any }) => T,
+) => {
   const lines = pict.split('\n')
   const header = lines.shift()!.split('\t')
+  const reducer = (prev: AnyObjectType, c: string, i: number) => {
+    prev[header[i]] = valueConverter(i === 0 ? c.replace(/^(\s|\t)+/, '') : c)
 
-  return lines.map((line) =>
-    formatter(
-      line
-        .split('\t')
-        .map((c, i) => valueConverter(i === 0 ? c.replace(/^(\s|\t)+/, '') : c))
-        .reduce((prev, c, i) => {
-          prev[header[i]] = c
+    return prev
+  }
 
-          return prev
-        }, {} as T),
-    ),
-  )
+  return lines.map((line) => formatter(line.split('\t').reduce(reducer, {} as T)))
 }
 
+const NUM_REGEX = /^([0-9]+|[0-9]+\.[0-9]+)$/
 const valueConverter = (v: string): any => {
   switch (v) {
     case 'undefined':
       return undefined
+    case 'null':
+      return null
     case 'true':
       return true
     case 'false':
@@ -30,8 +32,9 @@ const valueConverter = (v: string): any => {
       return <FaAddressBookIcon />
   }
 
-  if (/^[0-9]+$/.test(v)) {
-    return parseInt(v, 10)
+  // HINT: 整数 or 少数含む数値の場合数値変換
+  if (NUM_REGEX.test(v)) {
+    return parseFloat(v)
   }
 
   return v
