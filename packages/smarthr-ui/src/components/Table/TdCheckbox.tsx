c0d29b804ce53728768cb69830488178704d529a
Mizoue Atsushi <at.mizoue@gmail.com>
2026-03-04 15:52:45 +0900

    Update packages/smarthr-ui/src/components/Table/TdCheckbox.tsx
    Co-authored-by: Yoshiaki ITO <oti@users.noreply.github.com>

--- packages/smarthr-ui/src/components/Table/TdCheckbox.tsx
+++ packages/smarthr-ui/src/components/Table/TdCheckbox.tsx
@@ -7,7 +7,7 @@ import { VisuallyHiddenText } from '../VisuallyHiddenText'
 import { Td } from './Td'
 
 type AbstractProps = PropsWithChildren<{
-  /** RadioButtonのa11y nameとして設定するテキストを持つ同じ親Tr以下のTd,Th,もしくはそれらの子孫にある要素のid属性。複数要素を指定する場合は空白区切りでつなぐ */
+  /** Checkboxのaccessible nameとして設定するテキストを持つ同じ親Tr配下のTd,Th,もしくはそれらの子孫にある要素のid属性値。複数要素を指定する場合は空白区切りでつなぐ */
   'aria-labelledby': string
 }> &
   Pick<ComponentProps<typeof Td>, 'vAlign' | 'fixed'>
