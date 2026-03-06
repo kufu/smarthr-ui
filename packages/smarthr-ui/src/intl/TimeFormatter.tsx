4b520d60f524934aad873544237cca8c74546015
ntfd0 <viktor.makhnutin@smarthr.co.jp>
2026-03-06 13:55:16 +0900

    fix(TimeFormatter): メモ化して、事例をISOに変換

--- packages/smarthr-ui/src/intl/TimeFormatter.tsx
+++ packages/smarthr-ui/src/intl/TimeFormatter.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import dayjs from 'dayjs'
+import { memo } from 'react'
 
 import { useIntl } from './useIntl'
 
@@ -17,28 +18,25 @@ import type { FormatTimeProps } from './useIntl'
  * @returns フォーマットされた時刻を含むtime要素
  * @example
  * // 基本的な使用法（ロケールのデフォルト形式）
- * <TimeFormatter date={new Date()} />
- * // <time datetime="2024-01-15T10:30:00.000Z">10:30</time>
+ * <TimeFormatter date="2026-02-01T10:30:00+09:00" />
+ * // <time datetime="2026-02-01T10:30:00+09:00">10:30</time>
  *
  * // 秒を含めて表示
- * <TimeFormatter date={new Date()} parts={['hour', 'minute', 'second']} />
- * // <time datetime="2024-01-15T10:30:00.000Z">10:30:45</time>
+ * <TimeFormatter date="2026-02-01T10:30:45+09:00" parts={['hour', 'minute', 'second']} />
+ * // <time datetime="2026-02-01T10:30:45+09:00">10:30:45</time>
  *
  * // 時のみ表示
- * <TimeFormatter date={new Date()} parts={['hour']} />
- * // <time datetime="2024-01-15T10:30:00.000Z">10</time>
+ * <TimeFormatter date="2026-02-01T10:30:00+09:00" parts={['hour']} />
+ * // <time datetime="2026-02-01T10:30:00+09:00">10</time>
  *
  * // 12時間形式で表示
- * <TimeFormatter date={new Date()} options={{ hour12: true }} />
- * // <time datetime="2024-01-15T10:30:00.000Z">10:30 AM</time> (en-us)
+ * <TimeFormatter date="2026-02-01T10:30:00+09:00" options={{ hour12: true }} />
+ * // <time datetime="2026-02-01T10:30:00+09:00">10:30 AM</time> (en-us)
  */
-export const TimeFormatter = ({
-  date: orgDate,
-  ...rest
-}: Omit<FormatTimeProps, 'date'> & {
-  date: string | Date
-}) => {
-  const date = dayjs(orgDate).toDate()
-  const { formatTime } = useIntl()
-  return <time dateTime={date.toISOString()}>{formatTime({ ...rest, date })}</time>
-}
+export const TimeFormatter = memo<Omit<FormatTimeProps, 'date'> & { date: string | Date }>(
+  ({ date: orgDate, ...rest }) => {
+    const date = dayjs(orgDate).toDate()
+    const { formatTime } = useIntl()
+    return <time dateTime={date.toISOString()}>{formatTime({ ...rest, date })}</time>
+  },
+)
