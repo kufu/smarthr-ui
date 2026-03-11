43d8cf0376cb3753f9de4140c4a9f7d4f0502637
AtsushiM <at.mizoue@gmail.com>
2026-03-11 09:52:42 +0900

    chore: ボタン周りの設定を共通化する

--- packages/smarthr-ui/src/components/Dialog/StepFormDialog/StepFormDialog.tsx
+++ packages/smarthr-ui/src/components/Dialog/StepFormDialog/StepFormDialog.tsx
@@ -6,21 +6,26 @@ import {
   type FormEvent,
   type ReactNode,
   useCallback,
+  useContext,
+  useMemo,
   useRef,
 } from 'react'
 
-import { useObjectAttributes } from '../../../hooks/useObjectAttributes'
+import { useIntl } from '../../../intl'
 import { DialogContentInner } from '../DialogContentInner'
 import { useDialogPortal } from '../useDialogPortal'
 import { useObjectHeading } from '../useObjectHeading'
 
 import {
-  type ButtonArgType,
-  type ObjectCloseButtonType,
   StepFormDialogContentInner,
   type StepFormDialogContentInnerProps,
 } from './StepFormDialogContentInner'
-import { StepFormDialogProvider } from './StepFormDialogProvider'
+import { StepFormDialogContext, StepFormDialogProvider } from './StepFormDialogProvider'
+import {
+  type ButtonArgType,
+  type ObjectButtonType,
+  useStepFormDialogButton,
+} from './useStepFormDialogButton'
 
 import type { FocusTrapRef } from '../FocusTrap'
 import type { DialogProps /** コンテンツなにもないDialogの基本props */ } from '../types'
@@ -28,28 +33,44 @@ import type { DialogProps /** コンテンツなにもないDialogの基本props
 type ObjectHeadingType = Omit<StepFormDialogContentInnerProps['heading'], 'id'>
 type HeadingType = ReactNode | ObjectHeadingType
 
-type AbstractProps = Omit<StepFormDialogContentInnerProps, 'heading' | 'activeStep'> &
+type DefaultTextsType = Record<
+  'closeButtonLabel' | 'nextButtonLabel' | 'backButtonLabel',
+  ReactNode
+>
+
+type AbstractProps = Omit<
+  StepFormDialogContentInnerProps,
+  'heading' | 'activeStep' | 'submitButton' | 'closeButton' | 'backButton'
+> &
   DialogProps & {
     heading: HeadingType
+    submitButton: ButtonArgType | ObjectButtonType
+    closeButton: ButtonArgType | ObjectButtonType
+    backButton: ButtonArgType | ObjectButtonType
   }
 type Props = AbstractProps & Omit<ComponentProps<'div'>, keyof AbstractProps>
 
 const headingObjectConverter = (text: ReactNode) => ({ text })
-const closeButtonObjectConverter = (text: ButtonArgType): ObjectCloseButtonType => ({
-  text,
-  disabled: false,
-  hidden: false,
-})
 
-export const StepFormDialog: FC<Props> = ({
+export const StepFormDialog: FC<Props> = ({ portalParent, id, firstStep, ...rest }) => {
+  const { createPortal } = useDialogPortal(portalParent, id)
+
+  return createPortal(
+    <StepFormDialogProvider firstStep={firstStep}>
+      <ActualStepFormDialog {...rest} id={id} firstStep={firstStep} />
+    </StepFormDialogProvider>,
+  )
+}
+
+const ActualStepFormDialog: FC<Omit<Props, 'portalParent'>> = ({
   children,
   heading: orgHeading,
   stepLength,
   contentBgColor,
   contentPadding,
-  submitButton,
+  submitButton: originalSubmitButton,
   closeButton: originalCloseButton,
-  backButton,
+  backButton: originalBackButton,
   firstStep,
   onSubmit,
   onClickClose,
@@ -57,20 +78,68 @@ export const StepFormDialog: FC<Props> = ({
   onPressEscape = onClickClose,
   responseStatus,
   className,
-  portalParent,
   id,
   isOpen,
   ...rest
 }) => {
-  const { createPortal } = useDialogPortal(portalParent, id)
+  const { localize } = useIntl()
+  const defaultTexts: DefaultTextsType = useMemo(
+    () => ({
+      closeButtonLabel: localize({
+        id: 'smarthr-ui/StepFormDialog/closeButtonLabel',
+        defaultText: 'キャンセル',
+      }),
+      nextButtonLabel: localize({
+        id: 'smarthr-ui/StepFormDialog/nextButtonLabel',
+        defaultText: '次へ',
+      }),
+      backButtonLabel: localize({
+        id: 'smarthr-ui/StepFormDialog/backButtonLabel',
+        defaultText: '戻る',
+      }),
+    }),
+    [localize],
+  )
+  const { currentStep } = useContext(StepFormDialogContext)
+  const activeStep = useMemo(() => currentStep?.stepNumber ?? 1, [currentStep])
+
   const heading = useObjectHeading<HeadingType, ObjectHeadingType>(
     orgHeading,
     headingObjectConverter,
   )
-  const closeButton = useObjectAttributes<
-    ButtonArgType | ObjectCloseButtonType,
-    ObjectCloseButtonType
-  >(originalCloseButton, closeButtonObjectConverter)
+
+  const tempSubmitButton = useStepFormDialogButton({
+    button: originalSubmitButton,
+    currentStep,
+    defaultValues: {
+      text: defaultTexts.nextButtonLabel,
+      theme: 'primary' as const,
+    },
+  })
+  const submitButton = useMemo(
+    () => ({
+      ...tempSubmitButton,
+      text:
+        tempSubmitButton.functionCall.text || activeStep === stepLength
+          ? tempSubmitButton.text
+          : defaultTexts.nextButtonLabel,
+    }),
+    [tempSubmitButton, activeStep, stepLength, defaultTexts.nextButtonLabel],
+  )
+  const closeButton = useStepFormDialogButton({
+    button: originalCloseButton,
+    currentStep,
+    defaultValues: {
+      text: defaultTexts.closeButtonLabel,
+    },
+  })
+  const backButton = useStepFormDialogButton({
+    button: originalBackButton,
+    currentStep,
+    defaultValues: {
+      text: defaultTexts.backButtonLabel,
+    },
+  })
 
   const focusTrapRef = useRef<FocusTrapRef>(null)
 
@@ -98,33 +167,32 @@ export const StepFormDialog: FC<Props> = ({
     }
   }, [isOpen, onClickBack])
 
-  return createPortal(
-    <StepFormDialogProvider firstStep={firstStep}>
-      <DialogContentInner
-        {...rest}
-        isOpen={isOpen}
-        ariaLabelledby={heading.id}
-        className={className}
-        onPressEscape={closeButton.disabled ? undefined : onPressEscape}
-        focusTrapRef={focusTrapRef}
+  return (
+    <DialogContentInner
+      {...rest}
+      isOpen={isOpen}
+      ariaLabelledby={heading.id}
+      className={className}
+      onPressEscape={closeButton.disabled ? undefined : onPressEscape}
+      focusTrapRef={focusTrapRef}
+    >
+      <StepFormDialogContentInner
+        heading={heading}
+        activeStep={activeStep}
+        contentBgColor={contentBgColor}
+        contentPadding={contentPadding}
+        firstStep={firstStep}
+        stepLength={stepLength}
+        submitButton={submitButton}
+        closeButton={closeButton}
+        backButton={backButton}
+        onClickClose={actualOnClickClose}
+        onSubmit={onDelegateSubmit}
+        onClickBack={actualOnClickBack}
+        responseStatus={responseStatus}
       >
-        <StepFormDialogContentInner
-          heading={heading}
-          contentBgColor={contentBgColor}
-          contentPadding={contentPadding}
-          firstStep={firstStep}
-          stepLength={stepLength}
-          submitButton={submitButton}
-          closeButton={closeButton}
-          backButton={backButton}
-          onClickClose={actualOnClickClose}
-          onSubmit={onDelegateSubmit}
-          onClickBack={actualOnClickBack}
-          responseStatus={responseStatus}
-        >
-          {children}
-        </StepFormDialogContentInner>
-      </DialogContentInner>
-    </StepFormDialogProvider>,
+        {children}
+      </StepFormDialogContentInner>
+    </DialogContentInner>
   )
 }
