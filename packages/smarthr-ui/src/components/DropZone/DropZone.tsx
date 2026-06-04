df7927eb1aa4273370d03e4c681b3d8955f666dd
Otter-K <k.kono.techs@gmail.com>
2026-06-04 16:04:00 +0900

    feat(DropZone): multiple.appendable で DropZoneMultiplyAppendable に委譲する

--- packages/smarthr-ui/src/components/DropZone/DropZone.tsx
+++ packages/smarthr-ui/src/components/DropZone/DropZone.tsx
@@ -9,6 +9,7 @@ import {
 } from 'react'
 
 import { DropZoneBase } from './DropZoneBase'
+import { DropZoneMultiplyAppendable } from './DropZoneMultiplyAppendable'
 
 type CommonProps = PropsWithChildren<{
   accept?: string
@@ -25,10 +26,32 @@ type NormalModeProps = CommonProps & {
     files: FileList | null,
   ) => void
   files?: never
-} & Omit<ComponentPropsWithRef<'div'>, 'children' | 'multiple' | 'onSelectFiles' | 'files' | 'accept' | 'name' | 'disabled' | 'error'>
+} & Omit<
+    ComponentPropsWithRef<'div'>,
+    'children' | 'multiple' | 'onSelectFiles' | 'files' | 'accept' | 'name' | 'disabled' | 'error'
+  >
 
-type Props = NormalModeProps
+type AppendableModeProps = CommonProps & {
+  multiple: { appendable: true }
+  files: File[]
+  onSelectFiles: (e: DragEvent<HTMLElement> | ChangeEvent<HTMLInputElement>, files: File[]) => void
+} & Omit<
+    ComponentPropsWithRef<'div'>,
+    'children' | 'multiple' | 'onSelectFiles' | 'files' | 'accept' | 'name' | 'disabled' | 'error'
+  >
 
-export const DropZone = forwardRef<HTMLInputElement, Props>((props, ref) => (
-  <DropZoneBase {...props} ref={ref} />
-))
+type Props = NormalModeProps | AppendableModeProps
+
+function isAppendableMode(props: Props): props is AppendableModeProps {
+  return typeof props.multiple === 'object' && !!props.multiple.appendable
+}
+
+export const DropZone = forwardRef<HTMLInputElement, Props>((props, ref) => {
+  if (isAppendableMode(props)) {
+    const { multiple: _m, ...rest } = props
+    return <DropZoneMultiplyAppendable {...rest} ref={ref} />
+  }
+
+  const { files: _f, ...rest } = props
+  return <DropZoneBase {...rest} ref={ref} />
+})
