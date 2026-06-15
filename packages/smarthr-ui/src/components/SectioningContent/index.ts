7a58273ead47a0670a2711240c2f36853c6896cf
Qs-F <~@qsf.jp>
2026-06-16 00:52:39 +0900

    chore(SectioningContent): SectioningFragmentをsmarthr-ui内でのみexport

--- packages/smarthr-ui/src/components/SectioningContent/index.ts
+++ packages/smarthr-ui/src/components/SectioningContent/index.ts
@@ -1,3 +1,3 @@
-export { Article, Aside, Nav, Section } from './SectioningContent'
+export { Article, Aside, Nav, Section, SectioningFragment } from './SectioningContent'
 export { LevelContext } from './levelContext'
 export { useSectionWrapper } from './useSectioningWrapper'
