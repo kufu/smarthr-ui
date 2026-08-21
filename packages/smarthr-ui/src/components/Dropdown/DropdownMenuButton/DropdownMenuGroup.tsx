fd1ce268bb6bc7f38da0db505402479a8d8297f9
AtsushiM <at.mizoue@gmail.com>
2026-08-22 08:05:47 +0900

    refactor(DropdownMenuGroup): tv()の構成をslotsからbaseに簡潔化
    slotが group のみになったため、slots構成を使う必要がなくなった。
    base構成に変更し、classNameGenerator({ className })を直接呼び出す形に簡潔化する。

--- packages/smarthr-ui/src/components/Dropdown/DropdownMenuButton/DropdownMenuGroup.tsx
+++ packages/smarthr-ui/src/components/Dropdown/DropdownMenuButton/DropdownMenuGroup.tsx
@@ -19,34 +19,24 @@ type BaseProps = PropsWithChildren<{
 type Props = BaseProps & Omit<ComponentProps<'li'>, keyof BaseProps>
 
 const classNameGenerator = tv({
-  slots: {
-    group: [
-      'smarthr-ui-DropdownMenuGroup',
-      [
-        '[&:not(:first-child)]:shr-relative',
-        '[&:not(:first-child)]:shr-mt-0.5',
-        '[&:not(:first-child)]:shr-pt-0.5',
-        '[&:not(:first-child)]:shr-min-w-[8em]',
-        '[&:not(:first-child)]:before:shr-content-[""]',
-        '[&:not(:first-child)]:before:shr-absolute',
-        '[&:not(:first-child)]:before:shr-top-0',
-        '[&:not(:first-child)]:before:shr-inset-x-1',
-        '[&:not(:first-child)]:before:shr-h-px',
-        '[&:not(:first-child)]:before:shr-bg-border',
-      ],
-    ],
-  },
+  base: [
+    'smarthr-ui-DropdownMenuGroup',
+    '[&:not(:first-child)]:shr-relative',
+    '[&:not(:first-child)]:shr-mt-0.5',
+    '[&:not(:first-child)]:shr-pt-0.5',
+    '[&:not(:first-child)]:shr-min-w-[8em]',
+    '[&:not(:first-child)]:before:shr-content-[""]',
+    '[&:not(:first-child)]:before:shr-absolute',
+    '[&:not(:first-child)]:before:shr-top-0',
+    '[&:not(:first-child)]:before:shr-inset-x-1',
+    '[&:not(:first-child)]:before:shr-h-px',
+    '[&:not(:first-child)]:before:shr-bg-border',
+  ],
 })
 
 export const DropdownMenuGroup: FC<Props> = ({ name, children, className }) => {
   const subMenuId = useId()
-  const classNames = useMemo(() => {
-    const { group } = classNameGenerator()
-
-    return {
-      group: group({ className }),
-    }
-  }, [className])
+  const actualClassName = useMemo(() => classNameGenerator({ className }), [className])
 
   const subMenu = (
     <menu role="group" aria-labelledby={name ? subMenuId : undefined} className="shr-list-none">
@@ -55,7 +45,7 @@ export const DropdownMenuGroup: FC<Props> = ({ name, children, className }) => {
   )
 
   return (
-    <li role="presentation" className={classNames.group}>
+    <li role="presentation" className={actualClassName}>
       {name ? (
         <>
           <NameText id={subMenuId} className="shr-px-1 shr-py-0.5">
