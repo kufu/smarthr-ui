0a8d936e6aad0b94a9d858e7db0009e854e0fac1
oti <yoshiaki.ito@smarthr.co.jp>
2026-08-28 17:22:39 +0900

    feat: 文字の改行はブラウザに任せることにして、閉じるボタンがモーダルウィンドウ外にはみ出さないようにした

--- packages/smarthr-ui/src/components/Dialog/ModelessDialog/ModelessDialog.tsx
+++ packages/smarthr-ui/src/components/Dialog/ModelessDialog/ModelessDialog.tsx
@@ -411,7 +411,7 @@ export const ModelessDialog: FC<Props> = ({
               handleArrowKeyDown={functions.handleArrowKeyDown}
               className={classNames.dialogHandler}
             />
-            <div id={labelId} className="shr-my-1 shr-me-1 shr-min-w-0 shr-break-all">
+            <div id={labelId} className="shr-my-1 shr-me-1 shr-min-w-0">
               {/* eslint-disable-next-line smarthr/a11y-heading-in-sectioning-content */}
               <Heading>{heading}</Heading>
             </div>
