2e78438d9a8553d8cb3c2d8a11debe1e7c0abdb4
AtsushiM <at.mizoue@gmail.com>
2026-08-30 18:16:14 +0900

    chore: pathを将来に合わせて修正

--- packages/smarthr-ui/src/hooks/client/useEscapeCallbackRef.ts
+++ packages/smarthr-ui/src/hooks/client/useEscapeCallbackRef.ts
@@ -1,7 +1,7 @@
 import { useCallback } from 'react'
 
 // TODO: React v18を切れたらclientから移動できるか確認
-import { useCallbackRefCleanupForReact18 } from './client/useCallbackRefCleanupForReact18'
+import { useCallbackRefCleanupForReact18 } from './useCallbackRefCleanupForReact18'
 
 // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
 // Esc is a IE/Edge specific value
