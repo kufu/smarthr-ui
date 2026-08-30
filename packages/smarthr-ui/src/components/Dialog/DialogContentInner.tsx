2be5a8b31d9b730204998d6a451f20af77031e93
AtsushiM <at.mizoue@gmail.com>
2026-08-30 18:04:45 +0900

    chore: useEscapeCallbackRefに置き換え

--- packages/smarthr-ui/src/components/Dialog/DialogContentInner.tsx
+++ packages/smarthr-ui/src/components/Dialog/DialogContentInner.tsx
@@ -11,7 +11,7 @@ import {
 } from 'react'
 import { tv } from 'tailwind-variants'
 
-import { useHandleEscape } from '../../hooks/useHandleEscape'
+import { useEscapeCallbackRef } from '../../hooks/client/useEscapeCallbackRef'
 import { useLatest } from '../../hooks/useLatest'
 import { dialogSize } from '../../tailwind'
 
@@ -118,7 +118,7 @@ export const DialogContentInner: FC<Props> = ({
   // width は deprecated なので、size が指定されている場合は width を無視する
   const actualWidth = size ? undefined : typeof width === 'number' ? `${width}px` : width
 
-  const latest = useLatest({ onPressEscape, onClickOverlay })
+  const latest = useLatest({ isOpen, onPressEscape, onClickOverlay })
 
   const functions = useMemo(
     () => ({
@@ -128,7 +128,7 @@ export const DialogContentInner: FC<Props> = ({
     [latest],
   )
 
-  useHandleEscape(isOpen ? functions.handlePressEscape : undefined)
+  const callbackRef = useEscapeCallbackRef(functions.handlePressEscape)
 
   useEffect(() => {
     if (!isOpen) return
@@ -151,6 +151,7 @@ export const DialogContentInner: FC<Props> = ({
   return (
     <DialogOverlap isOpen={isOpen}>
       <div
+        ref={callbackRef}
         id={id}
         className={classNames.layout}
         style={actualWidth ? { width: actualWidth } : undefined}
