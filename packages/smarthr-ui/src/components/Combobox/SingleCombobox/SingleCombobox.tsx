965636799dfb58178fb631c5f4f930db124739c6
Mizoue Atsushi <at.mizoue@gmail.com>
2026-08-30 12:47:51 +0900

    refactor(Combobox): useImperativeHandleをuseMergeRefsに置き換え、animation frameのcancel処理をrefに統合 (#6865)

--- packages/smarthr-ui/src/components/Combobox/SingleCombobox/SingleCombobox.tsx
+++ packages/smarthr-ui/src/components/Combobox/SingleCombobox/SingleCombobox.tsx
@@ -9,8 +9,8 @@ import {
   type Ref,
   type RefObject,
   memo,
+  useCallback,
   useEffect,
-  useImperativeHandle,
   useMemo,
   useRef,
   useState,
@@ -21,6 +21,7 @@ import { tv } from 'tailwind-variants'
 import { useAnimationFrame } from '../../../hooks/useAnimationFrame'
 import { useClick } from '../../../hooks/useClick'
 import { useLatest } from '../../../hooks/useLatest'
+import { useMergeRefs } from '../../../hooks/useMergeRefs'
 import { useTheme } from '../../../hooks/useTheme'
 import { Localizer } from '../../../intl'
 import { genericsForwardRef } from '../../../libs/util'
@@ -199,12 +200,6 @@ const ActualSingleCombobox = <T,>(
   const [isComposing, setIsComposing] = useState(false)
   const [isEditing, setIsEditing] = useState(false)
 
-  useImperativeHandle<HTMLInputElement | null, HTMLInputElement | null>(
-    ref,
-    () => inputRef.current,
-    [],
-  )
-
   const { options } = useSingleOptions({
     items,
     selected: selectedItem,
@@ -247,9 +242,6 @@ const ActualSingleCombobox = <T,>(
       noResultText,
     },
   )
-
-  // TODO: callbackRefにまとめ直したい
-  useEffect(() => selectFrame.cancel, [selectFrame.cancel])
 
   const latest = useLatest({
     onChange,
@@ -408,6 +400,12 @@ const ActualSingleCombobox = <T,>(
     functions.unfocus,
   )
 
+  // HINT: useMergeRefsはv18でもcallbackRefのcleanup関数に対応している
+  // もしuseMergeRefsをなくす場合、react v18対応が不要になっているかどうか確認する
+  const cleanupCallbackRef = useCallback(() => selectFrame.cancel, [selectFrame.cancel])
+
+  const mergedRef = useMergeRefs(inputRef, cleanupCallbackRef, ref)
+
   // selectedItem.label はプリミティブ値でないデータ型の可能性があり、そのまま useEffect の依存配列に入れると意図せぬエフェクトの実行を引き起こしてしまう可能性があるので、プリミティブ値である string 型に変換したものを依存配列に入れています。
   const selectedItemLabelText = innerText(selectedItem?.label)
   useEffect(() => {
@@ -441,7 +439,7 @@ const ActualSingleCombobox = <T,>(
     >
       <Input
         {...rest}
-        ref={inputRef}
+        ref={mergedRef}
         type="text"
         role="combobox"
         name={name}
