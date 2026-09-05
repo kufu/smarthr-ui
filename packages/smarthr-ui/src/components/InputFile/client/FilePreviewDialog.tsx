e3454813e15790a3a0ed597b7b93ca558adae8cd
AtsushiM <at.mizoue@gmail.com>
2026-09-06 07:35:00 +0900

    refactor(FilePreviewDialog): blobUrl生成をuseEffectからuseLayoutEffectに変更する
    fileが新規セットされた直後はblobUrlが未設定のためLoaderが一度描画されてしまい、
    useEffect(ペイント後実行)だとLoader表示後にFileViewerへ切り替わるちらつきが
    発生しうる。useDialogPortal.tsの前例に倣いuseLayoutEffectに変更し、DOMコミット後・
    ペイント前に同期実行することでちらつきを防ぐ。

--- packages/smarthr-ui/src/components/InputFile/client/FilePreviewDialog.tsx
+++ packages/smarthr-ui/src/components/InputFile/client/FilePreviewDialog.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { type FC, memo, useEffect, useState } from 'react'
+import { type FC, memo, useLayoutEffect, useState } from 'react'
 
 import { useEnvironment } from '../../../hooks/client/useEnvironment'
 import { Localizer } from '../../../intl'
@@ -25,7 +25,7 @@ export const FilePreviewDialog: FC<Props> = memo(
     const isOpen = !!file
     const { mobile } = useEnvironment()
 
-    useEffect(() => {
+    useLayoutEffect(() => {
       if (!file) {
         setBlobUrl(undefined)
         return
