31a03f1a59a45e96e2535f8ccd14b3e5a26a40f4
AtsushiM <at.mizoue@gmail.com>
2026-09-05 21:20:49 +0900

    refactor(Button): classNameGeneratorをtv()のextendでbase/button/anchorに分割する
    compoundSlotsで表現していたbutton/anchor共通スタイルをvariantsに置き換え、
    baseClassNameGeneratorをbuttonClassNameGenerator/anchorClassNameGeneratorが
    extendする構成に変更した。呼び出し側のスロット名もbutton/anchor→wrapperに統一。

--- packages/smarthr-ui/src/components/Button/AnchorButton.tsx
+++ packages/smarthr-ui/src/components/Button/AnchorButton.tsx
@@ -13,7 +13,7 @@ import { OpenInNewTabIcon } from '../Icon'
 
 import { DisabledReason } from './DisabledReason'
 import { AnchorButtonInner } from './client'
-import { commonClassNameGenerator } from './style'
+import { anchorClassNameGenerator } from './style'
 
 import type { BaseProps as ButtonProps } from './types'
 import type { ElementRef, ElementRefProps } from '../../types'
@@ -51,10 +51,10 @@ const AnchorButton = forwardRef(
     ref: Ref<ElementRef<T>>,
   ): ReactElement => {
     const classNames = useMemo(() => {
-      const { anchor, inner } = commonClassNameGenerator()
+      const { wrapper, inner } = anchorClassNameGenerator()
 
       return {
-        wrapper: anchor({
+        wrapper: wrapper({
           variant,
           size,
           wide,
