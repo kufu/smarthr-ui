d27f66a662cea5a8a15852c81346fece0459c0d0
Mizoue Atsushi <at.mizoue@gmail.com>
2026-09-17 05:35:27 +0900

    refactor(Table): TableScroller/TableReelの循環依存を解消する (#7112)

--- packages/smarthr-ui/src/components/Table/client/index.ts
+++ packages/smarthr-ui/src/components/Table/client/index.ts
@@ -1,4 +1,3 @@
-export { FixedHeadTableScroller } from './FixedHeadTableScroller'
 export { EmptyTableBody } from './EmptyTableBody'
 export { TableReel } from './TableReel'
 export { BulkActionRow } from './BulkActionRow'
