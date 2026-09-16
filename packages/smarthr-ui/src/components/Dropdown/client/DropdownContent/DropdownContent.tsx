e8277fb74d8c3a1b652669fd49e47d8ccaaeb272
AtsushiM <at.mizoue@gmail.com>
2026-09-16 21:04:42 +0900

    chore: マージ解決漏れ(useCallbackRefCleanupForReact18のインライン化)を反映

--- packages/smarthr-ui/src/components/Dropdown/client/DropdownContent/DropdownContent.tsx
+++ packages/smarthr-ui/src/components/Dropdown/client/DropdownContent/DropdownContent.tsx
@@ -225,8 +225,6 @@ export const DropdownContent: FC<Props> = ({
     [triggerRect, latest],
   )
 
-  // HINT: useMergeRefsはv18でもcallbackRefのcleanup関数に対応している
-  // もしuseMergeRefsをなくす場合、react v18対応が不要になっているかどうか確認する
   const mergedRef = useMergeRefs(functions.callbackRef, layoutEffectRef)
 
   const styleAttr = {
