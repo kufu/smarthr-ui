5e9c246b76f26e0df877a6541f097228f152a3c7
AtsushiM <at.mizoue@gmail.com>
2026-09-21 07:01:54 +0900

    refactor(Table): colSpan自動算出ロジックをAutoColSpanTdとして切り出す
    EmptyTableBodyはtbodyにcountHeadCellRefを付けてcolSpanを算出していたが、
    Td自体にrefを付ける形のAutoColSpanTdコンポーネントに切り出す。
    
    あわせてEmptyTableBody自体はuseMemoのみでclient専用APIを使わないため、
    client/の外(Table/直下)に移動し、Server Componentからも使えるようにする。
    'use client'はAutoColSpanTd側にのみ必要。

--- packages/smarthr-ui/src/components/Table/client/index.ts
+++ packages/smarthr-ui/src/components/Table/client/index.ts
@@ -1,4 +1,4 @@
-export { EmptyTableBody } from './EmptyTableBody'
+export { AutoColSpanTd } from './AutoColSpanTd'
 export { TableReel } from './TableReel'
 export { BulkActionRow } from './BulkActionRow'
 export { ThCheckbox } from './ThCheckbox'
