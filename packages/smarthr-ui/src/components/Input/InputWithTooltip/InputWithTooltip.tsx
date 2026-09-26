990fbeef380066afa460575b8f5ef12c4b61f37e
AtsushiM <at.mizoue@gmail.com>
2026-09-26 12:30:13 +0900

    refactor(SearchInput): InputWithTooltipのforwardRefをouterRef propに置き換える
    InputWithTooltipはSearchInput(公開コンポーネント)からのみ呼ばれる非公開
    コンポーネントのため、forwardRefを使わずouterRef propでrefを受け取る形に
    変更する。呼び出し元のSearchInput.tsxもref={ref} -> outerRef={ref}に
    合わせて変更する。
    
    SearchInput自体は公開コンポーネントのためforwardRefのまま維持する。

--- packages/smarthr-ui/src/components/Input/InputWithTooltip/InputWithTooltip.tsx
+++ packages/smarthr-ui/src/components/Input/InputWithTooltip/InputWithTooltip.tsx
@@ -1,32 +1,37 @@
-import { type ComponentProps, type ReactNode, forwardRef, useMemo } from 'react'
+import { type ComponentProps, type FC, type ReactNode, type Ref, useMemo } from 'react'
 import { tv } from 'tailwind-variants'
 
 import { Tooltip } from '../../Tooltip'
 import { Input } from '../client'
 
-type Props = ComponentProps<typeof Input> & {
+type Props = Omit<ComponentProps<typeof Input>, 'ref'> & {
   /** 入力欄に紐付けるツールチップに表示するメッセージ */
   tooltipMessage: ReactNode
+  outerRef?: Ref<HTMLInputElement>
 }
 
 const classNameGenerator = tv({
   base: 'smarthr-ui-InputWithTooltip [&]:shr-overflow-y-visible',
 })
 
-export const InputWithTooltip = forwardRef<HTMLInputElement, Props>(
-  ({ tooltipMessage, width, className, ...rest }, ref) => {
-    const style = {
-      width: typeof width === 'number' ? `${width}px` : width,
-    }
+export const InputWithTooltip: FC<Props> = ({
+  tooltipMessage,
+  width,
+  className,
+  outerRef,
+  ...rest
+}) => {
+  const style = {
+    width: typeof width === 'number' ? `${width}px` : width,
+  }
 
-    const actualClassName = useMemo(() => classNameGenerator({ className }), [className])
+  const actualClassName = useMemo(() => classNameGenerator({ className }), [className])
 
-    return (
-      // eslint-disable-next-line smarthr/a11y-scroller-has-tabindex
-      <Tooltip tabIndex={-1} className={actualClassName} style={style} message={tooltipMessage}>
-        {/* eslint-disable-next-line smarthr/a11y-input-in-form-control */}
-        <Input {...rest} ref={ref} width={style.width} />
-      </Tooltip>
-    )
-  },
-)
+  return (
+    // eslint-disable-next-line smarthr/a11y-scroller-has-tabindex
+    <Tooltip tabIndex={-1} className={actualClassName} style={style} message={tooltipMessage}>
+      {/* eslint-disable-next-line smarthr/a11y-input-in-form-control */}
+      <Input {...rest} ref={outerRef} width={style.width} />
+    </Tooltip>
+  )
+}
